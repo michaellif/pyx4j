868985144095a3917ccbe9cabd5575673ef36cf0
Michael Lifshits <michaellif@propertyvista.com>
2009-12-23 12:40:08 +0000

    ria original commit

---
+++ gwt/pyx-gwt-ria/src/test/java/com/pyx4j/ria/client/RiaExampleApp.java
@@ -0,0 +1,30 @@
+package com.pyx4j.ria.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class RiaExampleApp implements EntryPoint {
+
+    public void onModuleLoad() {
+        final Button sendButton = new Button("Send");
+        final TextBox nameField = new TextBox();
+        nameField.setText("GWT User");
+
+        // We can add style names to widgets
+        sendButton.addStyleName("sendButton");
+
+        // Add the nameField and sendButton to the RootPanel
+        // Use RootPanel.get() to get the entire body element
+        RootPanel.get().add(nameField);
+
+        // Focus the cursor on the name field when the app loads
+        nameField.setFocus(true);
+        nameField.selectAll();
+
+    }
+}
