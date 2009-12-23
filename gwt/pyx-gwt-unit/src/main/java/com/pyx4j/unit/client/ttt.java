8bf9d921015920526f6de21a9f0a664f1a11930b
Michael Lifshits <michaellif@propertyvista.com>
2009-12-23 20:07:18 +0000

    test

---
+++ gwt/pyx-gwt-unit/src/main/java/com/pyx4j/unit/client/ttt.java
@@ -0,0 +1,13 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2009 pyx4j.com.
+ *
+ * Created on Dec 23, 2009
+ * @author Michael
+ * @version $Id$
+ */
+package com.pyx4j.unit.client;
+
+public class ttt {
+
+}
