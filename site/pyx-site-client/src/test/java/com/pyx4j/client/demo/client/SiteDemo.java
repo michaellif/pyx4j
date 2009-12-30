ec9dbc541c90c8cda59f906af02cc18efc100f80
Michael Lifshits <michaellif@propertyvista.com>
2009-12-30 13:03:03 +0000

    new layout

--- site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/SiteDemo.java
+++ site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/SiteDemo.java
@@ -12,9 +12,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.layout.client.Layout.Alignment;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -25,11 +30,40 @@ public class SiteDemo implements EntryPoint {
 
     public void onModuleLoad() {
 
-        VerticalPanel contentPanel = new VerticalPanel();
-        RootPanel.get().add(contentPanel);
+        SiteSection leftBorder = new SiteSection("Left", "green");
 
-        contentPanel.add(new Label("tttttttttt"));
+        LayoutPanel contentPanel = new LayoutPanel();
+        contentPanel.setWidth("400px");
 
+        SiteSection rightBorder = new SiteSection("Right", "green");
+
+        SiteSection center = new SiteSection("Center", "red");
+        contentPanel.add(center);
+        contentPanel.setWidgetHorizontalPosition(center, Alignment.STRETCH);
+
+        //        rootPanel.add(new SiteSection("West", "green"));
+        //        rootPanel.add(new SiteSection("East", "green"));
+        //
+        //        rootPanel.add(new SiteSection("South", "blue"));
+        //        rootPanel.add(new SiteSection("North", "blue"));
+
+        FlowPanel rootPanel = new FlowPanel();
+        rootPanel.add(leftBorder);
+        DOM.setStyleAttribute(leftBorder.getElement(), "cssFloat", "left");
+        rootPanel.add(contentPanel);
+        DOM.setStyleAttribute(contentPanel.getElement(), "cssFloat", "left");
+        rootPanel.add(rightBorder);
+        DOM.setStyleAttribute(rightBorder.getElement(), "cssFloat", "left");
+
+        RootLayoutPanel.get().add(rootPanel);
+
+    }
+
+    class SiteSection extends HTML {
+        SiteSection(String capture, String color) {
+            super(capture);
+            DOM.setStyleAttribute(getElement(), "border", "1px solid " + color);
+        }
     }
 
 }
