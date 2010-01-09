8c5a8aebbff8529da7e501edd98f288083a56b7e
Michael Lifshits <michaellif@propertyvista.com>
2010-01-09 13:54:14 +0000

    minor

--- site/pyx-site-client/src/main/java/com/pyx4j/site/client/themes/light/LightTheme.java
+++ site/pyx-site-client/src/main/java/com/pyx4j/site/client/themes/light/LightTheme.java
@@ -88,7 +88,7 @@ public class LightTheme extends WindowsTheme {
         style.addProperty("width", "900px");
         style.addProperty("height", "23px");
         style.addProperty("padding-top", "5px");
-        style.addProperty("padding-left", "10px");
+        style.addProperty("padding-left", "50px");
         addStyle(style);
 
         style = new Style("." + SiteCSSClass.pyx4j_Site_PrimaryNavigTab.name());
