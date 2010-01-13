fc704989b548c6c3cf893f2feec9566c53adfe60
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-01-13 20:19:04 +0000

    Do not cash root in development

--- gwt/pyx-gwt-server/src/main/java/com/pyx4j/gwt/server/GWTCacheFilter.java
+++ gwt/pyx-gwt-server/src/main/java/com/pyx4j/gwt/server/GWTCacheFilter.java
@@ -39,6 +39,8 @@ public class GWTCacheFilter implements Filter {
 
     private String cacheControl;
 
+    private boolean developmentMode = false;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         String rate = filterConfig.getInitParameter("cacheExpiresHours");
@@ -46,6 +48,7 @@ public class GWTCacheFilter implements Filter {
             cacheExpiresHours = Integer.parseInt(rate);
         }
         cacheControl = "public, max-age=" + ((long) Consts.HOURS2SEC * cacheExpiresHours);
+        developmentMode = (System.clearProperty("sun.desktop") != null);
     }
 
     @Override
@@ -56,10 +59,16 @@ public class GWTCacheFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if ((request instanceof HttpServletRequest) && (response instanceof HttpServletResponse)) {
             String uri = ((HttpServletRequest) request).getRequestURI();
+            System.out.println(uri);
             if (uri.indexOf(".cache.") != -1) {
                 ((HttpServletResponse) response).setDateHeader("Expires", System.currentTimeMillis() + Consts.HOURS2MSEC * cacheExpiresHours);
                 ((HttpServletResponse) response).setHeader("Cache-control", cacheControl);
             } else if ((uri.indexOf(".nocache.") != -1) || (uri.indexOf("test") != -1)) {
+                ((HttpServletResponse) response).setDateHeader("Expires", System.currentTimeMillis());
+                ((HttpServletResponse) response).setHeader("Pragma", "no-cache");
+                ((HttpServletResponse) response).setHeader("Cache-control", "no-cache, no-store, must-revalidate");
+            } else if (developmentMode && (uri.length() == 1)) {
+                // Do not cash root in development
                 ((HttpServletResponse) response).setDateHeader("Expires", System.currentTimeMillis());
                 ((HttpServletResponse) response).setHeader("Pragma", "no-cache");
                 ((HttpServletResponse) response).setHeader("Cache-control", "no-cache, no-store, must-revalidate");
