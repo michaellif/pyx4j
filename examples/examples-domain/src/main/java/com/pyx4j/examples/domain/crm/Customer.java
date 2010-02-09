64d14d14a918ebd78a916eb6e70b759416819e69
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-02-09 22:30:01 +0000

    Working on examples

---
+++ examples/examples-domain/src/main/java/com/pyx4j/examples/domain/crm/Customer.java
@@ -0,0 +1,48 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Feb 9, 2010
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.examples.domain.crm;
+
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.Owned;
+import com.pyx4j.entity.annotations.validator.NotNull;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.entity.shared.IPrimitiveSet;
+import com.pyx4j.entity.shared.ISet;
+
+public interface Customer extends IEntity<Customer> {
+
+    @NotNull
+    IPrimitive<String> name();
+
+    IPrimitiveSet<String> phone();
+
+    @Caption(name = "Address")
+    IPrimitive<String> street();
+
+    @Caption(name = "Zip/Postal")
+    IPrimitive<String> zip();
+
+    @Owned
+    ISet<Order> orders();
+
+    IPrimitiveSet<String> notes();
+}
