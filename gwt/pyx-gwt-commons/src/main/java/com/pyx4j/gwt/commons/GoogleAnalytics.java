ec4cb7d92316c21dd7f978415bef524488cb429b
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-02-12 06:43:12 +0000

    Injects Google Analytics Tracker code

--- gwt/pyx-gwt-commons/src/main/java/com/pyx4j/gwt/commons/GoogleAnalytics.java
+++ gwt/pyx-gwt-commons/src/main/java/com/pyx4j/gwt/commons/GoogleAnalytics.java
@@ -20,16 +20,38 @@
  */
 package com.pyx4j.gwt.commons;
 
+import java.util.Vector;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.ScriptElement;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 
+/**
+ * Injects Google Analytics Tracker code
+ * 
+ * @see http://code.google.com/apis/analytics/docs/tracking/gaTrackingOverview.html
+ * 
+ */
 public class GoogleAnalytics {
 
     private static Logger log = LoggerFactory.getLogger(GoogleAnalytics.class);
+
+    private static boolean alreadyInjected = false;
+
+    /** there are no callback in ga.js, so we use timer */
+    private static boolean loaded = false;
+
+    private static Timer loadTimer;
+
+    private static int timeoutCountdown = 10;
+
+    private static Vector<String> queuedActions;
 
     private static String googleAnalyticsTracker;
 
@@ -37,6 +59,36 @@ public class GoogleAnalytics {
         String h = Window.Location.getHostName();
         if (!"localhost".equals(h) && !"127.1.1.1".equals(h)) {
             GoogleAnalytics.googleAnalyticsTracker = googleAnalyticsTracker;
+            if (googleAnalyticsTracker != null) {
+                // Allow for "Standard Setup" in html page
+                if (isInstalled()) {
+                    loaded = true;
+                } else if (!alreadyInjected) {
+                    alreadyInjected = true;
+                    injectJS();
+                    loadTimer = new Timer() {
+                        @Override
+                        public void run() {
+                            if (isInstalled()) {
+                                loaded = true;
+                                loadTimer.cancel();
+                                loadTimer = null;
+                                fireQueuedActions();
+                            } else {
+                                timeoutCountdown--;
+                                if (timeoutCountdown == 0) {
+                                    loadTimer.cancel();
+                                    loadTimer = null;
+                                    // Disable, GoogleAnalytics, probably working OffLine 
+                                    GoogleAnalytics.googleAnalyticsTracker = null;
+                                    log.error("GoogleAnalytics load timeout");
+                                }
+                            }
+                        }
+                    };
+                    loadTimer.scheduleRepeating(7 * 1000);
+                }
+            }
         }
     }
 
@@ -44,8 +96,28 @@ public class GoogleAnalytics {
         if (googleAnalyticsTracker == null) {
             return;
         }
-        DeferredCommand.addCommand(new Command() {
-            public void execute() {
+        if (!loaded) {
+            if (queuedActions == null) {
+                queuedActions = new Vector<String>();
+            }
+            queuedActions.add(actionName);
+        } else {
+            DeferredCommand.addCommand(new Command() {
+                public void execute() {
+                    try {
+                        log.debug("googleAnalyticsTrack {}", actionName);
+                        trackPageView(googleAnalyticsTracker, actionName);
+                    } catch (Throwable e) {
+                        log.error("GoogleAnalytics error", e);
+                    }
+                }
+            });
+        }
+    }
+
+    private static void fireQueuedActions() {
+        if (queuedActions != null) {
+            for (String actionName : queuedActions) {
                 try {
                     log.debug("googleAnalyticsTrack {}", actionName);
                     trackPageView(googleAnalyticsTracker, actionName);
@@ -53,8 +125,22 @@ public class GoogleAnalytics {
                     log.error("GoogleAnalytics error", e);
                 }
             }
-        });
+            queuedActions.clear();
+            queuedActions = null;
+        }
     }
+
+    private static void injectJS() {
+        String protocolPrefix = Window.Location.getProtocol().equals("https:") ? "https://ssl" : "http://www";
+        Document doc = Document.get();
+        ScriptElement script = doc.createScriptElement();
+        script.setSrc(protocolPrefix + ".google-analytics.com/ga.js");
+        script.setType("text/javascript");
+        doc.getBody().appendChild(script);
+    }
+
+    private static final native boolean isInstalled()
+    /*-{ return typeof $wnd._gat != "undefined"; }-*/;
 
     private static native void trackPageView(String tracker, String pageUrl)
     /*-{ $wnd._gat._getTracker(tracker)._trackPageview(pageUrl); }-*/;
