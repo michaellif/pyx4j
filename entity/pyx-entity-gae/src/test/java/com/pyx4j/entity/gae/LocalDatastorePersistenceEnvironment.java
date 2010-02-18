c17e3863e6b7d1923485745ba0c268bf70182a3b
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-02-18 03:53:17 +0000

    Upgrade to GAE 1.3.1

--- entity/pyx-entity-gae/src/test/java/com/pyx4j/entity/gae/LocalDatastorePersistenceEnvironment.java
+++ entity/pyx-entity-gae/src/test/java/com/pyx4j/entity/gae/LocalDatastorePersistenceEnvironment.java
@@ -20,15 +20,11 @@
  */
 package com.pyx4j.entity.gae;
 
-import java.io.File;
-
 import org.junit.After;
 import org.junit.Before;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.dev.LocalDatastoreService;
-import com.google.appengine.tools.development.ApiProxyLocalImpl;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.apphosting.api.ApiProxy;
 import com.pyx4j.entity.server.ServerEntityFactory;
 import com.pyx4j.entity.shared.EntityFactory;
@@ -39,27 +35,21 @@ public class LocalDatastorePersistenceEnvironment extends PersistenceEnvironment
     /** true to store saved changes, default to false */
     protected boolean storeChanges = false;
 
+    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
     private static int uniqueCount = 0;
 
     @Override
     @Before
     public void setupDatastore() {
-        ApiProxy.setEnvironmentForCurrentThread(new TestEnvironment());
-        ApiProxyLocalImpl impl = new ApiProxyLocalImpl(new File(".")) {
-        };
-        impl.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY, Boolean.FALSE.toString());
-        ApiProxy.setDelegate(impl);
-        DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+        helper.setUp();
         EntityFactory.setImplementation(new ServerEntityFactory());
     }
 
     @Override
     @After
     public void teardownDatastore() {
-        ApiProxyLocalImpl proxy = (ApiProxyLocalImpl) ApiProxy.getDelegate();
-        LocalDatastoreService datastoreService = (LocalDatastoreService) proxy.getService(LocalDatastoreService.PACKAGE);
-        datastoreService.clearProfiles();
-        ApiProxy.setDelegate(null);
+        helper.tearDown();
         ApiProxy.setEnvironmentForCurrentThread(null);
     }
 
