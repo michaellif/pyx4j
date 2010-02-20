fa0ad3e901111a66388bfc092ecc2ccc6ccd1f33
Michael Lifshits <michaellif@propertyvista.com>
2010-02-20 21:48:29 +0000

    removed streetview overlay

--- examples/examples-site/src/main/java/com/pyx4j/examples/site/client/crm/MapPanel.java
+++ examples/examples-site/src/main/java/com/pyx4j/examples/site/client/crm/MapPanel.java
@@ -48,13 +48,6 @@ public class MapPanel extends InlineWidget {
         final VerticalPanel contentPanel = new VerticalPanel();
         setWidget(contentPanel);
 
-        if (Window.Location.getHost().endsWith("pyx4j-poc.appspot.com")) {
-            AjaxLoader.init("ABQIAAAAZuLUizjWCGkAYOfiIpZpgxRlOb26qSyU154aZeLwOrF4C7-DphRHsx1TXRHlwWkRQshNzrgUs_xpSQ");
-        } else {
-            // pyx4j.com
-            AjaxLoader.init("ABQIAAAAZuLUizjWCGkAYOfiIpZpgxT2nw7IAgYZCN3UZ-Glm95U7gTjpRTVD1pxXeXBpUR-ZQ5Z0YCQkesTkg");
-        }
-
         AjaxLoader.loadApi("maps", "2", new Runnable() {
             public void run() {
                 mapsLoaded(contentPanel);
@@ -75,27 +68,6 @@ public class MapPanel extends InlineWidget {
         contentPanel.add(map);
         contentPanel.setCellHorizontalAlignment(map, DockPanel.ALIGN_CENTER);
 
-        Button toggleStreetview = new Button("Toggle Street View Overlay");
-        toggleStreetview.addClickListener(new ClickListener() {
-            public void onClick(Widget sender) {
-                if (svShown) {
-                    map.removeOverlay(svOverlay);
-                } else {
-                    map.addOverlay(svOverlay);
-                }
-                svShown = !svShown;
-            }
-        });
-        contentPanel.add(toggleStreetview);
-        svOverlay = new StreetviewOverlay();
-        svOverlay.addStreetviewOverlayChangedHandler(new StreetviewOverlayChangedHandler() {
-
-            public void onChanged(StreetviewOverlayChangedEvent event) {
-
-            }
-
-        });
-
         //How to find panoid - http://diddling.blogspot.com/2008/01/hacking-google-street-view.html
         //http://maps.google.com/cbk?output=xml&ll=37.4451,-122.125577 - see xml
 
@@ -103,6 +75,6 @@ public class MapPanel extends InlineWidget {
 
         contentPanel
                 .add(new HTML(
-                        "<object type='application/x-shockwave-flash' name='panoflash1' id='panoflash1' align='middle' style='position: relative; visibility: visible; ' data='http://maps.gstatic.com/intl/en_ALL/mapfiles/cb/googlepano.104.swf' width='650' height='380'><param name='allowscriptaccess' value='always'><param name='scale' value='noScale'><param name='salign' value='lt'><param name='allowfullscreen' value='true'><param name='swliveconnect' value='false'><param name='wmode' value=''><param name='quality' value='high'><param name='bgcolor' value='#000000'><param name='flashvars' value='panoId=l3-YA_-GMcjy2yTsYx-GOg&amp;directionMap=N:N,W:W,S:S,E:E,NW:NW,NE:NE,SW:SW,SE:SE&amp;yaw=150.67939649880157&amp;zoom=0&amp;browser=chrome&amp;pitch=-10&amp;viewerId=1&amp;context=api&amp;useSsl=false&amp;csiCallback=&amp;userPhotoRepositories=all&amp;rtfArgs=hl:en,gl:,fs:1,sv:1,ph:0'></object>"));
+                        "<object type='application/x-shockwave-flash' name='panoflash1' id='panoflash1' align='middle' style='position: relative; visibility: visible; ' data='http://maps.gstatic.com/intl/en_ALL/mapfiles/cb/googlepano.104.swf' width='400' height='400'><param name='allowscriptaccess' value='always'><param name='scale' value='noScale'><param name='salign' value='lt'><param name='allowfullscreen' value='true'><param name='swliveconnect' value='false'><param name='wmode' value=''><param name='quality' value='high'><param name='bgcolor' value='#000000'><param name='flashvars' value='panoId=l3-YA_-GMcjy2yTsYx-GOg&amp;directionMap=N:N,W:W,S:S,E:E,NW:NW,NE:NE,SW:SW,SE:SE&amp;yaw=150.67939649880157&amp;zoom=0&amp;pitch=-10&amp;viewerId=1&amp;context=api&amp;useSsl=false&amp;csiCallback=&amp;userPhotoRepositories=all&amp;rtfArgs=hl:en,gl:,fs:1,sv:1,ph:0'></object>"));
     }
 }
