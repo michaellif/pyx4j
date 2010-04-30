7285f2246a13c4d8f5a8d853f1edec1e10bf0aa7
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-04-30 22:07:42 +0000

    TODO read CSV

---
+++ essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/csv/CSVLoad.java
@@ -0,0 +1,94 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on 2010-04-30
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.essentials.server.csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class CSVLoad {
+
+    public static int findHeaderLine(String[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i][0].startsWith("#")) {
+                continue;
+            }
+            return i;
+        }
+        return 0;
+    }
+
+    public static int findHeaderColumn(String[][] data, int header, String name) {
+        for (int col = 0; (col < data[header].length); col++) {
+            if (data[header][col].equals(name)) {
+                return col;
+            }
+        }
+        return -1;
+    }
+
+    public static int findHeaderColumn(String[] data, String name) {
+        for (int col = 0; (col < data.length); col++) {
+            if (data[col].equals(name)) {
+                return col;
+            }
+        }
+        return -1;
+    }
+
+    public static void loadFile(String fileName, CSVReciver reciver) {
+        InputStream is = null;
+        try {
+            is = CSVLoad.class.getClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new RuntimeException("Resouce not found");
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            boolean header = true;
+            while (((line = reader.readLine()) != null) && (reciver.canContuneLoad())) {
+                if (line.startsWith("#") || (line.length() == 0)) {
+                    continue;
+                }
+                if (header) {
+                    header = false;
+                    reciver.onHeader(CSVParser.parse(line));
+                } else {
+                    reciver.onRow(CSVParser.parse(line));
+                }
+            }
+            reader.close();
+        } catch (IOException ioe) {
+            throw new Error("Load file error:" + fileName, ioe);
+        } catch (Exception e) {
+            throw new Error("Load file error:" + fileName, e);
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException ignore) {
+                is = null;
+            }
+        }
+    }
+}
---
+++ essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/csv/CSVParser.java
@@ -0,0 +1,77 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on 2010-04-30
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.essentials.server.csv;
+
+import java.util.List;
+import java.util.Vector;
+
+public class CSVParser {
+
+    public static String[] parse(String line) {
+        List<String> columns = new Vector<String>();
+
+        StringBuilder col = new StringBuilder();
+        boolean escape = false;
+        boolean quoted = false;
+        boolean doubleQuote = false;
+        for (char c : line.toCharArray()) {
+            if (escape) {
+                col.append(c);
+                escape = false;
+            } else {
+                switch (c) {
+                case '"':
+                    if (quoted) {
+                        if (doubleQuote) {
+                            col.append(c);
+                            doubleQuote = false;
+                        }
+                    } else {
+                        if (col.length() == 0) {
+                            quoted = true;
+                        } else {
+                            // Just add it,, ignore errors,
+                            col.append(c);
+                        }
+                    }
+                    break;
+                case ',':
+                    columns.add(col.toString());
+                    col = new StringBuilder();
+                    quoted = false;
+                    break;
+                case '\\':
+                    escape = true;
+                    break;
+                default:
+                    col.append(c);
+                    doubleQuote = false;
+                    break;
+                }
+            }
+        }
+        if (col.length() > 0) {
+            columns.add(col.toString());
+        }
+
+        return columns.toArray(new String[columns.size()]);
+    }
+}
