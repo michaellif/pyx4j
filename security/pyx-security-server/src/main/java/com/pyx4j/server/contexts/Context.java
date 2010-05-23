7c13bab01acc4f9376a93d5c336efdfc1f114652
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-05-23 06:43:44 +0000

    Allow access to HttpServletRequest

--- security/pyx-security-server/src/main/java/com/pyx4j/server/contexts/Context.java
+++ security/pyx-security-server/src/main/java/com/pyx4j/server/contexts/Context.java
@@ -66,7 +66,7 @@ public class Context {
         }
     }
 
-    static HttpServletRequest getRequest() {
+    public static HttpServletRequest getRequest() {
         return request.get();
     }
 
