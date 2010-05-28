8d2288a7edb6144f54dba1febd063fd9d8cd6fe7
Michael Lifshits <michaellif@propertyvista.com>
2010-05-28 16:24:10 +0000

    Use crm skin

--- examples/examples-site/src/main/java/com/pyx4j/examples/site/client/ExamplesSitePanel.java
+++ examples/examples-site/src/main/java/com/pyx4j/examples/site/client/ExamplesSitePanel.java
@@ -26,9 +26,12 @@ import com.pyx4j.security.client.ClientContext;
 import com.pyx4j.site.client.AbstractSiteDispatcher;
 import com.pyx4j.site.client.CommandLink;
 import com.pyx4j.site.client.SitePanel;
+import com.pyx4j.site.client.SkinFactory;
+import com.pyx4j.site.client.themes.crm.CrmTheme;
 import com.pyx4j.site.shared.domain.Link;
 import com.pyx4j.site.shared.domain.Site;
 import com.pyx4j.widgets.client.dialog.Dialog;
+import com.pyx4j.widgets.client.style.Theme;
 
 public abstract class ExamplesSitePanel extends SitePanel {
 
@@ -38,6 +41,13 @@ public abstract class ExamplesSitePanel extends SitePanel {
 
     protected ExamplesSitePanel(Site site) {
         super(site);
+
+        setSkinFactory(new SkinFactory() {
+            @Override
+            public Theme createSkin(String skinName) {
+                return new CrmTheme();
+            }
+        });
 
         {
             boolean hasSeparator = false;
