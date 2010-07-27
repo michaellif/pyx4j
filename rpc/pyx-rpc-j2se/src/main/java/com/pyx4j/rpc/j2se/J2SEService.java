1359a7e9c6635f7ab3d8082f982a585dbcb9e540
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-07-27 21:50:25 +0000

    work on tests

--- rpc/pyx-rpc-j2se/src/main/java/com/pyx4j/rpc/j2se/J2SEService.java
+++ rpc/pyx-rpc-j2se/src/main/java/com/pyx4j/rpc/j2se/J2SEService.java
@@ -20,6 +20,7 @@
  */
 package com.pyx4j.rpc.j2se;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -28,19 +29,137 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.pyx4j.commons.RuntimeExceptionSerializable;
 import com.pyx4j.rpc.shared.Service;
 
 public class J2SEService {
 
+    private static final Logger log = LoggerFactory.getLogger(J2SEService.class);
+
     public static final String SERVER_SYSTEM_PROPERTY = "pyx-server-url";
+
+    private static String userAgent = "pyx-j2se/1.0";
 
     private static final String SESSION_HEADER = "JSESSIONID";
 
     private static String sessionID;
+
+    public static void googleLogin(String userName, String password) throws RuntimeException {
+        HttpURLConnection conn = null;
+        try {
+
+            String url = System.getProperty(SERVER_SYSTEM_PROPERTY);
+            URL u = new URL(url);
+            conn = (HttpURLConnection) u.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-agent", userAgent);
+            conn.setInstanceFollowRedirects(false);
+            conn.setUseCaches(false);
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
+                String redirect = conn.getHeaderField("Location");
+                log.debug("redirect {}", redirect);
+                conn.disconnect();
+                conn = (HttpURLConnection) new URL(redirect).openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("User-agent", userAgent);
+                conn.setUseCaches(false);
+                if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new RuntimeException("Failed to authenticate " + conn.getResponseMessage());
+                }
+                getSessionCookie(conn.getHeaderFields());
+                conn.disconnect();
+                gaeDevLoginPost(redirect, url, userName);
+            } else {
+                log.debug("{} no redirect ", conn.getResponseCode());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to login", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+
+    static void gaeDevLoginPost(String loginUrl, String url, String userName) {
+        HttpURLConnection conn = null;
+        OutputStream out = null;
+        try {
+            conn = (HttpURLConnection) new URL(loginUrl).openConnection();
+            conn.setRequestMethod("POST");
+            //conn.setRequestProperty("User-agent", userAgent);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+
+            if (sessionID != null) {
+                conn.setRequestProperty("Cookie", SESSION_HEADER + "=" + sessionID);
+            }
+            conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            conn.setRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+            conn.setRequestProperty("User-agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8");
+            conn.setRequestProperty("Referer", loginUrl);
+
+            String charsetName = "iso-8859-1";
+
+            StringBuffer form = new StringBuffer();
+
+            form.append("action=");
+            form.append(URLEncoder.encode("Log In", charsetName));
+            form.append("&");
+
+            form.append("continue=");
+            form.append(URLEncoder.encode(url, charsetName));
+            form.append("&");
+
+            form.append("email=");
+            form.append(URLEncoder.encode(userName, charsetName));
+            form.append("\r\n");
+
+            byte[] data = form.toString().getBytes(charsetName);
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length));
+            out = conn.getOutputStream();
+            out.write(data);
+            out.flush();
+            out.close();
+            out = null;
+
+            //            PrintWriter writer = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(), charsetName));
+            //            writer.write(form.toString());
+            //            writer.write("\r\n");
+            //            writer.flush();
+            //writer.close();
+
+            log.debug("login post responce {}", conn.getResponseCode());
+            printHeaders(conn.getHeaderFields());
+            DataInputStream dis = new DataInputStream(conn.getInputStream());
+            int ch;
+            while ((ch = dis.read()) != -1) {
+                System.out.print((char) ch);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to login", e);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
 
     public static <I extends Serializable, O extends Serializable> O execute(final Class<? extends Service<I, O>> serviceInterface, I request)
             throws RuntimeException {
@@ -63,7 +182,7 @@ public class J2SEService {
             }
             conn.setRequestProperty("Cache-Control", "no-cache, no-transform");
             conn.setRequestProperty("Content-Type", "application/binary");
-            conn.setRequestProperty("User-agent", "pyx-j2se/1.0");
+            conn.setRequestProperty("User-agent", userAgent);
 
             out = conn.getOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(out);
@@ -110,7 +229,7 @@ public class J2SEService {
                 }
             }
             if (conn != null) {
-                (conn).disconnect();
+                conn.disconnect();
             }
         }
     }
@@ -131,8 +250,19 @@ public class J2SEService {
             }
             int t = v.indexOf(';', eq);
             if (t != -1) {
-                sessionID = v.substring(eq + 1, t);
+                String session = v.substring(eq + 1, t);
+                if ((session != null) && (!session.equals(sessionID))) {
+                    log.debug("Set sessionID {}", sessionID);
+                }
             }
         }
+    }
+
+    static void printHeaders(Map<String, List<String>> headers) {
+        StringBuilder b = new StringBuilder();
+        for (Map.Entry<String, List<String>> me : headers.entrySet()) {
+            b.append(me.getKey()).append(" = ").append(me.getValue()).append("\n");
+        }
+        log.debug("{}", b.toString());
     }
 }
