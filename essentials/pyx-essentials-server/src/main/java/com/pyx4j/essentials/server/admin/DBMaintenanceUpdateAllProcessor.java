16d9694c1801d5536c8543673da837fbb63e8135
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-10-05 02:29:23 +0000

    Improving DB Maintenance procedures

---
+++ essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/admin/DBMaintenanceUpdateAllProcessor.java
@@ -0,0 +1,35 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Oct 4, 2010
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.essentials.server.admin;
+
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.essentials.rpc.admin.IDBMaintenanceProcessor;
+
+public class DBMaintenanceUpdateAllProcessor implements IDBMaintenanceProcessor<IEntity> {
+
+    private static final long serialVersionUID = -8817187303509545582L;
+
+    @Override
+    public boolean process(IEntity entity) {
+        return true;
+    }
+
+}
