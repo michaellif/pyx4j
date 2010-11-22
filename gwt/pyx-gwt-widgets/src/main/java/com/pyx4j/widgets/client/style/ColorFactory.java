186e877f4016e9328c819f73b0a831a033a13eb9
Michael Lifshits <michaellif@propertyvista.com>
2010-11-22 13:16:19 +0000

    rgbtohsb impl

--- gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/style/ColorFactory.java
+++ gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/style/ColorFactory.java
@@ -73,9 +73,54 @@ public class ColorFactory {
         return (r << 16) | (g << 8) | (b << 0);
     }
 
+    public static float[] RGBtoHSB(int rgb) {
+        int r = (rgb >> 16) & 0xFF;
+        int g = (rgb >> 8) & 0xFF;
+        int b = (rgb >> 0) & 0xFF;
+        float hue, saturation, brightness;
+        float[] hsbvals = new float[3];
+        int cmax = (r > g) ? r : g;
+        if (b > cmax)
+            cmax = b;
+        int cmin = (r < g) ? r : g;
+        if (b < cmin)
+            cmin = b;
+
+        brightness = (cmax) / 255.0f;
+        if (cmax != 0)
+            saturation = ((float) (cmax - cmin)) / ((float) cmax);
+        else
+            saturation = 0;
+        if (saturation == 0)
+            hue = 0;
+        else {
+            float redc = ((float) (cmax - r)) / ((float) (cmax - cmin));
+            float greenc = ((float) (cmax - g)) / ((float) (cmax - cmin));
+            float bluec = ((float) (cmax - b)) / ((float) (cmax - cmin));
+            if (r == cmax)
+                hue = bluec - greenc;
+            else if (g == cmax)
+                hue = 2.0f + redc - bluec;
+            else
+                hue = 4.0f + greenc - redc;
+            hue = hue / 6.0f;
+            if (hue < 0)
+                hue = hue + 1.0f;
+        }
+        hsbvals[0] = hue;
+        hsbvals[1] = saturation;
+        hsbvals[2] = brightness;
+        return hsbvals;
+    }
+
     public static int HSBVtoRGB(float hue, float saturation, float brightness, float vibrance) {
         float ns = saturation * vibrance;
         float nb = 1 - (1 - brightness) * vibrance;
         return HSBtoRGB(hue, ns, nb);
+    }
+
+    public static int RGBtoRGB(int rgb, float vibrance) {
+        float[] hsbvals = RGBtoHSB(rgb);
+        return HSBVtoRGB(hsbvals[0], hsbvals[1], hsbvals[2], vibrance);
     }
 }
