b5883c65f956997e6b7cc9cc78f058b88091c485
Slava Skarzhevskyy <vlads@propertyvista.com>
2010-11-30 06:37:53 +0000

    Security: #418 Support Class names obfuscation in GWT; Export Service names as pyx-service-manifest.rpc

--- rpc/pyx-rpc-server/src/main/java/com/pyx4j/rpc/server/RemoteServiceServlet.java
+++ rpc/pyx-rpc-server/src/main/java/com/pyx4j/rpc/server/RemoteServiceServlet.java
@@ -29,19 +29,18 @@ import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
-import com.google.gwt.user.server.rpc.SerializationPolicy;
 
 import com.pyx4j.commons.CommonsStringUtils;
 import com.pyx4j.config.server.ServerSideConfiguration;
 import com.pyx4j.config.server.rpc.IServiceFactory;
 import com.pyx4j.gwt.server.IOUtils;
 import com.pyx4j.rpc.shared.RemoteService;
+import com.pyx4j.server.contexts.Context;
 
 @SuppressWarnings("serial")
 public class RemoteServiceServlet extends com.google.gwt.user.server.rpc.RemoteServiceServlet implements RemoteService {
@@ -76,17 +75,21 @@ public class RemoteServiceServlet extends com.google.gwt.user.server.rpc.RemoteS
         implementation = new RemoteServiceImpl("GWT", serviceFactory);
     }
 
+    // GWT makes it impossible to Override any other method in RemoteServiceServlet and access moduleBaseURL from RCP request.
     @Override
-    protected SerializationPolicy doGetSerializationPolicy(HttpServletRequest request, String moduleBaseURL, String strongName) {
-        SerializationPolicy policy = super.doGetSerializationPolicy(request, moduleBaseURL, strongName);
-
-        // All error handling is already done in GWT
+    protected void onBeforeRequestDeserialized(String serializedRequest) {
         try {
-            String contextPath = request.getContextPath();
+            // 6|1|12|http://localhost:8888/g.site/|2005C2913F3EF6EE0AB1510ECABAE604|_|
+            int beginModuleBaseURL = 0;
+            for (int i = 0; i < 3; i++) {
+                beginModuleBaseURL = serializedRequest.indexOf('|', beginModuleBaseURL) + 1;
+            }
+            String moduleBaseURL = serializedRequest.substring(beginModuleBaseURL, serializedRequest.indexOf('|', beginModuleBaseURL));
+            String contextPath = Context.getRequest().getContextPath();
             String modulePath = new URL(moduleBaseURL).getPath();
             String moduleRelativePath = modulePath.substring(contextPath.length());
 
-            Map<String, String> servicePolicy = servicePolicyCache.get(getPermutationStrongName());
+            Map<String, String> servicePolicy = servicePolicyCache.get(moduleRelativePath);
             if (servicePolicy == null) {
                 InputStream is = this.getServletContext().getResourceAsStream(moduleRelativePath + "pyx-service-manifest.rpc");
                 try {
@@ -103,24 +106,24 @@ public class RemoteServiceServlet extends com.google.gwt.user.server.rpc.RemoteS
                             servicePolicy.put((String) me.getKey(), (String) me.getValue());
                         }
                     }
-                    servicePolicyCache.put(getPermutationStrongName(), servicePolicy);
+                    servicePolicyCache.put(moduleRelativePath, servicePolicy);
                     log.debug("{}", servicePolicy);
                 } finally {
                     IOUtils.closeQuietly(is);
                 }
             }
+            Context.getRequest().setAttribute("pyx.ServicePolicy", servicePolicy);
         } catch (Throwable t) {
             log.error("unable to load service-manifest", t);
             throw new IncompatibleRemoteServiceException();
         }
-
-        return policy;
     }
 
     @Override
     public Serializable execute(String serviceInterfaceClassName, Serializable serviceRequest, String userVisitHashCode) throws RuntimeException {
         String realServiceName = null;
-        Map<String, String> servicePolicy = servicePolicyCache.get(getPermutationStrongName());
+        @SuppressWarnings("unchecked")
+        Map<String, String> servicePolicy = (Map<String, String>) Context.getRequest().getAttribute("pyx.ServicePolicy");
         if (servicePolicy != null) {
             realServiceName = servicePolicy.get(serviceInterfaceClassName);
         }
