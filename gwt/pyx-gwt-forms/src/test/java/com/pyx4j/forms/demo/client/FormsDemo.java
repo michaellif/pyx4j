f5b0e5e01d673c83c70c3a33ba7201d3a176fb0e
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-01-18 06:09:36 +0000

    Form example

--- gwt/pyx-gwt-forms/src/test/java/com/pyx4j/forms/demo/client/FormsDemo.java
+++ gwt/pyx-gwt-forms/src/test/java/com/pyx4j/forms/demo/client/FormsDemo.java
@@ -20,14 +20,25 @@
  */
 package com.pyx4j.forms.demo.client;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
+import com.pyx4j.forms.client.ui.CComboBox;
+import com.pyx4j.forms.client.ui.CComponent;
+import com.pyx4j.forms.client.ui.CForm;
+import com.pyx4j.forms.client.ui.CForm.LabelAlignment;
+import com.pyx4j.forms.client.ui.CTextField;
 import com.pyx4j.log4gwt.client.ClientLogger;
 import com.pyx4j.widgets.client.style.StyleManger;
 import com.pyx4j.widgets.client.style.theme.WindowsTheme;
@@ -38,6 +49,8 @@ import com.pyx4j.widgets.client.style.theme.WindowsTheme;
 public class FormsDemo implements EntryPoint {
 
     final Logger log = LoggerFactory.getLogger(FormsDemo.class);
+
+    VerticalPanel wp;
 
     @Override
     public void onModuleLoad() {
@@ -50,6 +63,39 @@ public class FormsDemo implements EntryPoint {
         RootPanel.get().add(contentPanel);
 
         log.debug("This app is empty");
-        contentPanel.add(new HTML("This app is empty"));
+        //contentPanel.add(new HTML("This app is empty"));
+
+        contentPanel.add(wp = new VerticalPanel());
+        wp.add(new HTML("This app is empty"));
+
+        Button b;
+        wp.add(b = new Button("Show form"));
+
+        b.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                showForm();
+            }
+
+        });
+    }
+
+    private void showForm() {
+        log.debug("will show form now");
+
+        CTextField url = new CTextField("URL 1");
+        CTextField url2 = new CTextField("URL 2");
+
+        CComboBox<String> lb = new CComboBox<String>("LB");
+        lb.setOptions(Arrays.asList(new String[] { "A", "B" }));
+
+        CComponent[][] comps = new CComponent[][] {
+
+        { url, url },
+
+        { url2, lb } };
+
+        wp.add(CForm.createFormWidget(LabelAlignment.LEFT, comps));
     }
 }
