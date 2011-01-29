8926697ba6f22be8776ddef05907fffbd50ba475
Michael Lifshits <michaellif@propertyvista.com>
2011-01-29 00:13:34 +0000

    add gin dependency

--- site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/ClientFactory.java
+++ site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/ClientFactory.java
@@ -1,12 +1,13 @@
 package com.pyx4j.client.demo.client;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.inject.client.Ginjector;
 import com.google.gwt.place.shared.PlaceController;
 
 import com.pyx4j.client.demo.client.ui.GoodbyeView;
 import com.pyx4j.client.demo.client.ui.HelloView;
 
-public interface ClientFactory {
+public interface ClientFactory extends Ginjector {
     EventBus getEventBus();
 
     PlaceController getPlaceController();
