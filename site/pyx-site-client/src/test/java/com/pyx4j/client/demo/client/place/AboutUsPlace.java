b7eba73c58fd354ae91c0cbf6454bac2bd316a45
Michael Lifshits <michaellif@propertyvista.com>
2011-02-03 10:37:25 +0000

    added more places

---
+++ site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/place/AboutUsPlace.java
@@ -0,0 +1,8 @@
+package com.pyx4j.client.demo.client.place;
+
+public class AboutUsPlace extends BasicPlace {
+
+    public AboutUsPlace() {
+    }
+
+}
---
+++ site/pyx-site-client/src/test/java/com/pyx4j/client/demo/client/place/ContactUsPlace.java
@@ -0,0 +1,8 @@
+package com.pyx4j.client.demo.client.place;
+
+public class ContactUsPlace extends BasicPlace {
+
+    public ContactUsPlace() {
+    }
+
+}
