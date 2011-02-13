736b7ae55fa798652a9255def4a370cce13b6083
Dmitry Tumanov <dmitryt@birchwoodsoftwaregroup.com>
2011-02-13 00:01:10 +0000

    Cleaned up imports

--- vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/preloader/PreloadBuildings.java
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/preloader/PreloadBuildings.java
@@ -12,9 +12,6 @@
  */
 package com.propertyvista.portal.server.preloader;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
