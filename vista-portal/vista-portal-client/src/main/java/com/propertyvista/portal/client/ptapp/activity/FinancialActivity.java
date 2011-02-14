54064e9eb0fcc470b34965d6c0cb9d2d1ab3b1cd
Anton Kraev <antonk@birchwoodsoftwaregroup.com>
2011-02-14 00:15:59 +0000

    FinancialViewImpl

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ptapp/activity/FinancialActivity.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ptapp/activity/FinancialActivity.java
@@ -13,15 +13,21 @@
  */
 package com.propertyvista.portal.client.ptapp.activity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 import com.propertyvista.portal.client.ptapp.ui.FinancialView;
+import com.propertyvista.portal.domain.pt.PotentialTenantFinancial;
 
 import com.pyx4j.site.client.place.AppPlace;
 
 public class FinancialActivity extends AbstractActivity implements FinancialView.Presenter {
+
+    private static final Logger log = LoggerFactory.getLogger(FinancialActivity.class);
 
     private final FinancialView view;
 
@@ -38,6 +44,11 @@ public class FinancialActivity extends AbstractActivity implements FinancialView
     @Override
     public void start(AcceptsOneWidget panel, EventBus eventBus) {
         panel.setWidget(view);
+    }
+
+    @Override
+    public void save(PotentialTenantFinancial value) {
+        log.info("SAVED {}", value);
     }
 
 }
