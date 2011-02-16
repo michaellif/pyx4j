200c05ea414ada61b21bece0a3394a95975999ce
Michael Lifshits <michaellif@propertyvista.com>
2011-02-16 18:25:39 +0000

    Moved EventBus to pyx

---
+++ site/pyx-site-gwt/src/main/java/com/pyx4j/site/client/AppSiteGinModule.java
@@ -0,0 +1,53 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Dec 22, 2009
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.site.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.place.shared.PlaceHistoryHandler;
+import com.google.gwt.place.shared.PlaceHistoryMapper;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class AppSiteGinModule extends AbstractGinModule {
+
+    @Override
+    protected void configure() {
+
+        bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
+
+    }
+
+    public PlaceHistoryHandler getHistoryHandler(PlaceController placeController, EventBus eventBus) {
+        PlaceHistoryMapper historyMapper = GWT.create(PlaceHistoryMapper.class);
+        PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
+        return historyHandler;
+    }
+
+    @Provides
+    @Singleton
+    public PlaceController getPlaceController(EventBus eventBus) {
+        return new PlaceController(eventBus);
+    }
+}
