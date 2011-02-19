c61b4fc2083d808e19abea1e147bab3c76fbcc7f
Michael Lifshits <michaellif@propertyvista.com>
2011-02-19 16:35:22 +0000

    VISTA-61 Styling Create account page

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ptapp/ui/CreateAccountViewImpl.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ptapp/ui/CreateAccountViewImpl.java
@@ -17,16 +17,24 @@ import org.xnap.commons.i18n.I18n;
 import org.xnap.commons.i18n.I18nFactory;
 
 import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ResourcePrototype;
+import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.Event.NativePreviewHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.propertyvista.portal.client.ptapp.resources.SiteImages;
+import com.propertyvista.portal.client.ptapp.resources.SiteResources;
 import com.propertyvista.portal.domain.DemoData;
 import com.propertyvista.portal.rpc.pt.AccountCreationRequest;
 
@@ -37,7 +45,7 @@ import com.pyx4j.essentials.client.crud.CrudDebugId;
 import com.pyx4j.forms.client.ui.CCaptcha;
 import com.pyx4j.rpc.shared.UserRuntimeException;
 
-public class CreateAccountViewImpl extends VerticalPanel implements CreateAccountView {
+public class CreateAccountViewImpl extends FlowPanel implements CreateAccountView {
 
     private static I18n i18n = I18nFactory.getI18n(CreateAccountViewImpl.class);
 
@@ -53,22 +61,22 @@ public class CreateAccountViewImpl extends VerticalPanel implements CreateAccoun
 
     public CreateAccountViewImpl() {
 
-        Button signinButton = new Button(i18n.tr("Signin"));
-        signinButton.ensureDebugId("Signin");
-        signinButton.addClickHandler(new ClickHandler() {
+        FlowPanel leftColumn = new FlowPanel();
+        add(leftColumn);
 
-            @Override
-            public void onClick(ClickEvent event) {
-                presenter.goToSignin();
-            }
+        HTML requirements = new HTML(SiteResources.INSTANCE.requirements().getText());
+        requirements.getElement().getStyle().setPaddingLeft(95, Unit.PX);
+        requirements.getElement().getStyle().setPaddingBottom(45, Unit.PX);
 
-        });
-        signinButton.getElement().getStyle().setProperty("margin", "3px 20px 3px 8px");
-        add(signinButton);
+        requirements.getElement().getStyle().setProperty("background", "url(" + SiteImages.INSTANCE.requirements().getURL() + ") no-repeat");
+        leftColumn.add(requirements);
+
+        FlowPanel rightColumn = new FlowPanel();
+        add(rightColumn);
 
         form = new CreateAccountViewForm();
         form.populate(null);
-        add(form);
+        rightColumn.add(form);
 
         Button viewButton = new Button(i18n.tr("Let's Start"));
         viewButton.ensureDebugId(CrudDebugId.Criteria_Submit.toString());
@@ -90,16 +98,31 @@ public class CreateAccountViewImpl extends VerticalPanel implements CreateAccoun
 
         });
         viewButton.getElement().getStyle().setProperty("margin", "0.5em 6em 1em 0");
-        add(viewButton);
-        
+        rightColumn.add(viewButton);
+
         if (ApplicationMode.isDevelopment()) {
-            add(new HTML("<br/>This application is running in <B>DEVELOPMENT</B> mode.<br/>"));
-            add(new HTML("Press <i>Ctrl+Q</i> to create new user"));
+            rightColumn.add(new HTML("<br/>This application is running in <B>DEVELOPMENT</B> mode.<br/>"));
+            rightColumn.add(new HTML("Press <i>Ctrl+Q</i> to create new user"));
         }
 
-        setCellHorizontalAlignment(form.asWidget(), HasHorizontalAlignment.ALIGN_RIGHT);
-        setCellHorizontalAlignment(viewButton, HasHorizontalAlignment.ALIGN_RIGHT);
-        setWidth("60%");
+        Button signinButton = new Button(i18n.tr("Signin"));
+        signinButton.ensureDebugId("Signin");
+        signinButton.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                presenter.goToSignin();
+            }
+
+        });
+        signinButton.getElement().getStyle().setProperty("margin", "3px 20px 3px 8px");
+        rightColumn.add(signinButton);
+
+        getElement().getStyle().setMarginLeft(5, Unit.PCT);
+        getElement().getStyle().setMarginRight(5, Unit.PCT);
+        getElement().getStyle().setMarginTop(15, Unit.PX);
+        getElement().getStyle().setMarginBottom(15, Unit.PX);
+
     }
 
     @Override
