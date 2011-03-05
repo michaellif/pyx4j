943e352fdd1e75393288e85c2a4def857a75fb1c
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-03-05 07:58:54 +0000

    VISTA-61 completion panel for new applications

--- vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/pt/PotentialTenantServicesImpl.java
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/pt/PotentialTenantServicesImpl.java
@@ -118,6 +118,7 @@ public class PotentialTenantServicesImpl extends EntityServicesImpl implements P
                 progress.steps().add(createWizardStep(new SiteMap.Charges(), ApplicationWizardStep.Status.notVisited));
                 progress.steps().add(createWizardStep(new SiteMap.Summary(), ApplicationWizardStep.Status.notVisited));
                 progress.steps().add(createWizardStep(new SiteMap.Payment(), ApplicationWizardStep.Status.notVisited));
+                progress.steps().add(createWizardStep(new SiteMap.Completion(), ApplicationWizardStep.Status.notVisited));
                 progress.application().set(application);
                 secureSave(progress);
 
