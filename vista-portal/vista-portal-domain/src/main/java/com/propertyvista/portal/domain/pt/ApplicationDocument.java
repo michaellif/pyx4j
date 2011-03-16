646825157a27bbfb96a4959cad26e9934fb1f11f
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-03-16 06:22:28 +0000

    #VISTA-267 #time 30m Add interfaces to list uploaded documents.

---
+++ vista-portal/vista-portal-domain/src/main/java/com/propertyvista/portal/domain/pt/ApplicationDocument.java
@@ -0,0 +1,44 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 16, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.portal.domain.pt;
+
+import java.io.Serializable;
+
+import com.pyx4j.entity.annotations.RpcTransient;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+
+public interface ApplicationDocument extends IEntity, IBoundToApplication {
+
+    public static enum DocumentType implements Serializable {
+
+        securityInfo,
+
+        income;
+
+    }
+
+    // This is not shown in UI only defines where the document is attached.
+    IPrimitive<DocumentType> type();
+
+    /**
+     * This is actual BLOB of the Image or PDF stored on server
+     */
+    @RpcTransient
+    IPrimitive<byte[]> data();
+
+    IPrimitive<String> filename();
+
+    IPrimitive<Long> fileSize();
+}
---
+++ vista-portal/vista-portal-rpc/src/main/java/com/propertyvista/portal/rpc/pt/services/ApplicationDocumentsService.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 16, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.portal.rpc.pt.services;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.propertyvista.portal.domain.pt.ApplicationDocument;
+import com.propertyvista.portal.rpc.pt.ApplicationDocumentsList;
+
+public interface ApplicationDocumentsService {
+
+    /**
+     * Ideally we want to return List<ApplicationDocument> but we want to bind the results
+     * to EntityFolder for viewing in UI.
+     */
+    public void retrieveAttachments(AsyncCallback<ApplicationDocumentsList> callback, Long tenantId, ApplicationDocument.DocumentType documentType);
+}
