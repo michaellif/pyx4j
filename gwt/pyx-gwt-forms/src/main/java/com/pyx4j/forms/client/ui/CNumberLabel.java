b54ec72b3d99bb4b544e541e5fcc1cc631ee71b0
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-03-18 00:04:11 +0000

    Format support added.

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CNumberLabel.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CNumberLabel.java
@@ -8,8 +8,26 @@
  */
 package com.pyx4j.forms.client.ui;
 
-import com.pyx4j.forms.client.ui.CAbstractLabel;
+import com.google.gwt.i18n.client.NumberFormat;
 
 public class CNumberLabel extends CAbstractLabel<Number> {
 
+    public void setNumberFormat(final String format) {
+        if (format != null) {
+            setFormat(new IFormat<Number>() {
+
+                NumberFormat formatter = NumberFormat.getFormat(format);
+
+                @Override
+                public String format(Number value) {
+                    return formatter.format(value);
+                }
+
+                @Override
+                public Number parse(String string) {
+                    return null;
+                }
+            });
+        }
+    }
 }
