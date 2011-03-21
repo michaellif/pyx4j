24efcb4fc3d952cb8acfd688d4212e54907005f7
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-03-21 23:31:46 +0000

    VISTA-249 #time 1h 50m Adding GWT JUnit to modules

---
+++ vista-shared/vista-integration/src/test/java/com/propertyvista/unit/ExecuteSharedUnitTest.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-03-21
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.unit;
+
+import junit.framework.Assert;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pyx4j.selenium.DefaultSeleniumTestConfiguration;
+import com.pyx4j.selenium.ISeleniumTestConfiguration;
+import com.pyx4j.selenium.UnitTestExecutionTestCase;
+
+public class ExecuteSharedUnitTest extends UnitTestExecutionTestCase {
+
+    private static final Logger log = LoggerFactory.getLogger(ExecuteSharedUnitTest.class);
+
+    @Override
+    protected ISeleniumTestConfiguration getSeleniumTestConfiguration() {
+        return new DefaultSeleniumTestConfiguration() {
+            @Override
+            public String getTestUrl() {
+                return "http://www22.birchwoodsoftwaregroup.com/tester/";
+                //return "http://localhost:8888/vista/tester/";
+            }
+
+            @Override
+            public boolean reuseBrowser() {
+                return false;
+            }
+
+        };
+    }
+
+    public void testUnitTests() throws Exception {
+        VistaDevLogin.login(selenium);
+        executeAllClientUnitTests(180);
+        ExecutionStatus status = getExecutionStatus();
+
+        log.info("Success Count: {}", status.success);
+        log.info("Failed Count: {} ", status.failed);
+        log.info("Duration: {} ", status.duration);
+
+        if (status.success == 0) {
+            Assert.fail("Success Count is 0");
+        }
+        Assert.assertEquals("Failed tests Count", 0, status.failed);
+    }
+}
---
+++ vista-shared/vista-integration/src/test/java/com/propertyvista/unit/VistaDevLogin.java
@@ -0,0 +1,44 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-03-21
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.unit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pyx4j.essentials.j2se.J2SEServiceConnector;
+import com.pyx4j.essentials.j2se.J2SEServiceConnector.Credentials;
+import com.pyx4j.selenium.SeleniumExtended;
+
+public class VistaDevLogin {
+
+    private static final Logger log = LoggerFactory.getLogger(VistaDevLogin.class);
+
+    public static void login(SeleniumExtended selenium) {
+        if (!selenium.isElementPresent("id=googleSignIn")) {
+            log.debug("login not required");
+        }
+
+        Credentials credentials = J2SEServiceConnector.getCredentials(System.getProperty("user.dir", ".") + "/credentials.properties");
+
+        selenium.click("id=googleSignIn");
+
+        selenium.type("id=Email", credentials.email);
+        selenium.type("id=Passwd", credentials.password);
+        selenium.click("id=signIn");
+
+        selenium.click("id=continue");
+
+    }
+
+}
