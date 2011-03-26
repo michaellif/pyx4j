88d6b384f72509b6bef9a4d3102247574425b21d
Sergei Goshko <sergeig@birchwoodsoftwaregroup.com>
2011-03-26 23:42:57 +0000

    VISTA-267 (Back-end file upload functionality) header and imports fixes

--- vista-server/src/main/java/com/propertyvista/server/ci/ApplicationDocumentServlet.java
+++ vista-server/src/main/java/com/propertyvista/server/ci/ApplicationDocumentServlet.java
@@ -1,6 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 20, 2011
+ * @author sergei
+ * @version $Id:
  */
 
 package com.propertyvista.server.ci;
@@ -11,7 +20,6 @@ import com.pyx4j.entity.server.PersistenceServicesFactory;
 import com.pyx4j.entity.shared.criterion.EntityQueryCriteria;
 import com.pyx4j.entity.shared.criterion.PropertyCriterion;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- vista-server/src/main/java/com/propertyvista/server/ci/UploadServlet.java
+++ vista-server/src/main/java/com/propertyvista/server/ci/UploadServlet.java
@@ -1,7 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 20, 2011
+ * @author sergei
+ * @version $Id:
  */
+
 package com.propertyvista.server.ci;
 
 import gwtupload.server.UploadAction;
