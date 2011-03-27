d7a149bcac1077658d3c33bae1723c5792b25144
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-03-27 02:24:04 +0000

    VISTA-319 CMoney (Editable component) - validation/parsing tuned

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/DefaultMoneyFormatter.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/DefaultMoneyFormatter.java
@@ -48,13 +48,13 @@ public class DefaultMoneyFormatter implements IFormat<Money> {
 
     @Override
     public String format(Money value) {
-        String money = (value != null ? numberFormat.format(value.amount().getValue()) : "");
+        String money = (value != null && !value.amount().isNull() ? numberFormat.format(value.amount().getValue()) : "");
         switch (showCurrency) {
         case use$:
             money = "$" + money;
             break;
         case show:
-            money = money + " " + (value != null ? value.currency().getStringView() : "");
+            money = money + " " + (value != null && value.currency().isNull() ? value.currency().getStringView() : "");
             break;
         case hide:
             break;
