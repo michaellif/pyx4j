2a96838eac28028268c4e997122f7673d1dca37e
Sergei Goshko <sergeig@birchwoodsoftwaregroup.com>
2011-04-08 06:18:14 +0000

    VISTA-325 #time 1h Create simple instance of Quartz scheduler; create and schedule job to cleanup orphan ApplicationDocumentData records (still need to find a way to avoid deleting recent records)

---
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/maintenance/CleanOrphanApplicationDocumentDataRecordsJob.java
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Apr 8, 2011
+ * @author sergei
+ * @version $Id: code-templates.xml 7812 2011-01-10 20:13:00Z vlads $
+ */
+package com.propertyvista.portal.server.maintenance;
+
+import java.util.List;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.propertyvista.portal.domain.pt.ApplicationDocument;
+import com.propertyvista.server.domain.ApplicationDocumentData;
+import com.pyx4j.entity.server.PersistenceServicesFactory;
+import com.pyx4j.entity.shared.criterion.EntityQueryCriteria;
+import com.pyx4j.entity.shared.criterion.PropertyCriterion;
+
+public class CleanOrphanApplicationDocumentDataRecordsJob implements Job {
+
+    private final static Logger logger = LoggerFactory.getLogger(CleanOrphanApplicationDocumentDataRecordsJob.class);
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("CleanOrphanApplicationDocumentDataRecordsJob: STARTED");
+        //TODO do not delete recently created data records, since customer might be in a process of filling the application
+        List<Long> dataKeys = PersistenceServicesFactory.getPersistenceService().queryKeys(EntityQueryCriteria.create(ApplicationDocumentData.class));
+        logger.trace("dataKeys={}", dataKeys);
+        int deleted = 0;
+        for (Long dataKey : dataKeys) {
+            EntityQueryCriteria<ApplicationDocument> criteria = EntityQueryCriteria.create(ApplicationDocument.class);
+            criteria.add(PropertyCriterion.eq(criteria.proto().dataId(), dataKey));
+            ApplicationDocument doc = PersistenceServicesFactory.getPersistenceService().retrieve(criteria);
+            if (doc == null) {
+                logger.debug("CleanOrphanApplicationDocumentDataRecordsJob: Found orphan ApplicationDocumentData record - deleting. id={}", dataKey);
+                PersistenceServicesFactory.getPersistenceService().delete(ApplicationDocumentData.class, dataKey);
+                deleted++;
+            }
+        }
+        logger.info("CleanOrphanApplicationDocumentDataRecordsJob: {} ApplicationDocumentData record(s) deleted", deleted);
+    }
+}
---
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/scheduler/JobScheduler.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Apr 8, 2011
+ * @author sergei
+ * @version $Id: code-templates.xml 7812 2011-01-10 20:13:00Z vlads $
+ */
+package com.propertyvista.portal.server.scheduler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.quartz.DateBuilder;
+import org.quartz.DateBuilder.IntervalUnit;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.SimpleTrigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.propertyvista.portal.server.maintenance.CleanOrphanApplicationDocumentDataRecordsJob;
+
+public class JobScheduler extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private SchedulerFactory schedulerFactory = null;
+
+    private Scheduler scheduler = null;
+
+    private final static Logger logger = LoggerFactory.getLogger(JobScheduler.class);
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("JobScheduler.init() called");
+        try {
+            schedulerFactory = new StdSchedulerFactory();
+            scheduler = schedulerFactory.getScheduler();
+            scheduler.start();
+            JobDetail job = JobBuilder.newJob(CleanOrphanApplicationDocumentDataRecordsJob.class).withIdentity("job1", "group1").build();
+
+            SimpleTrigger simpleTrigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
+                    .startAt(DateBuilder.futureDate(30, IntervalUnit.SECOND))
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule().repeatForever().withIntervalInSeconds(30)).build();
+            scheduler.scheduleJob(job, simpleTrigger);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
