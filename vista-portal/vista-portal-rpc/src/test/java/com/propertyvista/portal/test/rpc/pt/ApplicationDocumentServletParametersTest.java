54a56fad102a6c7a6435738c221f92e3ba9e4e73
Sergei Goshko <sergeig@birchwoodsoftwaregroup.com>
2011-04-08 06:26:08 +0000

    VISTA-280 test cases convert to jUnit3

--- vista-portal/vista-portal-rpc/src/test/java/com/propertyvista/portal/test/rpc/pt/ApplicationDocumentServletParametersTest.java
+++ vista-portal/vista-portal-rpc/src/test/java/com/propertyvista/portal/test/rpc/pt/ApplicationDocumentServletParametersTest.java
@@ -5,25 +5,18 @@
 
 package com.propertyvista.portal.test.rpc.pt;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
+import junit.framework.TestCase;
 import com.propertyvista.portal.rpc.pt.ApplicationDocumentServletParameters;
-
 import com.pyx4j.essentials.rpc.report.DownloadFormat;
 
-public class ApplicationDocumentServletParametersTest {
+public class ApplicationDocumentServletParametersTest extends TestCase {
 
-    @Test
     public void testSUPPORTED_FILE_EXTENSIONS_contains_true() {
         assertTrue("BMP", ApplicationDocumentServletParameters.SUPPORTED_FILE_EXTENSIONS.contains(DownloadFormat.BMP));
         assertTrue("JPEG", ApplicationDocumentServletParameters.SUPPORTED_FILE_EXTENSIONS.contains(DownloadFormat.JPEG));
         assertTrue("PDF", ApplicationDocumentServletParameters.SUPPORTED_FILE_EXTENSIONS.contains(DownloadFormat.PDF));
     }
 
-    @Test
     public void testSUPPORTED_FILE_EXTENSIONS_contains_false() {
         assertFalse(ApplicationDocumentServletParameters.SUPPORTED_FILE_EXTENSIONS.contains(DownloadFormat.HTML));
     }
