3fd4058ffe827cd7bf232b737db0734636c27a69
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-04-27 00:48:49 +0000

    VISTA-249 cmd which runs build with GWT compilation but without tests ;)

---
+++ bg-no-tests.cmd
@@ -0,0 +1,17 @@
+@echo off
+rem @version $Revision$ ($Author$)  $Date$
+rem helper to run maven2
+rem
+
+for /f "tokens=*" %%I in ('CD') do @set CurDir=%%~nI
+title *%CurDir% - mvn with pyx and GWT Compile
+call mvn install -P pyx,gwtc %* -Dmaven.test.skip=true
+@if errorlevel 1 goto errormark
+title %CurDir%
+
+@goto endmark
+:errormark
+	echo   
+    pause
+:endmark
+@ENDLOCAL
