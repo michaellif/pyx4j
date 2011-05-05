46246682e22a4a84a54acd2ccd4f256875ff79c7
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-05-05 22:14:24 +0000

    VISTA-32 #time 2h Testing complete application

--- vista-shared/vista-integration/src/test/java/com/propertyvista/unit/portal/CreateCompleteApplicationTest.java
+++ vista-shared/vista-integration/src/test/java/com/propertyvista/unit/portal/CreateCompleteApplicationTest.java
@@ -13,6 +13,14 @@
  */
 package com.propertyvista.unit.portal;
 
+import com.pyx4j.commons.CompositeDebugId;
+import com.pyx4j.commons.IDebugId;
+import com.pyx4j.essentials.client.crud.CrudDebugId;
+import com.pyx4j.forms.client.ui.FormNavigationDebugId;
+import com.pyx4j.security.rpc.AuthenticationRequest;
+import com.pyx4j.selenium.D;
+import com.pyx4j.site.rpc.AppPlaceInfo;
+
 import com.propertyvista.portal.domain.DemoData;
 import com.propertyvista.portal.domain.User;
 import com.propertyvista.portal.domain.pt.Address;
@@ -20,9 +28,16 @@ import com.propertyvista.portal.domain.pt.Address.OwnedRented;
 import com.propertyvista.portal.domain.pt.ApartmentUnit;
 import com.propertyvista.portal.domain.pt.Application;
 import com.propertyvista.portal.domain.pt.EmergencyContact;
+import com.propertyvista.portal.domain.pt.IAddress;
+import com.propertyvista.portal.domain.pt.IncomeInfoEmployer;
+import com.propertyvista.portal.domain.pt.PotentialTenantFinancial;
 import com.propertyvista.portal.domain.pt.PotentialTenantInfo;
 import com.propertyvista.portal.domain.pt.PotentialTenantList;
 import com.propertyvista.portal.domain.pt.Summary;
+import com.propertyvista.portal.domain.pt.SummaryPotentialTenantFinancial;
+import com.propertyvista.portal.domain.pt.TenantAsset;
+import com.propertyvista.portal.domain.pt.TenantGuarantor;
+import com.propertyvista.portal.domain.pt.TenantIncome;
 import com.propertyvista.portal.domain.pt.UnitSelection;
 import com.propertyvista.portal.domain.pt.Vehicle;
 import com.propertyvista.portal.rpc.pt.AccountCreationRequest;
@@ -32,14 +47,6 @@ import com.propertyvista.portal.rpc.pt.VistaFormsDebugId;
 import com.propertyvista.portal.server.generator.SharedData;
 import com.propertyvista.portal.server.generator.VistaDataGenerator;
 import com.propertyvista.portal.server.pt.services.ApplicationServiceImpl;
-
-import com.pyx4j.commons.CompositeDebugId;
-import com.pyx4j.commons.IDebugId;
-import com.pyx4j.essentials.client.crud.CrudDebugId;
-import com.pyx4j.forms.client.ui.FormNavigationDebugId;
-import com.pyx4j.security.rpc.AuthenticationRequest;
-import com.pyx4j.selenium.D;
-import com.pyx4j.site.rpc.AppPlaceInfo;
 
 public class CreateCompleteApplicationTest extends PortalVerificationTestCase {
 
@@ -52,7 +59,10 @@ public class CreateCompleteApplicationTest extends PortalVerificationTestCase {
     }
 
     public void testFullFlow() throws Exception {
-        VistaDataGenerator generator = new VistaDataGenerator(DemoData.PT_GENERATION_SEED);
+        long seed = DemoData.PT_GENERATION_SEED;
+        seed = 50;
+        //seed = 2;
+        VistaDataGenerator generator = new VistaDataGenerator(seed);
         User user = createTestUser();
         Application application = generator.createApplication(user);
         Summary summary = generator.createSummary(application, null);
@@ -61,6 +71,7 @@ public class CreateCompleteApplicationTest extends PortalVerificationTestCase {
         enterUnitSelection();
         enterTenantsPage(summary);
         enterTestInfoPages(summary);
+        enterFinancialPages(summary);
 
         selenium.click(VistaFormsDebugId.Auth_LogOutTop);
 
@@ -75,6 +86,7 @@ public class CreateCompleteApplicationTest extends PortalVerificationTestCase {
         verifyTenantsPage(summary);
         //TODO
         //verifyInfoPages(summary);
+        //verifyFinancialPages(summary);
     }
 
     private void createAccount(User user) {
@@ -180,52 +192,153 @@ public class CreateCompleteApplicationTest extends PortalVerificationTestCase {
         }
     }
 
-    private void enterAddressForm(IDebugId fromDebugId, Address address) {
-        setValueOnForm(fromDebugId, address.street1());
-        setValueOnForm(fromDebugId, address.street2());
-        setValueOnForm(fromDebugId, address.city());
-        setValueOnForm(fromDebugId, address.phone());
-        setValueOnForm(fromDebugId, address.moveInDate());
-        setValueOnForm(fromDebugId, address.moveOutDate());
+    private void enterIAddressForm(IDebugId formDebugId, IAddress address) {
+        setValueOnForm(formDebugId, address.street1());
+        setValueOnForm(formDebugId, address.street2());
+        setValueOnForm(formDebugId, address.city());
+        setValueOnForm(formDebugId, address.country());
+        setValueOnForm(formDebugId, address.postalCode());
+        setValueOnForm(formDebugId, address.province());
+    }
 
-        setValueOnForm(fromDebugId, address.country());
-        setValueOnForm(fromDebugId, address.postalCode());
-        setValueOnForm(fromDebugId, address.province());
+    private void enterAddressForm(IDebugId formDebugId, Address address) {
+        enterIAddressForm(formDebugId, address);
 
-        setValueOnForm(fromDebugId, address.rented());
+        setValueOnForm(formDebugId, address.phone());
+        setValueOnForm(formDebugId, address.moveInDate());
+        setValueOnForm(formDebugId, address.moveOutDate());
+
+        setValueOnForm(formDebugId, address.rented());
         if (OwnedRented.Owned == address.rented().getValue()) {
-            assertNotVisible(D.id(fromDebugId, address.payment()));
-            assertNotVisible(D.id(fromDebugId, address.managerName()));
+            assertNotVisible(D.id(formDebugId, address.payment()));
+            assertNotVisible(D.id(formDebugId, address.managerName()));
         } else {
-            assertVisible(D.id(fromDebugId, address.payment()));
-            assertVisible(D.id(fromDebugId, address.managerName()));
-            setValueOnForm(fromDebugId, address.payment());
-            setValueOnForm(fromDebugId, address.managerName());
+            assertVisible(D.id(formDebugId, address.payment()));
+            assertVisible(D.id(formDebugId, address.managerName()));
+            setValueOnForm(formDebugId, address.payment());
+            setValueOnForm(formDebugId, address.managerName());
         }
 
     }
 
-    private void enterVehicleRow(IDebugId fromDebugId, Vehicle vehicle) {
-        setValueOnForm(fromDebugId, vehicle.plateNumber());
-        setValueOnForm(fromDebugId, vehicle.year());
-        setValueOnForm(fromDebugId, vehicle.make());
-        setValueOnForm(fromDebugId, vehicle.model());
-        setValueOnForm(fromDebugId, vehicle.country());
-        setValueOnForm(fromDebugId, vehicle.province());
+    private void enterVehicleRow(IDebugId formDebugId, Vehicle vehicle) {
+        setValueOnForm(formDebugId, vehicle.plateNumber());
+        setValueOnForm(formDebugId, vehicle.year());
+        setValueOnForm(formDebugId, vehicle.make());
+        setValueOnForm(formDebugId, vehicle.model());
+        setValueOnForm(formDebugId, vehicle.country());
+        setValueOnForm(formDebugId, vehicle.province());
     }
 
-    private void enterEmContactsForm(IDebugId fromDebugId, EmergencyContact contact) {
-        setValueOnForm(fromDebugId, contact.firstName());
-        setValueOnForm(fromDebugId, contact.middleName());
-        setValueOnForm(fromDebugId, contact.lastName());
-        setValueOnForm(fromDebugId, contact.homePhone());
-        setValueOnForm(fromDebugId, contact.mobilePhone());
-        setValueOnForm(fromDebugId, contact.workPhone());
-        setValueOnForm(fromDebugId, contact.address().street1());
-        setValueOnForm(fromDebugId, contact.address().street2());
-        setValueOnForm(fromDebugId, contact.address().city());
-        setValueOnForm(fromDebugId, contact.address().country());
-        setValueOnForm(fromDebugId, contact.address().postalCode());
-        setValueOnForm(fromDebugId, contact.address().province());
+    private void enterEmContactsForm(IDebugId fomrDebugId, EmergencyContact contact) {
+        setValueOnForm(fomrDebugId, contact.firstName());
+        setValueOnForm(fomrDebugId, contact.middleName());
+        setValueOnForm(fomrDebugId, contact.lastName());
+        setValueOnForm(fomrDebugId, contact.homePhone());
+        setValueOnForm(fomrDebugId, contact.mobilePhone());
+        setValueOnForm(fomrDebugId, contact.workPhone());
+        setValueOnForm(fomrDebugId, contact.address().street1());
+        setValueOnForm(fomrDebugId, contact.address().street2());
+        setValueOnForm(fomrDebugId, contact.address().city());
+        setValueOnForm(fomrDebugId, contact.address().country());
+        setValueOnForm(fomrDebugId, contact.address().postalCode());
+        setValueOnForm(fomrDebugId, contact.address().province());
     }
+
+    private void enterFinancialPages(Summary summary) {
+        int i = 0;
+        for (SummaryPotentialTenantFinancial tenantFin : summary.tenantFinancials()) {
+            // tenants and tenantFinancials are mapped one to one in created data
+            PotentialTenantInfo tenant = summary.tenantList().tenants().get(i);
+            i++;
+
+            if (ApplicationServiceImpl.shouldEnterInformation(tenant)) {
+                enterFinancialForm(detach(tenantFin.tenantFinancial()));
+                saveAndContinue();
+            }
+        }
+    }
+
+    private void enterFinancialForm(PotentialTenantFinancial tenantFin) {
+        //incomes
+        int num = 0;
+        for (TenantIncome income : tenantFin.incomes()) {
+            selenium.click(D.id(tenantFin.incomes(), FormNavigationDebugId.Form_Add));
+            enterIncomeRow(D.id(tenantFin.incomes(), num), detach(income));
+            num++;
+        }
+
+        num = 0;
+        for (TenantAsset asset : tenantFin.assets()) {
+            selenium.click(D.id(tenantFin.assets(), FormNavigationDebugId.Form_Add));
+            enterAssetRow(D.id(tenantFin.assets(), num), detach(asset));
+            num++;
+        }
+
+        num = 0;
+        for (TenantGuarantor guarantor : tenantFin.guarantors()) {
+            selenium.click(D.id(tenantFin.guarantors(), FormNavigationDebugId.Form_Add));
+            enterGuarantorRow(D.id(tenantFin.guarantors(), num), detach(guarantor));
+            num++;
+        }
+
+    }
+
+    private void enterIncomeRow(IDebugId formDebugId, TenantIncome income) {
+        setValueOnForm(formDebugId, income.incomeSource());
+
+        switch (income.incomeSource().getValue()) {
+        case fulltime:
+        case parttime:
+            enterEmployerForm(D.id(formDebugId, income.employer()), detach(income.employer()));
+            break;
+        case selfemployed:
+            // TODO Leon
+            // income.selfEmployed();
+            break;
+        case seasonallyEmployed:
+            // TODO Leon
+            // income.seasonallyEmployed()
+            break;
+        case socialServices:
+            // TODO Leon
+            // income.socialServices()
+            break;
+        case student:
+            // TODO Leon
+            // income.studentIncome()
+            break;
+        default:
+            // TODO Leon
+            // income.otherIncomeInfo());
+        }
+
+    }
+
+    private void enterEmployerForm(IDebugId formDebugId, IncomeInfoEmployer employer) {
+        setValueOnForm(formDebugId, employer.name());
+        enterIAddressForm(formDebugId, employer);
+        //TODO VladS
+        setValueOnForm(formDebugId, employer.monthlyAmount());
+        // TODO Leon
+    }
+
+    private void enterAssetRow(IDebugId formDebugId, TenantAsset asset) {
+        setValueOnForm(formDebugId, asset.assetType());
+        setValueOnForm(formDebugId, asset.percent());
+        //TODO VladS
+        setValueOnForm(formDebugId, asset.assetValue().amount());
+    }
+
+    private void enterGuarantorRow(IDebugId fromDebugId, TenantGuarantor guarantor) {
+        setValueOnForm(fromDebugId, guarantor.firstName());
+        setValueOnForm(fromDebugId, guarantor.middleName());
+        setValueOnForm(fromDebugId, guarantor.lastName());
+
+        // TODO Leon
+
+        setValueOnForm(fromDebugId, guarantor.birthDate());
+        setValueOnForm(fromDebugId, guarantor.email());
+    }
+
 }
