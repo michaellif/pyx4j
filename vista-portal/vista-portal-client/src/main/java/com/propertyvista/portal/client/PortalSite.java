374db59bd570cd13e5a884462c6289a5a79e621a
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-05-11 19:03:08 +0000

    VISTA-61 Move PT Application Wizard to new module - finalizing

---
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/PortalSite.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-05-11
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.portal.client;
+
+import com.google.gwt.user.client.Command;
+
+import com.propertyvista.common.client.VistaSite;
+
+public class PortalSite extends VistaSite {
+
+    @Override
+    public void showMessageDialog(String message, String title, String buttonText, Command command) {
+        // TODO Auto-generated method stub
+    }
+
+}
