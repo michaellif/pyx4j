cb3799eabb56b32d7521d41fe98a7e954b36d282
Michael Lifshits <michaellif@propertyvista.com>
2011-05-15 13:26:52 +0000

    VISTA-407 Package images separately for IE7

--- vista-portal/vista-portal-ptapp/src/main/java/com/propertyvista/portal/ptapp/client/resources/PortalImages.java
+++ vista-portal/vista-portal-ptapp/src/main/java/com/propertyvista/portal/ptapp/client/resources/PortalImages.java
@@ -16,143 +16,190 @@ package com.propertyvista.portal.ptapp.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.ImageResource.RepeatStyle;
 
 public interface PortalImages extends ClientBundle {
 
     PortalImages INSTANCE = GWT.create(PortalImages.class);
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("add.png")
     ImageResource addRow();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("add_hover.png")
     ImageResource addRowHover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("del.png")
     ImageResource delRow();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("del_hover.png")
     ImageResource delRowHover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("empty.png")
     ImageResource hideRemoveRow();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("bg_body.gif")
     ImageResource bodyBackground();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("bg_body_2.gif")
     ImageResource body2Background();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step.png")
     ImageResource step();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_latest.png")
     ImageResource stepLatest();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_valid.png")
     ImageResource stepValid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_invalid.png")
     ImageResource stepInvalid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_pointer.png")
     ImageResource stepPointer();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_pointer_latest.png")
     ImageResource stepPointerLatest();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_pointer_valid.png")
     ImageResource stepPointerValid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step_pointer_invalid.png")
     ImageResource stepPointerInvalid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("btn.gif")
     ImageResource buttonBackground();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("clip.png")
     ImageResource clip();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("bulet.gif")
     ImageResource bulet();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("dont_worry.png")
     ImageResource dontWorry();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("requirements.png")
     ImageResource requirements();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("time.png")
     ImageResource time();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("exclamation.png")
     ImageResource exclamation();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("check.png")
     ImageResource check();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("check_hover.png")
     ImageResource checkHover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("warning_el.png")
     ImageResource warning();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("warning_el_hover.png")
     ImageResource warningHover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("user_message_info.png")
     ImageResource userMessageInfo();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("info.png")
     ImageResource info();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("info_orange.png")
     ImageResource infoOrange();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("info_red.png")
     ImageResource infoRed();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("pointer_menu.png")
     ImageResource pointer();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("pointer_expanded.png")
     ImageResource pointerExpanded();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("pointer_collapsed.png")
     ImageResource pointerCollapsed();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("qv.png")
     ImageResource qv();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("qv_hover.png")
     ImageResource qvHover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-master.gif")
     ImageResource paymentMC();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-visa.gif")
     ImageResource paymentVISA();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-amex.gif")
     ImageResource paymentAMEX();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-discover.gif")
     ImageResource paymentDiscover();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-interact.gif")
     ImageResource paymentInterac();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("payment-ach.gif")
     ImageResource paymentACH();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("cheque-guide.jpg")
     ImageResource chequeGuide();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("bild.gif")
     ImageResource building();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("home.png")
     ImageResource floorplan();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("step2.png")
     ImageResource step2();
 
