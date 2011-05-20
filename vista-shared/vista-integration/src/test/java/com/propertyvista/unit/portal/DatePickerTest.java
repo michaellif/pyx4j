d2020123bc845c5fe6b8e5276f002cebdb512e68
Leon Tager <leont@birchwoodsoftwaregroup.com>
2011-05-20 21:10:54 +0000

    VISTA-32 Modified Date picker test, added function for selenium to navigate to specified date

--- vista-shared/vista-integration/src/test/java/com/propertyvista/unit/portal/DatePickerTest.java
+++ vista-shared/vista-integration/src/test/java/com/propertyvista/unit/portal/DatePickerTest.java
@@ -8,10 +8,23 @@
  * This notice and attribution to Property Vista Software Inc. may not be removed.
  *
  * Created on 2011-05-18
- * @author yura
+ * @author leont
  * @version $Id$
  */
 package com.propertyvista.unit.portal;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.google.gwt.user.datepicker.client.CalendarUtil;
+import com.propertyvista.common.domain.DemoData;
+import com.propertyvista.common.domain.User;
+import com.propertyvista.portal.domain.ptapp.Application;
+import com.propertyvista.portal.domain.ptapp.UnitSelection;
+import com.propertyvista.portal.rpc.ptapp.PtSiteMap;
+import com.propertyvista.portal.rpc.ptapp.VistaFormsDebugId;
+import com.propertyvista.portal.server.generator.PTGenerator;
+import com.propertyvista.portal.server.generator.SharedData;
 
 import com.pyx4j.commons.CompositeDebugId;
 import com.pyx4j.commons.IDebugId;
@@ -21,27 +34,24 @@ import com.pyx4j.forms.client.ui.CCompDebugId;
 import com.pyx4j.security.rpc.AuthenticationRequest;
 import com.pyx4j.selenium.D;
 import com.pyx4j.site.rpc.AppPlaceInfo;
-import com.pyx4j.widgets.client.datepicker.DatePickerDebugIDs;
-
-import com.propertyvista.common.domain.DemoData;
-import com.propertyvista.common.domain.User;
-import com.propertyvista.portal.domain.ptapp.Application;
-import com.propertyvista.portal.domain.ptapp.Summary;
-import com.propertyvista.portal.domain.ptapp.UnitSelection;
-import com.propertyvista.portal.rpc.ptapp.PtSiteMap;
-import com.propertyvista.portal.rpc.ptapp.VistaFormsDebugId;
-import com.propertyvista.portal.server.generator.PTGenerator;
-import com.propertyvista.portal.server.generator.SharedData;
+import com.pyx4j.widgets.client.datepicker.DatePickerIDs;
 
 public class DatePickerTest extends WizardBaseSeleniumTestCase {
 
-    private final IDebugId gridId = new CompositeDebugId(DatePickerDebugIDs.DatePicker, "0");
+    private final IDebugId gridId = new CompositeDebugId(DatePickerIDs.DatePicker, "0");
+
+    private final IDebugId backMonth = DatePickerIDs.MonthSelectorButton_BackwardsMonth;
+
+    private final IDebugId forwardMonth = DatePickerIDs.MonthSelectorButton_ForwardMonth;
+
+    private final IDebugId backYear = DatePickerIDs.MonthSelectorButton_BackwardsYear;
+
+    private final IDebugId forwardYear = DatePickerIDs.MonthSelectorButton_ForwardYear;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         SharedData.init();
-        // This is just to make the test more visual
         selenium.setFocusOnGetValue(true);
     }
 
@@ -49,8 +59,6 @@ public class DatePickerTest extends WizardBaseSeleniumTestCase {
         PTGenerator generator = new PTGenerator(DemoData.PT_GENERATION_SEED);
         User user = generator.createUser(1);
         Application application = generator.createApplication(user);
-        Summary summary = generator.createSummary(application, null);
-        UnitSelection unitSel = generator.createUnitSelection(application, null);
 
         selenium.click(VistaFormsDebugId.Auth_Login);
         selenium.type(D.id(proto(AuthenticationRequest.class).email()), user.email().getValue());
@@ -61,15 +69,47 @@ public class DatePickerTest extends WizardBaseSeleniumTestCase {
         startRentDateTest();
     }
 
+    @SuppressWarnings("deprecation")
     private void startRentDateTest() {
-        IDebugId image = D.id(D.id(proto(UnitSelection.class).selectionCriteria().availableFrom()), CCompDebugId.trigger);
-        selenium.click(image);
+        selenium.click(D.id(D.id(proto(UnitSelection.class).selectionCriteria().availableFrom()), CCompDebugId.trigger));
+        navigateToDate(90, 1);
         selenium.click(getCellDebugId(1, 1, gridId));
-        selenium.click(D.id(proto(UnitSelection.class).selectionCriteria().availableTo()));
+        selenium.click(D.id(D.id(proto(UnitSelection.class).selectionCriteria().availableTo()), CCompDebugId.trigger));
+        navigateToDate(111, 1);
+        selenium.click(getCellDebugId(1, 1, gridId));
     }
 
     private IDebugId getCellDebugId(int row, int column, IDebugId parent) {
         return new StringDebugId(parent.debugId() + "_" + row + "_" + column);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void navigateToDate(int year, int month) {
+        Date now = Calendar.getInstance().getTime();
+        int difference;
+        IDebugId button;
+        if (now.getYear() < year) {
+            button = forwardYear;
+            difference = 1;
+        } else {
+            button = backYear;
+            difference = -1;
+        }
+        while (now.getYear() != year) {
+            selenium.click(button);
+            CalendarUtil.addMonthsToDate(now, difference * 12);
+        }
+        if (now.getMonth() < month) {
+            button = forwardMonth;
+            difference = 1;
+        } else {
+            button = backMonth;
+            difference = -1;
+        }
+        while (now.getMonth() != month) {
+            selenium.click(button);
+            CalendarUtil.addMonthsToDate(now, difference);
+        }
     }
 
 }
