a101a489e3723da54aa41366b0ef32a20e7a142d
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-05-21 07:15:04 +0000

    VISTA-9 #time 20m Use BuildingDTO in service

---
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/BuildingCrudServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-05-21
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.crm.server.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.pyx4j.entity.rpc.EntitySearchResult;
+import com.pyx4j.entity.server.EntityServicesImpl;
+import com.pyx4j.entity.server.PersistenceServicesFactory;
+import com.pyx4j.entity.shared.criterion.EntitySearchCriteria;
+
+import com.propertyvista.crm.rpc.services.BuildingCrudService;
+import com.propertyvista.domain.property.asset.building.Building;
+import com.propertyvista.dto.BuildingDTO;
+
+public class BuildingCrudServiceImpl implements BuildingCrudService {
+
+    private final static Logger log = LoggerFactory.getLogger(BuildingCrudServiceImpl.class);
+
+    @Override
+    public void create(AsyncCallback<BuildingDTO> callback, BuildingDTO editableEntity) {
+        Building building = GenericConverter.down(editableEntity, Building.class);
+        PersistenceServicesFactory.getPersistenceService().persist(building);
+        callback.onSuccess(GenericConverter.up(building, BuildingDTO.class));
+    }
+
+    @Override
+    public void retrieve(AsyncCallback<BuildingDTO> callback, long entityId) {
+        Building building = PersistenceServicesFactory.getPersistenceService().retrieve(Building.class, entityId);
+        callback.onSuccess(GenericConverter.up(building, BuildingDTO.class));
+    }
+
+    @Override
+    public void save(AsyncCallback<BuildingDTO> callback, BuildingDTO editableEntity) {
+        Building building = GenericConverter.down(editableEntity, Building.class);
+        PersistenceServicesFactory.getPersistenceService().merge(building);
+        callback.onSuccess(GenericConverter.up(building, BuildingDTO.class));
+    }
+
+    @Override
+    public void search(AsyncCallback<EntitySearchResult<BuildingDTO>> callback, EntitySearchCriteria<BuildingDTO> criteria) {
+        EntitySearchCriteria<Building> c = GenericConverter.down(criteria, Building.class);
+        //TODO add building specific criteria
+        callback.onSuccess(GenericConverter.up(EntityServicesImpl.secureSearch(c), BuildingDTO.class));
+    }
+
+}
---
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/GenericConverter.java
@@ -0,0 +1,58 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-05-21
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.crm.server.services;
+
+import java.util.Vector;
+
+import com.pyx4j.entity.rpc.EntitySearchResult;
+import com.pyx4j.entity.shared.EntityFactory;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.criterion.EntitySearchCriteria;
+
+public class GenericConverter {
+
+    //TODO move to more generic place
+    public static <D extends IEntity> D down(D src, Class<D> dstClass) {
+        D dst = EntityFactory.create(dstClass);
+        dst.set(src);
+        return dst;
+    }
+
+    //TODO move to more generic place
+    public static <S extends IEntity, D extends S> D up(S src, Class<D> dstClass) {
+        D dst = EntityFactory.create(dstClass);
+        dst.set(src);
+        return dst;
+    }
+
+    //TODO move to more generic place
+    public static <E extends IEntity, D extends E> EntitySearchResult<D> up(EntitySearchResult<E> resultE, Class<D> dstClass) {
+        EntitySearchResult<D> result = new EntitySearchResult<D>();
+        result.setEncodedCursorReference(resultE.getEncodedCursorReference());
+        result.hasMoreData(resultE.hasMoreData());
+        Vector<D> data = new Vector<D>();
+        for (E entity : resultE.getData()) {
+            data.add(up(entity, dstClass));
+        }
+        result.setData(data);
+        return result;
+    }
+
+    //TODO move to more generic place
+    public static <D extends IEntity> EntitySearchCriteria<D> down(EntitySearchCriteria<? extends D> src, Class<D> dstClass) {
+        EntitySearchCriteria<D> dst = EntitySearchCriteria.create(dstClass);
+        //TODO convert search criteria
+        return dst;
+    }
+}
