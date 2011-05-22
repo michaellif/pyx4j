b098af523b39885b1f893ea9f0b1a112cb7cc938
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-05-22 03:06:13 +0000

    VISTA-412 DB connection initialization

--- vista-server/src/main/java/com/propertyvista/server/importer/Importer.java
+++ vista-server/src/main/java/com/propertyvista/server/importer/Importer.java
@@ -16,53 +16,54 @@ package com.propertyvista.server.importer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.propertyvista.portal.server.generator.SharedData;
-import com.propertyvista.portal.server.importer.XmlUtil;
-import com.propertyvista.portal.server.importer.bean.Residential;
+import com.pyx4j.config.server.ServerSideConfiguration;
 import com.pyx4j.entity.server.PersistenceServicesFactory;
 import com.pyx4j.entity.shared.IEntity;
 import com.pyx4j.gwt.server.IOUtils;
 
+import com.propertyvista.domain.property.asset.building.Building;
+import com.propertyvista.portal.server.generator.SharedData;
+import com.propertyvista.portal.server.importer.XmlUtil;
+import com.propertyvista.portal.server.importer.bean.Residential;
+import com.propertyvista.server.config.VistaServerSideConfiguration;
+
 public class Importer {
-	private static final Logger log = LoggerFactory.getLogger(Importer.class);
+    private static final Logger log = LoggerFactory.getLogger(Importer.class);
 
-	public Importer() {
-	}
+    public Importer() {
+    }
 
-	public void start() throws Exception {
-		// read
-		String xml = IOUtils.getTextResource(XmlUtil.resourceFileName(
-				XmlUtil.class, "data.xml"));
-		log.debug("Loaded " + xml);
+    public void start() throws Exception {
+        // read
+        String xml = IOUtils.getTextResource(XmlUtil.resourceFileName(XmlUtil.class, "data.xml"));
+        log.debug("Loaded " + xml);
 
-		Residential residential = XmlUtil.unmarshallResidential(xml);
-		log.info("Residential\n " + residential + "\n");
+        Residential residential = XmlUtil.unmarshallResidential(xml);
+        log.info("Residential\n " + residential + "\n");
 
-		// map
-		SharedData.init();
-		Mapper mapper = new Mapper();
-		mapper.load(residential);
+        // map
+        SharedData.init();
+        Mapper mapper = new Mapper();
+        mapper.load(residential);
 
-		// save
-		// VistaServerSideConfiguration conf = new
-		// VistaServerSideConfiguration();
-		// ServerSideConfiguration.setInstance(conf);
-		// for (Building building : mapper.getBuildings()) {
-		// persist(building);
-		// }
-	}
+        // save
+        for (Building building : mapper.getBuildings()) {
+            persist(building);
+        }
+    }
 
-	private static void persist(IEntity entity) {
-		PersistenceServicesFactory.getPersistenceService().persist(entity);
-	}
+    private static void persist(IEntity entity) {
+        PersistenceServicesFactory.getPersistenceService().persist(entity);
+    }
 
-	public static void main(String[] args) {
-		log.info("Importing new Data...");
-		try {
-			Importer importer = new Importer();
-			importer.start();
-		} catch (Exception e) {
-			log.error("Problem with generating xml", e);
-		}
-	}
+    public static void main(String[] args) {
+        ServerSideConfiguration.setInstance(new VistaServerSideConfiguration());
+        log.info("Importing new Data...");
+        try {
+            Importer importer = new Importer();
+            importer.start();
+        } catch (Exception e) {
+            log.error("Problem with generating xml", e);
+        }
+    }
 }
