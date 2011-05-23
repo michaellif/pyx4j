eed37874497f6ad870de4595398f8f43c5fa2844
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-05-23 16:33:25 +0000

    VISTA-9 #time 20m USe BuildingDTO in service

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/security/VistaCrmAccessControlList.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/security/VistaCrmAccessControlList.java
@@ -20,6 +20,7 @@ import com.pyx4j.rpc.shared.ServiceExecutePermission;
 import com.pyx4j.security.server.ServletContainerAclBuilder;
 
 import com.propertyvista.common.domain.ref.Country;
+import com.propertyvista.crm.rpc.services.BuildingCrudService;
 import com.propertyvista.crm.rpc.services.BuildingCrudServiceOld;
 import com.propertyvista.crm.rpc.services.UnitCrudService;
 import com.propertyvista.domain.property.asset.building.Building;
@@ -43,6 +44,7 @@ public class VistaCrmAccessControlList extends ServletContainerAclBuilder {
 
         grant(new EntityPermission(Building.class, EntityPermission.ALL));
         grant(new IServiceExecutePermission(BuildingCrudServiceOld.class));
+        grant(new IServiceExecutePermission(BuildingCrudService.class));
         grant(new EntityPermission(AptUnit.class, EntityPermission.ALL));
         grant(new IServiceExecutePermission(UnitCrudService.class));
 
