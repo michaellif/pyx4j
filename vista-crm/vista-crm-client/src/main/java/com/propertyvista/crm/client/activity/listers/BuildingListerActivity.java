f1b46f808de0149e298344b41381eb4c984c76e6
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-05-23 16:26:55 +0000

    VISTA-9 - Building domain usage implementation . .

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/listers/BuildingListerActivity.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/listers/BuildingListerActivity.java
@@ -18,16 +18,14 @@ import com.google.inject.Inject;
 
 import com.propertyvista.crm.client.ui.listers.IBuildingListerView;
 import com.propertyvista.crm.rpc.services.AbstractCrudService;
-import com.propertyvista.crm.rpc.services.BuildingCrudServiceOld;
-import com.propertyvista.domain.property.asset.building.Building;
+import com.propertyvista.crm.rpc.services.BuildingCrudService;
 import com.propertyvista.dto.BuildingDTO;
 
-public class BuildingListerActivity extends ListerActivityBase<Building, BuildingDTO> {
+public class BuildingListerActivity extends ListerActivityBase<BuildingDTO, BuildingDTO> {
 
     @Inject
     @SuppressWarnings("unchecked")
     public BuildingListerActivity(IBuildingListerView view) {
-        super(view, (AbstractCrudService<BuildingDTO>) GWT.create(BuildingCrudServiceOld.class), Building.class);
+        super(view, (AbstractCrudService<BuildingDTO>) GWT.create(BuildingCrudService.class), BuildingDTO.class);
     }
-
 }
