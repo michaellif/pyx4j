2b890ed2b6b5745c28c24d1987507b3e1dd96b52
Dmitry Tumanov <dmitryt@birchwoodsoftwaregroup.com>
2011-05-27 14:13:20 +0000

    Added a new class for marshalling and unmarshalling XML to Java

---
+++ essentials/pyx-essentials-j2se/src/main/java/com/pyx4j/essentials/j2se/util/MarshallUtil.java
@@ -0,0 +1,46 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2011 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on May 27, 2011
+ * @author dmitry
+ * @version $Id$
+ */
+package com.pyx4j.essentials.j2se.util;
+
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class MarshallUtil {
+
+    public static <T> T unmarshall(Class<T> clazz, String xml) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(clazz);
+        Unmarshaller um = context.createUnmarshaller();
+        @SuppressWarnings("unchecked")
+        T result = (T) um.unmarshal(new StringReader(xml));
+        return result;
+    }
+
+    public static <T> void marshall(T data) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(data.getClass());
+        Marshaller m = context.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        m.marshal(data, System.out);
+    }
+}
