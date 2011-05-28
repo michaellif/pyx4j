f4dc592a47b6dd0687edaeceec2519839b29eec0
Dmitry Tumanov <dmitryt@birchwoodsoftwaregroup.com>
2011-05-28 01:20:39 +0000

    VISTA-409 Migrated all calls from Example.java to YardiTransactions, sample xml content is now stored within the repository for validation and parsing

--- vista-shared/vista-yardi/src/main/java/com/propertyvista/yardi/YardiTransactions.java
+++ vista-shared/vista-yardi/src/main/java/com/propertyvista/yardi/YardiTransactions.java
@@ -14,6 +14,8 @@
 package com.propertyvista.yardi;
 
 import java.rmi.RemoteException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.xml.bind.JAXBException;
 
@@ -23,6 +25,14 @@ import org.slf4j.LoggerFactory;
 
 import com.yardi.ws.operations.GetPropertyConfigurations;
 import com.yardi.ws.operations.GetPropertyConfigurationsResponse;
+import com.yardi.ws.operations.GetResidentTransactions_ByChargeDate_Login;
+import com.yardi.ws.operations.GetResidentTransactions_ByChargeDate_LoginResponse;
+import com.yardi.ws.operations.GetResidentTransactions_Login;
+import com.yardi.ws.operations.GetResidentTransactions_LoginResponse;
+import com.yardi.ws.operations.GetResidentsLeaseCharges_Login;
+import com.yardi.ws.operations.GetResidentsLeaseCharges_LoginResponse;
+import com.yardi.ws.operations.GetUnitInformation_Login;
+import com.yardi.ws.operations.GetUnitInformation_LoginResponse;
 import com.yardi.ws.operations.Ping;
 import com.yardi.ws.operations.PingResponse;
 
@@ -33,6 +43,8 @@ import com.propertyvista.yardi.bean.Properties;
 public class YardiTransactions {
 
     private final static Logger log = LoggerFactory.getLogger(YardiTransactions.class);
+
+    public static final String USERNAME = "propertyvistaws";
 
     /**
      * The Ping function accepts no parameters, but will return the
@@ -63,7 +75,7 @@ public class YardiTransactions {
         c.transactionId = 2L;
         c.currentActionName = "GetPropertyConfigurations";
         GetPropertyConfigurations l = new GetPropertyConfigurations();
-        l.setUserName("propertyvistaws");
+        l.setUserName(USERNAME);
         l.setPassword("52673");
         l.setServerName("aspdb04");
         l.setDatabase("afqoml_live");
@@ -71,10 +83,108 @@ public class YardiTransactions {
         l.setInterfaceEntity("Property Vista");
         GetPropertyConfigurationsResponse response = c.getResidentTransactionsService().getPropertyConfigurations(l);
         String xml = response.getGetPropertyConfigurationsResult().getExtraElement().toString();
-        log.info("Result: " + xml);
-        // TODO need to load this XML into Java code generated from xsd
+
+        log.info("Result: {}", xml);
         Properties properties = MarshallUtil.unmarshall(Properties.class, xml);
-        log.info("Properties: " + properties);
+        log.info("Properties: {}", properties);
         return properties;
+    }
+
+    /**
+     * Allows export of all units and corresponding occupancy status for a given property/property list.
+     * 
+     * @param c
+     * @throws RemoteException
+     * @throws AxisFault
+     */
+    public static void getUnitInformationLogin(YardiClient c) throws AxisFault, RemoteException {
+        c.transactionId = 3L;
+
+        c.currentActionName = "GetUnitInformation";
+        GetUnitInformation_Login l = new GetUnitInformation_Login();
+        l.setUserName(USERNAME);
+        l.setPassword("52673");
+        l.setServerName("aspdb04");
+        l.setDatabase("afqoml_live");
+        l.setPlatform("SQL");
+        l.setYardiPropertyId("prvista1");
+        l.setInterfaceEntity("Property Vista");
+        GetUnitInformation_LoginResponse response = c.getResidentTransactionsService().getUnitInformation_Login(l);
+        String xml = response.getGetUnitInformation_LoginResult().getExtraElement().toString();
+        log.info("UnitInformationLogin: {}", xml);
+    }
+
+    public static void getResidentTransactionsLogin(YardiClient c) throws AxisFault, RemoteException {
+        c.transactionId++;
+        c.currentActionName = "GetResidentTransactions";
+
+        GetResidentTransactions_Login l = new GetResidentTransactions_Login();
+        l.setUserName(USERNAME);
+        l.setPassword("52673");
+        l.setServerName("aspdb04");
+        l.setDatabase("afqoml_live");
+        l.setPlatform("SQL");
+        l.setYardiPropertyId("prvista1");
+        l.setInterfaceEntity("Property Vista");
+
+        GetResidentTransactions_LoginResponse response = c.getResidentTransactionsService().getResidentTransactions_Login(l);
+        String xml = response.getGetResidentTransactions_LoginResult().getExtraElement().toString();
+        log.info("ResidentTransactionsLogin: {}", xml);
+    }
+
+    public static void getResidentTransactionsByChargeDate(YardiClient c) throws AxisFault, RemoteException {
+        c.transactionId++;
+        c.currentActionName = "GetResidentTransactions_ByChargeDate";
+        GetResidentTransactions_ByChargeDate_Login l = new GetResidentTransactions_ByChargeDate_Login();
+        l.setUserName(USERNAME);
+        l.setPassword("52673");
+        l.setServerName("aspdb04");
+        l.setDatabase("afqoml_live");
+        l.setPlatform("SQL");
+        l.setYardiPropertyId("prvista1");
+        l.setInterfaceEntity("Property Vista");
+
+        //Consumer may only query for a date range of 31 days or less
+        Calendar from = GregorianCalendar.getInstance();
+        from.set(Calendar.YEAR, 2011);
+        from.set(Calendar.MONTH, 2);
+        from.set(Calendar.DAY_OF_MONTH, 1);
+
+        l.setFromDate(from);
+
+        Calendar to = GregorianCalendar.getInstance();
+        to.set(Calendar.YEAR, 2011);
+        to.set(Calendar.MONTH, 2);
+        to.set(Calendar.DAY_OF_MONTH, 28);
+
+        l.setToDate(to);
+
+        GetResidentTransactions_ByChargeDate_LoginResponse response = c.getResidentTransactionsService().getResidentTransactions_ByChargeDate_Login(l);
+        String xml = response.getGetResidentTransactions_ByChargeDate_LoginResult().getExtraElement().toString();
+        log.info("GetResidentTransactionsByChargeDate: {}", xml);
+    }
+
+    public static void getResidentsLeaseCharges(YardiClient c) throws AxisFault, RemoteException {
+        c.transactionId++;
+
+        c.currentActionName = "GetResidentsLeaseCharges";
+        GetResidentsLeaseCharges_Login l = new GetResidentsLeaseCharges_Login();
+        l.setUserName(USERNAME);
+        l.setPassword("52673");
+        l.setServerName("aspdb04");
+        l.setDatabase("afqoml_live");
+        l.setPlatform("SQL");
+        l.setYardiPropertyId("prvista1");
+        l.setInterfaceEntity("Property Vista");
+
+        Calendar when = GregorianCalendar.getInstance();
+        when.set(Calendar.YEAR, 2011);
+        when.set(Calendar.MONTH, 1);
+
+        l.setPostMonth(when);
+
+        GetResidentsLeaseCharges_LoginResponse response = c.getResidentTransactionsService().getResidentsLeaseCharges_Login(l);
+        String xml = response.getGetResidentsLeaseCharges_LoginResult().getExtraElement().toString();
+        log.info("ResidentsLeaseCharges: {}", xml);
     }
 }
--- vista-shared/vista-yardi/src/test/java/com/propertyvista/yardi/YardiExample.java
+++ vista-shared/vista-yardi/src/test/java/com/propertyvista/yardi/YardiExample.java
@@ -13,16 +13,8 @@
  */
 package com.propertyvista.yardi;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.yardi.ws.operations.GetResidentTransactions_ByChargeDate_Login;
-import com.yardi.ws.operations.GetResidentTransactions_Login;
-import com.yardi.ws.operations.GetResidentsLeaseCharges_Login;
-import com.yardi.ws.operations.GetUnitInformation_Login;
 
 public class YardiExample {
 
@@ -34,89 +26,10 @@ public class YardiExample {
         try {
             YardiTransactions.ping(c);
             YardiTransactions.getPropertyConfigurations(c);
-
-            c.transactionId = 3L;
-            {
-                // Allows export of all units and corresponding occupancy status
-                // for a given property/property list.
-                c.currentActionName = "GetUnitInformation";
-                GetUnitInformation_Login l = new GetUnitInformation_Login();
-                l.setUserName("propertyvistaws");
-                l.setPassword("52673");
-                l.setServerName("aspdb04");
-                l.setDatabase("afqoml_live");
-                l.setPlatform("SQL");
-                l.setYardiPropertyId("prvista1");
-                l.setInterfaceEntity("Property Vista");
-                c.getResidentTransactionsService().getUnitInformation_Login(l);
-            }
-
-            c.transactionId = 4L;
-            {
-                c.currentActionName = "GetResidentTransactions";
-                GetResidentTransactions_Login l = new GetResidentTransactions_Login();
-                l.setUserName("propertyvistaws");
-                l.setPassword("52673");
-                l.setServerName("aspdb04");
-                l.setDatabase("afqoml_live");
-                l.setPlatform("SQL");
-                l.setYardiPropertyId("prvista1");
-                l.setInterfaceEntity("Property Vista");
-
-                c.getResidentTransactionsService().getResidentTransactions_Login(l);
-            }
-
-            c.transactionId = 5L;
-            {
-                c.currentActionName = "GetResidentTransactions_ByChargeDate";
-                GetResidentTransactions_ByChargeDate_Login l = new GetResidentTransactions_ByChargeDate_Login();
-                l.setUserName("propertyvistaws");
-                l.setPassword("52673");
-                l.setServerName("aspdb04");
-                l.setDatabase("afqoml_live");
-                l.setPlatform("SQL");
-                l.setYardiPropertyId("prvista1");
-                l.setInterfaceEntity("Property Vista");
-
-                //Consumer may only query for a date range of 31 days or less
-                Calendar from = GregorianCalendar.getInstance();
-                from.set(Calendar.YEAR, 2011);
-                from.set(Calendar.MONTH, 2);
-                from.set(Calendar.DAY_OF_MONTH, 1);
-
-                l.setFromDate(from);
-
-                Calendar to = GregorianCalendar.getInstance();
-                to.set(Calendar.YEAR, 2011);
-                to.set(Calendar.MONTH, 2);
-                to.set(Calendar.DAY_OF_MONTH, 28);
-
-                l.setToDate(to);
-
-                c.getResidentTransactionsService().getResidentTransactions_ByChargeDate_Login(l);
-            }
-
-            c.transactionId = 6L;
-            {
-                c.currentActionName = "GetResidentsLeaseCharges";
-                GetResidentsLeaseCharges_Login l = new GetResidentsLeaseCharges_Login();
-                l.setUserName("propertyvistaws");
-                l.setPassword("52673");
-                l.setServerName("aspdb04");
-                l.setDatabase("afqoml_live");
-                l.setPlatform("SQL");
-                l.setYardiPropertyId("prvista1");
-                l.setInterfaceEntity("Property Vista");
-
-                Calendar when = GregorianCalendar.getInstance();
-                when.set(Calendar.YEAR, 2011);
-                when.set(Calendar.MONTH, 1);
-
-                l.setPostMonth(when);
-
-                c.getResidentTransactionsService().getResidentsLeaseCharges_Login(l);
-            }
-
+            YardiTransactions.getUnitInformationLogin(c);
+            YardiTransactions.getResidentTransactionsLogin(c);
+            YardiTransactions.getResidentTransactionsByChargeDate(c);
+            YardiTransactions.getResidentsLeaseCharges(c);
         } catch (Throwable e) {
             log.error("error", e);
         }
