3a322b50bd58a6acce9731df5bf0bf9e12cef938
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-06-07 16:24:38 +0000

    VISTA-437 - CMS domain structure refactoring .

---
+++ vista-portal/vista-portal-domain/src/main/java/com/propertyvista/portal/domain/site/PageDescriptor.java
@@ -0,0 +1,51 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-06-07
+ * @author Vlad
+ * @version $Id$
+ */
+package com.propertyvista.portal.domain.site;
+
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.Owned;
+import com.pyx4j.entity.annotations.ToString;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IList;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.i18n.shared.I18nEnum;
+import com.pyx4j.i18n.shared.Translatable;
+
+public interface PageDescriptor extends IEntity {
+
+    @Translatable
+    enum Type {
+
+        staticContent,
+
+        findApartment,
+
+        residence;
+
+        @Override
+        public String toString() {
+            return I18nEnum.tr(this);
+        }
+    }
+
+    @ToString(index = 0)
+    @MemberColumn(name = "pageType")
+    IPrimitive<Type> type();
+
+    @ToString(index = 1)
+    IPrimitive<String> caption();
+
+    @Owned
+    IList<PageDescriptor> childPages();
+}
