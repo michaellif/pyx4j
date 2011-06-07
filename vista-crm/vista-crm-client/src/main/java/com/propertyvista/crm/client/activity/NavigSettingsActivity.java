4ea19169dad4733103bb98b9ac4b832e4c4f84f4
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-06-07 02:40:18 +0000

    VISTA-436 - Administration functionality MVP elements implementation

---
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/NavigSettingsActivity.java
@@ -0,0 +1,83 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Feb 1, 2011
+ * @author Misha
+ * @version $Id$
+ */
+package com.propertyvista.crm.client.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.activity.shared.AbstractActivity;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.Place;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.inject.Inject;
+
+import com.pyx4j.site.client.AppSite;
+import com.pyx4j.site.rpc.AppPlace;
+
+import com.propertyvista.crm.client.ui.NavigView;
+import com.propertyvista.crm.rpc.CrmSiteMap;
+
+public class NavigSettingsActivity extends AbstractActivity implements NavigView.MainNavigPresenter {
+
+    private final NavigView view;
+
+    @Inject
+    public NavigSettingsActivity(NavigView view) {
+        this.view = view;
+        view.setPresenter(this);
+    }
+
+    public NavigSettingsActivity withPlace(Place place) {
+        return this;
+    }
+
+    @Override
+    public void start(AcceptsOneWidget panel, EventBus eventBus) {
+        panel.setWidget(view);
+    }
+
+    @Override
+    public void navigTo(Place place) {
+        AppSite.getPlaceController().goTo(place);
+    }
+
+    @Override
+    public String getNavigLabel(AppPlace place) {
+        return AppSite.getHistoryMapper().getPlaceInfo(place).getNavigLabel();
+    }
+
+    @Override
+    public Place getWhere() {
+        return AppSite.getPlaceController().getWhere();
+    }
+
+    @Override
+    public List<NavigFolder> getNavigFolders() {
+        ArrayList<NavigFolder> list = new ArrayList<NavigFolder>();
+
+        //Properties
+        NavigFolder folder = new NavigFolder("Settings");
+        folder.addNavigItem(new CrmSiteMap.Settings.Policy());
+        folder.addNavigItem(new CrmSiteMap.Settings.UserRole());
+        list.add(folder);
+
+        //Tenants
+        folder = new NavigFolder("Portal");
+        folder.addNavigItem(new CrmSiteMap.Settings.General());
+        folder.addNavigItem(new CrmSiteMap.Settings.Content());
+        list.add(folder);
+
+        return list;
+    }
+}
