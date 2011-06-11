d8120b62c75c3d42bcf7321e0ac17268137b1760
Michael Lifshits <michaellif@propertyvista.com>
2011-06-11 16:14:22 +0000

    parseQueryString moved to AppPlace

--- site/pyx-site-gwt/src/main/java/com/pyx4j/site/client/place/AppPlaceHistoryMapper.java
+++ site/pyx-site-gwt/src/main/java/com/pyx4j/site/client/place/AppPlaceHistoryMapper.java
@@ -1,17 +1,8 @@
 package com.pyx4j.site.client.place;
 
 import static com.pyx4j.site.shared.meta.NavigNode.ARGS_GROUP_SEPARATOR;
-import static com.pyx4j.site.shared.meta.NavigNode.ARGS_SEPARATOR;
-import static com.pyx4j.site.shared.meta.NavigNode.NAME_VALUE_SEPARATOR;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.http.client.URL;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 
@@ -20,8 +11,6 @@ import com.pyx4j.site.rpc.AppPlaceInfo;
 import com.pyx4j.site.shared.meta.SiteMap;
 
 public class AppPlaceHistoryMapper implements PlaceHistoryMapper {
-
-    private static final Logger log = LoggerFactory.getLogger(AppPlaceHistoryMapper.class);
 
     private final static AppPlaceListing listing = GWT.create(AppPlaceListing.class);
 
@@ -48,7 +37,7 @@ public class AppPlaceHistoryMapper implements PlaceHistoryMapper {
         AppPlace place = listing.getPlace(siteMapClass, placeId);
 
         if (queryString != null) {
-            place.setArgs(parseQueryString(queryString));
+            place.parseArgs(queryString);
         }
         return place;
     }
@@ -56,7 +45,7 @@ public class AppPlaceHistoryMapper implements PlaceHistoryMapper {
     @Override
     public String getToken(Place place) {
         if (place instanceof AppPlace) {
-            return AppPlaceInfo.getPlaceId(place.getClass()) + createQueryString(((AppPlace) place).getArgs());
+            return ((AppPlace) place).getToken();
         }
         return null;
     }
@@ -74,48 +63,6 @@ public class AppPlaceHistoryMapper implements PlaceHistoryMapper {
 
     public AppPlaceInfo getPlaceInfo(AppPlace place) {
         return listing.getPlaceInfo(place);
-    }
-
-    protected static Map<String, String> parseQueryString(String queryString) {
-        if (queryString.startsWith(ARGS_GROUP_SEPARATOR)) {
-            queryString = queryString.substring(1);
-        }
-        Map<String, String> args = new HashMap<String, String>();
-        if (queryString.length() == 0) {
-            return args;
-        }
-        String[] nameValues = queryString.split(ARGS_SEPARATOR);
-        if (nameValues.length > 0) {
-            for (int i = 0; i < nameValues.length; i++) {
-                String[] nameAndValue = nameValues[i].split(NAME_VALUE_SEPARATOR);
-                if (nameAndValue.length == 2) {
-                    args.put(nameAndValue[0], URL.decodeQueryString(nameAndValue[1]));
-                } else {
-                    log.warn("Can't pars argument {}", nameValues[i]);
-                }
-            }
-        }
-        return args;
-    }
-
-    protected String createQueryString(Map<String, String> args) {
-        if (args == null) {
-            return "";
-        }
-        StringBuilder queryString = new StringBuilder();
-        boolean first = true;
-        for (Map.Entry<String, String> me : args.entrySet()) {
-            if (first) {
-                queryString.append(ARGS_GROUP_SEPARATOR);
-                first = false;
-            } else {
-                queryString.append(ARGS_SEPARATOR);
-            }
-            queryString.append(me.getKey());
-            queryString.append(NAME_VALUE_SEPARATOR);
-            queryString.append(URL.encodeQueryString(me.getValue()));
-        }
-        return queryString.toString();
     }
 
 }
--- site/pyx-site-shared/src/main/java/com/pyx4j/site/rpc/AppPlace.java
+++ site/pyx-site-shared/src/main/java/com/pyx4j/site/rpc/AppPlace.java
@@ -20,33 +20,72 @@
  */
 package com.pyx4j.site.rpc;
 
+import static com.pyx4j.site.shared.meta.NavigNode.ARGS_GROUP_SEPARATOR;
+import static com.pyx4j.site.shared.meta.NavigNode.ARGS_SEPARATOR;
+import static com.pyx4j.site.shared.meta.NavigNode.NAME_VALUE_SEPARATOR;
+
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gwt.http.client.URL;
 import com.google.gwt.place.shared.Place;
 
 public class AppPlace extends Place {
 
+    private static final Logger log = LoggerFactory.getLogger(AppPlace.class);
+
     private Map<String, String> args;
 
     public AppPlace() {
-        args = Collections.emptyMap();
     }
 
-    public void setArgs(Map<String, String> args) {
-        this.args = Collections.unmodifiableMap(args);
+    public void putArg(String key, String value) {
+        if (args == null) {
+            args = new HashMap<String, String>();
+        }
+        this.args.put(key, value);
+    }
+
+    public void putAllArgs(Map<String, String> map) {
+        if (args == null) {
+            args = new HashMap<String, String>();
+        }
+        this.args.putAll(map);
+    }
+
+    public void parseArgs(String queryString) {
+        if (args == null) {
+            args = new HashMap<String, String>();
+        }
+        args.putAll(parseQueryString(queryString));
     }
 
     public Map<String, String> getArgs() {
-        return args;
+        return Collections.unmodifiableMap(args);
+    }
+
+    public String getArg(String key) {
+        if (args != null) {
+            return args.get(key);
+        } else {
+            return null;
+        }
+    }
+
+    public String getToken() {
+        return AppPlaceInfo.getPlaceId(getClass()) + createQueryString();
     }
 
     @Override
     public boolean equals(Object other) {
         if (getClass() == other.getClass()) {
-            if (args == null && ((AppPlace) other).getArgs() == null) {
+            if (args == null && ((AppPlace) other).args == null) {
                 return true;
-            } else if (args != null && args.equals(((AppPlace) other).getArgs())) {
+            } else if (args != null && args.equals(((AppPlace) other).args)) {
                 return true;
             }
         }
@@ -60,5 +99,52 @@ public class AppPlace extends Place {
             hash = hash * 31 + args.hashCode();
         }
         return hash;
+    }
+
+    protected String createQueryString() {
+        if (args == null) {
+            return "";
+        }
+        StringBuilder queryString = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry<String, String> me : args.entrySet()) {
+            if (first) {
+                queryString.append(ARGS_GROUP_SEPARATOR);
+                first = false;
+            } else {
+                queryString.append(ARGS_SEPARATOR);
+            }
+            queryString.append(me.getKey());
+            queryString.append(NAME_VALUE_SEPARATOR);
+            queryString.append(URL.encodeQueryString(me.getValue()));
+        }
+        return queryString.toString();
+    }
+
+    protected static Map<String, String> parseQueryString(String queryString) {
+        if (queryString.startsWith(ARGS_GROUP_SEPARATOR)) {
+            queryString = queryString.substring(1);
+        }
+        Map<String, String> args = new HashMap<String, String>();
+        if (queryString.length() == 0) {
+            return args;
+        }
+        String[] nameValues = queryString.split(ARGS_SEPARATOR);
+        if (nameValues.length > 0) {
+            for (int i = 0; i < nameValues.length; i++) {
+                String[] nameAndValue = nameValues[i].split(NAME_VALUE_SEPARATOR);
+                if (nameAndValue.length == 2) {
+                    args.put(nameAndValue[0], URL.decodeQueryString(nameAndValue[1]));
+                } else {
+                    log.warn("Can't pars argument {}", nameValues[i]);
+                }
+            }
+        }
+        return args;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + createQueryString();
     }
 }
