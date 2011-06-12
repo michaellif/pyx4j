a6d11326d0a3c21ac0622f5945e77e865c349810
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-06-12 19:17:31 +0000

    VISTA-444 #time 30m Adding new module for Onboarding UI

---
+++ vista-portal/vista-portal-rpc/src/main/java/com/propertyvista/portal/rpc/corp/CorpAccountCreationRequest.java
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-06-12
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.portal.rpc.corp;
+
+import com.pyx4j.commons.Pair;
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.Editor;
+import com.pyx4j.entity.annotations.Editor.EditorType;
+import com.pyx4j.entity.annotations.validator.NotNull;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+
+public interface CorpAccountCreationRequest extends IEntity {
+
+    @Editor(type = EditorType.email)
+    @NotNull
+    IPrimitive<String> email();
+
+    @NotNull
+    @Editor(type = EditorType.password)
+    IPrimitive<String> password();
+
+    @NotNull
+    IPrimitive<String> dnsName();
+
+    /**
+     * Text from image for human verification.
+     */
+    @NotNull
+    @Caption(name = "Enter the code")
+    @Editor(type = Editor.EditorType.captcha)
+    IPrimitive<Pair<String, String>> captcha();
+
+}
---
+++ vista-portal/vista-portal-rpc/src/main/java/com/propertyvista/portal/rpc/corp/services/CorpActivationService.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2011-06-12
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.portal.rpc.corp.services;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.pyx4j.rpc.shared.IService;
+import com.pyx4j.security.rpc.AuthenticationResponse;
+
+import com.propertyvista.portal.rpc.corp.CorpAccountCreationRequest;
+
+public interface CorpActivationService extends IService {
+
+    public void createAccount(AsyncCallback<AuthenticationResponse> callback, CorpAccountCreationRequest request);
+
+}
