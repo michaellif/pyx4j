4d55fca50ddfe4f76fee4f059591cb6f9e2eb729
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-06-15 14:57:33 +0000

    header comment corrected...

--- site/pyx-site-shared/src/main/java/com/pyx4j/site/rpc/services/AbstractCrudService.java
+++ site/pyx-site-shared/src/main/java/com/pyx4j/site/rpc/services/AbstractCrudService.java
@@ -1,12 +1,19 @@
 /*
- * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ * Pyx4j framework
+ * Copyright (C) 2006-2010 pyx4j.com.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
- * you entered into with Property Vista Software Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- * This notice and attribution to Property Vista Software Inc. may not be removed.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
  * Created on Apr 17, 2011
  * @author vlads
  * @version $Id$
