79505167694f10402b2171cced074049cff9c19d
Vadim Spirkov <vadims@birchwoodsoftwaregroup.com>
2011-06-16 03:17:49 +0000

    VISTA-404 Login Form

---
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ui/LoginViewForm.java
@@ -0,0 +1,56 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Jun 15, 2011
+ * @author Dad
+ * @version $Id$
+ */
+package com.propertyvista.portal.client.ui;
+
+import static com.pyx4j.commons.HtmlUtils.h2;
+
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.IsWidget;
+
+import com.pyx4j.entity.client.ui.flex.CEntityForm;
+import com.pyx4j.security.rpc.AuthenticationRequest;
+
+import com.propertyvista.common.client.ui.decorations.DecorationData;
+import com.propertyvista.common.client.ui.decorations.VistaWidgetDecorator;
+
+public class LoginViewForm extends CEntityForm<AuthenticationRequest> {
+
+    private static I18n i18n = I18nFactory.getI18n(LoginViewForm.class);
+
+    private final DecorationData decor;
+
+    public LoginViewForm() {
+        super(AuthenticationRequest.class);
+        decor = new DecorationData(7d, 10d);
+    }
+
+    @Override
+    public IsWidget createContent() {
+        HTML header = new HTML(h2(i18n.tr("Login to Your Account")));
+        header.getElement().getStyle().setMarginBottom(1, Unit.EM);
+
+        FlowPanel main = new FlowPanel();
+        main.add(header);
+        main.add(new VistaWidgetDecorator(inject(proto().email()), decor));
+        main.add(new VistaWidgetDecorator(inject(proto().password()), decor));
+        main.add(new VistaWidgetDecorator(inject(proto().captcha()), decor));
+        return main;
+    }
+
+}
