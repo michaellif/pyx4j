06254e521d0cc678ddbb114e578eb756af35cde2
Vadim Spirkov <vadims@birchwoodsoftwaregroup.com>
2011-07-03 20:13:09 +0000

    VISTA-404 more styling

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ui/decorations/PortalHeaderDecorator.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/client/ui/decorations/PortalHeaderDecorator.java
@@ -29,7 +29,7 @@ public class PortalHeaderDecorator extends VistaHeaderDecorator {
         rightpanel = new HorizontalPanel();
         rightpanel.getElement().getStyle().setFloat(Style.Float.RIGHT);
         rightpanel.getElement().getStyle().setMarginRight(15d, Unit.PX);
-        rightpanel.setVerticalAlignment(HorizontalPanel.ALIGN_MIDDLE);
+        rightpanel.setHorizontalAlignment(HorizontalPanel.ALIGN_RIGHT);
         add(rightpanel);
         setCellVerticalAlignment(rightpanel, HorizontalPanel.ALIGN_MIDDLE);
     }
