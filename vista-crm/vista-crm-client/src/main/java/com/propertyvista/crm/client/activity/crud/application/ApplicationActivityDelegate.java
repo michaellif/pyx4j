f8963b6e53f7e21818e755a06cff300f2fe59be4
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-07-19 01:55:07 +0000

    VISTA-506 - Application CRUD MVP implementation

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/application/ApplicationActivityDelegate.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/application/ApplicationActivityDelegate.java
@@ -22,7 +22,7 @@ import com.pyx4j.site.client.ui.crud.IListerView.Presenter;
 import com.pyx4j.site.rpc.services.AbstractCrudService;
 
 import com.propertyvista.crm.client.ui.crud.application.ApplicationView;
-import com.propertyvista.crm.rpc.services.PotentialTenantCrudService;
+import com.propertyvista.crm.rpc.services.ApplicationTenantCrudService;
 import com.propertyvista.crm.rpc.services.UnitCrudService;
 import com.propertyvista.dto.AptUnitDTO;
 import com.propertyvista.portal.domain.ptapp.PotentialTenantInfo;
@@ -40,7 +40,7 @@ public class ApplicationActivityDelegate implements ApplicationView.Presenter {
                 AptUnitDTO.class);
 
         tenantsLister = new ListerActivityBase<PotentialTenantInfo>(view.getTenantListerView(),
-                (AbstractCrudService<PotentialTenantInfo>) GWT.create(PotentialTenantCrudService.class), PotentialTenantInfo.class);
+                (AbstractCrudService<PotentialTenantInfo>) GWT.create(ApplicationTenantCrudService.class), PotentialTenantInfo.class);
     }
 
     public void populate(Key parentID) {
