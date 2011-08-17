4341e7ae996ea447331f6e6203d57cf653bf81ae
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-08-17 22:19:51 +0000

    VISTA-553 Adding building importer

---
+++ vista-shared/vista-importer/src/main/java/com/propertyvista/interfaces/importer/xml/ImportXMLEntityFactory.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Aug 17, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.interfaces.importer.xml;
+
+import com.pyx4j.essentials.server.xml.XMLEntityFactoryStrict;
+
+import com.propertyvista.domain.contact.Phone;
+import com.propertyvista.interfaces.importer.model.AddressIO;
+import com.propertyvista.interfaces.importer.model.AdvertisingBlurbIO;
+import com.propertyvista.interfaces.importer.model.AmenityIO;
+import com.propertyvista.interfaces.importer.model.AptUnitIO;
+import com.propertyvista.interfaces.importer.model.BuildingIO;
+import com.propertyvista.interfaces.importer.model.ContactIO;
+import com.propertyvista.interfaces.importer.model.FloorplanIO;
+import com.propertyvista.interfaces.importer.model.ImportIO;
+import com.propertyvista.interfaces.importer.model.MarketingIO;
+import com.propertyvista.interfaces.importer.model.MediaIO;
+import com.propertyvista.interfaces.importer.model.ParkingIO;
+
+public class ImportXMLEntityFactory extends XMLEntityFactoryStrict {
+
+    public ImportXMLEntityFactory() {
+        super(new ImportXMLEntityName());
+    }
+
+    @Override
+    protected void bind() {
+        bind(ImportIO.class);
+        bind(AddressIO.class);
+        bind(AdvertisingBlurbIO.class);
+        bind(AmenityIO.class);
+        bind(AptUnitIO.class);
+        bind(BuildingIO.class);
+        bind(ContactIO.class);
+        bind(FloorplanIO.class);
+        bind(MarketingIO.class);
+        bind(MediaIO.class);
+        bind(ParkingIO.class);
+
+        // Taken from domain directly
+        bind(Phone.class);
+    }
+}
---
+++ vista-shared/vista-importer/src/test/java/com/propertyvista/interfaces/importer/BuildingImport.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Aug 17, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.interfaces.importer;
+
+import java.io.File;
+import java.io.FileReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import com.pyx4j.commons.TimeUtils;
+import com.pyx4j.essentials.server.xml.XMLEntityParser;
+
+import com.propertyvista.interfaces.importer.model.BuildingIO;
+import com.propertyvista.interfaces.importer.model.ImportIO;
+import com.propertyvista.interfaces.importer.xml.ImportXMLEntityFactory;
+
+public class BuildingImport {
+
+    private final static Logger log = LoggerFactory.getLogger(BuildingImport.class);
+
+    public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
+
+        String fileName;
+
+        //fileName = "all-buildings-example.xml";
+        fileName = "buildings.xml";
+
+        XMLEntityParser parser = new XMLEntityParser(new ImportXMLEntityFactory());
+
+        ImportIO importIO = parser.parse(ImportIO.class, getDom(new File(fileName)).getDocumentElement());
+
+        for (BuildingIO building : importIO.buildings()) {
+            new BuildingImporter().persist(building);
+        }
+
+        log.info("Total time {} msec", TimeUtils.since(start));
+    }
+
+    private static Document getDom(File xmlFile) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setIgnoringComments(true);
+        factory.setValidating(false);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        builder.setErrorHandler(null);
+        return builder.parse(new InputSource(new FileReader(xmlFile)));
+    }
+}
