210a32f20af83e1a7facc428be66b8b2b675e3c1
Michael Lifshits <michaellif@propertyvista.com>
2011-08-23 13:40:14 +0000

    VISTA-556 Theme and color customization

--- vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/pages/BaseWebPage.java
+++ vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/pages/BaseWebPage.java
@@ -13,18 +13,35 @@
  */
 package com.propertyvista.pmsite.server.pages;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.resources.StyleSheetReference;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.resource.TextTemplateResourceReference;
 
 public abstract class BaseWebPage extends WebPage {
 
     public BaseWebPage() {
         super();
-        add(new BookmarkablePageLink("homeLink", HomeWebPage.class));
 
+        add(new StyleSheetReference("stylesheet", new TextTemplateResourceReference(BaseWebPage.class, "template1.css", "text/css",
+                new LoadableDetachableModel<Map<String, Object>>() {
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Map<String, Object> load() {
+                        final Map<String, Object> vars = new HashMap<String, Object>();
+                        vars.put("color1", "#bababa");
+                        return vars;
+                    }
+                })));
+
+        add(new BookmarkablePageLink("homeLink", HomeWebPage.class));
         add(new BookmarkablePageLink("servicesLink", HomeWebPage.class));
         add(new BookmarkablePageLink("aboutUsLink", HomeWebPage.class));
         add(new BookmarkablePageLink("contactLink", ContactWebPage.class));
     }
-
 }
