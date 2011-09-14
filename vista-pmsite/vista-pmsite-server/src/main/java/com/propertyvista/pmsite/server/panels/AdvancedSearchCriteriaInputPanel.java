c425c8f81ffede09409ee66b615c1659f47aa424
Stan Porter <stanp@propertyvista.com>
2011-09-14 04:22:39 +0000

    VISTA-556 AdvSearch, AptList pages styled; jQuery search type switch added; JS city list by province added

--- vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/panels/AdvancedSearchCriteriaInputPanel.java
+++ vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/panels/AdvancedSearchCriteriaInputPanel.java
@@ -18,8 +18,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.wicket.AttributeModifier;
+import js.JSResources;
+
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBoxMultipleChoice;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Radio;
@@ -32,7 +35,6 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.validation.validator.MinimumValidator;
 
 import com.propertyvista.pmsite.server.PMSiteContentManager;
-import com.propertyvista.pmsite.server.model.AttributeClassModifier;
 import com.propertyvista.pmsite.server.model.SearchCriteriaModel;
 
 public class AdvancedSearchCriteriaInputPanel extends Panel {
@@ -41,98 +43,28 @@ public class AdvancedSearchCriteriaInputPanel extends Panel {
     public AdvancedSearchCriteriaInputPanel(String id, CompoundPropertyModel<SearchCriteriaModel> model) {
         super(id, model);
 
+        getApplication().getSharedResources().putClassAlias(JSResources.class, "js");
+//        add(new JavaScriptReference("jquery", JSResources.class, "jquery-1.6.3.min.js"));
+
         // add Error Message panel
         add(new FeedbackPanel("form_messages"));
 
         // add searchType radio selectors
-        RadioGroup<SearchCriteriaModel.SearchType> searchTypeRadio = new RadioGroup<SearchCriteriaModel.SearchType>("searchType") {
-            // TODO replace this BS (server round trip) with JS (onclick handler)
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            protected boolean wantOnSelectionChangedNotifications() {
-                return true;
-            }
-
-            @Override
-            protected void onSelectionChanged(Object newType) {
-                // get components
-                @SuppressWarnings("unchecked")
-                DropDownChoice<String> city = (DropDownChoice<String>) getParent().get("city");
-                @SuppressWarnings("unchecked")
-                DropDownChoice<String> prov = (DropDownChoice<String>) getParent().get("province");
-                @SuppressWarnings("unchecked")
-                TextField<String> lctn = (TextField<String>) getParent().get("location");
-                @SuppressWarnings("unchecked")
-                TextField<Integer> dist = (TextField<Integer>) getParent().get("distance");
-
-                // TODO replace this BS with JS
-                if (newType == SearchCriteriaModel.SearchType.City) {
-                    if (lctn != null) {
-                        lctn.add(new AttributeClassModifier(null, "input_disabled"));
-                        lctn.add(new SimpleAttributeModifier("disabled", "true"));
-                    }
-                    if (dist != null) {
-                        dist.add(new AttributeClassModifier(null, "input_disabled"));
-                        dist.add(new SimpleAttributeModifier("disabled", "true"));
-                    }
-                    if (city != null) {
-                        city.add(new AttributeClassModifier("input_disabled", null));
-                        city.add(new AttributeModifier("disabled", new Model<String>(AttributeModifier.VALUELESS_ATTRIBUTE_REMOVE)));
-                    }
-                    if (prov != null) {
-                        prov.add(new AttributeClassModifier("input_disabled", null));
-                        prov.add(new AttributeModifier("disabled", new Model<String>(AttributeModifier.VALUELESS_ATTRIBUTE_REMOVE)));
-                    }
-                } else {
-                    if (lctn != null) {
-                        lctn.add(new AttributeClassModifier("input_disabled", null));
-                        lctn.add(new AttributeModifier("disabled", new Model<String>(AttributeModifier.VALUELESS_ATTRIBUTE_REMOVE)));
-                    }
-                    if (dist != null) {
-                        dist.add(new AttributeClassModifier("input_disabled", null));
-                        dist.add(new AttributeModifier("disabled", new Model<String>(AttributeModifier.VALUELESS_ATTRIBUTE_REMOVE)));
-                    }
-                    if (city != null) {
-                        city.add(new AttributeClassModifier(null, "input_disabled"));
-                        city.add(new SimpleAttributeModifier("disabled", "true"));
-                    }
-                    if (prov != null) {
-                        prov.add(new AttributeClassModifier(null, "input_disabled"));
-                        prov.add(new SimpleAttributeModifier("disabled", "true"));
-                    }
-                }
-            }
-        };
-        searchTypeRadio.add(new Radio<SearchCriteriaModel.SearchType>("searchByType", new Model<SearchCriteriaModel.SearchType>(
+        RadioGroup<SearchCriteriaModel.SearchType> searchTypeRadio = new RadioGroup<SearchCriteriaModel.SearchType>("searchType");
+        searchTypeRadio.add(new Radio<SearchCriteriaModel.SearchType>("searchByCity", new Model<SearchCriteriaModel.SearchType>(
                 SearchCriteriaModel.SearchType.City)));
         searchTypeRadio.add(new Radio<SearchCriteriaModel.SearchType>("searchByProx", new Model<SearchCriteriaModel.SearchType>(
                 SearchCriteriaModel.SearchType.Proximity)));
         add(searchTypeRadio.setRequired(true));
+
         // add Province drop-down
         final Map<String, List<String>> provCityMap = PMSiteContentManager.getProvinceCityMap();
         List<String> provinces = new ArrayList<String>(provCityMap.keySet());
-        DropDownChoice<String> provChoice = new DropDownChoice<String>("province", provinces) {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            protected boolean wantOnSelectionChangedNotifications() {
-                return true;
-            }
-
-            @Override
-            protected void onSelectionChanged(String newProv) {
-                // get city component
-                @SuppressWarnings("unchecked")
-                DropDownChoice<String> city = (DropDownChoice<String>) getParent().get("city");
-                if (city != null)
-                    city.setChoices(provCityMap.get(newProv));
-            }
-        };
-        if (model.getObject().getSearchType() == SearchCriteriaModel.SearchType.Proximity) {
-            provChoice.add(new SimpleAttributeModifier("disabled", "true"));
-        }
+        DropDownChoice<String> provChoice = new DropDownChoice<String>("province", provinces);
+        provChoice.add(new SimpleAttributeModifier("onChange",
+                "setSelectionOptions('citySelect', provCity[this.options[this.selectedIndex].text], 'Choose One')"));
         add(provChoice);
+
         // add City drop-down
         List<String> cities;
         String selProv = model.getObject().getProvince();
@@ -143,24 +75,27 @@ public class AdvancedSearchCriteriaInputPanel extends Panel {
         }
         DropDownChoice<String> cityChoice = new DropDownChoice<String>("city", cities);
         add(cityChoice);
+
+        // add JS city list
+        String jsCityList = "\nvar provCity = {};\n";
+        for (String _prov : provCityMap.keySet()) {
+            String _list = "";
+            for (String _city : provCityMap.get(_prov)) {
+                _list += ("".equals(_list) ? "" : ",") + "'" + StringEscapeUtils.escapeJavaScript(_city) + "'";
+            }
+            jsCityList += "provCity['" + StringEscapeUtils.escapeJavaScript(_prov) + "'] = [" + _list + "];\n";
+        }
+        add(new Label("jsCityList", jsCityList).setEscapeModelStrings(false));
+
         // add location input
         TextField<String> lctnInput = new TextField<String>("location");
         add(lctnInput);
+
         // add distance input
         TextField<Integer> distInput = new TextField<Integer>("distance");
         distInput.add(new MinimumValidator<Integer>(1));
         add(distInput);
-        if (model.getObject().getSearchType() == SearchCriteriaModel.SearchType.City) {
-            lctnInput.add(new SimpleAttributeModifier("disabled", "true"));
-            lctnInput.add(new AttributeClassModifier(null, "input_disabled"));
-            distInput.add(new SimpleAttributeModifier("disabled", "true"));
-            distInput.add(new AttributeClassModifier(null, "input_disabled"));
-        } else if (model.getObject().getSearchType() == SearchCriteriaModel.SearchType.Proximity) {
-            cityChoice.add(new SimpleAttributeModifier("disabled", "true"));
-            cityChoice.add(new AttributeClassModifier(null, "input_disabled"));
-            provChoice.add(new SimpleAttributeModifier("disabled", "true"));
-            provChoice.add(new AttributeClassModifier(null, "input_disabled"));
-        }
+
         // add common fields
         // bedrooms
         add(new DropDownChoice<SearchCriteriaModel.BedroomChoice>("bedsMin", Arrays.asList(SearchCriteriaModel.BedroomChoice.values())) {
