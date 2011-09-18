4a2149f3cbedb1b3bac0890b512a91bcd8e2f40c
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-09-18 23:19:10 +0000

    VISTA-582 migration for FloorplanCounters

--- src/upgrade/2011-09-18/post-update.sql
+++ src/upgrade/2011-09-18/post-update.sql
@@ -6,5 +6,9 @@ UPDATE Media SET visibility = 'global';
 
 UPDATE PropertyPhone SET visibility = 'global';
 
+DELETE FROM FloorplanCounters;
+
+INSERT INTO FloorplanCounters (ns, id, _unitCount) SELECT ns, floorplan, COUNT(*) FROM AptUnit WHERE floorplan IS NOT NULL GROUP BY ns, floorplan;
+
 COMMIT;
 
