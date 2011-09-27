af78945d4ee05b32a2f53c70c7b358b13d9f0b50
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-09-27 18:35:15 +0000

    Support for polymorphic entities

---
+++ entity/pyx-entity-shared/src/main/java/com/pyx4j/entity/annotations/Inheritance.java
@@ -0,0 +1,37 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2011 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Sep 27, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.entity.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Use entity class hierarchy to find possible instances, discriminator column would be added to owner table automatically when this class is used as member.
+ * Just like javax.persistence.Inheritance
+ * 
+ */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inheritance {
+
+}
