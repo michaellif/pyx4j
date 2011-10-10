386f86964956c0383f30f495dbba84decf263631
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-10-10 19:35:41 +0000

    VISTA-588 Spelling corrections

--- vista-portal/vista-portal-domain/src/main/java/com/propertyvista/portal/domain/dto/ResidentDTO.java
+++ vista-portal/vista-portal-domain/src/main/java/com/propertyvista/portal/domain/dto/ResidentDTO.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -30,7 +30,7 @@ public interface ResidentDTO extends Person {
     PriorAddress currentAddress();
 
     @Length(3)
-    @Caption(name = "Vehicls")
+    @Caption(name = "Vehicles")
     IList<Vehicle> vehicles();
 
     @Length(3)
