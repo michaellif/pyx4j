a330ea1455d71f716ce4eb7cae161f1a4b5aa815
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-10-20 06:40:01 +0000

    VISTA-588 rebuild translations

---
+++ bg-prod.cmd
@@ -0,0 +1,17 @@
+@echo off
+rem @version $Revision$ ($Author$)  $Date$
+rem helper to run maven2
+rem
+
+for /f "tokens=*" %%I in ('CD') do @set CurDir=%%~nI
+title *%CurDir% - mvn with pyx and GWT Compile in production mode
+call mvn install -P pyx,gwtc -P !draft -P !developer-env %*
+@if errorlevel 1 goto errormark
+title %CurDir%
+
+@goto endmark
+:errormark
+	echo   
+    pause
+:endmark
+@ENDLOCAL
