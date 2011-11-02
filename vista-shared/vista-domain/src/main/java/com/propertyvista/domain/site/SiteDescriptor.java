8ec82033e8498f20765967caaddb6d6ec043f861
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-02 00:21:24 +0000

    VISTA-75 fixed column name in HSQL

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/site/SiteDescriptor.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/site/SiteDescriptor.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -14,6 +14,7 @@
 package com.propertyvista.domain.site;
 
 import com.pyx4j.entity.annotations.Owned;
+import com.pyx4j.entity.annotations.MemberColumn;
 import com.pyx4j.entity.annotations.ToString;
 import com.pyx4j.entity.annotations.validator.NotNull;
 import com.pyx4j.entity.shared.IEntity;
@@ -60,5 +61,6 @@ public interface SiteDescriptor extends IEntity {
 
     @Owned
     @I18n(strategy = I18n.I18nStrategy.IgnoreThis)
+    @MemberColumn(name = "updateFlag")
     SiteDescriptorChanges _updateFlag();
 }
