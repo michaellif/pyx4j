628a03ec018f3baf68b605f6a68578e185df4bbe
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-05 16:09:55 +0000

    name fixed

---
+++ entity/pyx-entity-server/src/main/java/com/pyx4j/entity/server/AbstractCrudServiceImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2011 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Nov 5, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.entity.server;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.pyx4j.commons.Key;
+import com.pyx4j.entity.rpc.AbstractCrudService;
+import com.pyx4j.entity.shared.IEntity;
+
+public abstract class AbstractCrudServiceImpl<E extends IEntity> extends AbstractListServiceImpl<E> implements AbstractCrudService<E> {
+
+    protected AbstractCrudServiceImpl(Class<E> dboClass) {
+        super(dboClass);
+    }
+
+    protected void enhanceRetrieve(E entity) {
+    }
+
+    protected void persistDBO(E dbo) {
+        EntityServicesImpl.secureSave(dbo);
+    }
+
+    @Override
+    public void create(AsyncCallback<E> callback, E entity) {
+        persistDBO(entity);
+        callback.onSuccess(entity);
+    }
+
+    @Override
+    public void retrieve(AsyncCallback<E> callback, Key entityId) {
+        E entity = EntityServicesImpl.secureRetrieve(dboClass, entityId);
+        enhanceRetrieve(entity);
+        callback.onSuccess(entity);
+    }
+
+    protected void enhanceSave(E entity) {
+    }
+
+    @Override
+    public void save(AsyncCallback<E> callback, E entity) {
+        persistDBO(entity);
+        enhanceSave(entity);
+        callback.onSuccess(entity);
+    }
+
+}
