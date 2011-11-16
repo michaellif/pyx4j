41503c2346f47413efd5cf1ad6893bcc4027b284
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-16 00:23:04 +0000

    Fixed assert condition

--- entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/mapping/ValueAdapterEntityVirtual.java
+++ entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/mapping/ValueAdapterEntityVirtual.java
@@ -109,7 +109,8 @@ public class ValueAdapterEntityVirtual implements ValueAdapter {
             stmt.setNull(parameterIndex, sqlTypeKey);
             stmt.setNull(parameterIndex + 1, sqlTypeDiscriminator);
         } else {
-            assert impClasses.containsKey(childEntity.getInstanceValueClass()) : "Unexpected class " + childEntity.getInstanceValueClass() + " " + value;
+            assert impClasses.containsValue(childEntity.getInstanceValueClass()) : "Unexpected class " + childEntity.getInstanceValueClass() + "\n"
+                    + impClasses.values() + "\n" + value;
             stmt.setLong(parameterIndex, primaryKey.asLong());
             DiscriminatorValue discriminator = childEntity.getInstanceValueClass().getAnnotation(DiscriminatorValue.class);
             stmt.setString(parameterIndex + 1, discriminator.value());
