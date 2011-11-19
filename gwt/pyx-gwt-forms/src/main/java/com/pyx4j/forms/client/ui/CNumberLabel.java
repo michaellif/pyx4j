e65f3fc9aa5484c8eb12a3914463a2e9a63be7a1
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-19 07:42:20 +0000

    Extending CNumberLabel format options

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CNumberLabel.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CNumberLabel.java
@@ -8,24 +8,60 @@
  */
 package com.pyx4j.forms.client.ui;
 
+import java.text.ParseException;
+
+import com.google.gwt.i18n.client.NumberFormat;
+
 import com.pyx4j.commons.SimpleMessageFormat;
 
 public class CNumberLabel extends CAbstractLabel<Number> {
 
-    public void setNumberFormat(final String format) {
+    public void setNumberFormat(String format, boolean useMessageFormat) {
         if (format != null) {
-            setFormat(new IFormat<Number>() {
-
-                @Override
-                public String format(Number value) {
-                    return SimpleMessageFormat.format(format, value);
-                }
-
-                @Override
-                public Number parse(String string) {
-                    return null;
-                }
-            });
+            if (useMessageFormat) {
+                setFormat(new ViewNumberSimpleMessageFormat<Number>(format));
+            } else {
+                setFormat(new ViewNumberFormat<Number>(format));
+            }
         }
     }
+
+    public static class ViewNumberFormat<T extends Number> implements IFormat<T> {
+
+        private final NumberFormat formatter;
+
+        ViewNumberFormat(String format) {
+            formatter = NumberFormat.getFormat(format);
+        }
+
+        @Override
+        public String format(T value) {
+            return formatter.format(value);
+        }
+
+        @Override
+        public T parse(String string) throws ParseException {
+            return null;
+        }
+    }
+
+    public static class ViewNumberSimpleMessageFormat<T extends Number> implements IFormat<T> {
+
+        private final String format;
+
+        ViewNumberSimpleMessageFormat(String format) {
+            this.format = format;
+        }
+
+        @Override
+        public String format(T value) {
+            return SimpleMessageFormat.format(format, value);
+        }
+
+        @Override
+        public T parse(String string) throws ParseException {
+            return null;
+        }
+    }
+
 }
