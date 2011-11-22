c334653d6b9310883ffe9c837124db39c84fc9b0
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-22 16:58:37 +0000

    VISTA-741 Starlight visibility for building

--- bg-prod.cmd
+++ bg-prod.cmd
@@ -5,7 +5,7 @@ rem
 
 for /f "tokens=*" %%I in ('CD') do @set CurDir=%%~nI
 title *%CurDir% - mvn with pyx and GWT Compile in production mode
-call mvn install -P pyx,gwtc -P !draft -P !developer-env %*
+call mvn install -P full,pyx,gwtc -P !draft -P !developer-env %*
 @if errorlevel 1 goto errormark
 title %CurDir%
 
