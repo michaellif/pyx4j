306f2dedee7074c6367daa29a5603c188a6e9cbf
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2011-11-23 22:36:55 +0000

    VISTA-716 summary charges section: fixed charges sub section so that they become invisible if contain no data.

--- vista-portal/vista-portal-ptapp/src/main/java/com/propertyvista/portal/ptapp/client/ui/steps/charges/ChargesViewForm.java
+++ vista-portal/vista-portal-ptapp/src/main/java/com/propertyvista/portal/ptapp/client/ui/steps/charges/ChargesViewForm.java
@@ -25,6 +25,7 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 
+import com.pyx4j.entity.client.CEntityEditor;
 import com.pyx4j.entity.client.ui.IEditableComponentFactory;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 
@@ -34,6 +35,7 @@ import com.propertyvista.common.client.ui.components.editors.CMoneyLabel;
 import com.propertyvista.common.client.ui.components.folders.ChargeLineFolder;
 import com.propertyvista.common.client.ui.decorations.DecorationUtils;
 import com.propertyvista.common.client.ui.decorations.VistaLineSeparator;
+import com.propertyvista.domain.charges.ChargeLineList;
 import com.propertyvista.domain.financial.Money;
 import com.propertyvista.portal.domain.ptapp.Charges;
 import com.propertyvista.portal.rpc.ptapp.ChargesSharedCalculation;
@@ -69,26 +71,16 @@ public class ChargesViewForm extends CEntityDecoratableEditor<Charges> {
         FormFlexPanel main = new FormFlexPanel();
 
         int row = -1;
-        main.setH1(++row, 0, 1, proto().monthlyCharges().getMeta().getCaption());
-        main.setWidget(++row, 0, inject(proto().monthlyCharges().charges(), new ChargeLineFolder(isEditable())));
-        main.setWidget(++row, 0, createTotal(proto().monthlyCharges().total()));
-
-        main.setH1(++row, 0, 1, proto().oneTimeCharges().getMeta().getCaption());
-        main.setWidget(++row, 0, inject(proto().oneTimeCharges().charges(), new ChargeLineFolder(isEditable())));
-        main.setWidget(++row, 0, createTotal(proto().oneTimeCharges().total()));
-
-        main.setH1(++row, 0, 1, proto().proratedCharges().getMeta().getCaption());
-        main.setWidget(++row, 0, inject(proto().proratedCharges().charges(), new ChargeLineFolder(isEditable())));
-        main.setWidget(++row, 0, createTotal(proto().proratedCharges().total()));
-
-        main.setH1(++row, 0, 1, proto().applicationCharges().getMeta().getCaption());
-        main.setWidget(++row, 0, inject(proto().applicationCharges().charges(), new ChargeLineFolder(isEditable())));
-        main.setWidget(++row, 0, createTotal(proto().applicationCharges().total()));
+        main.setWidget(++row, 0, inject(proto().monthlyCharges(), new ChargesSubCategoryViewForm(factory, proto().monthlyCharges().getMeta().getCaption())));
+        main.setWidget(++row, 0, inject(proto().oneTimeCharges(), new ChargesSubCategoryViewForm(factory, proto().oneTimeCharges().getMeta().getCaption())));
+        main.setWidget(++row, 0, inject(proto().proratedCharges(), new ChargesSubCategoryViewForm(factory, proto().proratedCharges().getMeta().getCaption())));
+        main.setWidget(++row, 0,
+                inject(proto().applicationCharges(), new ChargesSubCategoryViewForm(factory, proto().applicationCharges().getMeta().getCaption())));
 
         int row1 = -1;
         splitCharges.setH1(++row1, 0, 1, proto().paymentSplitCharges().getMeta().getCaption());
         splitCharges.setWidget(++row1, 0, inject(proto().paymentSplitCharges().charges(), new ChargeSplitListFolder(isEditable())));
-        splitCharges.setWidget(++row1, 0, createTotal(proto().paymentSplitCharges().total()));
+        splitCharges.setWidget(++row1, 0, createTotal(this, proto().paymentSplitCharges().total()));
         main.setWidget(++row, 0, splitCharges);
 
         return main;
@@ -100,20 +92,51 @@ public class ChargesViewForm extends CEntityDecoratableEditor<Charges> {
         splitCharges.setVisible(value.paymentSplitCharges().charges().size() > 1);
     }
 
-    private Widget createTotal(Money member) {
+    private static Widget createTotal(CEntityEditor<?> form, Money member) {
         FlowPanel totalRow = new FlowPanel();
 
         Widget sp = new VistaLineSeparator(38, Unit.EM);
         sp.getElement().getStyle().setPadding(0, Unit.EM);
         sp.getElement().getStyle().setProperty("border", "1px dotted black");
         totalRow.add(sp);
-
         HTML total = new HTML("<b>" + member.getMeta().getCaption() + "</b>");
         total.getElement().getStyle().setPaddingLeft(0.7, Unit.EM);
         totalRow.add(DecorationUtils.inline(total, "30.5em", null));
-        totalRow.add(DecorationUtils.inline(inject(member, new CMoneyLabel()), "7em"));
-        get(member).asWidget().getElement().getStyle().setFontWeight(FontWeight.BOLD);
-
+        totalRow.add(DecorationUtils.inline(form.inject(member, new CMoneyLabel()), "7em"));
+        form.get(member).asWidget().getElement().getStyle().setFontWeight(FontWeight.BOLD);
         return totalRow;
+    }
+
+    /**
+     * Self charges view form for sub category that renders itself to invisible sate when no charges are available.
+     * 
+     * @author ArtyomB
+     * 
+     */
+    private static class ChargesSubCategoryViewForm extends CEntityDecoratableEditor<ChargeLineList> {
+        private final String caption;
+
+        public ChargesSubCategoryViewForm(IEditableComponentFactory factory, String caption) {
+            super(ChargeLineList.class, factory);
+            setEditable(factory instanceof VistaEditorsComponentFactory);
+            this.caption = caption;
+        }
+
+        @Override
+        public IsWidget createContent() {
+            FormFlexPanel content = new FormFlexPanel();
+            int row = -1;
+            content.setH1(++row, 0, 1, caption);
+            content.setWidget(++row, 0, inject(proto().charges(), new ChargeLineFolder(isEditable())));
+            content.setWidget(++row, 0, createTotal(this, proto().total()));
+
+            return content;
+        }
+
+        @Override
+        public void populate(ChargeLineList entity) {
+            super.populate(entity);
+            this.asWidget().setVisible(!entity.charges().isEmpty());
+        }
     }
 }
