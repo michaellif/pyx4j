7c3989b922c49efe3d7b08a10f3a566b318c4f58
Slava Skarzhevskyy <vlads@propertyvista.com>
2011-11-24 07:24:36 +0000

    VISTA-747 fixed building sorts

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/BuildingLister.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/BuildingLister.java
@@ -13,11 +13,7 @@
  */
 package com.propertyvista.crm.client.ui.crud.building;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.pyx4j.entity.client.ui.datatable.ColumnDescriptor;
-import com.pyx4j.entity.client.ui.datatable.ColumnDescriptorFactory;
+import com.pyx4j.entity.client.ui.datatable.MemberColumnDescriptor;
 import com.pyx4j.site.client.ui.crud.lister.ListerBase;
 
 import com.propertyvista.crm.rpc.CrmSiteMap;
@@ -28,68 +24,77 @@ public class BuildingLister extends ListerBase<BuildingDTO> {
     public BuildingLister() {
         super(BuildingDTO.class, CrmSiteMap.Properties.Building.class, false, true);
 
-//        // add custom actions (Buttons) here:
-//        getListPanel().getDataTable().setHasCheckboxColumn(true);
-//
-//        addActionButton(new Button("Action1", new ClickHandler() {
-//            @Override
-//            public void onClick(ClickEvent event) {
-//                // TODO Auto-generated method stub
-//            }
-//        }));
-//
-//        addActionButton(new Button("Action2", new ClickHandler() {
-//            @Override
-//            public void onClick(ClickEvent event) {
-//                // TODO Auto-generated method stub
-//            }
-//        }));
+        setColumnDescriptors(
 
-        List<ColumnDescriptor<BuildingDTO>> columnDescriptors = new ArrayList<ColumnDescriptor<BuildingDTO>>();
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().complex(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().propertyCode(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().propertyManager(), true));
+        new MemberColumnDescriptor.Builder(proto().complex()).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().name(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().type(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().shape(), false));
+        new MemberColumnDescriptor.Builder(proto().complex(), false).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().streetNumber(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().streetNumberSuffix(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().streetName(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().streetType(), false));
+        new MemberColumnDescriptor.Builder(proto().propertyCode(), true).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().streetDirection(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().city(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().country(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().province(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().address().country(), false));
+        new MemberColumnDescriptor.Builder(proto().propertyManager(), true).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().totalStoreys(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().residentialStoreys(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().structureType(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().structureBuildYear(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().constructionType(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().foundationType(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().floorType(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().landArea(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().waterSupply(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().centralAir(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().info().centralHeat(), false));
+        new MemberColumnDescriptor.Builder(proto().info().name(), true).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().contacts().website(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createTitledColumnDescriptor(proto(), proto().contacts().email().address(), i18n.tr("Email"), false));
+        new MemberColumnDescriptor.Builder(proto().info().type(), true).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().dateAcquired(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().purchasePrice(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().marketPrice(), false));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().lastAppraisalDate(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().lastAppraisalValue(), true));
-        columnDescriptors.add(ColumnDescriptorFactory.createColumnDescriptor(proto(), proto().financial().currency(), false));
+        new MemberColumnDescriptor.Builder(proto().info().shape(), false).build(),
 
-        columnDescriptors.add(ColumnDescriptorFactory.createTitledColumnDescriptor(proto(), proto().marketing().name(), i18n.tr("Marketing Name"), false));
-        setColumnDescriptors(columnDescriptors);
+        new MemberColumnDescriptor.Builder(proto().info().address().streetNumber(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().streetNumberSuffix(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().streetName(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().streetType(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().streetDirection(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().city(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().province(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().address().country(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().totalStoreys(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().residentialStoreys(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().structureType(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().structureBuildYear(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().constructionType(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().foundationType(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().floorType(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().landArea(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().waterSupply(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().centralAir(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().info().centralHeat(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().contacts().website(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().contacts().email().address(), false).title(i18n.tr("Email")).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().dateAcquired(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().purchasePrice(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().marketPrice(), false).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().lastAppraisalDate(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().lastAppraisalValue(), true).build(),
+
+        new MemberColumnDescriptor.Builder(proto().financial().currency().name(), false).title(proto().financial().currency()).build(),
+
+        new MemberColumnDescriptor.Builder(proto().marketing().name(), false).title(i18n.tr("Marketing Name")).build());
 
     }
-
 }
