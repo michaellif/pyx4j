7f0212de8f890921ed64382f23ab886ff046c970
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2011-11-24 16:36:46 +0000

    VISTA-61 - various UI correction.

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/tenant/TenantInLease.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/tenant/TenantInLease.java
@@ -103,7 +103,7 @@ public interface TenantInLease extends IBoundToApplication {
     @MemberColumn(name = "status")
     IPrimitive<Role> role();
 
-    @Caption(name = "Take Ownership", description = "By checking the box TAKE OWNERSHIP you are agreeing that the MAIN APPLICANT will have access to your personal information and that you are present during the Application Process. The MAIN APPLICANT account will be the USERNAME for future communications with the Property Manager. This Box is only recommended for Family Members. Should you wish to have a separate and secure Login Access please leave the check box blank and an e-mail alert with individual username and passwords will be automatically be sent to all Tenants and Guarantors. ")
+    @Caption(name = "Take Ownership", description = "By checking the box TAKE OWNERSHIP you are agreeing that the MAIN APPLICANT will have access to your personal information and that you are present during the Application Process. The MAIN APPLICANT account will be the USERNAME for future communications with the Property Manager. This Box is only recommended for Family Members. Should you wish to have a separate and secure Login Access please leave the check box blank and an e-mail alert with individual username and passwords will be automatically be sent to all Tenants and Guarantors")
     IPrimitive<Boolean> takeOwnership();
 
     /**
