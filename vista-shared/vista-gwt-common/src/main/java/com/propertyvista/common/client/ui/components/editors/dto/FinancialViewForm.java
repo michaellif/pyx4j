64265488f029b12dd64a0a5a1a60e936e901c6b4
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2011-12-08 21:53:31 +0000

    VISTA-713 added question mark and text

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/dto/FinancialViewForm.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/dto/FinancialViewForm.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -14,10 +14,12 @@
 package com.propertyvista.common.client.ui.components.editors.dto;
 
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 
 import com.pyx4j.entity.client.CEntityEditor;
 import com.pyx4j.entity.client.ui.IEditableComponentFactory;
+import com.pyx4j.forms.client.ImageFactory;
 import com.pyx4j.forms.client.ui.CComponent;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.forms.client.validators.EditableValueValidator;
@@ -51,11 +53,14 @@ public class FinancialViewForm extends CEntityEditor<TenantFinancialDTO> {
         main.setWidget(++row, 0, inject(proto().incomes(), new PersonalIncomeFolder(isEditable())));
         main.setWidget(++row, 0, new HTML());
 
+        Image info = new Image(ImageFactory.getImages().formTooltipInfo());
+        info.setTitle("A Guarantor Is An Individual That Will Guarantee The Term Of The Lease. The Guarantor Cannot Be An Occupant Of the Suite And Is There To Assist The Applicant In The Approval Process. The Guarantor(s) Will Receive A Seperate Email With Instructions To Complete The Applications. Reminder: Only Completed Applications Will Be Processed.");
+
         main.setH1(++row, 0, 1, proto().assets().getMeta().getCaption());
         main.setWidget(++row, 0, inject(proto().assets(), new PersonalAssetFolder(isEditable())));
         main.setWidget(++row, 0, new HTML());
 
-        main.setH1(++row, 0, 1, proto().guarantors().getMeta().getCaption());
+        main.setH1(++row, 0, 1, proto().guarantors().getMeta().getCaption(), info);
         main.setWidget(++row, 0, inject(proto().guarantors(), new TenantGuarantorFolder(isEditable())));
         main.setWidget(++row, 0, new HTML());
 
