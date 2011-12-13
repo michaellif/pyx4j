e8adaa6f146d92e8d7f9fbec52e08eae1632f141
Michael Lifshits <michaellif@propertyvista.com>
2011-12-13 12:51:10 +0000

    VISTA-250 use discard instead of populate(null)

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/login/LoginViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/login/LoginViewImpl.java
@@ -74,7 +74,7 @@ public class LoginViewImpl extends FlowPanel implements LoginView {
 
         form.initContent();
         form.get(form.proto().captcha()).setVisible(false);
-        form.populate(null);
+        form.discard();
 
         add(form);
 
