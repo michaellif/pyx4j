a9ec42db494439cb1fb176bae6623bf36bd58458
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-01-06 17:01:43 +0000

    VISTA-910 node selection mechanism was changed (uses combo boxes instead of dialog)

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/common/PolicyDTOTabPanelBasedEditorForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/common/PolicyDTOTabPanelBasedEditorForm.java
@@ -14,53 +14,38 @@
 package com.propertyvista.crm.client.ui.crud.policies.common;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.event.shared.SimpleEventBus;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.DeckLayoutPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 
 import com.pyx4j.entity.client.CEntityEditor;
+import com.pyx4j.entity.client.ui.CEntityComboBox;
+import com.pyx4j.entity.client.ui.CEntityLabel;
 import com.pyx4j.entity.client.ui.IEditableComponentFactory;
-import com.pyx4j.entity.rpc.AbstractCrudService;
 import com.pyx4j.entity.shared.EntityFactory;
-import com.pyx4j.entity.shared.IEntity;
 import com.pyx4j.forms.client.events.PropertyChangeEvent;
 import com.pyx4j.forms.client.events.PropertyChangeHandler;
-import com.pyx4j.forms.client.ui.CButton;
 import com.pyx4j.forms.client.ui.CComboBox;
-import com.pyx4j.forms.client.ui.CLabel;
+import com.pyx4j.forms.client.ui.CComponent;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
-import com.pyx4j.site.client.activity.crud.ListerActivityBase;
-import com.pyx4j.site.client.ui.crud.lister.IListerView;
-import com.pyx4j.site.client.ui.crud.lister.ListerBase.ItemSelectionHandler;
-import com.pyx4j.site.client.ui.crud.lister.ListerInternalViewImplBase;
-import com.pyx4j.widgets.client.dialog.OkCancelDialog;
 
 import com.propertyvista.common.client.ui.components.VistaTabLayoutPanel;
 import com.propertyvista.crm.client.themes.VistaCrmTheme;
 import com.propertyvista.crm.client.ui.crud.CrmEntityForm;
-import com.propertyvista.crm.client.ui.crud.building.SelectedBuildingLister;
-import com.propertyvista.crm.client.ui.crud.unit.SelectedUnitLister;
 import com.propertyvista.crm.client.ui.decorations.CrmScrollPanel;
-import com.propertyvista.crm.rpc.CrmSiteMap;
-import com.propertyvista.crm.rpc.services.SelectBuildingCrudService;
-import com.propertyvista.crm.rpc.services.SelectUnitCrudService;
 import com.propertyvista.domain.policy.DefaultPoliciesNode;
 import com.propertyvista.domain.policy.OrganizationPoliciesNode;
 import com.propertyvista.domain.policy.PolicyNode;
 import com.propertyvista.domain.policy.dto.PolicyDTOBase;
 import com.propertyvista.domain.property.asset.Complex;
 import com.propertyvista.domain.property.asset.building.Building;
-import com.propertyvista.domain.property.asset.unit.AptUnit;
 import com.propertyvista.domain.ref.Country;
 import com.propertyvista.domain.ref.Province;
 
@@ -120,12 +105,13 @@ public abstract class PolicyDTOTabPanelBasedEditorForm<POLICY_DTO extends Policy
         selectPolicyScopeBox.setEditable(isEditable());
         selectPolicyScopeBox.setOptions(NODE_TYPE_OPTIONS);
         selectPolicyScopeBox.setMandatory(true);
+        // add value change handler that resets the node when node type is changed 
         selectPolicyScopeBox.addValueChangeHandler(new ValueChangeHandler<NodeTypeWrapper<?>>() {
             @Override
             public void onValueChange(ValueChangeEvent<NodeTypeWrapper<?>> event) {
                 if (event.getValue() != null) {
-                    get(proto().node()).setVisible(true);
-
+                    // disable node selector if it's a root node (Organization/PMC)
+                    get(proto().node()).setVisible(!event.getValue().getType().equals(OrganizationPoliciesNode.class));
                     if (!event.getValue().getType().equals(getValue().node().getInstanceValueClass())) {
                         get(proto().node()).populate(EntityFactory.create(event.getValue().getType()));
                     }
@@ -161,47 +147,64 @@ public abstract class PolicyDTOTabPanelBasedEditorForm<POLICY_DTO extends Policy
     }
 
     /**
-     * A component that just shows the string view of a node and in edit mode and allows to choose the node via dialog that shows a list of nodes.
+     * A component that to choose the polymorphic PolicyNode
      * 
      * @author ArtyomB
      */
-    private class PolicyNodeEditor extends CEntityEditor<PolicyNode> {
+    private static class PolicyNodeEditor extends CEntityEditor<PolicyNode> {
+
+        private static final List<Class<? extends PolicyNode>> ASSIGNABLE_NODE_TYPES = Arrays.asList(Building.class, Complex.class, Province.class,
+                Country.class);
 
         public PolicyNodeEditor() {
             super(PolicyNode.class);
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         public IsWidget createContent() {
             FormFlexPanel content = new FormFlexPanel();
 
-            int col = -1;
-            if (isEditable()) {
-                content.setWidget(0, ++col, new CButton(i18n.tr("Select") + "...", new Command() {
-                    @Override
-                    public void execute() {
-                        OkCancelDialog selectScopeDialog = selectScopeDialogOf(getValue().getInstanceValueClass());
-                        if (selectScopeDialog != null) {
-                            selectScopeDialog.show();
-                        } else {
-                            Window.alert("NOT IMPLEMENTED YET: here be node selection dialog for node type: " + (getValue().getInstanceValueClass().getName()));
+            final Map<Class<? extends PolicyNode>, CComponent<?, ?>> nodeTypeToComponentMap = new HashMap<Class<? extends PolicyNode>, CComponent<?, ?>>();
+            for (Class<? extends PolicyNode> policyNodeType : ASSIGNABLE_NODE_TYPES) {
+                CComponent<? extends PolicyNode, ?> comp = null;
+                if (isEditable()) {
+                    CEntityComboBox<? extends PolicyNode> comboBox = new CEntityComboBox(policyNodeType);
+                    comboBox.addValueChangeHandler(new ValueChangeHandler() {
+                        @Override
+                        public void onValueChange(ValueChangeEvent event) {
+                            setValue((PolicyNode) event.getValue());
                         }
-                    }
-                }));
-
-                content.getFlexCellFormatter().getElement(0, col).getStyle().setPaddingRight(2d, Unit.EM);
+                    });
+                    comp = comboBox;
+                } else {
+                    comp = new CEntityLabel();
+                }
+                comp.setVisible(false);
+                nodeTypeToComponentMap.put(policyNodeType, comp);
             }
 
-            final CLabel scopeRepresentation = new CLabel();
-            scopeRepresentation.setWidth("20em");
-            content.setWidget(0, ++col, scopeRepresentation);
+            int row = -1;
+            for (CComponent<?, ?> nodeComponent : nodeTypeToComponentMap.values()) {
+                content.setWidget(++row, 0, nodeComponent);
+            }
 
             // TODO not sure why I use both property change handler and value change handler, value change supposed to be enough            
             addPropertyChangeHandler(new PropertyChangeHandler() {
                 @Override
                 public void onPropertyChange(PropertyChangeEvent event) {
                     if (event.getPropertyName().equals(PropertyChangeEvent.PropertyName.repopulated)) {
-                        scopeRepresentation.setValue(!getValue().isEmpty() ? getValue().cast().getStringView() : i18n.tr("Nothing selected"));
+                        for (CComponent<?, ?> nodeComponent : nodeTypeToComponentMap.values()) {
+                            nodeComponent.setVisible(false);
+                            nodeComponent.setMandatory(false);
+                        }
+                        Class<? extends PolicyNode> nodeType = (Class<? extends PolicyNode>) getValue().getInstanceValueClass();
+                        if (nodeTypeToComponentMap.containsKey(nodeType)) {
+                            CComponent<PolicyNode, ?> comp = (CComponent<PolicyNode, ?>) nodeTypeToComponentMap.get(nodeType);
+                            comp.setVisible(true);
+                            comp.setMandatory(true);
+                            comp.setValue((PolicyNode) getValue().cast());
+                        }
                     }
                 }
             });
@@ -209,27 +212,10 @@ public abstract class PolicyDTOTabPanelBasedEditorForm<POLICY_DTO extends Policy
             addValueChangeHandler(new ValueChangeHandler<PolicyNode>() {
                 @Override
                 public void onValueChange(ValueChangeEvent<PolicyNode> event) {
-                    scopeRepresentation.setValue(!event.getValue().isEmpty() ? event.getValue().cast().getStringView() : i18n.tr("Nothing selected"));
                 }
             });
 
             return content;
-        }
-
-        private <E extends IEntity> OkCancelDialog selectScopeDialogOf(Class<E> policyNodeClass) {
-            PolicyNodeSetter policyNodeSetter = new PolicyNodeSetter() {
-                @Override
-                public void setPolicyNode(PolicyNode policyNode) {
-                    PolicyNodeEditor.this.setValue(policyNode);
-                }
-            };
-
-            if (AptUnit.class.equals(policyNodeClass)) {
-                return new AptUnitSelectDialog(policyNodeSetter);
-            } else if (Building.class.equals(policyNodeClass)) {
-                return new BuildingSelectDialog(policyNodeSetter);
-            }
-            return null;
         }
     }
 
@@ -267,98 +253,6 @@ public abstract class PolicyDTOTabPanelBasedEditorForm<POLICY_DTO extends Policy
 
         public Class<T> getType() {
             return nodeType;
-        }
-    }
-
-    private interface PolicyNodeSetter {
-        void setPolicyNode(PolicyNode policyNode);
-    }
-
-    private abstract static class ListerBasedSelectDialog<E extends PolicyNode> extends OkCancelDialog {
-
-        private final PolicyNodeSetter policyNodeSetter;
-
-        private final DeckLayoutPanel containerPanel;
-
-        public ListerBasedSelectDialog(Class<E> clazz, PolicyNodeSetter policyNodeSetter) {
-            super(i18n.tr("Select {0}...", EntityFactory.getEntityMeta(clazz).getCaption()));
-            this.policyNodeSetter = policyNodeSetter;
-            this.containerPanel = new DeckLayoutPanel();
-            this.containerPanel.setSize("800px", "600px");
-            setBody(containerPanel);
-        }
-
-        @Override
-        public boolean onClickOk() {
-            PolicyNode policyNode = getSelectedItem();
-            if (policyNode != null) {
-                policyNodeSetter.setPolicyNode(getSelectedItem());
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        protected DeckLayoutPanel getContainerPanel() {
-            return containerPanel;
-        }
-
-        protected abstract E getSelectedItem();
-    }
-
-    private static class AptUnitSelectDialog extends ListerBasedSelectDialog<AptUnit> {
-
-        private final IListerView<AptUnit> unitListerView;
-
-        // FIXME this class is not finished yet!
-        public AptUnitSelectDialog(PolicyNodeSetter policyNodeSetter) {
-            super(AptUnit.class, policyNodeSetter);
-
-            unitListerView = new ListerInternalViewImplBase<AptUnit>(new SelectedUnitLister());
-            @SuppressWarnings("unchecked")
-            final ListerActivityBase<AptUnit> unitListerActivity = new ListerActivityBase<AptUnit>(new CrmSiteMap.Settings.Policies(), unitListerView,
-                    (AbstractCrudService<AptUnit>) GWT.create(SelectUnitCrudService.class), AptUnit.class);
-            unitListerActivity.start(getContainerPanel(), new SimpleEventBus());
-
-            final IListerView<Building> buildingListerView = new ListerInternalViewImplBase<Building>(new SelectedBuildingLister());
-            @SuppressWarnings("unchecked")
-            final ListerActivityBase<Building> buildingListerActivity = new ListerActivityBase<Building>(new CrmSiteMap.Settings.Policies(),
-                    buildingListerView, (AbstractCrudService<Building>) GWT.create(SelectBuildingCrudService.class), Building.class);
-            buildingListerActivity.start(getContainerPanel(), new SimpleEventBus());
-
-            buildingListerView.getLister().addItemSelectionHandler(new ItemSelectionHandler<Building>() {
-
-                @Override
-                public void onSelect(Building selectedItem) {
-                    //                     
-                }
-            });
-            getContainerPanel().setSize("800px", "300px");
-        }
-
-        @Override
-        protected AptUnit getSelectedItem() {
-            return unitListerView.getLister().getSelectedItem();
-        }
-    }
-
-    private static class BuildingSelectDialog extends ListerBasedSelectDialog<Building> {
-
-        private final IListerView<Building> buildingListerView;
-
-        public BuildingSelectDialog(PolicyNodeSetter policyNodeSetter) {
-            super(Building.class, policyNodeSetter);
-
-            buildingListerView = new ListerInternalViewImplBase<Building>(new SelectedBuildingLister());
-            @SuppressWarnings("unchecked")
-            final ListerActivityBase<Building> buildingListerActivity = new ListerActivityBase<Building>(new CrmSiteMap.Settings.Policies(),
-                    buildingListerView, (AbstractCrudService<Building>) GWT.create(SelectBuildingCrudService.class), Building.class);
-            buildingListerActivity.start(getContainerPanel(), new SimpleEventBus());
-        }
-
-        @Override
-        protected Building getSelectedItem() {
-            return buildingListerView.getLister().getSelectedItem();
         }
     }
 }
