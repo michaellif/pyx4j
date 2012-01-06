1a8346a1c9e22b4939f447135eb72b94e2bb229d
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-01-06 03:43:14 +0000

    Correction to error handling

--- entity/pyx-entity-j2se/src/main/java/com/pyx4j/entity/server/impl/MemberMetaImpl.java
+++ entity/pyx-entity-j2se/src/main/java/com/pyx4j/entity/server/impl/MemberMetaImpl.java
@@ -161,6 +161,9 @@ public class MemberMetaImpl implements MemberMeta {
         rpcTransient = (method.getAnnotation(RpcTransient.class) != null);
         Owned aOwned = method.getAnnotation(Owned.class);
         boolean hasEmbedded = (method.getAnnotation(EmbeddedEntity.class) != null);
+        if (hasEmbedded && ((objectClassType == ObjectClassType.Primitive) || (objectClassType != ObjectClassType.PrimitiveSet))) {
+            throw new RuntimeException("Unexpected @EmbeddedEntity annotation in member " + fieldName + " of " + method.getDeclaringClass().getSimpleName());
+        }
         if ((!hasEmbedded) && (objectClassType != ObjectClassType.Primitive) && (objectClassType != ObjectClassType.PrimitiveSet)) {
             hasEmbedded = (valueClass.getAnnotation(EmbeddedEntity.class) != null);
         }
