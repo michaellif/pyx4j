41260d928dc7a9512d460e922bb9485cc26fabc3
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-01-09 21:32:00 +0000

    VISTA-910 added ApplicationDocumentationPolicy

---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/policy/policies/ApplicationDocumentationPolicy.java
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Jan 9, 2012
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.domain.policy.policies;
+
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.DiscriminatorValue;
+import com.pyx4j.entity.shared.IList;
+import com.pyx4j.entity.shared.IPrimitive;
+
+import com.propertyvista.domain.policy.Policy;
+import com.propertyvista.domain.policy.policies.specials.IdentificationDocument;
+
+@DiscriminatorValue("ApplicationDocumentationPolicy")
+public interface ApplicationDocumentationPolicy extends Policy {
+
+    @Caption(description = "The number of the IDs that is required for an application.")
+    IPrimitive<Integer> numberOfRequiredIDs();
+
+    @Caption(description = "IDs/Documentations that accepted as valid IDs.")
+    IList<IdentificationDocument> allowedIDs();
+}
