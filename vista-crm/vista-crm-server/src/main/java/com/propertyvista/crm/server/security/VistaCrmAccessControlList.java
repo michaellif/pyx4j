a765ea3641ef64539510d8425a0b84b9e3e98ae0
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-01-11 22:01:00 +0000

    VISTA-979 started for on portfolio base access

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/security/VistaCrmAccessControlList.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/security/VistaCrmAccessControlList.java
@@ -69,6 +69,7 @@ import com.propertyvista.domain.property.asset.unit.AptUnitItem;
 import com.propertyvista.domain.property.asset.unit.AptUnitOccupancy;
 import com.propertyvista.domain.ref.Country;
 import com.propertyvista.domain.security.VistaBasicBehavior;
+import com.propertyvista.domain.security.VistaDataAccessBehavior;
 import com.propertyvista.domain.tenant.Tenant;
 import com.propertyvista.domain.tenant.TenantInLease;
 import com.propertyvista.domain.tenant.TenantScreening;
@@ -199,7 +200,7 @@ public class VistaCrmAccessControlList extends ServletContainerAclBuilder {
 //        }
 
         // Date Access
-        //grant(VistaDataAccessBehavior.BuildingsAssigned, new BuildingDatasetAccessRule(), Building.class);
+        grant(VistaDataAccessBehavior.BuildingsAssigned, new BuildingDatasetAccessRule(), Building.class);
 
         freeze();
     }
