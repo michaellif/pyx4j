073eea5684441860b8705b8f5c75c46a4237071e
Anya Vassilieva <anya.vassilieva@propertyvista.com>
2012-01-20 07:16:31 +0000

    VISTA-75 add more DB connections

--- vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfiguration33.java
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfiguration33.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -36,6 +36,11 @@ public class VistaServerSideConfiguration33 extends VistaServerSideConfiguration
             @Override
             public String password() {
                 return "vista33";
+            }
+
+            @Override
+            public int maxPoolSize() {
+                return 100;
             }
         };
     }
