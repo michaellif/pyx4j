1f683660dbd852e6ef6f43ce4fd9c545d63f4179
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-01-25 19:48:05 +0000

    VISTA-1012 added sql script to normalize unit numbers

---
+++ src/upgrade/2012-01-25/VISTA-1012-fix-normalizes-unit-numbers.sql
@@ -0,0 +1,7 @@
+UPDATE AptUnit SET info_unitNumber = SUBSTRING(info_unitNumber, 2)  WHERE info_unitNumber LIKE "#%";
+
+UPDATE AptUnit SET info_unitNumber = SUBSTRING(info_unitNumber, 2)  WHERE info_unitNumber LIKE "0%";
+
+UPDATE AptUnit SET info_unitNumber = SUBSTRING(info_unitNumber, 2)  WHERE info_unitNumber LIKE "0%";
+
+UPDATE AptUnit SET info_unitNumber = SUBSTRING(info_unitNumber, 2)  WHERE info_unitNumber LIKE "0%";
