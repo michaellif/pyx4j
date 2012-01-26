f9e2d88c791907e2f9f35c1caa8d9d2865665577
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-01-26 22:08:46 +0000

    VISTA-1012 added missing files

---
+++ vista-shared/vista-importer/src/main/java/com/propertyvista/interfaces/importer/VistaXMLImportAdapter.java
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Jan 26, 2012
+ * @author yuriyl
+ * @version $Id$
+ */
+package com.propertyvista.interfaces.importer;
+
+import java.io.ByteArrayInputStream;
+
+import org.xml.sax.InputSource;
+
+import com.pyx4j.essentials.rpc.report.DownloadFormat;
+
+import com.propertyvista.interfaces.importer.model.ImportIO;
+
+public class VistaXMLImportAdapter implements ImportAdapter {
+
+    @Override
+    public ImportIO parse(byte[] data, DownloadFormat format) {
+        switch (format) {
+        case XML:
+            return ImportUtils.parse(ImportIO.class, new InputSource(new ByteArrayInputStream(data)));
+        default:
+            throw new Error("Unsupported file format");
+        }
+    }
+
+}
