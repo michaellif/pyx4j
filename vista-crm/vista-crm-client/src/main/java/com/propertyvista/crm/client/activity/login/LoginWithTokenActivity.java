03753bcfcc074f6ebd8ea7d661e174a3fc0babfa
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-01-31 15:38:53 +0000

    VISTA-1033: refactoring: CRM - LoginWithTokenActivity was subclassed from AbstractLoginWithTokenActivity

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/login/LoginWithTokenActivity.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/login/LoginWithTokenActivity.java
@@ -13,57 +13,21 @@
  */
 package com.propertyvista.crm.client.activity.login;
 
-import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.Place;
-import com.google.gwt.user.client.ui.AcceptsOneWidget;
 
-import com.pyx4j.i18n.shared.I18n;
-import com.pyx4j.rpc.client.DefaultAsyncCallback;
-import com.pyx4j.security.client.ClientContext;
-import com.pyx4j.security.rpc.AuthenticationResponse;
 import com.pyx4j.security.rpc.AuthenticationService;
-import com.pyx4j.site.client.AppSite;
-import com.pyx4j.site.rpc.AppPlace;
-import com.pyx4j.widgets.client.dialog.MessageDialog;
 
+import com.propertyvista.common.client.ui.components.login.AbstractLoginWithTokenActivity;
 import com.propertyvista.common.client.ui.components.login.LoginWithTokenView;
 import com.propertyvista.crm.client.ui.viewfactories.LoginViewFactory;
 import com.propertyvista.crm.rpc.CrmSiteMap;
 import com.propertyvista.crm.rpc.services.pub.CrmAuthenticationService;
 
-public class LoginWithTokenActivity extends AbstractActivity {
-
-    private static final I18n i18n = I18n.get(LoginWithTokenActivity.class);
-
-    private final LoginWithTokenView view;
-
-    private final String authToken;
+public class LoginWithTokenActivity extends AbstractLoginWithTokenActivity {
 
     public LoginWithTokenActivity(Place place) {
-        assert (place instanceof AppPlace);
-        authToken = ((AppPlace) place).getFirstArg(AuthenticationService.AUTH_TOKEN_ARG);
-        view = LoginViewFactory.instance(LoginWithTokenView.class);
+        super(place, LoginViewFactory.instance(LoginWithTokenView.class), GWT.<AuthenticationService> create(CrmAuthenticationService.class),
+                new CrmSiteMap.PasswordReset(), new CrmSiteMap.Login());
     }
-
-    @Override
-    public void start(AcceptsOneWidget panel, EventBus eventBus) {
-        panel.setWidget(view);
-
-        GWT.<AuthenticationService> create(CrmAuthenticationService.class).authenticateWithToken(new DefaultAsyncCallback<AuthenticationResponse>() {
-            @Override
-            public void onSuccess(AuthenticationResponse result) {
-                ClientContext.authenticated(result);
-                AppSite.getPlaceController().goTo(new CrmSiteMap.PasswordReset());
-            }
-
-            @Override
-            public void onFailure(Throwable caught) {
-                AppSite.getPlaceController().goTo(new CrmSiteMap.Login());
-                MessageDialog.error(i18n.tr("Authentication failed"), i18n.tr("The URL you have used is either incorrect or expired."));
-            }
-        }, ClientContext.getClientSystemInfo(), authToken);
-    }
-
 }
