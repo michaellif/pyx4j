25fa50ef004452df638f01cc2290678db4f8f2a7
Igor Kravtsov <igork@birchwoodsoftwaregroup.com>
2012-02-02 04:32:55 +0000

    VISTA-970

--- tester/pyx-tester-selenium/src/test/java/com/pyx4j/tester/selenium/ui/FormBasicTest.java
+++ tester/pyx-tester-selenium/src/test/java/com/pyx4j/tester/selenium/ui/FormBasicTest.java
@@ -23,6 +23,7 @@ package com.pyx4j.tester.selenium.ui;
 import com.pyx4j.entity.shared.EntityFactory;
 import com.pyx4j.forms.client.ui.decorators.WidgetDecorator;
 import com.pyx4j.selenium.D;
+import com.pyx4j.tester.client.domain.CComponentProperties;
 import com.pyx4j.tester.client.domain.test.DomainFactory;
 import com.pyx4j.tester.client.domain.test.EntityI;
 import com.pyx4j.tester.client.view.form.FormTesterViewImpl;
@@ -53,12 +54,39 @@ public class FormBasicTest extends TesterSeleniumTestBase {
         assertVisible(D.id(entityIempty.textBox()));
         assertValueOnForm(entityIempty.textBox());
 
+        // How to click on a Label for a component
+        // It will populate component properties form
+        selenium.click(D.id(D.id(proto(EntityI.class).textBox()), WidgetDecorator.DebugIds.Label.debugId()));
+
+        // Set mandatory flag on component properties panel
+        selenium.click(D.id(proto(CComponentProperties.class).mandatory()));
+
+        // Check mandatory asterisk" visible
+        assertVisible(D.id(D.id(proto(EntityI.class).textBox()), WidgetDecorator.DebugIds.MandatoryImage.debugId()));
+
+        // Click again
+        selenium.click(D.id(proto(CComponentProperties.class).mandatory()));
+
+        // Check mandatory "asterisk" disappeared
+        assertNotVisible(D.id(D.id(proto(EntityI.class).textBox()), WidgetDecorator.DebugIds.MandatoryImage.debugId()));
+
+        // Click populate button
         selenium.click(D.id(FormTesterViewImpl.DebugIds.ButtonPanel, FormTesterViewImpl.DebugIds.PopulateButton));
 
         EntityI entityIpopulated = DomainFactory.createEntityI();
         // Verify the form is populated with data from factory
         assertValueOnForm(entityIpopulated.textBox());
 
-        selenium.click(D.id(D.id(proto(EntityI.class).textBox()), WidgetDecorator.DebugIds.Label.debugId()));
+        // Visible is checked by default. Click to un-check. 
+        selenium.click(D.id(proto(CComponentProperties.class).visible()));
+
+        // Check that component invisible
+        assertNotVisible(D.id(proto(EntityI.class).textBox()));
+
+        // Set visible
+        selenium.click(D.id(proto(CComponentProperties.class).visible()));
+
+        // Check that component visible
+        assertVisible(D.id(proto(EntityI.class).textBox()));
     }
 }
