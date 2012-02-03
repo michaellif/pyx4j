4015716ba83901c19ae255e19506420d09879d16
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-02-03 17:04:09 +0000

    VISTA-1087 added some validations

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/ContractEditor.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/ContractEditor.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -13,14 +13,25 @@
  */
 package com.propertyvista.common.client.ui.components.editors;
 
+import java.util.Date;
+
 import com.google.gwt.user.client.ui.IsWidget;
 
+import com.pyx4j.commons.LogicalDate;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.forms.client.ui.CComponent;
+import com.pyx4j.forms.client.ui.RevalidationTrigger;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
+import com.pyx4j.forms.client.validators.EditableValueValidator;
+import com.pyx4j.forms.client.validators.ValidationFailure;
+import com.pyx4j.i18n.shared.I18n;
 
 import com.propertyvista.common.client.ui.components.c.CEntityDecoratableEditor;
 import com.propertyvista.domain.property.vendor.Contract;
 
 public class ContractEditor extends CEntityDecoratableEditor<Contract> {
+
+    private static final I18n i18n = I18n.get(ContractEditor.class);
 
     public ContractEditor() {
         super(Contract.class);
@@ -45,6 +56,49 @@ public class ContractEditor extends CEntityDecoratableEditor<Contract> {
         main.getColumnFormatter().setWidth(0, "50%");
         main.getColumnFormatter().setWidth(1, "50%");
 
+        getValidations();
+
         return main;
+    }
+
+    private void getValidations() {
+
+        get(proto().start()).addValueChangeHandler(new RevalidationTrigger<LogicalDate>(get(proto().end()))); //connects validation of both fields
+        get(proto().end()).addValueChangeHandler(new RevalidationTrigger<LogicalDate>(get(proto().start())));
+
+        get(proto().start()).addValueValidator(new EditableValueValidator<Date>() {
+
+            @Override
+            public ValidationFailure isValid(CComponent<Date, ?> component, Date value) {
+                if (getValue() == null || getValue().isEmpty()) {
+                    return null;
+                }
+                IPrimitive<LogicalDate> end = getValue().end();
+                if (end.isNull()) {
+                    return null;
+                }
+                return (value != null) && value.before(end.getValue()) ? null : new ValidationFailure(i18n
+                        .tr("Contract Expiry Date should be after its Start Date"));
+            }
+
+        });
+
+        get(proto().end()).addValueValidator(new EditableValueValidator<Date>() {
+
+            @Override
+            public ValidationFailure isValid(CComponent<Date, ?> component, Date value) {
+                if (getValue() == null || getValue().isEmpty()) {
+                    return null;
+                }
+                IPrimitive<LogicalDate> start = getValue().start();
+                if (start.isNull()) {
+                    return null;
+                }
+                return (value != null) && value.after(start.getValue()) ? null : new ValidationFailure(i18n
+                        .tr("Contract Expiry Date should be after its Start Date"));
+            }
+
+        });
+
     }
 }
