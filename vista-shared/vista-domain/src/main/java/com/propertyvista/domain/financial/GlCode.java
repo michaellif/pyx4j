ff817f65a61a90b1b91f01f44a69050713bc6241
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-02-09 21:05:23 +0000

    VISTA-1093 GlCode field type change

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/financial/GlCode.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/financial/GlCode.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -13,6 +13,8 @@
  */
 package com.propertyvista.domain.financial;
 
+import com.pyx4j.entity.annotations.Editor;
+import com.pyx4j.entity.annotations.Editor.EditorType;
 import com.pyx4j.entity.shared.IEntity;
 import com.pyx4j.entity.shared.IPrimitive;
 
@@ -20,6 +22,7 @@ public interface GlCode extends IEntity {
 
     IPrimitive<Integer> glId();
 
+    @Editor(type = EditorType.textarea)
     IPrimitive<String> description();
 
     // not used currently!
