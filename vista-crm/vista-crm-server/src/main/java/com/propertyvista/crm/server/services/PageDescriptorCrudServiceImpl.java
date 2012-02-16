0210c11e8b529063b0cb1352de4c8bf752b7e0ce
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-02-16 05:19:49 +0000

    VISTA-1119 fixed site persistence

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/PageDescriptorCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/PageDescriptorCrudServiceImpl.java
@@ -15,48 +15,41 @@ package com.propertyvista.crm.server.services;
 
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import com.pyx4j.entity.server.AbstractCrudServiceImpl;
 import com.pyx4j.entity.server.Persistence;
 import com.pyx4j.entity.shared.criterion.EntityQueryCriteria;
 
 import com.propertyvista.crm.rpc.services.PageDescriptorCrudService;
-import com.propertyvista.crm.server.util.GenericCrudServiceImpl;
 import com.propertyvista.domain.site.PageCaption;
 import com.propertyvista.domain.site.PageContent;
 import com.propertyvista.domain.site.PageDescriptor;
 import com.propertyvista.domain.site.SiteDescriptor;
 
-public class PageDescriptorCrudServiceImpl extends GenericCrudServiceImpl<PageDescriptor> implements PageDescriptorCrudService {
-
-    private final static Logger log = LoggerFactory.getLogger(PageDescriptorCrudServiceImpl.class);
+public class PageDescriptorCrudServiceImpl extends AbstractCrudServiceImpl<PageDescriptor> implements PageDescriptorCrudService {
 
     public PageDescriptorCrudServiceImpl() {
         super(PageDescriptor.class);
     }
 
     @Override
-    protected void enhanceRetrieved(PageDescriptor entity, boolean fromList) {
-        if (!fromList) {
-            // load content caption:
-            for (PageContent content : entity.content()) {
+    protected void enhanceRetrieved(PageDescriptor entity) {
+        // load content caption:
+        for (PageContent content : entity.content()) {
 
-                // TODO VladS remove this hack!
-                content.descriptor().set(entity);
+            // TODO VladS remove this hack!
+            content.descriptor().set(entity);
 
-                for (PageCaption caption : entity.caption()) {
-                    if (content.locale().equals(caption.locale())) {
-                        content._caption().set(caption);
-                        break;
-                    }
+            for (PageCaption caption : entity.caption()) {
+                if (content.locale().equals(caption.locale())) {
+                    content._caption().set(caption);
+                    break;
                 }
             }
         }
     }
 
     @Override
-    protected void persistDBO(PageDescriptor dbo) {
+    protected void persist(PageDescriptor dbo) {
         // update caption:
         dbo.caption().clear();
         for (PageContent content : dbo.content()) {
@@ -64,21 +57,12 @@ public class PageDescriptorCrudServiceImpl extends GenericCrudServiceImpl<PageDe
             dbo.caption().add(content._caption());
         }
 
-        boolean isCreate = dbo.id().isNull();
         if (dbo.type().isNull()) {
             dbo.type().setValue(PageDescriptor.Type.staticContent);
         }
 
-        super.persistDBO(dbo);
+        super.persist(dbo);
 
-        // update parent child list: 
-        if (isCreate) {
-            log.debug("1. create child of ", dbo);
-            Persistence.service().retrieve(dbo.parent());
-            dbo.parent().childPages().add(dbo);
-            log.debug("2. create child of ", dbo);
-            Persistence.service().merge(dbo.parent());
-        }
         // set update flag
         EntityQueryCriteria<SiteDescriptor> criteria = EntityQueryCriteria.create(SiteDescriptor.class);
         SiteDescriptor siteDescriptor = Persistence.service().retrieve(criteria);
