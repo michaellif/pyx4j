99f5b96881bf5271b9b4834165e5ab394d61f847
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-02-18 09:05:46 +0000

    VISTA-1115 Add native Display Name for Locale

--- vista-shared/vista-domain/src/main/java/com/propertyvista/shared/CompiledLocale.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/shared/CompiledLocale.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -18,39 +18,50 @@ import java.io.Serializable;
 import com.pyx4j.i18n.annotations.Translate;
 import com.pyx4j.i18n.shared.I18nEnum;
 
-//TODO Use compiler do define available in compiled GWT locales  
+//TODO Use compiler do define available in compiled GWT locales
+// Use http://www.cylog.org/tools/utf8_converter.jsp to avoid UTF-8 format in this file
 public enum CompiledLocale implements Serializable {
 
     @Translate(value = "English")
-    en,
+    en("English"),
 
     @Translate(value = "English (Canada)")
-    en_CA,
+    en_CA("English"),
 
     @Translate(value = "English (United States)")
-    en_US,
+    en_US("English"),
 
     @Translate(value = "French")
-    fr,
+    fr("fran\u00e7ais"),
 
     @Translate(value = "French (Canada)")
-    fr_CA,
+    fr_CA("fran\u00e7ais"),
 
     @Translate(value = "Spanish")
-    es,
+    es("Espa\u00f1ol"),
 
     @Translate(value = "Russian")
-    ru,
+    ru("\u0420\u0443\u0441\u0441\u043a\u0438\u0439"),
 
-    @Translate(value = "Chinese (Simplified)")
-    zh_CN,
+    @Translate(value = "Simplified Chinese")
+    zh_CN("\u7b80\u4f53\u4e2d\u6587"),
 
-    @Translate(value = "Chinese (Traditional)")
-    zh_TW;
+    @Translate(value = "Traditional Chinese")
+    zh_TW("\u7e41\u9ad4\u4e2d\u6587");
+
+    private final String nativeDisplayName;
+
+    CompiledLocale(String nativeDisplayName) {
+        this.nativeDisplayName = nativeDisplayName;
+    }
 
     @Override
     public String toString() {
         return I18nEnum.toString(this);
+    }
+
+    public String getNativeDisplayName() {
+        return nativeDisplayName;
     }
 
     public String getLanguage() {
