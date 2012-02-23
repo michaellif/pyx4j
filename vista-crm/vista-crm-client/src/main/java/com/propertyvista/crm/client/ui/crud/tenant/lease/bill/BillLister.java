926134b26964cdfb616273333dfadcf7562bf80b
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-02-23 20:08:48 +0000

    VISTA-1133

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/tenant/lease/bill/BillLister.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/tenant/lease/bill/BillLister.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -27,8 +27,8 @@ public class BillLister extends ListerBase<Bill> {
 
         setColumnDescriptors(//@formatter:off
             new MemberColumnDescriptor.Builder(proto().billStatus()).build(),
-            new MemberColumnDescriptor.Builder(proto().recurringFeatureCharges()).build(),
             new MemberColumnDescriptor.Builder(proto().oneTimeFeatureCharges()).build(),
+            new MemberColumnDescriptor.Builder(proto().recurringFeatureCharges()).build(),
             new MemberColumnDescriptor.Builder(proto().totalAdjustments()).build(),
             new MemberColumnDescriptor.Builder(proto().previousBalanceAmount()).build(),
             new MemberColumnDescriptor.Builder(proto().paymentReceivedAmount()).build(),
