e101094e43a89992dee110eebfa34b1cc267d16a
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-03-02 22:37:15 +0000

    VISTA-1172 added change password button

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/tenant/screening/guarantor/GuarantorViewerActivity.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/tenant/screening/guarantor/GuarantorViewerActivity.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -17,14 +17,19 @@ import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.Place;
 
+import com.pyx4j.commons.Key;
 import com.pyx4j.entity.rpc.AbstractCrudService;
+import com.pyx4j.site.client.AppSite;
 import com.pyx4j.site.client.activity.crud.ListerActivityBase;
 import com.pyx4j.site.client.ui.crud.lister.IListerView;
 import com.pyx4j.site.client.ui.crud.lister.IListerView.Presenter;
+import com.pyx4j.site.rpc.AppPlace;
 
+import com.propertyvista.common.client.ui.components.security.PasswordChangeView;
 import com.propertyvista.crm.client.activity.crud.CrmViewerActivity;
 import com.propertyvista.crm.client.ui.crud.tenant.screening.guarantor.GuarantorViewerView;
 import com.propertyvista.crm.client.ui.crud.viewfactories.TenantViewFactory;
+import com.propertyvista.crm.rpc.CrmSiteMap;
 import com.propertyvista.crm.rpc.services.tenant.screening.GuarantorCrudService;
 import com.propertyvista.crm.rpc.services.tenant.screening.PersonScreeningCrudService;
 import com.propertyvista.domain.tenant.Guarantor;
@@ -45,8 +50,19 @@ public class GuarantorViewerActivity extends CrmViewerActivity<GuarantorDTO> imp
     }
 
     @Override
-    public Presenter getScreeningPresenter() {
+    public Presenter getScreeningListerPresenter() {
         return screeningLister;
+    }
+
+    @Override
+    public void goToChangePassword(Key guarantorPrincipalPk, String guarantorName) {
+        if (guarantorPrincipalPk != null) {
+            AppPlace passwordChangePlace = new CrmSiteMap.PasswordChange();
+            passwordChangePlace.arg(PasswordChangeView.Presenter.PRINCIPAL_PK_ARG, guarantorPrincipalPk.toString());
+            passwordChangePlace.arg(PasswordChangeView.Presenter.PRINCIPAL_NAME_ARG, guarantorName);
+            passwordChangePlace.arg(PasswordChangeView.Presenter.PRINCIPAL_CLASS, PasswordChangeView.Presenter.PrincipalClass.GUARANTOR.toString());
+            AppSite.getPlaceController().goTo(passwordChangePlace);
+        }
     }
 
     @Override
