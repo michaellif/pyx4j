4443fe38b361932d4cf45a4e9fd6413897e0b6ac
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-03-15 19:47:43 +0000

    VISTA-1224 initial check in for ArrearsManagerTest (the test itself is not functional yet)

---
+++ vista-shared/vista-server-common/src/test/java/com/propertyvista/server/common/util/arrears/ArrearsManagerTest.java
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 15, 2012
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.server.common.util.arrears;
+
+public class ArrearsManagerTest extends ArrearsManagerTestBase {
+
+    public void test() {
+        // something like:
+//        bill().dueTo("2011-01-01").amount("500").createOn("2010-15-01");
+//
+//        pay("2011-15-01", "300");
+//
+//        expectArrears().on("2011-15-01").arr0to30("0").arr30to60("0").arr60to90("0").arrOver90("0").total("0").arBalance("0");
+//
+//        bill().dueTo("2011-02-01").amount("500").createOn("2011-15-01");
+//
+//        expectArrears().on("2011-02-01").arr0to30("0").arr30to60("0").arr60to90("0").arrOver90("0").total("0").arBalance("0");
+
+    }
+
+}
