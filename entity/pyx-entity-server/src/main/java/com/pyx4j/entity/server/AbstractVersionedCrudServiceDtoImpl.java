86ca128dc1acd0244336940e3613d35bf410ec8e
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-16 18:09:29 +0000

    Add AbstractVersionedCrudServiceDtoImpl

---
+++ entity/pyx-entity-server/src/main/java/com/pyx4j/entity/server/AbstractVersionedCrudServiceDtoImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2011 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Mar 16, 2012
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.entity.server;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.pyx4j.commons.Key;
+import com.pyx4j.entity.rpc.AbstractVersionedCrudService;
+import com.pyx4j.entity.shared.IVersionedEntity;
+import com.pyx4j.rpc.shared.VoidSerializable;
+
+public abstract class AbstractVersionedCrudServiceDtoImpl<E extends IVersionedEntity<?>, DTO extends IVersionedEntity<?>> extends
+        AbstractCrudServiceDtoImpl<E, DTO> implements AbstractVersionedCrudService<DTO> {
+
+    protected AbstractVersionedCrudServiceDtoImpl(Class<E> entityClass, Class<DTO> dtoClass) {
+        super(entityClass, dtoClass);
+    }
+
+    @Override
+    public void approveFinal(AsyncCallback<VoidSerializable> callback, Key entityId) {
+        // TODO Auto-generated method stub
+    }
+
+}
