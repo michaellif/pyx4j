e893c5bdca226a54324330e783e5a37159993786
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-03-16 14:33:28 +0000

    VISTA-1216 positioned logo better

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/FooterViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/FooterViewImpl.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -39,6 +39,7 @@ public class FooterViewImpl extends SimplePanel implements FooterView {
                 Window.open("http://www.propertyvista.com", Media.Type.externalUrl.name(), null);
             }
         });
+        panel.setHeight("45px");
         panel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
         panel.add(image);
         setWidget(panel);
