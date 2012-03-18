b666606bc1fd4741899aa9294e5ca4930d6eab2b
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-18 04:04:29 +0000

    VISTA-959 show application version in CRM

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/FooterViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/FooterViewImpl.java
@@ -15,11 +15,16 @@ package com.propertyvista.crm.client.ui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DragEvent;
+import com.google.gwt.event.dom.client.DragHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import com.pyx4j.config.client.ClientApplicationVersion;
+import com.pyx4j.widgets.client.Tooltip;
 
 import com.propertyvista.common.client.resources.VistaImages;
 import com.propertyvista.common.client.theme.CrmSitePanelTheme;
@@ -31,7 +36,7 @@ public class FooterViewImpl extends SimplePanel implements FooterView {
         setSize("100%", "100%");
         setStyleName(CrmSitePanelTheme.StyleName.SiteViewFooter.name());
         VerticalPanel panel = new VerticalPanel();
-        Image image = new Image(VistaImages.INSTANCE.logo().getSafeUri());
+        final Image image = new Image(VistaImages.INSTANCE.logo().getSafeUri());
 
         image.addClickHandler(new ClickHandler() {
             @Override
@@ -39,6 +44,14 @@ public class FooterViewImpl extends SimplePanel implements FooterView {
                 Window.open("http://www.propertyvista.com", Media.Type.externalUrl.name(), null);
             }
         });
+
+        image.addDragHandler(new DragHandler() {
+            @Override
+            public void onDrag(DragEvent event) {
+                Tooltip.tooltip(image, ClientApplicationVersion.instance().getBuildInformation());
+            }
+        });
+
         panel.setHeight("45px");
         panel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
         panel.add(image);
