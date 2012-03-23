8725509250e56c5e91c8dc4374a77061e6a273a1
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-23 19:59:03 +0000

    VISTA-1264 Adding pangroup Prod config

---
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationProdPangroup.java
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Aug 26, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.server.config;
+
+import com.pyx4j.config.server.IMailServiceConfigConfiguration;
+import com.pyx4j.config.server.IPersistenceConfiguration;
+
+public class VistaServerSideConfigurationProdPangroup extends VistaServerSideConfigurationProd {
+
+    @Override
+    public String getApplicationURLNamespace() {
+        return ".prod02.birchwoodsoftwaregroup.com/";
+    }
+
+    @Override
+    public boolean isDevelopmentBehavior() {
+        return true;
+    }
+
+    @Override
+    public boolean openIdrequired() {
+        return true;
+    }
+
+    @Override
+    public boolean openDBReset() {
+        return false;
+    }
+
+    @Override
+    public String getApplicationEmailSender() {
+        return "\"Property Vista Support\" <support.www22@birchwoodsoftwaregroup.com>";
+    }
+
+    @Override
+    public IMailServiceConfigConfiguration getMailServiceConfigConfiguration() {
+        return VistaSMTPMailServiceConfig.getGmailConfig("prod-pangroup-");
+    }
+
+    @Override
+    public IPersistenceConfiguration getPersistenceConfiguration() {
+        return new VistaConfigurationMySQL() {
+
+            @Override
+            public String dbName() {
+                return "vista_pangroup";
+            }
+
+            @Override
+            public String userName() {
+                return "vista_pangroup";
+            }
+
+            @Override
+            public String password() {
+                return "12aswedrqX@sd$fnjSz&";
+            }
+        };
+    }
+
+}
