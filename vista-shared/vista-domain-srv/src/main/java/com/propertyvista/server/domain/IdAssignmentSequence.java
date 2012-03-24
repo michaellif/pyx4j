e4367f33d5dcd80f2b0980d0a1b154cd1afebe0d
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-24 18:53:04 +0000

    VISTA-1286 domain definition

---
+++ vista-shared/vista-domain-srv/src/main/java/com/propertyvista/server/domain/IdAssignmentSequence.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 24, 2012
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.server.domain;
+
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+
+import com.propertyvista.domain.policy.policies.domain.IdAssignmentItem;
+
+public interface IdAssignmentSequence extends IEntity {
+
+    IPrimitive<IdAssignmentItem.IdTarget> target();
+
+    IPrimitive<Long> number();
+
+}
---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/policy/policies/domain/IdAssignmentItem.java
@@ -0,0 +1,69 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 24, 2012
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.domain.policy.policies.domain;
+
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.ReadOnly;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.i18n.annotations.I18n;
+import com.pyx4j.i18n.annotations.Translate;
+import com.pyx4j.i18n.shared.I18nEnum;
+
+public interface IdAssignmentItem extends IEntity {
+
+    @I18n
+    public enum IdTarget {
+
+        @Translate("Building property code")
+        propertyCode,
+
+        lease,
+
+        application,
+
+        tenant,
+
+        lead;
+
+        @Override
+        public String toString() {
+            return I18nEnum.toString(this);
+        }
+    }
+
+    @I18n
+    public enum IdType {
+
+        userEditable,
+
+        userCreated,
+
+        generatedNumber,
+
+        generatedAlphabetical;
+
+        @Override
+        public String toString() {
+            return I18nEnum.toString(this);
+        }
+    }
+
+    @ReadOnly
+    IPrimitive<IdTarget> target();
+
+    @MemberColumn(name = "tp")
+    IPrimitive<IdType> type();
+
+}
