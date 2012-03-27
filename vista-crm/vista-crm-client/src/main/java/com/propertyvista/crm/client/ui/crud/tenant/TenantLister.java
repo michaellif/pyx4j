83cf5601e4dffbed39c441fb1e83bbf557674245
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-03-27 15:23:34 +0000

    VISTA-1304 - sorting is closed.

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/tenant/TenantLister.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/tenant/TenantLister.java
@@ -37,7 +37,7 @@ public class TenantLister extends ListerBase<TenantDTO> {
                 new Builder(proto().person().mobilePhone(), false).sortable(false).build(),
                 new Builder(proto().person().workPhone(), false).sortable(false).build(),
                 new Builder(proto().person().email()).title("E-mail address").build(),
-                new Builder(proto().lease()).build()
+                new Builder(proto().lease()).sortable(false).build()
             ); // @formatter:on
     }
 }
