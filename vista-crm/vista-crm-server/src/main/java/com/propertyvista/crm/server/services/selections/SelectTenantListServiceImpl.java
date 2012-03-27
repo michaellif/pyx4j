6c3b867446092fe539a3b6a9ea61ba73f28fd788
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-27 17:04:19 +0000

    VISTA-1264 Add Tenant to lease fixed

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/selections/SelectTenantListServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/selections/SelectTenantListServiceImpl.java
@@ -26,6 +26,7 @@ public class SelectTenantListServiceImpl extends AbstractListServiceImpl<Tenant>
 
     @Override
     protected void bind() {
+        bind(dtoProto.id(), dboProto.id());
         bindCompleateDBO();
     }
 }
