856fb05d74eb092ce92695d9748df847e8bd0cec
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-28 18:56:44 +0000

    VISTA-1264 email template printer

---
+++ vista-shared/vista-server-common/src/test/java/com/propertyvista/server/common/mail/templates/PrintEmailTemplateVariables.java
@@ -0,0 +1,31 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2012-03-28
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.server.common.mail.templates;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.pyx4j.essentials.server.xml.XMLEntitySchemaWriter;
+
+import com.propertyvista.server.common.mail.templates.model.EmailTemplateRoot;
+
+public class PrintEmailTemplateVariables {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        XMLEntitySchemaWriter.printSchema(new FileOutputStream(new File("target", "EmailTemplateVariables.xsd")), true, EmailTemplateRoot.class,
+                EmailTemplateRoot.class);
+    }
+
+}
