d80e8fef1e260977fad2e211c8c3d147795c9856
Michael Lifshits <michaellif@propertyvista.com>
2012-03-28 22:37:02 +0000

    VISTA-490 billing impl - renaming packages

--- vista-shared/vista-billing/src/main/java/com/propertyvista/server/financial/productcatalog/ProductCatalogFacadeImpl.java
+++ vista-shared/vista-billing/src/main/java/com/propertyvista/server/financial/productcatalog/ProductCatalogFacadeImpl.java
@@ -11,7 +11,7 @@
  * @author michaellif
  * @version $Id$
  */
-package com.propertyvista.server.productcatalog;
+package com.propertyvista.server.financial.productcatalog;
 
 import java.math.BigDecimal;
 import java.util.List;
--- vista-shared/vista-server-common/src/main/java/com/propertyvista/server/financial/productcatalog/ProductCatalogFacade.java
+++ vista-shared/vista-server-common/src/main/java/com/propertyvista/server/financial/productcatalog/ProductCatalogFacade.java
@@ -11,7 +11,7 @@
  * @author vlads
  * @version $Id$
  */
-package com.propertyvista.server.productcatalog;
+package com.propertyvista.server.financial.productcatalog;
 
 import java.math.BigDecimal;
 
