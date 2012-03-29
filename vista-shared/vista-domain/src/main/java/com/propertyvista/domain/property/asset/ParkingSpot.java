c41cfe483ec8b278498d4dd3d9e15d17001e0d90
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-03-29 17:41:50 +0000

    VISTA-1315 Parking spot totals should now work

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/property/asset/ParkingSpot.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/property/asset/ParkingSpot.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -26,6 +26,8 @@ import com.pyx4j.entity.shared.IEntity;
 import com.pyx4j.entity.shared.IPrimitive;
 import com.pyx4j.i18n.annotations.I18n;
 import com.pyx4j.i18n.shared.I18nEnum;
+
+import com.propertyvista.shared.adapters.ParkingSpotCountersAdapter;
 
 public interface ParkingSpot extends IEntity {
 
@@ -55,7 +57,7 @@ public interface ParkingSpot extends IEntity {
     Parking belongsTo();
 
     @ToString(index = 1)
-    @MemberColumn(name = "spotType")
+    @MemberColumn(name = "spotType", modificationAdapters = { ParkingSpotCountersAdapter.class })
     IPrimitive<Type> type();
 
     @ToString(index = 0)
---
+++ vista-shared/vista-server-common/src/main/java/com/propertyvista/server/adapters/ParkingSpotCountersAdapterImpl.java
@@ -0,0 +1,86 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Mar 28, 2012
+ * @author yuriyl
+ * @version $Id$
+ */
+package com.propertyvista.server.adapters;
+
+import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.shared.meta.MemberMeta;
+
+import com.propertyvista.domain.property.asset.Parking;
+import com.propertyvista.domain.property.asset.ParkingSpot;
+import com.propertyvista.domain.property.asset.ParkingSpot.Type;
+import com.propertyvista.shared.adapters.ParkingSpotCountersAdapter;
+
+public class ParkingSpotCountersAdapterImpl implements ParkingSpotCountersAdapter {
+
+    @Override
+    public boolean allowModifications(ParkingSpot entity, MemberMeta meta, Object valueOrig, Object valueNew) {
+        // comparing enums so != is OK
+        Parking parking = Persistence.service().retrieve(Parking.class, entity.belongsTo().getPrimaryKey());
+        if (valueNew != null && valueOrig != valueNew) {
+            if (valueOrig != null) {
+                ParkingSpot.Type typeOrig = (Type) valueOrig;
+                int i = 0;
+                switch (typeOrig) {
+                case disabled:
+                    i = parking.disabledSpaces().getValue();
+                    parking.disabledSpaces().setValue(i - 1);
+                    break;
+                case wide:
+                    i = parking.wideSpaces().getValue();
+                    parking.wideSpaces().setValue(i - 1);
+                    break;
+                case narrow:
+                    i = parking.narrowSpaces().getValue();
+                    parking.narrowSpaces().setValue(i - 1);
+                    break;
+                case regular:
+                    i = parking.regularSpaces().getValue();
+                    parking.regularSpaces().setValue(i - 1);
+                    break;
+                }
+            }
+
+            if (valueNew != null) {
+                ParkingSpot.Type typeNew = (Type) valueNew;
+                int i = 0;
+                switch (typeNew) {
+                case disabled:
+                    i = parking.disabledSpaces().getValue();
+                    parking.disabledSpaces().setValue(i + 1);
+                    break;
+                case wide:
+                    i = parking.wideSpaces().getValue();
+                    parking.wideSpaces().setValue(i + 1);
+                    break;
+                case narrow:
+                    i = parking.narrowSpaces().getValue();
+                    parking.narrowSpaces().setValue(i + 1);
+                    break;
+                case regular:
+                    i = parking.regularSpaces().getValue();
+                    parking.regularSpaces().setValue(i + 1);
+                    break;
+                }
+                if (valueOrig == null) {
+                    i = parking.totalSpaces().getValue();
+                    parking.totalSpaces().setValue(i + 1);
+                }
+            }
+//        } else if (valueOrig == null) {
+
+        }
+        Persistence.service().persist(parking);
+        return true;
+    }
+}
