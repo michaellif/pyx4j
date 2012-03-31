a832b500409c8deb1aa95795fcad03121fcb385a
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-03-31 04:41:27 +0000

    VISTA-1264 fixed npe

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/login/AbstractLoginActivty.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/login/AbstractLoginActivty.java
@@ -78,7 +78,7 @@ public abstract class AbstractLoginActivty extends AbstractActivity implements L
             @Override
             public void onSuccess(Boolean result) {
                 if (HTML5Storage.isSupported()) {
-                    if (request.rememberID().getValue()) {
+                    if (request.rememberID().isBooleanTrue()) {
                         HTML5Storage.getLocalStorage().setItem(AbstractLoginActivty.HTML5_KEY, request.email().getValue());
                     } else {
                         HTML5Storage.getLocalStorage().removeItem(AbstractLoginActivty.HTML5_KEY);
