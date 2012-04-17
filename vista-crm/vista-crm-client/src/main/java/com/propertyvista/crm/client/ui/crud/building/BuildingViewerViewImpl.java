4b3dcc993ebb3ba915125def483b7316550e4c4b
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-04-17 20:29:58 +0000

    VISTA-1403 - dialog box implementation

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/BuildingViewerViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/BuildingViewerViewImpl.java
@@ -13,14 +13,32 @@
  */
 package com.propertyvista.crm.client.ui.crud.building;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumSet;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
 
+import com.pyx4j.commons.LogicalDate;
+import com.pyx4j.entity.annotations.validator.NotNull;
+import com.pyx4j.entity.shared.EntityFactory;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.forms.client.ui.CComboBox;
+import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.crud.lister.IListerView;
 import com.pyx4j.site.client.ui.crud.lister.ListerInternalViewImplBase;
 import com.pyx4j.widgets.client.Button;
+import com.pyx4j.widgets.client.dialog.OkCancelDialog;
 
+import com.propertyvista.common.client.ui.components.c.CEntityDecoratableEditor;
 import com.propertyvista.crm.client.ui.crud.CrmViewerViewImplBase;
 import com.propertyvista.crm.client.ui.crud.building.catalog.ConcessionLister;
 import com.propertyvista.crm.client.ui.crud.building.catalog.FeatureLister;
@@ -39,6 +57,7 @@ import com.propertyvista.domain.financial.billing.BillingRun;
 import com.propertyvista.domain.financial.offering.Concession;
 import com.propertyvista.domain.financial.offering.Feature;
 import com.propertyvista.domain.financial.offering.Service;
+import com.propertyvista.domain.tenant.lease.Lease.PaymentFrequency;
 import com.propertyvista.dto.AptUnitDTO;
 import com.propertyvista.dto.BoilerDTO;
 import com.propertyvista.dto.BuildingDTO;
@@ -105,7 +124,15 @@ public class BuildingViewerViewImpl extends CrmViewerViewImplBase<BuildingDTO> i
         runBillAction = new Button(i18n.tr("Run Bill"), new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                ((BuildingViewerView.Presenter) presenter).runBill();
+                new RunBillDataBox() {
+                    @Override
+                    public boolean onClickOk() {
+                        // TODO: pass data from Box into the service call: 
+                        ((BuildingViewerView.Presenter) presenter).runBill(/* getValue() */);
+                        return true;
+                    }
+                }.show();
+
             }
         });
         if (!VistaTODO.removedForProduction) {
@@ -174,4 +201,72 @@ public class BuildingViewerViewImpl extends CrmViewerViewImplBase<BuildingDTO> i
     }
 
     // Internals:
+
+    public interface RunBillData extends IEntity {
+
+        @NotNull
+        IPrimitive<PaymentFrequency> paymentFrequency();
+
+        @NotNull
+        IPrimitive<LogicalDate> billingPeriodStartDate();
+    }
+
+    private abstract class RunBillDataBox extends OkCancelDialog {
+
+        private CEntityDecoratableEditor<RunBillData> content;
+
+        public RunBillDataBox() {
+            super(i18n.tr("Please select"));
+
+            RunBillData data = EntityFactory.create(RunBillData.class);
+            data.paymentFrequency().setValue(PaymentFrequency.Monthly);
+            data.billingPeriodStartDate().setValue(new LogicalDate());
+
+            setBody(createBody(data));
+        }
+
+        protected Widget createBody(RunBillData data) {
+            getOkButton().setEnabled(true);
+
+            content = new CEntityDecoratableEditor<RunBillData>(RunBillData.class) {
+
+                private final CComboBox<PaymentFrequency> paymentFrequency = new CComboBox<PaymentFrequency>();
+                {
+                    Collection<PaymentFrequency> opt = new ArrayList<PaymentFrequency>(EnumSet.allOf(PaymentFrequency.class));
+                    opt.removeAll(Arrays.asList(PaymentFrequency.SemiAnnyally, PaymentFrequency.Annually));
+                    paymentFrequency.setOptions(opt);
+                    paymentFrequency.addValueChangeHandler(new ValueChangeHandler<PaymentFrequency>() {
+                        @Override
+                        public void onValueChange(ValueChangeEvent<PaymentFrequency> event) {
+                            // TODO Auto-generated method stub
+                        }
+                    });
+                }
+
+                @Override
+                public IsWidget createContent() {
+                    FormFlexPanel main = new FormFlexPanel();
+
+                    main.setWidget(1, 0, new DecoratorBuilder(inject(proto().paymentFrequency(), paymentFrequency), 10).build());
+                    main.setWidget(2, 0, new DecoratorBuilder(inject(proto().billingPeriodStartDate()), 10).build());
+
+                    return main;
+                }
+
+                @Override
+                public void addValidations() {
+                    super.addValidations();
+
+                }
+            };
+
+            content.initContent();
+            content.populate(data);
+            return content.asWidget();
+        }
+
+        public RunBillData getValue() {
+            return content.getValue();
+        }
+    }
 }
