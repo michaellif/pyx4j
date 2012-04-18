59f366128317248a189a6f54f1b1139a41437c52
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-04-18 18:07:25 +0000

    VISTA-1373 create PersonScreening

--- vista-dev/vista-dev-generator/src/main/java/com/propertvista/generator/ScreeningGenerator.java
+++ vista-dev/vista-dev-generator/src/main/java/com/propertvista/generator/ScreeningGenerator.java
@@ -14,7 +14,10 @@
 package com.propertvista.generator;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import com.propertvista.generator.util.CommonsGenerator;
 import com.propertvista.generator.util.RandomUtil;
@@ -27,6 +30,12 @@ import com.pyx4j.gwt.server.DateUtils;
 import com.propertyvista.domain.LegalQuestions;
 import com.propertyvista.domain.PriorAddress;
 import com.propertyvista.domain.tenant.PersonScreening;
+import com.propertyvista.domain.tenant.income.IncomeInfoEmployer;
+import com.propertyvista.domain.tenant.income.IncomeInfoSelfEmployed;
+import com.propertyvista.domain.tenant.income.IncomeSource;
+import com.propertyvista.domain.tenant.income.PersonalAsset;
+import com.propertyvista.domain.tenant.income.PersonalAsset.AssetType;
+import com.propertyvista.domain.tenant.income.PersonalIncome;
 
 public class ScreeningGenerator {
 
@@ -44,8 +53,11 @@ public class ScreeningGenerator {
         screening.legalQuestions().set(createLegalQuestions());
 
         // Incomes
+        screening.incomes().addAll(createIncomes());
 
         // Assets
+        int minAssets = (screening.incomes().size() == 0) ? 1 : 0;
+        screening.assets().addAll(createAssets(minAssets));
 
         return screening;
     }
@@ -96,5 +108,80 @@ public class ScreeningGenerator {
         lq.filedBankruptcy().setValue(RandomUtil.randomBoolean());
 
         return lq;
+    }
+
+    private Collection<PersonalIncome> createIncomes() {
+        List<PersonalIncome> incomes = new ArrayList<PersonalIncome>();
+
+        {
+            PersonalIncome income = EntityFactory.create(PersonalIncome.class);
+            income.incomeSource().setValue(IncomeSource.fulltime);
+            income.employer().set(createEmployer());
+            // TODO Add documents
+            incomes.add(income);
+        }
+        for (int i = 0; i < RandomUtil.randomInt(2); i++) {
+            PersonalIncome income = EntityFactory.create(PersonalIncome.class);
+            income.incomeSource().setValue(IncomeSource.selfemployed);
+            income.selfEmployed().set(createSelfEmployed());
+            // TODO Add documents
+            incomes.add(income);
+        }
+
+        return incomes;
+    }
+
+    public static IncomeInfoEmployer createEmployer() {
+        IncomeInfoEmployer employer = EntityFactory.create(IncomeInfoEmployer.class);
+
+        employer.address().set(CommonsGenerator.createAddress());
+
+        employer.name().setValue(RandomUtil.random(PreloadData.EMPLOYER_NAMES));
+        employer.supervisorName().setValue("Mr. " + DataGenerator.randomLastName());
+        employer.supervisorPhone().setValue(RandomUtil.randomPhone());
+        employer.monthlyAmount().setValue(new BigDecimal(1000 + RandomUtil.randomInt(4000)));
+        employer.position().setValue(RandomUtil.random(PreloadData.OCCUPATIONS));
+
+        int startYear = 1990 + RandomUtil.randomInt(20);
+        int endYear = startYear + 1 + RandomUtil.randomInt(8);
+
+        employer.starts().setValue(new LogicalDate(DateUtils.createDate(startYear, RandomUtil.randomInt(12), RandomUtil.randomInt(28)).getTime()));
+        employer.ends().setValue(new LogicalDate(DateUtils.createDate(endYear, RandomUtil.randomInt(12), RandomUtil.randomInt(28)).getTime()));
+
+        return employer;
+    }
+
+    public static IncomeInfoSelfEmployed createSelfEmployed() {
+        IncomeInfoSelfEmployed selfEmpl = EntityFactory.create(IncomeInfoSelfEmployed.class);
+
+        selfEmpl.address().set(CommonsGenerator.createAddress());
+
+        selfEmpl.name().setValue(RandomUtil.random(PreloadData.EMPLOYER_NAMES));
+        selfEmpl.supervisorName().setValue("Mr. " + DataGenerator.randomLastName());
+        selfEmpl.supervisorPhone().setValue(RandomUtil.randomPhone());
+        selfEmpl.monthlyAmount().setValue(new BigDecimal(1000 + RandomUtil.randomInt(4000)));
+        selfEmpl.position().setValue(RandomUtil.random(PreloadData.OCCUPATIONS));
+
+        int startYear = 1990 + RandomUtil.randomInt(20);
+        int endYear = startYear + 1 + RandomUtil.randomInt(8);
+
+        selfEmpl.starts().setValue(new LogicalDate(DateUtils.createDate(startYear, RandomUtil.randomInt(12), RandomUtil.randomInt(28)).getTime()));
+        selfEmpl.ends().setValue(new LogicalDate(DateUtils.createDate(endYear, RandomUtil.randomInt(12), RandomUtil.randomInt(28)).getTime()));
+
+        return selfEmpl;
+    }
+
+    private Collection<PersonalAsset> createAssets(int minAssets) {
+        List<PersonalAsset> assets = new ArrayList<PersonalAsset>();
+        for (int i = 0; i < minAssets + RandomUtil.randomInt(3); i++) {
+            PersonalAsset asset = EntityFactory.create(PersonalAsset.class);
+
+            asset.assetType().setValue(RandomUtil.random(AssetType.values()));
+            asset.percent().setValue((double) RandomUtil.randomInt(100));
+            asset.assetValue().setValue(BigDecimal.valueOf(500 + RandomUtil.randomDouble(500)));
+
+            assets.add(asset);
+        }
+        return assets;
     }
 }
