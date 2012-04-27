b170e7a622e04671fc865d7bb2e6e104fcfc2f7b
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-04-27 22:08:56 +0000

    VISTA-1379 Correction to PG DB reset

--- vista-server/src/main/java/com/propertyvista/server/ci/DBResetServlet.java
+++ vista-server/src/main/java/com/propertyvista/server/ci/DBResetServlet.java
@@ -171,21 +171,19 @@ public class DBResetServlet extends HttpServlet {
                             if (EnumSet.of(ResetType.prodReset, ResetType.all, ResetType.allMini, ResetType.allWithMockup, ResetType.clear).contains(type)) {
                                 SchedulerHelper.shutdown();
                                 RDBUtils.resetDatabase();
-                                SchedulerHelper.dbReset();
-                                Thread.sleep(150);
-                                SchedulerHelper.init();
-                                if (((EntityPersistenceServiceRDB) Persistence.service()).getMultitenancyType() != MultitenancyType.SeparateSchemas) {
-                                    RDBUtils.initAllEntityTables();
-                                }
-                                CacheService.resetAll();
-
+                                // Initialize Admin
                                 NamespaceManager.setNamespace(Pmc.adminNamespace);
-                                RDBUtils.ensureNamespace();
-                                // TODO Hack for non implemented SeparateSchemas DML 
-                                {
-                                    ((EntityPersistenceServiceRDB) Persistence.service()).resetMapping();
-                                }
                                 try {
+                                    RDBUtils.ensureNamespace();
+                                    SchedulerHelper.dbReset();
+                                    Thread.sleep(150);
+                                    ((EntityPersistenceServiceRDB) Persistence.service()).resetMapping();
+                                    SchedulerHelper.init();
+                                    if (((EntityPersistenceServiceRDB) Persistence.service()).getMultitenancyType() == MultitenancyType.SeparateSchemas) {
+                                        RDBUtils.initNameSpaceSpecificEntityTables();
+                                    }
+                                    CacheService.resetAll();
+
                                     new VistaAminDataPreloaders().preloadAll();
                                     Persistence.service().commit();
                                 } finally {
