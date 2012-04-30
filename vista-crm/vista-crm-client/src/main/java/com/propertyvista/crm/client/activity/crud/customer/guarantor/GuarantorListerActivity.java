dd29191bcb9e92a03e729324c6df4fb701177720
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-04-30 03:14:42 +0000

    VISTA-1447 show only guarantors in final version of the lease

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/customer/guarantor/GuarantorListerActivity.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/customer/guarantor/GuarantorListerActivity.java
@@ -17,6 +17,8 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.place.shared.Place;
 
 import com.pyx4j.entity.rpc.AbstractCrudService;
+import com.pyx4j.entity.shared.EntityFactory;
+import com.pyx4j.entity.shared.criterion.PropertyCriterion;
 import com.pyx4j.site.client.activity.crud.ListerActivityBase;
 
 import com.propertyvista.crm.client.ui.crud.customer.guarantor.GuarantorListerView;
@@ -30,6 +32,13 @@ public class GuarantorListerActivity extends ListerActivityBase<GuarantorDTO> {
     public GuarantorListerActivity(Place place) {
         super(place, CustomerViewFactory.instance(GuarantorListerView.class), (AbstractCrudService<GuarantorDTO>) GWT.create(GuarantorCrudService.class),
                 GuarantorDTO.class);
+
+        // filter out just current tenants:
+        GuarantorDTO proto = EntityFactory.getEntityPrototype(GuarantorDTO.class);
+        //TODO add Past Guarantors addPreDefinedFilter(PropertyCriterion.in(proto.leaseV().status(), Lease.Status.current()));
+        // and finalize lease only:
+        addPreDefinedFilter(PropertyCriterion.isNotNull(proto.leaseV().fromDate()));
+        addPreDefinedFilter(PropertyCriterion.isNull(proto.leaseV().toDate()));
     }
 
     @Override
