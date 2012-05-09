b2c1f9179793a2efac8e0b1774b567ce6e57d9f2
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-05-09 16:19:59 +0000

    VISTA-1464 - Payment UI related: payment set disable for draft leases

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/lease/LeaseForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/lease/LeaseForm.java
@@ -16,6 +16,7 @@ package com.propertyvista.crm.client.ui.crud.lease;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 
@@ -25,6 +26,8 @@ import com.propertyvista.dto.LeaseDTO;
 import com.propertyvista.misc.VistaTODO;
 
 public class LeaseForm extends LeaseFormBase<LeaseDTO> {
+
+    private Widget paymentsTab;
 
     public LeaseForm() {
         this(false);
@@ -44,7 +47,7 @@ public class LeaseForm extends LeaseFormBase<LeaseDTO> {
             tabPanel.setLastTabDisabled(isEditable());
             tabPanel.add(isEditable() ? new HTML() : ((LeaseViewerView) getParentView()).getBillListerView().asWidget(), i18n.tr("Bills"));
             tabPanel.setLastTabDisabled(isEditable());
-            tabPanel.add(isEditable() ? new HTML() : ((LeaseViewerView) getParentView()).getPaymentListerView().asWidget(), i18n.tr("Payments"));
+            tabPanel.add(paymentsTab = isEditable() ? new HTML() : ((LeaseViewerView) getParentView()).getPaymentListerView().asWidget(), i18n.tr("Payments"));
             tabPanel.setLastTabDisabled(isEditable());
             tabPanel.add(isEditable() ? new HTML() : createFinancialTransactionHistoryTab(), i18n.tr("Financial Summary"));
             tabPanel.setLastTabDisabled(isEditable());
@@ -52,6 +55,13 @@ public class LeaseForm extends LeaseFormBase<LeaseDTO> {
 
         tabPanel.setSize("100%", "100%");
         return tabPanel;
+    }
+
+    @Override
+    protected void onPopulate() {
+        super.onPopulate();
+
+        tabPanel.setTabDisabled(paymentsTab, getValue().version().status().getValue().isDraft());
     }
 
     private IsWidget createFinancialTransactionHistoryTab() {
