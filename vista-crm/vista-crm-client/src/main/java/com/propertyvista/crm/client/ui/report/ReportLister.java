0e164dd84dd24aebf04714e9a899833b4bac7694
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-05-15 15:19:47 +0000

    VISTA-896 - problem fix: confirmation box has been added.

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/report/ReportLister.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/report/ReportLister.java
@@ -20,6 +20,8 @@ import com.pyx4j.entity.client.ui.datatable.MemberColumnDescriptor;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.crud.lister.ListerBase;
 import com.pyx4j.widgets.client.Button;
+import com.pyx4j.widgets.client.dialog.ConfirmDecline;
+import com.pyx4j.widgets.client.dialog.MessageDialog;
 
 import com.propertyvista.domain.dashboard.DashboardMetadata;
 
@@ -35,10 +37,19 @@ public class ReportLister extends ListerBase<DashboardMetadata> {
         addActionItem(new Button(i18n.tr("Delete Checked"), new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                for (DashboardMetadata item : getDataTablePanel().getDataTable().getCheckedItems()) {
-                    getPresenter().delete(item.getPrimaryKey());
+                MessageDialog.confirm(i18n.tr("Confirm"), i18n.tr("Do you really want to delete checked items?"), new ConfirmDecline() {
+                    @Override
+                    public void onConfirmed() {
+                        for (DashboardMetadata item : getDataTablePanel().getDataTable().getCheckedItems()) {
+                            getPresenter().delete(item.getPrimaryKey());
 
-                }
+                        }
+                    }
+
+                    @Override
+                    public void onDeclined() {
+                    }
+                });
             }
         }));
 
