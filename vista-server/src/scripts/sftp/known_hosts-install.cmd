a272f6dc6bd6b0056016d9fbcb6a5cdf1e86963f
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-05-24 17:01:36 +0000

    VISTA-1121 Testing Caledon eCheck pad files interface locally

---
+++ vista-server/src/scripts/sftp/known_hosts-install.cmd
@@ -0,0 +1,9 @@
+@echo off
+rem $Id$
+mkdir "%USERPROFILE%\.ssh"
+copy "%~dp0known_hosts" "%USERPROFILE%\.ssh\known_hosts"
+if errorlevel 1 (
+    echo Error calling copy
+    pause
+    exit /b 1
+)
