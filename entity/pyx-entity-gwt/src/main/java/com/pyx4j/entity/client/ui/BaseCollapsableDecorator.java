23de7f202beb55142b959a51c0085d10c75d9318
Michael Lifshits <michaellif@propertyvista.com>
2012-05-30 16:32:08 +0000

    fixing collapse image width

--- entity/pyx-entity-gwt/src/main/java/com/pyx4j/entity/client/ui/BaseCollapsableDecorator.java
+++ entity/pyx-entity-gwt/src/main/java/com/pyx4j/entity/client/ui/BaseCollapsableDecorator.java
@@ -1,3 +1,23 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on May 26, 2010
+ * @author stanp
+ * @version $Id$
+ */
 package com.pyx4j.entity.client.ui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -32,6 +52,8 @@ public class BaseCollapsableDecorator<E extends IEntity> extends SimplePanel imp
             }
         });
         mainPanel.add(collapseImage);
+        reset();
+        mainPanel.setCellWidth(collapseImage, collapseImage.getWidth() + "px");
         contentHolder = new SimplePanel();
 //        contentHolder.setStyleName(EntityFolderBoxItem.name());
         mainPanel.add(contentHolder);
