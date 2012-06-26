716bc9d33ea11e71f683fe120a46e322ee7520ff
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-06-26 17:19:00 +0000

    VISTA-1538 - bugfix

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/dto/bill/BillForm.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/dto/bill/BillForm.java
@@ -31,6 +31,8 @@ public class BillForm extends CEntityDecoratableForm<BillDTO> {
 
     private static final I18n i18n = I18n.get(BillForm.class);
 
+    private final FormFlexPanel billPanel = new FormFlexPanel();
+
     private final boolean justPreviewBill;
 
     public BillForm() {
@@ -49,69 +51,65 @@ public class BillForm extends CEntityDecoratableForm<BillDTO> {
     public IsWidget createContent() {
 
         // form top panel:
-        FormFlexPanel top = new FormFlexPanel();
+        FormFlexPanel infoPanel = new FormFlexPanel();
         int row = -1;
 
         if (!justPreviewBill) {
-            top.setH1(++row, 0, 2, i18n.tr("Info"));
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingAccount().lease().unit())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().building())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingCycleStartDate())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingCycleEndDate())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingType().paymentFrequency())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().executionTargetDate())).build());
+            infoPanel.setH1(++row, 0, 2, i18n.tr("Info"));
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingAccount().lease().unit())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().building())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingCycleStartDate())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingCycleEndDate())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().billingType().paymentFrequency())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billingCycle().executionTargetDate())).build());
 
-            top.setH1(++row, 0, 2, i18n.tr("Status"));
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billSequenceNumber())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billType())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billStatus())).build());
-            top.setWidget(++row, 0, new DecoratorBuilder(inject(proto().rejectReason())).build());
+            infoPanel.setH1(++row, 0, 2, i18n.tr("Status"));
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billSequenceNumber())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billType())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().billStatus())).build());
+            infoPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().rejectReason())).build());
         }
 
-        // form bottom panel:
-        FormFlexPanel bottom = new FormFlexPanel();
+        // form bills panel:
         row = -1;
-
         if (!justPreviewBill) {
-            bottom.setH1(++row, 0, 2, i18n.tr("Last Bill"));
-            bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().balanceForwardAmount())).build());
-            bottom.setWidget(++row, 0, inject(proto().depositRefundLineItems(), new LineItemCollapsibleViewer()));
-            bottom.setWidget(++row, 0, inject(proto().immediateAccountAdjustmentLineItems(), new LineItemCollapsibleViewer()));
-            bottom.setWidget(++row, 0, inject(proto().nsfChargeLineItems(), new LineItemCollapsibleViewer()));
-            bottom.setWidget(++row, 0, inject(proto().withdrawalLineItems(), new LineItemCollapsibleViewer()));
-            bottom.setWidget(++row, 0, inject(proto().rejectedPaymentLineItems(), new LineItemCollapsibleViewer()));
-            bottom.setWidget(++row, 0, inject(proto().paymentLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setH1(++row, 0, 2, i18n.tr("Last Bill"));
+            billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().balanceForwardAmount())).build());
+            billPanel.setWidget(++row, 0, inject(proto().depositRefundLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setWidget(++row, 0, inject(proto().immediateAccountAdjustmentLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setWidget(++row, 0, inject(proto().nsfChargeLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setWidget(++row, 0, inject(proto().withdrawalLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setWidget(++row, 0, inject(proto().rejectedPaymentLineItems(), new LineItemCollapsibleViewer()));
+            billPanel.setWidget(++row, 0, inject(proto().paymentLineItems(), new LineItemCollapsibleViewer()));
 
-            bottom.setHR(++row, 0, 2);
-            bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().pastDueAmount())).build());
+            billPanel.setHR(++row, 0, 2);
+            billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().pastDueAmount())).build());
         }
 
-        bottom.setH1(++row, 0, 2, i18n.tr("Current Bill"));
-        bottom.setHeight("50px");
-        bottom.setWidget(++row, 0, inject(proto().serviceChargeLineItems(), new LineItemCollapsibleViewer()));
-        bottom.setWidget(++row, 0, inject(proto().recurringFeatureChargeLineItems(), new LineItemCollapsibleViewer()));
-        bottom.setWidget(++row, 0, inject(proto().onetimeFeatureChargeLineItems(), new LineItemCollapsibleViewer()));
-        bottom.setWidget(++row, 0, inject(proto().pendingAccountAdjustmentLineItems(), new LineItemCollapsibleViewer()));
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().latePaymentFees())).build());
-        bottom.setWidget(++row, 0, inject(proto().depositLineItems(), new LineItemCollapsibleViewer()));
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().productCreditAmount())).build());
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().carryForwardCredit())).build());
+        billPanel.setH1(++row, 0, 2, i18n.tr("Current Bill"));
+        billPanel.setWidget(++row, 0, inject(proto().serviceChargeLineItems(), new LineItemCollapsibleViewer()));
+        billPanel.setWidget(++row, 0, inject(proto().recurringFeatureChargeLineItems(), new LineItemCollapsibleViewer()));
+        billPanel.setWidget(++row, 0, inject(proto().onetimeFeatureChargeLineItems(), new LineItemCollapsibleViewer()));
+        billPanel.setWidget(++row, 0, inject(proto().pendingAccountAdjustmentLineItems(), new LineItemCollapsibleViewer()));
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().latePaymentFees())).build());
+        billPanel.setWidget(++row, 0, inject(proto().depositLineItems(), new LineItemCollapsibleViewer()));
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().productCreditAmount())).build());
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().carryForwardCredit())).build());
 
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().currentAmount())).build());
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().taxes())).build());
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().currentAmount())).build());
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().taxes())).build());
 
         // Dues:
-        bottom.setHR(++row, 0, 2);
-        bottom.setWidget(++row, 0, new DecoratorBuilder(inject(proto().totalDueAmount())).build());
+        billPanel.setHR(++row, 0, 2);
+        billPanel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().totalDueAmount())).build());
 
-        top.getColumnFormatter().setWidth(1, "200px");
-        bottom.getColumnFormatter().setWidth(0, "50%");
-        bottom.getColumnFormatter().setWidth(1, "50%");
+        billPanel.getColumnFormatter().setWidth(0, "50%");
+        billPanel.getColumnFormatter().setWidth(1, "50%");
 
         // form main panel:
         FormFlexPanel main = new FormFlexPanel();
-        main.setWidget(0, 0, top);
-        main.setWidget(1, 0, bottom);
+        main.setWidget(0, 0, infoPanel);
+        main.setWidget(1, 0, billPanel);
 
         return main;
     }
@@ -120,14 +118,16 @@ public class BillForm extends CEntityDecoratableForm<BillDTO> {
     protected void onPopulate() {
         super.onPopulate();
 
+        billPanel.setVisible(getValue().billStatus().getValue() != BillStatus.Failed);
+
         if (!justPreviewBill) {
             get(proto().rejectReason()).setVisible(getValue().billStatus().getValue() == BillStatus.Rejected);
-            if (getValue().productCreditAmount().getValue().compareTo(BigDecimal.ZERO) == 0)
-                get(proto().productCreditAmount()).setVisible(false);
-            if (getValue().latePaymentFees().getValue().compareTo(BigDecimal.ZERO) == 0)
-                get(proto().latePaymentFees()).setVisible(false);
-            if (getValue().carryForwardCredit().getValue().compareTo(BigDecimal.ZERO) == 0)
-                get(proto().carryForwardCredit()).setVisible(false);
+            get(proto().productCreditAmount()).setVisible(
+                    getValue().productCreditAmount().isNull() || getValue().productCreditAmount().getValue().compareTo(BigDecimal.ZERO) == 0);
+            get(proto().latePaymentFees()).setVisible(
+                    getValue().latePaymentFees().isNull() || getValue().latePaymentFees().getValue().compareTo(BigDecimal.ZERO) == 0);
+            get(proto().carryForwardCredit()).setVisible(
+                    getValue().carryForwardCredit().isNull() || getValue().carryForwardCredit().getValue().compareTo(BigDecimal.ZERO) == 0);
 
             hideLines(getValue().depositRefundLineItems(), proto().depositRefundLineItems());
             hideLines(getValue().immediateAccountAdjustmentLineItems(), proto().immediateAccountAdjustmentLineItems());
@@ -142,7 +142,6 @@ public class BillForm extends CEntityDecoratableForm<BillDTO> {
         hideLines(getValue().onetimeFeatureChargeLineItems(), proto().onetimeFeatureChargeLineItems());
         hideLines(getValue().pendingAccountAdjustmentLineItems(), proto().pendingAccountAdjustmentLineItems());
         hideLines(getValue().depositLineItems(), proto().depositLineItems());
-
     }
 
     protected class DecoratorBuilder extends WidgetDecorator.Builder { //builder specifically for this form (as it uses mixed formats)
