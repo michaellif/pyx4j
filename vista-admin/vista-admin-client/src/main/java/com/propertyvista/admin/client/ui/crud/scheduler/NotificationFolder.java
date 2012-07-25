e878cbcd7182e264712749cecca3c43898ff26b0
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-07-25 20:29:58 +0000

    VISTA-1478 fixed notification folder

--- vista-admin/vista-admin-client/src/main/java/com/propertyvista/admin/client/ui/crud/scheduler/NotificationFolder.java
+++ vista-admin/vista-admin-client/src/main/java/com/propertyvista/admin/client/ui/crud/scheduler/NotificationFolder.java
@@ -8,14 +8,18 @@
  */
 package com.propertyvista.admin.client.ui.crud.scheduler;
 
+import com.google.gwt.user.client.ui.IsWidget;
+
 import com.pyx4j.entity.client.ui.folder.BoxFolderItemDecorator;
 import com.pyx4j.entity.client.ui.folder.IFolderItemDecorator;
 import com.pyx4j.entity.shared.IObject;
 import com.pyx4j.forms.client.ui.CComponent;
+import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 
 import com.propertyvista.admin.domain.scheduler.TriggerNotification;
 import com.propertyvista.common.client.resources.VistaImages;
+import com.propertyvista.common.client.ui.components.c.CEntityDecoratableForm;
 import com.propertyvista.common.client.ui.components.folders.VistaBoxFolder;
 
 public class NotificationFolder extends VistaBoxFolder<TriggerNotification> {
@@ -32,6 +36,24 @@ public class NotificationFolder extends VistaBoxFolder<TriggerNotification> {
             return new NotificationEditor();
         } else {
             return super.create(member);
+        }
+    }
+
+    public class NotificationEditor extends CEntityDecoratableForm<TriggerNotification> {
+
+        public NotificationEditor() {
+            super(TriggerNotification.class);
+        }
+
+        @Override
+        public IsWidget createContent() {
+            FormFlexPanel content = new FormFlexPanel(i18n.tr("General"));
+            int row = -1;
+
+            content.setWidget(++row, 0, new DecoratorBuilder(inject(proto().event()), 10).build());
+            content.setWidget(++row, 0, new DecoratorBuilder(inject(proto().user()), 20).build());
+
+            return content;
         }
     }
 
