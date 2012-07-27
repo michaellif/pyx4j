80468741063fa7e640fb46493d2ce1f5158cf115
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-07-27 19:57:00 +0000

    VISTA-1478 enabled dev mode for demo for pmsite

--- vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/panels/SignInPanel.java
+++ vista-pmsite/vista-pmsite-server/src/main/java/com/propertyvista/pmsite/server/panels/SignInPanel.java
@@ -63,6 +63,7 @@ import com.propertyvista.pmsite.server.model.WicketUtils.JSActionLink;
 import com.propertyvista.pmsite.server.model.WicketUtils.PageLink;
 import com.propertyvista.pmsite.server.pages.PwdResetPage;
 import com.propertyvista.portal.rpc.portal.services.PortalAuthenticationService;
+import com.propertyvista.shared.config.VistaDemo;
 
 public class SignInPanel extends Panel {
 
@@ -211,7 +212,7 @@ public class SignInPanel extends Panel {
             add(new PageLink("pwdreset", PwdResetPage.class).setText("Forgot Password"));
             add(new Button("signIn").add(AttributeModifier.replace("value", i18n.tr("Sign In"))));
 
-            if (ApplicationMode.isDevelopment()) {
+            if (ApplicationMode.isDevelopment() || VistaDemo.isDemo()) {
                 Label devNotes = new Label("signInHint", "This application is running in <b>" + DemoData.applicationModeName()
                         + "</b> mode.<br/><i>Username and password are both<br/>");
                 add(devNotes.setEscapeModelStrings(false));
