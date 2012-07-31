fe54450c4ca26005a858a3d8e9f386c5deeac511
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-07-31 16:11:21 +0000

    VISTA-1764 reporting framework

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/CrmReportsViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/CrmReportsViewImpl.java
@@ -13,47 +13,64 @@
  */
 package com.propertyvista.crm.client.ui.reports;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gwt.user.client.ui.Widget;
 
-import com.pyx4j.entity.client.CEntityForm;
 import com.pyx4j.forms.client.ui.decorators.WidgetDecorator;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.site.client.ui.reports.AbstractReportsView;
 import com.pyx4j.site.client.ui.reports.AdvancedReportSettingsForm;
+import com.pyx4j.site.client.ui.reports.IReportSettingsForm;
+import com.pyx4j.site.client.ui.reports.ReportFactory;
 import com.pyx4j.site.client.ui.reports.ReportSettings;
 
 public class CrmReportsViewImpl extends AbstractReportsView implements CrmReportsView {
 
-    @Override
-    public CEntityForm<? extends ReportSettings> getReportSettingsForm(ReportSettings reportSettings) {
-        if (reportSettings == null) {
-            return null;
-        } else if (reportSettings.getInstanceValueClass().equals(MockupReportSettings.class)) {
-            return new AdvancedReportSettingsForm<MockupReportSettings>(MockupReportSettings.class) {
+    private static Map<Class<? extends ReportSettings>, ReportFactory> factoryMap;
 
-                @Override
-                public Widget createSimpleSettingsPanel() {
-                    FormFlexPanel simple = new FormFlexPanel();
-                    int row = -1;
-                    simple.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().valueX())).build());
-                    simple.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().valueY())).build());
-                    return simple;
-                }
+    static {
+        factoryMap = new HashMap<Class<? extends ReportSettings>, ReportFactory>();
 
-                @Override
-                public Widget createAdvancedSettingsPanel() {
-                    FormFlexPanel advanced = new FormFlexPanel();
-                    int row = -1;
-                    advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueX())).build());
-                    advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueY())).build());
-                    advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueZ())).build());
-                    return advanced;
-                }
+        factoryMap.put(MockupReportSettings.class, new ReportFactory() {
+            @Override
+            public IReportSettingsForm<? extends ReportSettings> getReportSettingsForm(ReportSettings reportSettings) {
 
-            };
-        } else {
-            return null;
-        }
+                AdvancedReportSettingsForm<MockupReportSettings> form = new AdvancedReportSettingsForm<MockupReportSettings>(MockupReportSettings.class) {
+                    @Override
+                    public Widget createSimpleSettingsPanel() {
+                        FormFlexPanel simple = new FormFlexPanel();
+                        int row = -1;
+                        simple.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().valueX())).build());
+                        simple.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().valueY())).build());
+                        return simple;
+                    }
+
+                    @Override
+                    public Widget createAdvancedSettingsPanel() {
+                        FormFlexPanel advanced = new FormFlexPanel();
+                        int row = -1;
+                        advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueX())).build());
+                        advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueY())).build());
+                        advanced.setWidget(++row, 0, new WidgetDecorator.Builder(inject(proto().advancedValueZ())).build());
+                        return advanced;
+                    }
+                };
+                form.initContent();
+                return form;
+            }
+
+            @Override
+            public Widget getReport() {
+                return new FormFlexPanel();
+            }
+        });
+
+    }
+
+    public CrmReportsViewImpl() {
+        super(factoryMap);
     }
 
 }
