55c999aad1a5fa975450abed964de04aefec6184
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-08-07 21:07:46 +0000

    VISTA-1478 added a check, dns name should starts with a letter (else we have a problem w/ schemas in posgresql).

--- vista-admin/vista-onboarding-interface/src/test/java/com/propertyvista/onboarding/example/model/CheckAvailabilityRequest.java
+++ vista-admin/vista-onboarding-interface/src/test/java/com/propertyvista/onboarding/example/model/CheckAvailabilityRequest.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -25,7 +25,7 @@ public class CheckAvailabilityRequest extends Request {
      */
     @XmlElement(required = true)
     @Size(max = 64)
-    @Pattern(regexp = "[A-Za-z0-9]+")
+    @Pattern(regexp = "[A-Za-z]{1}[A-Za-z0-9]+")
     public String dnsName;
 
 }
