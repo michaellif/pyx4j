8eb58a46eb657e5096aa7a7d049a70d9050957cc
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-08-13 16:51:37 +0000

    VISTA-1478 changed dns name check to accept capitals

--- vista-admin/vista-admin-server/src/main/java/com/propertyvista/admin/server/onboarding/PmcNameValidator.java
+++ vista-admin/vista-admin-server/src/main/java/com/propertyvista/admin/server/onboarding/PmcNameValidator.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -76,7 +76,7 @@ public class PmcNameValidator {
     }
 
     public static boolean isDnsNameValid(String value) {
-        return (value.length() < 63) && value.matches("[a-z][a-z0-9-]*");
+        return (value.length() < 63) && value.matches("[a-zA-Z][a-zA-Z0-9-]*");
     }
 
     public static boolean isDnsReserved(String dnsName) {
