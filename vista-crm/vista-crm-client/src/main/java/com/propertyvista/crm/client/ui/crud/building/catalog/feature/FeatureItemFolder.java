3cb8b5d8358332423d977ca64f2771fae8e23dc5
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2012-08-27 17:54:42 +0000

    Merged revision(s) 8268 from VISTA/branches/1.0.4: VISTA-1899 CRM : Building->Product Catalog - Move Element before Description ........

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/feature/FeatureItemFolder.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/feature/FeatureItemFolder.java
@@ -58,9 +58,9 @@ class FeatureItemFolder extends VistaTableFolder<ProductItem> {
         ArrayList<EntityFolderColumnDescriptor> columns = new ArrayList<EntityFolderColumnDescriptor>();
         columns.add(new EntityFolderColumnDescriptor(proto().type(), "20em"));
         columns.add(new EntityFolderColumnDescriptor(proto().price(), "8em"));
-        columns.add(new EntityFolderColumnDescriptor(proto().description(), "25em"));
-        columns.add(new EntityFolderColumnDescriptor(proto().element(), "15em"));
         columns.add(new EntityFolderColumnDescriptor(proto().isDefault(), "5em"));
+        columns.add(new EntityFolderColumnDescriptor(proto().element(), "15em"));
+        columns.add(new EntityFolderColumnDescriptor(proto().description(), "25em"));
         return columns;
     }
 
--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/service/ServiceItemFolder.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/service/ServiceItemFolder.java
@@ -53,8 +53,8 @@ class ServiceItemFolder extends VistaTableFolder<ProductItem> {
         ArrayList<EntityFolderColumnDescriptor> columns = new ArrayList<EntityFolderColumnDescriptor>();
         columns.add(new EntityFolderColumnDescriptor(proto().type(), "20em"));
         columns.add(new EntityFolderColumnDescriptor(proto().price(), "8em"));
-        columns.add(new EntityFolderColumnDescriptor(proto().description(), "25em"));
         columns.add(new EntityFolderColumnDescriptor(proto().element(), "15em"));
+        columns.add(new EntityFolderColumnDescriptor(proto().description(), "25em"));
         return columns;
     }
 
