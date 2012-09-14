12290b2eb683e3ab02c54ed92fd26565e2dc0f9c
Michael Lifshits <michaellif@propertyvista.com>
2012-09-14 21:40:49 +0000

    VISTA-250 RadioGroup styles

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/theme/NewPaymentMethodEditorTheme.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/theme/NewPaymentMethodEditorTheme.java
@@ -17,7 +17,7 @@ import com.pyx4j.commons.css.IStyleDependent;
 import com.pyx4j.commons.css.IStyleName;
 import com.pyx4j.commons.css.Style;
 import com.pyx4j.commons.css.Theme;
-import com.pyx4j.widgets.client.RadioGroup;
+import com.pyx4j.widgets.client.DefaultWidgetsTheme;
 
 public class NewPaymentMethodEditorTheme extends Theme {
 
@@ -38,7 +38,7 @@ public class NewPaymentMethodEditorTheme extends Theme {
         style.addProperty("width", "100px");
         addStyle(style);
 
-        style = new Style(".", StyleName.PaymentEditorButtons, " .", RadioGroup.StyleName.RadioGroupItem);
+        style = new Style(".", StyleName.PaymentEditorButtons, " .", DefaultWidgetsTheme.StyleName.RadioGroupItem);
         style.addProperty("width", "100%");
         style.addProperty("padding-top", "3px");
         style.addProperty("height", "30px");
@@ -48,7 +48,8 @@ public class NewPaymentMethodEditorTheme extends Theme {
 
         addStyle(style);
 
-        style = new Style(".", StyleName.PaymentEditorButtons, ".", RadioGroup.StyleName.RadioGroupItem, "-", RadioGroup.StyleDependent.selected);
+        style = new Style(".", StyleName.PaymentEditorButtons, ".", DefaultWidgetsTheme.StyleName.RadioGroupItem, "-",
+                DefaultWidgetsTheme.StyleDependent.pushed);
 //        style.addProperty("border-top", "1px solid #bbb");
 //        style.addProperty("border-bottom", "1px solid #bbb");
         style.addProperty("width", "100px");
