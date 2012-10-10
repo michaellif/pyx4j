287d3838f2fc04a56ba99a0ad96084f7428b4e03
Alexandre Kinareevski <akinareevski@propertyvista.com>
2012-10-10 18:02:22 +0000

    VISTA-1709 Kill all sessions but the one running swap_prod_dev.sql

--- src/upgrade/dba_scripts/swap_prod_dev.sql
+++ src/upgrade/dba_scripts/swap_prod_dev.sql
@@ -8,8 +8,8 @@
 **/
 
 ALTER USER vista NOLOGIN;
-SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'vista_prod';
-SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'vista';
+SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'vista_prod' AND procpid != pg_backend_pid();
+SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'vista' AND procpid != pg_backend_pid();
 DROP DATABASE IF EXISTS vista;
 CREATE DATABASE vista TEMPLATE vista_prod OWNER vista;
 ALTER USER vista LOGIN;
