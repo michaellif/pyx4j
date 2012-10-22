9d315b54b4e986955818017c0fbb43aa97255818
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-10-22 21:05:14 +0000

    VISTA-2126 fixed late fee policy form (fromats for percent and money propagation depending on calculation type)

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/leasebilling/LeaseBillingPolicyForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/leasebilling/LeaseBillingPolicyForm.java
@@ -13,6 +13,8 @@
  */
 package com.propertyvista.crm.client.ui.crud.policies.leasebilling;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
@@ -20,11 +22,17 @@ import java.util.List;
 
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.NumberFormat;
 
+import com.pyx4j.commons.CommonsStringUtils;
 import com.pyx4j.entity.shared.EntityFactory;
 import com.pyx4j.forms.client.ui.CComboBox;
+import com.pyx4j.forms.client.ui.CComponent;
+import com.pyx4j.forms.client.ui.CTextFieldBase;
+import com.pyx4j.forms.client.ui.IFormat;
 import com.pyx4j.forms.client.ui.folder.EntityFolderColumnDescriptor;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
+import com.pyx4j.i18n.annotations.I18nContext;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.dialogs.SelectEnumDialog;
 
@@ -32,11 +40,87 @@ import com.propertyvista.common.client.ui.components.folders.VistaTableFolder;
 import com.propertyvista.crm.client.ui.crud.policies.common.PolicyDTOTabPanelBasedForm;
 import com.propertyvista.domain.payment.PaymentType;
 import com.propertyvista.domain.policy.dto.LeaseBillingPolicyDTO;
+import com.propertyvista.domain.policy.policies.domain.LateFeeItem;
+import com.propertyvista.domain.policy.policies.domain.LateFeeItem.BaseFeeType;
+import com.propertyvista.domain.policy.policies.domain.LateFeeItem.MaxTotalFeeType;
 import com.propertyvista.domain.policy.policies.domain.NsfFeeItem;
 
 public class LeaseBillingPolicyForm extends PolicyDTOTabPanelBasedForm<LeaseBillingPolicyDTO> {
 
     private final static I18n i18n = I18n.get(LeaseBillingPolicyForm.class);
+
+    class MoneyFormat implements IFormat<BigDecimal> {
+
+        private final NumberFormat nf;
+
+        @I18nContext(javaFormatFlag = true)
+        MoneyFormat() {
+            nf = NumberFormat.getFormat(i18n.tr("#,##0.00"));
+        }
+
+        @Override
+        public String format(BigDecimal value) {
+            if (value == null) {
+                return "";
+            } else {
+                return "$" + nf.format(value);
+            }
+        }
+
+        @Override
+        public BigDecimal parse(String string) throws ParseException {
+            if (CommonsStringUtils.isEmpty(string)) {
+                return null; // empty value case
+            }
+            try {
+                string = string.trim();
+                if (string.startsWith("$")) {
+                    string = string.substring(1);
+                }
+                if (string.endsWith("%")) {
+                    throw new NumberFormatException();
+                }
+                return new BigDecimal(nf.parse(string));
+            } catch (NumberFormatException e) {
+                throw new ParseException(i18n.tr("Invalid money format. Enter valid number"), 0);
+            }
+        }
+
+    }
+
+    class PercentFormat implements IFormat<BigDecimal> {
+
+        private final NumberFormat nf = NumberFormat.getFormat(i18n.tr("#,##0.00"));
+
+        @Override
+        public String format(BigDecimal value) {
+            if (value == null) {
+                return "";
+            } else {
+                return nf.format(value) + "%";
+            }
+
+        }
+
+        @Override
+        public BigDecimal parse(String string) throws ParseException {
+            if (CommonsStringUtils.isEmpty(string)) {
+                return null;
+            } else {
+                try {
+                    string = string.trim();
+                    if (string.endsWith("%")) {
+                        string = string.substring(0, string.length() - 1);
+                    }
+                    return new BigDecimal(string);
+                } catch (NumberFormatException e) {
+                    throw new ParseException(i18n.tr("Invalid percent format"), 0);
+                }
+
+            }
+        }
+
+    }
 
     public LeaseBillingPolicyForm() {
         this(false);
@@ -86,10 +170,45 @@ public class LeaseBillingPolicyForm extends PolicyDTOTabPanelBasedForm<LeaseBill
 
         int row = -1;
         panel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().lateFee().baseFeeType()), 10).build());
+        get(proto().lateFee().baseFeeType()).addValueChangeHandler(new ValueChangeHandler<LateFeeItem.BaseFeeType>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<BaseFeeType> event) {
+                CComponent<BigDecimal, ?> comp = get(proto().lateFee().baseFee());
+                if (comp instanceof CTextFieldBase) {
+                    CTextFieldBase<BigDecimal, ?> castedComp = ((CTextFieldBase<BigDecimal, ?>) comp);
+                    if (event.getValue() == BaseFeeType.FlatAmount) {
+                        castedComp.setWatermark(i18n.tr("$0.00"));
+                        castedComp.setFormat(new MoneyFormat());
+                    } else {
+                        castedComp.setWatermark(i18n.tr("0.00%"));
+                        castedComp.setFormat(new PercentFormat());
+                    }
+                }
+                comp.revalidate();
+            }
+        });
         panel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().lateFee().baseFee()), 6).build());
 
         row = -1;
         panel.setWidget(++row, 1, new DecoratorBuilder(inject(proto().lateFee().maxTotalFeeType()), 10).build());
+        get(proto().lateFee().maxTotalFeeType()).addValueChangeHandler(new ValueChangeHandler<LateFeeItem.MaxTotalFeeType>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<MaxTotalFeeType> event) {
+                CComponent<BigDecimal, ?> comp = get(proto().lateFee().maxTotalFee());
+                comp.setVisible(event.getValue() != MaxTotalFeeType.Unlimited);
+                if (comp instanceof CTextFieldBase) {
+                    CTextFieldBase<BigDecimal, ?> castedComp = ((CTextFieldBase<BigDecimal, ?>) comp);
+                    if (event.getValue() == MaxTotalFeeType.FlatAmount) {
+                        castedComp.setWatermark(i18n.tr("$0.00"));
+                        castedComp.setFormat(new MoneyFormat());
+                    } else {
+                        castedComp.setWatermark(i18n.tr("0.00%"));
+                        castedComp.setFormat(new PercentFormat());
+                    }
+                }
+                comp.revalidate();
+            }
+        });
         panel.setWidget(++row, 1, new DecoratorBuilder(inject(proto().lateFee().maxTotalFee()), 6).build());
 
         panel.getColumnFormatter().setWidth(0, "50%");
