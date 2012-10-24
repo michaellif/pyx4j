08d61b06c3679055ffe009be1b9ed1b9fe5dcbc2
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-10-24 18:39:16 +0000

    VISTA-2144 added links to lease or lease application from occupancy lister

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/unit/UnitOccupancyLister.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/unit/UnitOccupancyLister.java
@@ -14,14 +14,19 @@
 package com.propertyvista.crm.client.ui.crud.unit;
 
 import com.pyx4j.forms.client.ui.datatable.MemberColumnDescriptor;
+import com.pyx4j.site.client.AppPlaceEntityMapper;
+import com.pyx4j.site.client.AppSite;
 import com.pyx4j.site.client.ui.crud.lister.ListerBase;
 
+import com.propertyvista.crm.rpc.CrmSiteMap;
 import com.propertyvista.domain.property.asset.unit.occupancy.AptUnitOccupancySegment;
+import com.propertyvista.domain.property.asset.unit.occupancy.AptUnitOccupancySegment.Status;
+import com.propertyvista.domain.tenant.lease.Lease;
 
 public class UnitOccupancyLister extends ListerBase<AptUnitOccupancySegment> {
 
     public UnitOccupancyLister() {
-        super(AptUnitOccupancySegment.class, false);
+        super(AptUnitOccupancySegment.class, true, false);
         getDataTablePanel().setFilteringEnabled(false);
 
         setColumnDescriptors(//@formatter:off
@@ -33,5 +38,14 @@ public class UnitOccupancyLister extends ListerBase<AptUnitOccupancySegment> {
             new MemberColumnDescriptor.Builder(proto().lease().leaseFrom()).build(),
             new MemberColumnDescriptor.Builder(proto().lease().leaseTo()).build()
         );//@formatter:on
+    }
+
+    @Override
+    protected void onItemSelect(AptUnitOccupancySegment item) {
+        if (item.status().getValue() == Status.leased) {
+            AppSite.getPlaceController().goTo(AppPlaceEntityMapper.resolvePlace(Lease.class).formViewerPlace(item.lease().getPrimaryKey()));
+        } else if (item.status().getValue() == Status.reserved) {
+            AppSite.getPlaceController().goTo(new CrmSiteMap.Tenants.LeaseApplication().formViewerPlace(item.lease().getPrimaryKey()));
+        }
     }
 }
