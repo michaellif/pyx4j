6b8945d6f747c41c5a2b3dea7662c3ee205f13f9
Stan Porter <stanp@propertyvista.com>
2012-11-02 01:45:49 +0000

    Setters fixed

--- pyx-commons/src/main/java/com/pyx4j/commons/MinMaxPair.java
+++ pyx-commons/src/main/java/com/pyx4j/commons/MinMaxPair.java
@@ -44,10 +44,10 @@ public class MinMaxPair<E extends Serializable> extends Pair<E, E> {
     }
 
     public void setMax(E max) {
-        super.setA(max);
+        super.setB(max);
     }
 
     public void setMin(E min) {
-        super.setB(min);
+        super.setA(min);
     }
 }
