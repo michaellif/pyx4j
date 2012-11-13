eeed973d3058c1e436f228ecccc4f1144cd6034a
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2012-11-13 21:55:02 +0000

    VISTA-2230 annotation added

--- vista-yardi/vista-yardi-interface/src/main/java/com/propertyvista/oapi/LeaseService.java
+++ vista-yardi/vista-yardi-interface/src/main/java/com/propertyvista/oapi/LeaseService.java
@@ -14,10 +14,15 @@
 package com.propertyvista.oapi;
 
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import com.propertyvista.oapi.model.Lease;
 
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public interface LeaseService {
 
     void createLease(Lease lease);
