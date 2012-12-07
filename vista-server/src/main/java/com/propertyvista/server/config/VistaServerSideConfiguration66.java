2c607da846ab962d821a3bdd562a7e886b59b37f
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-12-07 00:17:09 +0000

    VISTA-75 add vista66

---
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfiguration66.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Feb 22, 2011
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.server.config;
+
+import com.pyx4j.config.server.IMailServiceConfigConfiguration;
+import com.pyx4j.config.server.IPersistenceConfiguration;
+
+public class VistaServerSideConfiguration66 extends VistaServerSideConfiguration {
+
+    @Override
+    public IPersistenceConfiguration getPersistenceConfiguration() {
+        return new VistaConfigurationPostgreSQL() {
+            @Override
+            public String dbName() {
+                return "vista66";
+            }
+
+            @Override
+            public String userName() {
+                return "vista66";
+            }
+
+            @Override
+            public String password() {
+                return "vista66";
+            }
+        };
+    }
+
+    @Override
+    public boolean openIdrequired() {
+        return true;
+    }
+
+    @Override
+    protected String getApplicationDeploymentProtocol() {
+        return "https";
+    }
+
+    @Override
+    public String getApplicationURLNamespace() {
+        return "-66.birchwoodsoftwaregroup.com/";
+    }
+
+    @Override
+    public String getApplicationEmailSender() {
+        return "\"Property Vista Support66\" <support.www22@birchwoodsoftwaregroup.com>";
+    }
+
+    @Override
+    public IMailServiceConfigConfiguration getMailServiceConfigConfiguration() {
+        return VistaSMTPMailServiceConfig.getGmailConfig("www22-");
+    }
+}
