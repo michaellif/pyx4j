6a0ed82e664f809a2af8da5b3008e69c4d84ff65
Slava Skarzhevskyy <vlads@propertyvista.com>
2012-12-20 06:39:02 +0000

    Merged revision(s) 9746 from VISTA/branches/1.0.5: VISTA-2014 disable SFTP on demo servers ........

--- vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationProdCustomersDemo.java
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationProdCustomersDemo.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -86,4 +86,8 @@ public class VistaServerSideConfigurationProdCustomersDemo extends VistaServerSi
         return VistaSettings.googleAnalyticsProdKey;
     }
 
+    @Override
+    public int paymentSSHDPort() {
+        return 0;
+    }
 }
--- vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationProdSalesDemo.java
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationProdSalesDemo.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -62,5 +62,10 @@ public class VistaServerSideConfigurationProdSalesDemo extends VistaServerSideCo
     @Override
     public String getDefaultBaseURLvistaAdmin() {
         return "https://prod-demo.birchwoodsoftwaregroup.com/" + LoggerConfig.getContextName() + "/" + DeploymentConsts.ADMIN_URL;
+    }
+
+    @Override
+    public int paymentSSHDPort() {
+        return 0;
     }
 }
