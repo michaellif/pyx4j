80f815ab78a3387e1163896cea7dd3946b220d02
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-12-27 16:57:07 +0000

    VISTA-2272 creating DTO for the Equifax (Credit Check) Setup Wizard

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/wizard/creditcheck/CreditCheckWizardForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/wizard/creditcheck/CreditCheckWizardForm.java
@@ -13,11 +13,16 @@
  */
 package com.propertyvista.crm.client.ui.wizard.creditcheck;
 
+import com.pyx4j.forms.client.ui.CRadioGroupEnum;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.wizard.IWizardView;
 import com.pyx4j.site.client.ui.wizard.WizardForm;
+import com.pyx4j.widgets.client.RadioGroup.Layout;
 
+import com.propertyvista.common.client.ui.components.editors.AddressSimpleEditor;
+import com.propertyvista.common.client.ui.components.editors.NameEditor;
+import com.propertyvista.common.client.ui.components.editors.payments.CreditCardInfoEditor;
 import com.propertyvista.dto.CreditCheckSetupDTO;
 
 public class CreditCheckWizardForm extends WizardForm<CreditCheckSetupDTO> {
@@ -36,6 +41,11 @@ public class CreditCheckWizardForm extends WizardForm<CreditCheckSetupDTO> {
         FormFlexPanel main = new FormFlexPanel(title);
         int row = 0;
         main.setH1(row++, 0, 2, i18n.tr("Pricing"));
+        main.setWidget(
+                ++row,
+                0,
+                inject(proto().creditReportOption(), new CRadioGroupEnum<CreditCheckSetupDTO.CreditReportOption>(CreditCheckSetupDTO.CreditReportOption.class,
+                        Layout.HORISONTAL)));
         return main;
     }
 
@@ -43,13 +53,23 @@ public class CreditCheckWizardForm extends WizardForm<CreditCheckSetupDTO> {
         FormFlexPanel main = new FormFlexPanel(title);
         int row = 0;
         main.setH1(row++, 0, 2, i18n.tr("Business Information"));
+        main.setWidget(++row, 0, inject(proto().companyType()));
+        main.setWidget(++row, 0, inject(proto().businessAddress(), new AddressSimpleEditor()));
+        main.setWidget(++row, 0, inject(proto().businessNumber()));
+        main.setWidget(++row, 0, inject(proto().businessEstablishedDate()));
         return main;
     }
 
     private FormFlexPanel createPersonalInfoStep(String title) {
         FormFlexPanel main = new FormFlexPanel(title);
         int row = 0;
-        main.setH1(row++, 0, 2, i18n.tr("Personal Information"));
+        main.setH1(++row, 0, 2, i18n.tr("Personal Information"));
+        main.setWidget(++row, 0, inject(proto().name(), new NameEditor()));
+        main.setWidget(++row, 0, inject(proto().personalAddress(), new AddressSimpleEditor()));
+        main.setWidget(++row, 0, inject(proto().email()));
+        main.setWidget(++row, 0, inject(proto().dateOfBirth()));
+        main.setWidget(++row, 0, inject(proto().sin()));
+
         return main;
     }
 
@@ -57,6 +77,8 @@ public class CreditCheckWizardForm extends WizardForm<CreditCheckSetupDTO> {
         FormFlexPanel main = new FormFlexPanel(title);
         int row = 0;
         main.setH1(row++, 0, 2, i18n.tr("Confirmation"));
+        main.setWidget(++row, 0, inject(proto().creditCardInfo(), new CreditCardInfoEditor()));
+
         return main;
     }
 }
--- vista-shared/vista-domain/src/main/java/com/propertyvista/dto/CreditCheckSetupDTO.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/dto/CreditCheckSetupDTO.java
@@ -13,10 +13,91 @@
  */
 package com.propertyvista.dto;
 
+import javax.xml.bind.annotation.XmlType;
+
+import com.pyx4j.commons.LogicalDate;
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.Editor;
+import com.pyx4j.entity.annotations.Editor.EditorType;
 import com.pyx4j.entity.annotations.Transient;
+import com.pyx4j.entity.annotations.validator.NotNull;
 import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.i18n.annotations.I18n;
+import com.pyx4j.i18n.shared.I18nEnum;
+
+import com.propertyvista.domain.contact.AddressSimple;
+import com.propertyvista.domain.payment.CreditCardInfo;
+import com.propertyvista.domain.person.Name;
 
 @Transient
 public interface CreditCheckSetupDTO extends IEntity {
 
+    @I18n(context = "Credit Report Option")
+    @XmlType(name = "CreditReportOption")
+    public enum CreditReportOption {
+
+        ReccomendationReport,
+
+        FullCreditReport;
+
+        @Override
+        public String toString() {
+            return I18nEnum.toString(this);
+        };
+
+    }
+
+    @I18n(context = "Company Type")
+    @XmlType(name = "CompanyType")
+    public enum CompanyType {
+
+        SoleProprietorship, Partnership, Corporation, Cooperative;
+
+    }
+
+    // PRICING SECTION --------------------------------------------------------
+
+    @NotNull
+    IPrimitive<CreditReportOption> creditReportOption();
+
+    // BUSINESS INFORMATION SECTION--------------------------------------------
+
+    // TODO not sure that this is actually what was the poet's intention in the specs...
+    // TODO also I'm suspecting that the set acceptable values can change based on the country
+    @NotNull
+    IPrimitive<CompanyType> companyType();
+
+    @NotNull
+    IPrimitive<AddressSimple> businessAddress();
+
+    @Caption(name = "Business Number / Employer Identification")
+    IPrimitive<String> businessNumber();
+
+    IPrimitive<LogicalDate> businessEstablishedDate();
+
+    // TODO add buisness proof license upload
+
+    // TODO add Articles of Incorporation upload
+
+    // PERSONAL INFORMATION SECTION -------------------------------------------
+
+    @NotNull
+    IPrimitive<Name> name();
+
+    IPrimitive<AddressSimple> personalAddress();
+
+    @NotNull
+    @Editor(type = EditorType.email)
+    IPrimitive<String> email();
+
+    IPrimitive<LogicalDate> dateOfBirth();
+
+    IPrimitive<String> sin();
+
+    // TODO add id doc upload
+
+    // CONFIRMATION SECTION ---------------------------------------------------
+
+    IPrimitive<CreditCardInfo> creditCardInfo();
 }
