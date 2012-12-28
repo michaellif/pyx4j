e694dc7480bc0977614968abb952e2bfc3b877f4
Michael Lifshits <michaellif@propertyvista.com>
2012-12-28 17:38:59 +0000

    CollapsablePanel implementation

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/BaseCollapsableDecorator.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/BaseCollapsableDecorator.java
@@ -20,67 +20,26 @@
  */
 package com.pyx4j.forms.client.ui;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.SimplePanel;
-
 import com.pyx4j.commons.IDebugId;
 import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.forms.client.ui.decorators.IDecorator;
+import com.pyx4j.widgets.client.CollapsablePanel;
 import com.pyx4j.widgets.client.images.WidgetsImages;
 
-public class BaseCollapsableDecorator<E extends IEntity> extends SimplePanel implements ICollapsableDecorator<E> {
-
-    private final WidgetsImages images;
-
-    private final SimplePanel contentHolder;
-
-    private final Image collapseImage = new Image();
-
-    private CEntityCollapsableViewer<E> viewer;
+public class BaseCollapsableDecorator<E extends IEntity> extends CollapsablePanel implements IDecorator<CEntityContainer<E>> {
 
     public BaseCollapsableDecorator(WidgetsImages images) {
-        this.images = images;
-
-        HorizontalPanel mainPanel = new HorizontalPanel();
-        mainPanel.setWidth("100%");
-        collapseImage.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                toggleCollapsed();
-            }
-        });
-        mainPanel.add(collapseImage);
-        collapseImage.setResource(images.collapse());
-        mainPanel.setCellWidth(collapseImage, collapseImage.getWidth() + "px");
-        contentHolder = new SimplePanel();
-//        contentHolder.setStyleName(EntityFolderBoxItem.name());
-        mainPanel.add(contentHolder);
-
-        setWidget(mainPanel);
+        super(images);
     }
 
-    private void toggleCollapsed() {
-        viewer.setCollapsed(!viewer.isCollapsed());
-        reset();
-    }
-
-    private void reset() {
-        collapseImage.setResource(viewer.isCollapsed() ? images.expand() : images.collapse());
+    @Override
+    public void setComponent(CEntityContainer<E> viewer) {
+        setWidget(viewer.createContent());
     }
 
     @Override
     public void onSetDebugId(IDebugId parentDebugId) {
         // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void setComponent(CEntityCollapsableViewer<E> viewer) {
-        this.viewer = viewer;
-        contentHolder.setWidget(viewer.createContent());
-        reset();
     }
 
 }
---
+++ gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/CollapsablePanel.java
@@ -0,0 +1,97 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on May 26, 2010
+ * @author stanp
+ * @version $Id$
+ */
+package com.pyx4j.widgets.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+import com.pyx4j.widgets.client.event.shared.BeforeToggleEvent;
+import com.pyx4j.widgets.client.event.shared.BeforeToggleHandler;
+import com.pyx4j.widgets.client.event.shared.HasToggleHandlers;
+import com.pyx4j.widgets.client.event.shared.ToggleEvent;
+import com.pyx4j.widgets.client.event.shared.ToggleHandler;
+import com.pyx4j.widgets.client.images.WidgetsImages;
+
+public class CollapsablePanel extends SimplePanel implements HasToggleHandlers {
+
+    private final WidgetsImages images;
+
+    private final SimplePanel contentHolder;
+
+    private final Image collapseImage;
+
+    private boolean collapsed = false;
+
+    public CollapsablePanel(WidgetsImages images) {
+        this.images = images;
+
+        HorizontalPanel mainPanel = new HorizontalPanel();
+        mainPanel.setWidth("100%");
+
+        collapseImage = new Image();
+        collapseImage.setResource(images.collapse());
+        collapseImage.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                setCollapsed(!collapsed);
+            }
+        });
+        mainPanel.add(collapseImage);
+
+        mainPanel.setCellWidth(collapseImage, collapseImage.getWidth() + "px");
+        contentHolder = new SimplePanel();
+        mainPanel.add(contentHolder);
+
+        super.setWidget(mainPanel);
+    }
+
+    public void setCollapsed(boolean collapsed) {
+        BeforeToggleEvent event = BeforeToggleEvent.fire(this, !collapsed);
+        if (event != null && event.isCanceled()) {
+            return;
+        }
+        collapseImage.setResource(collapsed ? images.expand() : images.collapse());
+        this.collapsed = collapsed;
+
+        ToggleEvent.fire(this, !collapsed);
+    }
+
+    @Override
+    public HandlerRegistration addBeforeToggleHandler(BeforeToggleHandler handler) {
+        return addHandler(handler, BeforeToggleEvent.getType());
+    }
+
+    @Override
+    public HandlerRegistration addToggleHandler(ToggleHandler handler) {
+        return addHandler(handler, ToggleEvent.getType());
+    }
+
+    @Override
+    public void setWidget(IsWidget w) {
+        contentHolder.setWidget(w);
+    }
+
+}
