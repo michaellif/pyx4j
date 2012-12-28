f2ffe69c8ef9ca62c873eb9949a6b37adbddf4f2
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2012-12-28 23:18:29 +0000

    VISTA-2272 adding plumbing for BusinessId uploading

---
+++ vista-crm/vista-crm-rpc/src/main/java/com/propertyvista/crm/rpc/services/BusinessIdUploadService.java
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2012-12-28
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.crm.rpc.services;
+
+import java.util.Collection;
+import java.util.EnumSet;
+
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.gwt.rpc.upload.UploadService;
+import com.pyx4j.gwt.shared.DownloadFormat;
+
+public interface BusinessIdUploadService extends UploadService<IEntity, IEntity> {
+
+    public static final Collection<DownloadFormat> SUPPORTED_FORMATS = EnumSet.of(DownloadFormat.JPEG, DownloadFormat.GIF, DownloadFormat.PNG,
+            DownloadFormat.BMP);
+
+}
---
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/BusinessIdUploadServiceImpl.java
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2012-12-28
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.crm.server.services;
+
+import java.util.Collection;
+
+import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.shared.EntityFactory;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.essentials.server.upload.AbstractUploadServiceImpl;
+import com.pyx4j.essentials.server.upload.UploadData;
+import com.pyx4j.essentials.server.upload.UploadDeferredProcess;
+import com.pyx4j.gwt.rpc.upload.UploadResponse;
+import com.pyx4j.gwt.shared.DownloadFormat;
+import com.pyx4j.i18n.shared.I18n;
+
+import com.propertyvista.crm.rpc.services.BusinessIdUploadService;
+import com.propertyvista.server.domain.BusinessIdBlob;
+
+public class BusinessIdUploadServiceImpl extends AbstractUploadServiceImpl<IEntity, IEntity> implements BusinessIdUploadService {
+
+    private static final I18n i18n = I18n.get(I18n.class);
+
+    @Override
+    public long getMaxSize() {
+        return EntityFactory.getEntityPrototype(BusinessIdBlob.class).data().getMeta().getLength();
+    }
+
+    @Override
+    public String getUploadFileTypeName() {
+        return i18n.tr("Business Identification");
+    }
+
+    @Override
+    public Collection<String> getSupportedExtensions() {
+        return DownloadFormat.getExtensions(SUPPORTED_FORMATS);
+    }
+
+    @Override
+    public com.pyx4j.essentials.server.upload.UploadReciver.ProcessingStatus onUploadRecived(UploadData data, UploadDeferredProcess<IEntity, IEntity> process,
+            UploadResponse<IEntity> response) {
+        BusinessIdBlob blob = EntityFactory.create(BusinessIdBlob.class);
+        blob.data().setValue(data.data);
+        blob.contentType().setValue(response.fileContentType);
+
+        Persistence.service().persist(blob);
+        Persistence.service().commit();
+        response.uploadKey = blob.getPrimaryKey();
+        return ProcessingStatus.completed;
+    }
+
+}
