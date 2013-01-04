a54e9477e667157d1e46b3778bce5a8560d3adda
Michael Lifshits <michaellif@propertyvista.com>
2013-01-04 17:12:33 +0000

    EntityContainerCollapsableDecorator impl

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/decorators/EntityContainerCollapsableDecorator.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/decorators/EntityContainerCollapsableDecorator.java
@@ -20,21 +20,106 @@
  */
 package com.pyx4j.forms.client.ui.decorators;
 
+import static com.pyx4j.forms.client.ui.folder.DefaultEntityFolderTheme.StyleName.EntityFolderBoxItem;
+
+import java.util.ArrayList;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
 import com.pyx4j.commons.IDebugId;
 import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.forms.client.events.PropertyChangeEvent;
+import com.pyx4j.forms.client.events.PropertyChangeEvent.PropertyName;
+import com.pyx4j.forms.client.events.PropertyChangeHandler;
+import com.pyx4j.forms.client.ui.CComponent;
 import com.pyx4j.forms.client.ui.CEntityContainer;
+import com.pyx4j.forms.client.validators.ValidationError;
+import com.pyx4j.forms.client.validators.ValidationResults;
 import com.pyx4j.widgets.client.CollapsablePanel;
+import com.pyx4j.widgets.client.event.shared.ToggleEvent;
+import com.pyx4j.widgets.client.event.shared.ToggleHandler;
 import com.pyx4j.widgets.client.images.WidgetsImages;
 
 public class EntityContainerCollapsableDecorator<E extends IEntity> extends CollapsablePanel implements IDecorator<CEntityContainer<E>> {
 
+    private final EntityContainerDecoratorToolbar toolbar;
+
+    private final SimplePanel contentHolder;
+
     public EntityContainerCollapsableDecorator(WidgetsImages images) {
         super(images);
+        addToggleHandler(new ToggleHandler() {
+
+            @Override
+            public void onToggle(ToggleEvent event) {
+                contentHolder.setVisible(event.isToggleOn());
+                toolbar.update(event.isToggleOn());
+            }
+        });
+
+        VerticalPanel mainPanel = new VerticalPanel();
+        mainPanel.setWidth("100%");
+        setWidget(mainPanel);
+
+        toolbar = new EntityContainerDecoratorToolbar(images);
+        toolbar.addCaptionHolderClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                setExpended(!isExpended());
+            }
+        });
+        toolbar.update(isExpended());
+
+        mainPanel.add(toolbar);
+
+        contentHolder = new SimplePanel();
+        contentHolder.setStyleName(EntityFolderBoxItem.name());
+
+        mainPanel.add(contentHolder);
+
     }
 
     @Override
-    public void setComponent(CEntityContainer<E> viewer) {
+    public void setComponent(final CEntityContainer<E> viewer) {
         setWidget(viewer.createContent());
+        toolbar.setEntityContainer(viewer);
+
+        viewer.addPropertyChangeHandler(new PropertyChangeHandler() {
+            @Override
+            public void onPropertyChange(PropertyChangeEvent event) {
+                if (event.isEventOfType(PropertyName.repopulated)) {
+                    toolbar.update(isExpended());
+                }
+                if (event.isEventOfType(PropertyName.valid, PropertyName.repopulated, PropertyName.showErrorsUnconditional)) {
+                    String message = null;
+                    if (viewer.isUnconditionalValidationErrorRendering()) {
+                        message = viewer.getValidationResults().getValidationShortMessage();
+                    } else {
+                        ArrayList<ValidationError> errors = viewer.getValidationResults().getValidationErrors();
+                        ValidationResults results = new ValidationResults();
+                        for (ValidationError validationError : errors) {
+                            CComponent<?, ?> originator = validationError.getOriginator();
+                            if ((originator.isUnconditionalValidationErrorRendering() || originator.isVisited()) && !originator.isValid()) {
+                                results.appendValidationError(validationError);
+                            }
+                        }
+                        message = results.getValidationShortMessage();
+                    }
+                    toolbar.setWarningMessage(message.isEmpty() ? null : message);
+                }
+            }
+        });
+        viewer.addValueChangeHandler(new ValueChangeHandler<E>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<E> event) {
+                toolbar.update(isExpended());
+            }
+        });
     }
 
     @Override
