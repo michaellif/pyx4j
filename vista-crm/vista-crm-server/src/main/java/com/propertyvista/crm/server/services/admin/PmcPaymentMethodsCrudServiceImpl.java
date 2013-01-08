a84e929c5fdd64a93af346f27e7b8d5acfd36582
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-08 23:36:28 +0000

    VISTA-2272 Wizards security

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/PmcPaymentMethodsCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/PmcPaymentMethodsCrudServiceImpl.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -51,7 +51,7 @@ public class PmcPaymentMethodsCrudServiceImpl implements PmcPaymentMethodsCrudSe
             @Override
             public Vector<PmcPaymentMethod> call() throws Exception {
                 Persistence.service().retrieveMember(pmc.equifaxInfo());
-                Persistence.service().retrieveMember(pmc.equifaxInfo().paymentMethod());
+                Persistence.service().retrieve(pmc.equifaxInfo().paymentMethod());
 
                 Vector<PmcPaymentMethod> paymentMethods = retrievePaymentMethods(pmc);
                 for (PmcPaymentMethod paymentMethod : paymentMethods) {
