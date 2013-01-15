a21a464eec2e0c417f5a4e02e40270018570f102
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-15 22:34:25 +0000

    VISTA-75 main example

--- vista-yardi/vista-yardi-voyager/src/test/java/com/propertyvista/yardi/YardiServicesClient.java
+++ vista-yardi/vista-yardi-voyager/src/test/java/com/propertyvista/yardi/YardiServicesClient.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -13,7 +13,15 @@
  */
 package com.propertyvista.yardi;
 
+import com.pyx4j.config.server.ServerSideConfiguration;
+import com.pyx4j.entity.rdb.cfg.Configuration.DatabaseType;
+import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.server.contexts.Lifecycle;
+import com.pyx4j.server.contexts.NamespaceManager;
+
 import com.propertyvista.config.tests.VistaTestDBSetup;
+import com.propertyvista.config.tests.VistaTestsServerSideConfiguration;
+import com.propertyvista.domain.DemoData.DemoPmc;
 
 public class YardiServicesClient {
 
@@ -39,7 +47,14 @@ public class YardiServicesClient {
         yp.setYardiPropertyId(YardiConstants.YARDI_PROPERTY_ID);
 
         //db setup
-        VistaTestDBSetup.init();
+        if (false) {
+            VistaTestDBSetup.init();
+        } else {
+            ServerSideConfiguration.setInstance(new VistaTestsServerSideConfiguration(DatabaseType.PostgreSQL));
+            NamespaceManager.setNamespace(DemoPmc.star.name());
+            Persistence.service().startBackgroundProcessTransaction();
+            Lifecycle.startElevatedUserContext();
+        }
 
         //test services
 //        propertyService.updateBuildings(yp);
