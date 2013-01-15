590773497b83b957b69b200dba54b2b37f3f1dde
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-15 18:48:46 +0000

    VISTA-75 star preload changed for yardy

--- vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/preloader/policy/subpreloaders/IdAssignmentPolicyPreloader.java
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/preloader/policy/subpreloaders/IdAssignmentPolicyPreloader.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -13,6 +13,9 @@
  */
 package com.propertyvista.portal.server.preloader.policy.subpreloaders;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 import com.pyx4j.entity.shared.EntityFactory;
 
 import com.propertyvista.domain.policy.policies.IdAssignmentPolicy;
@@ -20,8 +23,11 @@ import com.propertyvista.domain.policy.policies.domain.IdAssignmentItem;
 import com.propertyvista.domain.policy.policies.domain.IdAssignmentItem.IdAssignmentType;
 import com.propertyvista.domain.policy.policies.domain.IdAssignmentItem.IdTarget;
 import com.propertyvista.portal.server.preloader.policy.util.AbstractPolicyPreloader;
+import com.propertyvista.shared.config.VistaFeatures;
 
 public class IdAssignmentPolicyPreloader extends AbstractPolicyPreloader<IdAssignmentPolicy> {
+
+    private final Set<IdTarget> yardiIntegrationAssignedId = EnumSet.of(IdTarget.propertyCode, IdTarget.lease);
 
     public IdAssignmentPolicyPreloader() {
         super(IdAssignmentPolicy.class);
@@ -37,7 +43,12 @@ public class IdAssignmentPolicyPreloader extends AbstractPolicyPreloader<IdAssig
 
             IdAssignmentItem item = EntityFactory.create(IdAssignmentItem.class);
             item.target().setValue(target);
-            item.type().setValue(IdAssignmentType.generatedNumber);
+
+            if (VistaFeatures.instance().yardiIntegration() && (yardiIntegrationAssignedId.contains(target))) {
+                item.type().setValue(IdAssignmentType.userAssigned);
+            } else {
+                item.type().setValue(IdAssignmentType.generatedNumber);
+            }
 
             policy.items().add(item);
         }
