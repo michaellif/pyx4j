8617e912693a2a6b7ac1abcc3e4484499527c050
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-16 22:09:54 +0000

    VISTA-75 add AbstractPmcUser to enable communication

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/security/common/AbstractPmcUser.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/security/common/AbstractPmcUser.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
  * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
  * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
@@ -7,17 +7,17 @@
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
  *
- * Created on Jan 10, 2012
+ * Created on 2013-01-16
  * @author vlads
  * @version $Id$
  */
-package com.propertyvista.domain.security;
+package com.propertyvista.domain.security.common;
 
-import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.AbstractEntity;
+import com.pyx4j.i18n.annotations.I18n;
 
-import com.propertyvista.domain.security.common.AbstractPmcUser;
-
-@Caption(name = "User")
-public interface CustomerUser extends AbstractPmcUser {
+@AbstractEntity
+@I18n(strategy = I18n.I18nStrategy.IgnoreThis)
+public interface AbstractPmcUser extends AbstractUser {
 
 }
