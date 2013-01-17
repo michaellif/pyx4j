7b260134ff269ca3fa2bdf40b422a48b191eb09b
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-17 19:00:45 +0000

    VISTA-75 allow to test demo mode

--- vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationDev.java
+++ vista-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationDev.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -27,6 +27,12 @@ public class VistaServerSideConfigurationDev extends VistaServerSideConfiguratio
     public static int devServerPort = 8888;
 
     public static String devContextPath = "/vista";
+
+    @Override
+    public boolean isVistaDemo() {
+        return false;
+        //return true;
+    }
 
     @Override
     public ServerSideConfiguration selectInstanceByContextName(ServletContext servletContext, String contextName) {
