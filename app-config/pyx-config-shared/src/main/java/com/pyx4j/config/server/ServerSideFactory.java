137ff557cbde71f872c1ceae95752cfb2cb3c41d
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-18 22:30:27 +0000

    Add FacadeFactory

--- app-config/pyx-config-shared/src/main/java/com/pyx4j/config/server/ServerSideFactory.java
+++ app-config/pyx-config-shared/src/main/java/com/pyx4j/config/server/ServerSideFactory.java
@@ -22,12 +22,27 @@ package com.pyx4j.config.server;
 
 public class ServerSideFactory {
 
+    /**
+     * Created new instance of interface implementation by finding class name+Factory first, if not exists finds name+Impl
+     */
     @SuppressWarnings("unchecked")
     public static <T> T create(Class<T> interfaceCalss) {
         String interfaceClassName = interfaceCalss.getName();
         if (interfaceClassName.contains(".shared.")) {
             interfaceClassName = interfaceClassName.replace(".shared.", ".server.");
         }
+
+        String factoryClassName = interfaceClassName + "Factory";
+        try {
+            Class<FacadeFactory<T>> factoryKlass = (Class<FacadeFactory<T>>) Class.forName(factoryClassName);
+            FacadeFactory<T> factory = factoryKlass.newInstance();
+            return factory.create();
+        } catch (ClassNotFoundException ignore) {
+
+        } catch (Throwable e) {
+            throw new RuntimeException("Can't create " + factoryClassName, e);
+        }
+
         if (interfaceCalss.getSimpleName().startsWith("I") && Character.isUpperCase(interfaceCalss.getSimpleName().charAt(1))) {
             interfaceClassName = interfaceClassName.replace(interfaceCalss.getSimpleName(), "Server" + interfaceCalss.getSimpleName().substring(1));
         } else {
@@ -40,5 +55,4 @@ public class ServerSideFactory {
             throw new RuntimeException("Can't create " + interfaceClassName, e);
         }
     }
-
 }
