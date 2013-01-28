a75bfafae6e715e029314c90630a6f6521679c2d
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-28 13:47:05 +0000

    VISTA-1709 correction to path on windows

--- src/upgrade/1.0.6/migration/migrate_vista_prod2vista.cmd
+++ src/upgrade/1.0.6/migration/migrate_vista_prod2vista.cmd
@@ -3,7 +3,7 @@ rem @version $Revision$ ($Author$)  $Date$
 title psql
 set PGPASSWORD=root
 
-psql --username postgres -d "vista" --file ../dba_scripts/dba_functions.sql
+psql --username postgres -d "vista" --file ..\..\dba_scripts\dba_functions.sql
 @if errorlevel 1 goto errormark
 
 psql --username postgres -d "vista" --file migrate.sql
