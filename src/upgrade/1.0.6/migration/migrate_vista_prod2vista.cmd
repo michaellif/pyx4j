1309dcfecc048b494476fd4c4da1fde109f9a856
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-28 20:03:50 +0000

    VISTA-1709 correction to tests on windows

--- src/upgrade/1.0.6/migration/migrate_vista_prod2vista.cmd
+++ src/upgrade/1.0.6/migration/migrate_vista_prod2vista.cmd
@@ -3,7 +3,10 @@ rem @version $Revision$ ($Author$)  $Date$
 title psql
 set PGPASSWORD=root
 
-psql --username postgres -d "vista" --file ..\..\dba_scripts\dba_functions.sql
+psql --username postgres --file ../../dba_scripts/swap_prod_dev.sql
+@if errorlevel 1 goto errormark
+
+psql --username postgres -d "vista" --file ../../dba_scripts/dba_functions.sql
 @if errorlevel 1 goto errormark
 
 psql --username postgres -d "vista" --file migrate.sql
