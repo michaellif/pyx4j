76c67e385c5a4dd4b96cc67e1de122781933c377
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-01-28 16:59:00 +0000

    VISTA-2331 add Crtl+Q to Check

--- vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/payments/CheckInfoEditor.java
+++ vista-shared/vista-gwt-common/src/main/java/com/propertyvista/common/client/ui/components/editors/payments/CheckInfoEditor.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -15,11 +15,16 @@ package com.propertyvista.common.client.ui.components.editors.payments;
 
 import com.google.gwt.user.client.ui.IsWidget;
 
+import com.pyx4j.config.shared.ApplicationMode;
+import com.pyx4j.forms.client.events.DevShortcutEvent;
+import com.pyx4j.forms.client.events.DevShortcutHandler;
+import com.pyx4j.forms.client.ui.CTextFieldBase;
 import com.pyx4j.forms.client.ui.IEditableComponentFactory;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 
 import com.propertyvista.common.client.ui.components.c.CEntityDecoratableForm;
+import com.propertyvista.domain.payment.AccountType;
 import com.propertyvista.domain.payment.CheckInfo;
 
 public class CheckInfoEditor extends CEntityDecoratableForm<CheckInfo> {
@@ -48,5 +53,30 @@ public class CheckInfoEditor extends CEntityDecoratableForm<CheckInfo> {
         panel.setWidget(++row, 0, new DecoratorBuilder(inject(proto().accountNo()), 15).build());
 
         return panel;
+    }
+
+    @Override
+    public void addValidations() {
+        if (ApplicationMode.isDevelopment()) {
+            this.addDevShortcutHandler(new DevShortcutHandler() {
+                @Override
+                public void onDevShortcut(DevShortcutEvent event) {
+                    if (event.getKeyCode() == 'Q') {
+                        event.consume();
+                        devGenerateEcheck();
+                    }
+                }
+            });
+        }
+    }
+
+    private void devGenerateEcheck() {
+        get(proto().nameOn()).setValue("Dev");
+        get(proto().bankName()).setValue("Nowhere Bank");
+        get(proto().accountType()).setValue(AccountType.Chequing);
+        get(proto().checkNo()).setValue("1");
+        get(proto().institutionNo()).setValue("123");
+        get(proto().transitNo()).setValue("12345");
+        ((CTextFieldBase<?, ?>) get(proto().accountNo())).setValueByString("2345 6789");
     }
 }
