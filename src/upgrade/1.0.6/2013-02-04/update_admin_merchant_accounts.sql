1211a28c64b783b8da0bbf45fda081ec4d736789
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-02-04 23:53:45 +0000

    VISTA-1458 Update for _admin_.admin_onboarding_merchant_account

---
+++ src/upgrade/1.0.6/2013-02-04/update_admin_merchant_accounts.sql
@@ -0,0 +1,15 @@
+/**
+***     ======================================================================
+***             
+***             Update merchant accounts
+***
+***     ======================================================================
+**/
+
+UPDATE  _admin_.admin_onboarding_merchant_account
+SET     onboarding_bank_account_id = id;
+
+
+
+
+
