27c52a5d3d4888cdf0aa2493af2f9fa1676373d7
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-02-06 20:21:45 +0000

    VISTA-2486 added CradServiceSimulation

--- vista-admin/vista-admin-client/src/main/java/com/propertyvista/admin/client/ui/crud/cardservicesimulation/CardServiceSimulationListerViewImpl.java
+++ vista-admin/vista-admin-client/src/main/java/com/propertyvista/admin/client/ui/crud/cardservicesimulation/CardServiceSimulationListerViewImpl.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -28,15 +28,14 @@ public class CardServiceSimulationListerViewImpl extends ListerViewImplBase<Card
             setColumnDescriptors(//@formatter:off
                     new MemberColumnDescriptor.Builder(proto().cardType()).build(),
                     new MemberColumnDescriptor.Builder(proto().number()).build(),
-                    new MemberColumnDescriptor.Builder(proto().token()).build(),
                     new MemberColumnDescriptor.Builder(proto().expiryDate()).build(),
                     new MemberColumnDescriptor.Builder(proto().balance()).build(),
                     new MemberColumnDescriptor.Builder(proto().responseCode()).build()
-            );//@formatter:off            
+            );//@formatter:off
         }
-        
+
     }
-    
+
     public CardServiceSimulationListerViewImpl() {
         setLister(new CardServiceSimulationLister());
     }
