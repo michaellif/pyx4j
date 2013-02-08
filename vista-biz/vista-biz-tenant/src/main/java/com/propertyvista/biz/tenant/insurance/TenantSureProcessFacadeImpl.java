7cd68184d54ad4295b0f5c5033d83a10f0ac2d60
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-02-08 18:46:27 +0000

    VISTA-2216 added more columns to reports

--- vista-biz/vista-biz-tenant/src/main/java/com/propertyvista/biz/tenant/insurance/TenantSureProcessFacadeImpl.java
+++ vista-biz/vista-biz-tenant/src/main/java/com/propertyvista/biz/tenant/insurance/TenantSureProcessFacadeImpl.java
@@ -28,6 +28,7 @@ import com.pyx4j.essentials.server.report.ReportTableFormater;
 import com.propertyvista.admin.domain.scheduler.RunStats;
 import com.propertyvista.admin.domain.tenantsure.TenantSureHQUpdateFile;
 import com.propertyvista.domain.tenant.insurance.InsuranceTenantSure;
+import com.propertyvista.domain.tenant.insurance.InsuranceTenantSure.Status;
 import com.propertyvista.domain.tenant.insurance.InsuranceTenantSureReport;
 import com.propertyvista.domain.tenant.insurance.InsuranceTenantSureReport.ReportedStatus;
 import com.propertyvista.server.jobs.StatisticsUtils;
@@ -63,6 +64,7 @@ public class TenantSureProcessFacadeImpl implements TenantSureProcessFacade {
         formater.header("Monthly Payable");
         formater.header("Status");
         formater.header("Status From");
+        formater.header("Cancellation");
         formater.newRow();
 
         EntityQueryCriteria<InsuranceTenantSureReport> criteria = EntityQueryCriteria.create(InsuranceTenantSureReport.class);
@@ -81,12 +83,25 @@ public class TenantSureProcessFacadeImpl implements TenantSureProcessFacade {
         try {
             while (iterator.hasNext()) {
                 InsuranceTenantSureReport reportedStatusHolder = iterator.next();
-
+                reportedStatusHolder = updateReportStatus(reportedStatusHolder);
+                
                 formater.cell(reportedStatusHolder.insurance().client().tenant().customer().person().name().firstName().getValue());
                 formater.cell(reportedStatusHolder.insurance().client().tenant().customer().person().name().lastName().getValue());
                 formater.cell(reportedStatusHolder.insurance().insuranceCertificate().insuranceCertificateNumber().getStringView());
-                formater.cell(reportedStatusHolder.insurance().monthlyPayable().getValue().toString());
-                formater.cell(updateReportStatus(reportedStatusHolder));
+                formater.cell(reportedStatusHolder.insurance().monthlyPayable().getValue().toString());                
+                formater.cell(reportedStatusHolder.reportedStatus().getValue());
+                formater.cell(SimpleMessageFormat.format("{0,date,short}", reportedStatusHolder.statusFrom().getValue()));
+                
+                String specialFlag = "";
+                if (reportedStatusHolder.insurance().status().getValue() == Status.PendingCancellation) {
+                    specialFlag = SimpleMessageFormat.format(//@formatter:off
+                            "{0} due to {1} since {2,date,short}",
+                            Status.PendingCancellation,
+                            reportedStatusHolder.insurance().cancellation().getValue(),
+                            reportedStatusHolder.insurance().cancellationDate().getValue()
+                    );//@formatter:on
+                }
+                formater.cell(specialFlag);
 
                 formater.newRow();
 
@@ -112,16 +127,17 @@ public class TenantSureProcessFacadeImpl implements TenantSureProcessFacade {
     /**
      * @param reportedStatusHolder
      * @return updated reported status
-     * @throws IllegalArgumentException if previously reported status and current insurance status cannot be updated
+     * @throws IllegalArgumentException
+     *             if previously reported status and current insurance status cannot be updated
      */
-    private InsuranceTenantSureReport.ReportedStatus updateReportStatus(InsuranceTenantSureReport reportedStatusHolder) {
+    private InsuranceTenantSureReport updateReportStatus(InsuranceTenantSureReport reportedStatusHolder) {
         ReportedStatus reportClientStatus = null;
         LogicalDate statusFrom = null;
-        
-        boolean needsUpdate = false;        
+
+        boolean needsUpdate = false;
         switch (reportedStatusHolder.insurance().status().getValue()) {
         case Cancelled:
-            if (reportedStatusHolder.reportedStatus().getValue() != ReportedStatus.Cancelled ) {
+            if (reportedStatusHolder.reportedStatus().getValue() != ReportedStatus.Cancelled) {
                 reportClientStatus = ReportedStatus.Cancelled;
                 statusFrom = reportedStatusHolder.insurance().expiryDate().getValue();
             }
@@ -129,14 +145,16 @@ public class TenantSureProcessFacadeImpl implements TenantSureProcessFacade {
 
         case Active:
         case PendingCancellation:
-            if (reportedStatusHolder.reportedStatus().getValue() != ReportedStatus.Active) {
+            if (reportedStatusHolder.reportedStatus().getValue() == null) {
                 reportClientStatus = ReportedStatus.New;
                 statusFrom = reportedStatusHolder.insurance().inceptionDate().getValue();
                 needsUpdate = true;
-                
-            } else if ((reportedStatusHolder.reportedStatus().getValue() == ReportedStatus.Active)  | (reportedStatusHolder.reportedStatus().getValue() == ReportedStatus.New)) {
-                reportClientStatus = ReportedStatus.Active;                
+
+            } else if ((reportedStatusHolder.reportedStatus().getValue() == ReportedStatus.Active)
+                    | (reportedStatusHolder.reportedStatus().getValue() == ReportedStatus.New)) {
+                reportClientStatus = ReportedStatus.Active;
                 needsUpdate = reportedStatusHolder.reportedStatus().getValue() == ReportedStatus.New;
+                statusFrom = reportedStatusHolder.statusFrom().getValue();
             }
             break;
 
@@ -146,16 +164,17 @@ public class TenantSureProcessFacadeImpl implements TenantSureProcessFacade {
 
         if (reportClientStatus == null) {
             throw new IllegalArgumentException(SimpleMessageFormat.format("wrong insurance status for report: {0} id = {1},  status = {2}",
-                    InsuranceTenantSure.class.getSimpleName(), reportedStatusHolder.insurance().getPrimaryKey(), reportedStatusHolder.insurance().status().getValue()));
+                    InsuranceTenantSure.class.getSimpleName(), reportedStatusHolder.insurance().getPrimaryKey(), reportedStatusHolder.insurance().status()
+                            .getValue()));
         }
 
         if (needsUpdate) {
             reportedStatusHolder.reportedStatus().setValue(reportClientStatus);
             reportedStatusHolder.statusFrom().setValue(statusFrom);
-            
+
             Persistence.service().persist(reportedStatusHolder);
         }
 
-        return reportedStatusHolder.reportedStatus().getValue();
+        return reportedStatusHolder;
     }
 }
