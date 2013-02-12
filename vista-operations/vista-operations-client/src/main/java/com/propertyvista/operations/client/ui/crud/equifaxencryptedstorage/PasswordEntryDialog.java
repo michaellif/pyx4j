1c8fe14e248c7531629a26c59f9bb938804ae439
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-02-12 23:22:18 +0000

    VISTA-2545 fixed PasswordEntryForm

--- vista-operations/vista-operations-client/src/main/java/com/propertyvista/operations/client/ui/crud/equifaxencryptedstorage/PasswordEntryDialog.java
+++ vista-operations/vista-operations-client/src/main/java/com/propertyvista/operations/client/ui/crud/equifaxencryptedstorage/PasswordEntryDialog.java
@@ -78,6 +78,7 @@ public abstract class PasswordEntryDialog extends OkCancelDialog {
 
         form = new PasswordEntryForm();
         form.initContent();
+        form.populateNew();
 
         setBody(form);
     }
