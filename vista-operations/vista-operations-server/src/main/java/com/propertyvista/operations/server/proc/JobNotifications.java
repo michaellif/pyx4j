26095cb502cf02a56dbd41896d2a854f565e8eab
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-02-13 02:30:44 +0000

    VISTA-75 Operations email corrections

--- vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/proc/JobNotifications.java
+++ vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/proc/JobNotifications.java
@@ -13,20 +13,18 @@
  */
 package com.propertyvista.operations.server.proc;
 
+import com.pyx4j.commons.SimpleMessageFormat;
 import com.pyx4j.config.server.ServerSideConfiguration;
-import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.server.mail.Mail;
 import com.pyx4j.server.mail.MailMessage;
 
+import com.propertyvista.domain.security.OperationsUser;
 import com.propertyvista.operations.domain.scheduler.Run;
 import com.propertyvista.operations.domain.scheduler.Trigger;
 import com.propertyvista.operations.domain.scheduler.TriggerNotification;
 import com.propertyvista.operations.domain.scheduler.TriggerNotificationEvent;
-import com.propertyvista.domain.security.OperationsUser;
 
 public class JobNotifications {
-
-    private static I18n i18n = I18n.get(JobNotifications.class);
 
     public static void notify(Trigger trigger, Run run) {
 
@@ -55,11 +53,16 @@ public class JobNotifications {
         MailMessage m = new MailMessage();
         m.setTo(user.email().getValue());
         m.setSender(ServerSideConfiguration.instance().getApplicationEmailSender());
-        m.setSubject(i18n.tr("Trigger Run notification {0} {1}", trigger.name().getValue(), run.status().getValue()));
+        m.setSubject(SimpleMessageFormat.format("Trigger Run notification {0} {1}", trigger.name(), run.status()));
 
-        String message = i18n.tr("Execution of process {0} is {1},<br/>\n{2}\n", trigger.name().getValue(), run.status().getValue(), run.stats()
-                .getStringView());
-
+        String message = "<html><body>";
+        message += SimpleMessageFormat.format("Execution of process {0} is {1},<br/>\n", trigger.name(), run.status());
+        message += SimpleMessageFormat.format("For Date: {0}<br/>\n", run.forDate());
+        message += SimpleMessageFormat.format("Statistics: {0}<br/>\n", run.stats().getStringView());
+        if (!run.errorMessage().isNull()) {
+            message += SimpleMessageFormat.format("Error Message: {0}<br/>\n", run.errorMessage());
+        }
+        message += "</body></html>";
         m.setHtmlBody(message);
         Mail.send(m);
     }
