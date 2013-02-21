603a4174e211eeb4468f13433850b1c9b3772e0a
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-02-21 18:37:00 +0000

    VISTA-1458 Changes to support password and gordonnelson lease

--- src/upgrade/1.0.6/2013-02-19/fix_starlight_floorplans.sql
+++ src/upgrade/1.0.6/2013-02-19/fix_starlight_floorplans.sql
@@ -20,8 +20,8 @@ BEGIN
                 WHEN name ~* '^Junior (1 Bedroom|1bdrm)' THEN  'j1bdrm'
                 WHEN name ~* '^Junior 2bdrm' THEN 'j2bdrm'
                 WHEN name ~* '^Junior 3bdrm' THEN 'j3bdrm'
-                ELSE name  END
-        WHERE   id IN (SELECT DISTINCT floorplan FROM starlight.apt_unit);
+                ELSE name  END;
+       -- WHERE   id IN (SELECT DISTINCT floorplan FROM starlight.apt_unit);
         
         
         FOR v_building,v_floorplan_name IN
@@ -29,11 +29,21 @@ BEGIN
         FROM    starlight.floorplan
         WHERE   id IN (SELECT DISTINCT floorplan FROM starlight.apt_unit)
         LOOP
-                SELECT  MIN(id)
+                SELECT  MAX(id)
                 INTO    v_floorplan_id
                 FROM    starlight.floorplan
                 WHERE   building = v_building
                 AND     name = v_floorplan_name;
+                
+                IF NOT EXISTS ( SELECT 'x' FROM starlight.floorplan_amenity
+                                WHERE floorplan = v_floorplan_id) 
+                THEN
+                        INSERT INTO starlight.floorplan_amenity (id,description,name,floorplan_type,floorplan,order_in_parent)
+                        (SELECT nextval('public.floorplan_amenity_seq') AS id,description,name,floorplan_type,v_floorplan_id,order_in_parent
+                        FROM starlight.floorplan_amenity
+                        WHERE floorplan = (SELECT MIN(id) FROM starlight.floorplan WHERE building = v_building AND name = v_floorplan_name));
+                END IF;
+                           
                 
                 UPDATE  starlight.apt_unit
                 SET     floorplan = v_floorplan_id
@@ -45,15 +55,38 @@ END;
 $$
 LANGUAGE plpgsql VOLATILE;
 
+CREATE OR REPLACE FUNCTION _dba_.remove_extra_starlight_media() RETURNS VOID AS
+$$
+DECLARE 
+        v_min_id        BIGINT;
+        v_md5           VARCHAR(32);
+BEGIN
+        FOR v_min_id,v_md5 IN
+        SELECT  MIN(id),md5(content)
+        FROM    starlight.file_blob
+        GROUP BY md5(content)
+        HAVING COUNT(id) > 1
+        LOOP
+                UPDATE  starlight.media
+                SET     media_file_blob_key = v_min_id
+                WHERE   media_file_blob_key IN 
+                (SELECT id FROM starlight.file_blob WHERE md5(content) = v_md5);
+        END LOOP; 
+       
+        DELETE FROM starlight.file_blob
+        WHERE id NOT IN (SELECT DISTINCT media_file_blob_key FROM starlight.media);
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
 BEGIN TRANSACTION;
-
-ALTER TABLE starlight.apt_unit ADD COLUMN orig_floorplan BIGINT;
-
-UPDATE  starlight.apt_unit
-SET     orig_floorplan = floorplan;
 
 SELECT * FROM _dba_.fix_starlight_floorplans();
 
+DELETE FROM starlight.floorplan_amenity WHERE floorplan NOT IN (SELECT DISTINCT floorplan FROM starlight.apt_unit);
+
+
+DELETE FROM starlight.floorplan WHERE id NOT IN (SELECT DISTINCT floorplan FROM starlight.apt_unit);
 
 -- ROLLBACK;
         
