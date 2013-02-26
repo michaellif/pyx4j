a10496efcd6c40461a07e010100e0737da678829
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-02-26 03:49:49 +0000

    Do not start new connection if already running in Suppress mode

--- entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/EntityPersistenceServiceRDB.java
+++ entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/EntityPersistenceServiceRDB.java
@@ -265,7 +265,9 @@ public class EntityPersistenceServiceRDB implements IEntityPersistenceService, I
             createTransactionContext(persistenceContext, TransactionType.ExplicitTransaction, backgroundProcess);
             break;
         case Suppress:
-            createTransactionContext(persistenceContext, TransactionType.AutoCommit, backgroundProcess);
+            if ((persistenceContext == null) || (!persistenceContext.isExplicitTransaction())) {
+                createTransactionContext(persistenceContext, TransactionType.AutoCommit, backgroundProcess);
+            }
             break;
         }
     }
