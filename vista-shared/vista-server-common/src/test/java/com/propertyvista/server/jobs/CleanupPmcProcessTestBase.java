42f52cc7801a6d881db31627ca25b72f968fc1d2
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-02-27 04:12:03 +0000

    VISTA-2597 Use UofW test fixed

--- vista-shared/vista-server-common/src/test/java/com/propertyvista/server/jobs/CleanupPmcProcessTestBase.java
+++ vista-shared/vista-server-common/src/test/java/com/propertyvista/server/jobs/CleanupPmcProcessTestBase.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -19,6 +19,7 @@ import java.util.Date;
 
 import com.pyx4j.commons.Key;
 import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.server.TransactionScopeOption;
 import com.pyx4j.entity.shared.EntityFactory;
 
 import com.propertyvista.config.tests.VistaDBTestBase;
@@ -37,7 +38,7 @@ public class CleanupPmcProcessTestBase extends VistaDBTestBase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        Persistence.service().startTransaction();
+        Persistence.service().startTransaction(TransactionScopeOption.Suppress, false);
         mockupConfig = new MockupCleanupPmcProcessConfig();
         cleanupProcessInstance = new CleanupPmcProcess(mockupConfig);
     }
