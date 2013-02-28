bab92a3805959f9fe1f9979839d6f1eaee4ee978
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-02-28 03:40:35 +0000

    VISTA-1458 minor change to production data loading script

--- src/upgrade/dba_scripts/load_prod_data.sh
+++ src/upgrade/dba_scripts/load_prod_data.sh
@@ -5,6 +5,9 @@
 SQLDIR='/home/akinareevski/tmp/import';
 DBNAME='vista_trunk';
 
+psql -U vista -h localhost -d $DBNAME -c 'ALTER SCHEMA _admin_ RENAME TO new_admin';
+psql -U vista -h localhost -d $DBNAME -c 'ALTER SCHEMA _expiring_ RENAME TO new_expiring';
+psql -U vista -h localhost -d $DBNAME -c 'ALTER SCHEMA public RENAME TO new_public';
 
 cd $SQLDIR;
 
