db7f948bfbd8b2df8f13593b169f97b115887b36
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-03-01 00:31:09 +0000

    VISTA-558 correction to reconciliation parser '5' -> 0.05$

--- vista-shared/vista-payment/src/main/java/com/propertyvista/payment/pad/CaledonPadUtils.java
+++ vista-shared/vista-payment/src/main/java/com/propertyvista/payment/pad/CaledonPadUtils.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -35,7 +35,10 @@ public class CaledonPadUtils {
             String valueCents;
             String valueDollars;
             int len = value.length();
-            if (len <= 2) {
+            if (len == 1) {
+                valueCents = "0" + value;
+                valueDollars = "0";
+            } else if (len == 2) {
                 valueCents = value;
                 valueDollars = "0";
             } else {
