b9915efb16fdbc714b0f6b2a1e0be6336c163989
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-03-07 00:25:04 +0000

    VISTA-1709 _admin_ schema change - check constraint modifications

--- src/upgrade/1.0.8/migration/migrate_admin_schema.sql
+++ src/upgrade/1.0.8/migration/migrate_admin_schema.sql
@@ -33,6 +33,10 @@ SET search_path = '_admin_';
         ***     ======================================================================================================
         **/
         
+        -- Check constraints
+        ALTER TABLE dev_card_service_simulation_transaction DROP CONSTRAINT dev_card_service_simulation_transaction_transaction_type_e_ck;
+        ALTER TABLE scheduler_trigger DROP CONSTRAINT scheduler_trigger_trigger_type_e_ck;
+
        
         
         /**
@@ -57,11 +61,19 @@ SET search_path = '_admin_';
         ***
         ***     ========================================================================================================
         **/
-        
-        
+               
+        -- Check constraints
+        ALTER TABLE dev_card_service_simulation_transaction ADD CONSTRAINT dev_card_service_simulation_transaction_tp_e_ck 
+                CHECK ((tp) IN ('completion', 'preAuthorization', 'preAuthorizationReversal', 'returnVoid', 'sale'));
+ 
+        ALTER TABLE scheduler_trigger ADD CONSTRAINT scheduler_trigger_trigger_type_e_ck 
+                CHECK ((trigger_type) IN ('billing', 'cleanup', 'equifaxRetention', 'initializeFutureBillingCycles', 'leaseActivation', 
+                'leaseCompletion', 'leaseRenewal', 'paymentsBmoReceive', 'paymentsIssue', 'paymentsPadReceiveAcknowledgment', 
+                'paymentsPadReceiveReconciliation', 'paymentsPadSend', 'paymentsScheduledCreditCards', 'paymentsScheduledEcheck', 
+                'paymentsTenantSure', 'tenantSureCancellation', 'tenantSureHQUpdate', 'tenantSureReports', 'tenantSureTransactionReports', 
+                'test', 'updateArrears', 'updatePaymentsSummary', 'vistaBusinessReport', 'yardiBatchProcess', 'yardiImportProcess'));
 
-
-                
+              
 
         /**
         ***     ============================================================================================================
