89b9042ef1b830ba74b1e20d37e41b96865b234b
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-03-19 04:46:15 +0000

    Print more system information

--- essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/admin/ConfigInfoServlet.java
+++ essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/admin/ConfigInfoServlet.java
@@ -22,6 +22,7 @@ package com.pyx4j.essentials.server.admin;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -30,7 +31,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.pyx4j.config.server.ApplicationVersion;
 import com.pyx4j.config.server.ServerSideConfiguration;
+import com.pyx4j.config.server.SystemDateManager;
 import com.pyx4j.log4j.LoggerConfig;
+import com.pyx4j.server.contexts.Context;
 
 @SuppressWarnings("serial")
 public class ConfigInfoServlet extends HttpServlet {
@@ -50,16 +53,21 @@ public class ConfigInfoServlet extends HttpServlet {
 
     protected String buildConfigurationText() {
         StringBuilder b = new StringBuilder();
-        b.append("Application Version     : ").append(ApplicationVersion.getProductVersion()).append("\n");
-        b.append("Application Build       : ").append(ApplicationVersion.getBuildLabel()).append("\n");
-        b.append("Application BuildTime   : ").append(ApplicationVersion.getBuildDate()).append("\n");
-        b.append("Application ScmRevision : ").append(ApplicationVersion.getScmRevision()).append("\n\n");
+        b.append("Application Version      : ").append(ApplicationVersion.getProductVersion()).append("\n");
+        b.append("Application Build        : ").append(ApplicationVersion.getBuildLabel()).append("\n");
+        b.append("Application BuildTime    : ").append(ApplicationVersion.getBuildDate()).append("\n");
+        b.append("Application ScmRevision  : ").append(ApplicationVersion.getScmRevision()).append("\n");
+        b.append("\n");
 
-        b.append("Pyx4j Version     : ").append(ApplicationVersion.getPyxBuildLabel()).append("\n");
-        b.append("Pyx4j BuildTime   : ").append(ApplicationVersion.getPyxBuildDate()).append("\n");
-        b.append("Pyx4j ScmRevision : ").append(ApplicationVersion.getPyxScmRevision()).append("\n\n");
+        b.append("Pyx4j Version            : ").append(ApplicationVersion.getPyxBuildLabel()).append("\n");
+        b.append("Pyx4j BuildTime          : ").append(ApplicationVersion.getPyxBuildDate()).append("\n");
+        b.append("Pyx4j ScmRevision        : ").append(ApplicationVersion.getPyxScmRevision()).append("\n");
+        b.append("\n");
 
-        b.append("ContextName : ").append(LoggerConfig.getContextName()).append("\n");
+        b.append("ContextName              : ").append(LoggerConfig.getContextName()).append("\n");
+        b.append("ServerInfo               : ").append(Context.getRequest().getServletContext().getServerInfo()).append("\n");
+        b.append("SystemDate               : ").append(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z").format(SystemDateManager.getDate())).append("\n");
+        b.append("\n");
 
         ServerSideConfiguration conf = ServerSideConfiguration.instance();
         b.append("ServerSideConfiguration  : ").append(conf.getClass().getName()).append("\n");
@@ -68,11 +76,21 @@ public class ConfigInfoServlet extends HttpServlet {
         b.append("MainApplicationURL       : ").append(conf.getMainApplicationURL()).append("\n");
         b.append("SessionCookieName        : ").append(conf.getSessionCookieName()).append("\n");
         b.append("persistenceNamePrefix    : ").append(conf.persistenceNamePrefix()).append("\n");
-        b.append("\nPersistenceConfiguration :\n  ").append(conf.getPersistenceConfiguration().toString().replaceAll("\n", "\n  ")).append("\n");
+        b.append("\n");
+
+        b.append("PersistenceConfiguration :\n  ").append(conf.getPersistenceConfiguration().toString().replaceAll("\n", "\n  ")).append("\n");
 
         b.append(applicationConfigurationText());
+        b.append("\n");
+        b.append("\n");
 
-        b.append("\n\nSystem Properties:\n").append(ServerSideConfiguration.getSystemProperties());
+        b.append("System:\n");
+        b.append("Free memory              : ").append(Runtime.getRuntime().freeMemory() / (1024 * 1024)).append(" MB\n");
+        b.append("Total memory             : ").append(Runtime.getRuntime().totalMemory() / (1024 * 1024)).append(" MB\n");
+        b.append("Max memory               : ").append(Runtime.getRuntime().maxMemory() / (1024 * 1024)).append(" MB\n");
+        b.append("\n");
+
+        b.append("System Properties:\n").append(ServerSideConfiguration.getSystemProperties());
 
         return b.toString();
     }
