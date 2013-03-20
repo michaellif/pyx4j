4ccb38c80ad6f4880e9641f3a6f52cbd9d05bc74
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2013-03-20 23:11:35 +0000

    VISTA-2605 correction to domain definition

--- vista-shared/vista-domain/src/main/java/com/propertyvista/domain/policy/policies/domain/LeaseBillingTypePolicyItem.java
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/policy/policies/domain/LeaseBillingTypePolicyItem.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -14,17 +14,39 @@
 package com.propertyvista.domain.policy.policies.domain;
 
 import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.ColumnId;
+import com.pyx4j.entity.annotations.Detached;
+import com.pyx4j.entity.annotations.Indexed;
+import com.pyx4j.entity.annotations.JoinColumn;
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.OrderColumn;
+import com.pyx4j.entity.annotations.Owner;
 import com.pyx4j.entity.annotations.validator.NotNull;
 import com.pyx4j.entity.shared.IEntity;
 import com.pyx4j.entity.shared.IPrimitive;
 
 import com.propertyvista.domain.financial.BillingAccount.BillingPeriod;
+import com.propertyvista.domain.policy.policies.LeaseBillingPolicy;
 
 //TODO Find a better name
 
 //TODO See VISTA-2605 for comments
 
 public interface LeaseBillingTypePolicyItem extends IEntity {
+
+    @Owner
+    @Detached
+    @NotNull
+    @MemberColumn(notNull = true)
+    @Indexed
+    @JoinColumn
+    LeaseBillingPolicy leaseBillingPolicy();
+
+    interface OrderId extends ColumnId {
+    }
+
+    @OrderColumn(OrderId.class)
+    IPrimitive<Long> orderInParent();
 
     IPrimitive<BillingPeriod> billingPeriod();
 
