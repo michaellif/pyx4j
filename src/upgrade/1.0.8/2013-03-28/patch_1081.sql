14c42a2d53ac62933e2b08524a8564a950e8e850
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-03-29 20:09:40 +0000

    VISTA-1709 patch 1.0.8.1

--- src/upgrade/1.0.8/2013-03-28/patch_1081.sql
+++ src/upgrade/1.0.8/2013-03-28/patch_1081.sql
@@ -10,12 +10,28 @@
 
 CREATE OR REPLACE FUNCTION _dba_.patch_1081(v_schema_name TEXT) RETURNS VOID AS
 $$
+DECLARE 
+        v_row_count     INT := 0;
 BEGIN
+        -- payment_record
+        
         EXECUTE 'ALTER TABLE '||v_schema_name||'.payment_record ADD COLUMN yardi_document_number VARCHAR(500)';
         
         EXECUTE 'UPDATE '||v_schema_name||'.payment_record '
                 ||'SET yardi_document_number = ''eCheque (EFT):''||id ';
+                
+        -- resident_portal_settings - only when table is empty
         
+        EXECUTE 'SELECT COUNT(id) '
+                ||'FROM         '||v_schema_name||'.resident_portal_settings '
+                INTO    v_row_count;
+                
+        IF ( v_row_count = 0 ) 
+        THEN
+                EXECUTE 'INSERT INTO '||v_schema_name||'.resident_portal_settings (id,enabled,use_custom_html) '
+                        ||' VALUES (nextval(''public.resident_portal_settings_seq''),TRUE,FALSE)';
+        END IF;
+                      
 END;
 $$
 LANGUAGE plpgsql VOLATILE;
