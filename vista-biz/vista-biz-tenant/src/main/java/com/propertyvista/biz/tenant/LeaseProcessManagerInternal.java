f5ec14d0dc73c73799f15bcc144dee36ec9d5a5f
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-04-02 15:55:51 +0000

    VISTA-393 Adding LeaseProcessFacade

---
+++ vista-biz/vista-biz-tenant/src/main/java/com/propertyvista/biz/tenant/LeaseProcessManagerInternal.java
@@ -0,0 +1,33 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2013-04-02
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.biz.tenant;
+
+import com.pyx4j.commons.LogicalDate;
+
+import com.propertyvista.biz.ExecutionMonitor;
+
+public class LeaseProcessManagerInternal {
+
+    public void leaseActivation(ExecutionMonitor executionMonitor, LogicalDate date) {
+        // TODO Auto-generated method stub
+    }
+
+    public void leaseRenewal(ExecutionMonitor executionMonitor, LogicalDate date) {
+        // TODO Auto-generated method stub
+    }
+
+    public void leaseCompletion(ExecutionMonitor executionMonitor, LogicalDate date) {
+        // TODO Auto-generated method stub
+    }
+}
