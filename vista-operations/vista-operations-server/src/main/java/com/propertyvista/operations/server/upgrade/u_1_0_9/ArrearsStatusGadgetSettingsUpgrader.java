377fcb11f3426a5c68e2be3919017f3b7015f27f
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-04-11 18:45:56 +0000

    VISTA-2832 added upgrade procedure with migration of arrears status gadget settings category (DebitType -> ARCode.Type)

---
+++ vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/upgrade/u_1_0_9/ArrearsStatusGadgetSettingsUpgrader.java
@@ -0,0 +1,80 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2013-04-11
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.operations.server.upgrade.u_1_0_9;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.propertyvista.domain.financial.ARCode;
+
+class ArrearsStatusGadgetSettingsUpgrader {
+
+    private static final Logger log = LoggerFactory.getLogger(ArrearsStatusGadgetSettingsUpgrader.class);
+
+    private static final Pattern categoryPattern = Pattern.compile("(\\<category>(\\w*)\\</category>)");
+
+    private static final Map<String, String> categoryUpgradeMap;
+    static {
+        HashMap<String, String> map = new HashMap<String, String>();
+        // debit types:
+        // lease, parking, pet, addOn, utility, locker, booking, deposit, accountCharge, nsf, latePayment, other, total;
+
+        map.put("total", "");
+        map.put("lease", ARCode.Type.Residential.name()); // TODO we also have 'short term' and 'commercial' ARCodes
+        map.put("parking", ARCode.Type.Parking.name());
+        map.put("pet", ARCode.Type.Pet.name());
+        map.put("addOn", ARCode.Type.AddOn.name());
+        map.put("utility", ARCode.Type.Utility.name());
+        map.put("locker", ARCode.Type.Locker.name());
+        // TODO map DebitType: 'booking' ?
+        map.put("deposit", ARCode.Type.Deposit.name());
+        map.put("accountCharge", ARCode.Type.AccountCharge.name());
+        map.put("nsf", ARCode.Type.NSF.name());
+        map.put("latePayment", ARCode.Type.LatePayment.name());
+        map.put("other", ARCode.Type.ExternalCharge.name());
+
+        categoryUpgradeMap = Collections.unmodifiableMap(map);
+    }
+
+    /**
+     * converts category: in previous versions it was DebitType, but in 1.0.9 it's going to be ARCode.Type
+     */
+    public static final String upgradeSettings(String settingsXml) {
+        Matcher m = categoryPattern.matcher(settingsXml);
+        if (m.find()) {
+            String beforeCategoryName = m.group(2);
+            String upgradedCategoryName = categoryUpgradeMap.get(beforeCategoryName);
+            if (upgradedCategoryName == null) {
+                log.warn("couldn't find a matching upgradable ARCodeType for DebitType '" + beforeCategoryName
+                        + "', the default mapping for 'total' will be used. settings: '" + settingsXml + "'");
+                upgradedCategoryName = "";
+            }
+            String upgradedConfigPortion =//@formatter:off 
+                "<filterByCategory>" + !"".equals(upgradedCategoryName) + "</filterByCategory>" +
+                "<category>" + upgradedCategoryName + "</category>";
+            //@formatter:on
+
+            return m.replaceAll(upgradedConfigPortion);
+        } else {
+            log.warn("could not find category in arrears status gadget settings for the following settings: '" + settingsXml + "'");
+            return settingsXml;
+        }
+    }
+}
---
+++ vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/upgrade/u_1_0_9/UpgradeProcedure109.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2013-04-11
+ * @author ArtyomB
+ * @version $Id$
+ */
+package com.propertyvista.operations.server.upgrade.u_1_0_9;
+
+import java.util.List;
+
+import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.shared.criterion.EntityQueryCriteria;
+
+import com.propertyvista.domain.dashboard.GadgetMetadataHolder;
+import com.propertyvista.domain.dashboard.gadgets.type.ArrearsSummaryGadgetMetadata;
+import com.propertyvista.operations.server.upgrade.UpgradeProcedure;
+
+public class UpgradeProcedure109 implements UpgradeProcedure {
+
+    @Override
+    public int getUpgradeStepsCount() {
+        return 1;
+    }
+
+    @Override
+    public void runUpgradeStep(int upgradeStep) {
+        switch (upgradeStep) {
+        case 1:
+            runMigrateArrearsGadgetSettigns();
+            break;
+        default:
+            throw new IllegalArgumentException("Wrong step number: " + upgradeStep);
+        }
+    }
+
+    private void runMigrateArrearsGadgetSettigns() {
+        EntityQueryCriteria<GadgetMetadataHolder> criteria = EntityQueryCriteria.create(GadgetMetadataHolder.class);
+        criteria.eq(criteria.proto().className(), ArrearsSummaryGadgetMetadata.class);
+        List<GadgetMetadataHolder> gadgetSettingsList = Persistence.service().query(criteria);
+        for (GadgetMetadataHolder gadgetSettings : gadgetSettingsList) {
+            gadgetSettings.serializedForm().setValue(ArrearsStatusGadgetSettingsUpgrader.upgradeSettings(gadgetSettings.serializedForm().getValue()));
+        }
+    }
+
+}
