68d2e61c36b606ee14552f189bf3c6c3036b5ef7
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2013-04-17 22:36:38 +0000

    VISTA-2862 edited a test for pad payment amounts

--- vista-biz/vista-biz-financial/src/test/java/com/propertyvista/biz/financial/PaymentSuite.java
+++ vista-biz/vista-biz-financial/src/test/java/com/propertyvista/biz/financial/PaymentSuite.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -16,14 +16,15 @@ package com.propertyvista.biz.financial;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.propertyvista.biz.financial.payment.PadPaymentMethodCancellationTest;
+import com.propertyvista.biz.financial.payment.PadPaymentAmountValidationTest;
 import com.propertyvista.biz.financial.payment.PadPaymentChargeBaseSunnyDayScenarioTest;
+import com.propertyvista.biz.financial.payment.PadPaymentMethodCancellationTest;
 import com.propertyvista.biz.financial.payment.PadTransactionUtilsTest;
 import com.propertyvista.biz.financial.payment.PaymentMethodPersistenceEcheckTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ PaymentMethodPersistenceEcheckTest.class, PadTransactionUtilsTest.class, PadPaymentChargeBaseSunnyDayScenarioTest.class,
-        PadPaymentMethodCancellationTest.class })
+        PadPaymentMethodCancellationTest.class, PadPaymentAmountValidationTest.class })
 public class PaymentSuite {
 
 }
--- vista-biz/vista-biz-financial/src/test/java/com/propertyvista/biz/financial/payment/PadPaymentAmountValidationTest.java
+++ vista-biz/vista-biz-financial/src/test/java/com/propertyvista/biz/financial/payment/PadPaymentAmountValidationTest.java
@@ -15,6 +15,7 @@ package com.propertyvista.biz.financial.payment;
 
 import java.math.BigDecimal;
 
+import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
 import com.pyx4j.config.server.ServerSideFactory;
@@ -24,10 +25,12 @@ import com.propertyvista.biz.financial.FinancialTestBase.RegressionTests;
 import com.propertyvista.biz.financial.ar.ARFacade;
 import com.propertyvista.biz.financial.billing.BillTester;
 import com.propertyvista.domain.policy.policies.LeaseBillingPolicy;
+import com.propertyvista.domain.tenant.lease.BillableItem;
 import com.propertyvista.domain.tenant.lease.BillableItemAdjustment.Type;
 import com.propertyvista.test.mock.MockConfig;
 
 @Category(RegressionTests.class)
+@Ignore
 public class PadPaymentAmountValidationTest extends FinancialTestBase {
 
     @Override
@@ -40,6 +43,83 @@ public class PadPaymentAmountValidationTest extends FinancialTestBase {
 
     public void testScenario() throws Exception {
 
+        setSysDate("31-Mar-2013");
+
+        setBillingBatchProcess();
+        setLeaseBatchProcess();
+        setDepositBatchProcess();
+        setPaymentBatchProcess();
+
+        createLease("1-Apr-2013", "31-Mar-2014", new BigDecimal(977.77), null);
+        BillableItem parking = addOutdoorParking("1-Apr-2013", "31-Mar-2014"); // $80
+        BillableItem locker = addLargeLocker("1-Apr-2013", "31-Mar-2014"); // $60
+
+        addFeatureAdjustment(parking.uid().getValue(), "-80", Type.monetary, "1-Apr-2013", "31-Mar-2014");
+        addFeatureAdjustment(locker.uid().getValue(), "-20", Type.monetary, "1-Apr-2013", "31-Mar-2014");
+
+        approveApplication(true);
+
+        advanceSysDate("1-Apr-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("0.00").
+        taxes("122.13").
+        totalDueAmount("2210.20"); // 1070.30(deposit) + 977.77(lease) + 122.13(tax) + 140(locker + parking) - 100(adjustments)
+        // @formatter:on
+
+        advanceSysDate("1-May-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("0.00").
+        taxes("122.13").
+        totalDueAmount("3400.10"); // 2210.20(previous) + 1139.90(monthly charges) + 50(late payment fees)
+        // @formatter:on
+
+        setPreauthorizedPayment("1");
+
+        advanceSysDate("1-Jun-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("0.00").
+        taxes("122.13").
+        totalDueAmount("4590.00"); // 3400.10(previous) + 1139.90(monthly charges) + 50(late payment fees)
+        // @formatter:on
+
+        advanceSysDate("1-Jul-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("-1229.50"). // 3 lockers + 1 month worth of rent + tax
+        taxes("122.13").
+        totalDueAmount("4550.40"); // 4590.00(previous) + 1139.90(monthly charges) + 50(late payment fees) - 1229.50(received amount)
+        // @formatter:on
+
+        advanceSysDate("1-Aug-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("-1139.90"). // 1 locker + 1 month worth of rent + tax
+        taxes("122.13").
+        totalDueAmount("4600.40"); // 4550.00(previous) + 50(late payment fees)
+        // @formatter:on
+
+        advanceSysDate("1-Dec-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("-1139.90"). // 1 locker + 1 month worth of rent + tax
+        taxes("122.13").
+        totalDueAmount("4800.40"); // 4600.40(previous) + 200(4 late payments)
+        // @formatter:on
+
+        printTransactionHistory(ServerSideFactory.create(ARFacade.class).getTransactionHistory(retrieveLease().billingAccount()));
+
+    }
+
+    public void testScenario2() throws Exception {
         setSysDate("31-Mar-2013");
 
         setBillingBatchProcess();
@@ -71,7 +151,7 @@ public class PadPaymentAmountValidationTest extends FinancialTestBase {
         new BillTester(getLatestBill()).
         paymentReceivedAmount("0.00").
         taxes("122.13").
-        totalDueAmount("3400.10"); // 2210.20(previous) + 1139.90(monthly charges and adjustments all in one) + 50(late payment fees) = 3400.10
+        totalDueAmount("3400.10"); // 2210.20(previous) + 1139.90(monthly charges) + 50(late payment fees) = 3400.10
         // @formatter:on
 
         setPreauthorizedPayment("1");
@@ -82,25 +162,34 @@ public class PadPaymentAmountValidationTest extends FinancialTestBase {
         new BillTester(getLatestBill()).
         paymentReceivedAmount("0.00").
         taxes("122.13").
-        totalDueAmount("4590.00"); // 3400.10(previous) + 1139.90(monthly charges and adjustments all in one) + 50(late payment fees) = 4590.00
+        totalDueAmount("4590.00"); // 3400.10(previous) + 1139.90(monthly charges) + 50(late payment fees) = 4590.00
         // @formatter:on
 
         advanceSysDate("1-Jul-2013");
 
         // @formatter:off
         new BillTester(getLatestBill()).
-        paymentReceivedAmount("-1453.50"). // 3 lockers + 3 parkings + tax + 1 month worth of rent
+        paymentReceivedAmount("-1453.50"). // 3 lockers + 1 month worth of rent + tax
         taxes("122.13").
-        totalDueAmount("4326.40"); // 4590.00(previous) + 1139.90(monthly charges and adjustments all in one) + 50(late payment fees) - 1453.50(received amount)
+        totalDueAmount("4326.40"); // 4590.00(previous) + 1139.90(monthly charges) + 50(late payment fees) - 1453.50(received amount)
         // @formatter:on
 
         advanceSysDate("1-Aug-2013");
 
         // @formatter:off
         new BillTester(getLatestBill()).
-        paymentReceivedAmount("-1610.30"). // 4 lockers + 4 parkings (????) + 1 month worth of rent + tax
+        paymentReceivedAmount("1139.90"). // locker + 1 month worth of rent + tax
         taxes("122.13").
-        totalDueAmount("3906.00");
+        totalDueAmount("4376.40"); // 4326.40(previous) + 50(late payment fees)
+        // @formatter:on
+
+        advanceSysDate("2-Dec-2013");
+
+        // @formatter:off
+        new BillTester(getLatestBill()).
+        paymentReceivedAmount("-1139.90"). // locker + 1 month worth of rent + tax
+        taxes("122.13").
+        totalDueAmount("4576.40"); //4376.40(previous) + 200(4 months late fees)
         // @formatter:on
 
         printTransactionHistory(ServerSideFactory.create(ARFacade.class).getTransactionHistory(retrieveLease().billingAccount()));
