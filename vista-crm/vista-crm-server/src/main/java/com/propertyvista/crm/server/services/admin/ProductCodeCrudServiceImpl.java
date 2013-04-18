481b381e3806533dd7f938d0490a8f3249dbf2ea
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-04-18 15:26:02 +0000

    VISTA-2868 added validation for uniqueness of yardi charge codes

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/ProductCodeCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/ProductCodeCrudServiceImpl.java
@@ -13,12 +13,26 @@
  */
 package com.propertyvista.crm.server.services.admin;
 
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import com.pyx4j.commons.CommonsStringUtils;
+import com.pyx4j.commons.Key;
+import com.pyx4j.commons.UserRuntimeException;
 import com.pyx4j.entity.server.AbstractCrudServiceImpl;
+import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.shared.criterion.EntityQueryCriteria;
+import com.pyx4j.i18n.shared.I18n;
 
 import com.propertyvista.crm.rpc.services.admin.ProductCodeCrudService;
 import com.propertyvista.domain.financial.ARCode;
+import com.propertyvista.domain.financial.offering.YardiChargeCode;
+import com.propertyvista.shared.config.VistaFeatures;
 
 public class ProductCodeCrudServiceImpl extends AbstractCrudServiceImpl<ARCode> implements ProductCodeCrudService {
+
+    private static final I18n i18n = I18n.get(ProductCodeCrudServiceImpl.class);
 
     public ProductCodeCrudServiceImpl() {
         super(ARCode.class);
@@ -27,5 +41,43 @@ public class ProductCodeCrudServiceImpl extends AbstractCrudServiceImpl<ARCode> 
     @Override
     protected void bind() {
         bindCompleteDBO();
+    }
+
+    @Override
+    public void save(AsyncCallback<Key> callback, ARCode dto) {
+        assertIsValid(dto);
+        super.save(callback, dto);
+    }
+
+    @Override
+    public void create(AsyncCallback<Key> callback, ARCode dto) {
+        assertIsValid(dto);
+        super.create(callback, dto);
+    }
+
+    private void assertIsValid(ARCode arCode) {
+        if (VistaFeatures.instance().yardiIntegration()) {
+            for (YardiChargeCode yardiCode : arCode.yardiChargeCodes()) {
+                assertValidYardiCode(arCode, yardiCode.yardiChargeCode().getValue());
+            }
+        }
+    }
+
+    private void assertValidYardiCode(ARCode arCode, String yardiCode) {
+        if (!CommonsStringUtils.isStringSet(yardiCode)) {
+            throw new UserRuntimeException(i18n.tr("Yardi Charge Code cannot be empty"));
+        }
+
+        EntityQueryCriteria<ARCode> criteria = EntityQueryCriteria.create(ARCode.class);
+        criteria.eq(criteria.proto().yardiChargeCodes().$().yardiChargeCode(), yardiCode);
+        List<ARCode> arCodes = Persistence.service().query(criteria);
+        for (ARCode otherCode : arCodes) {
+            if (!otherCode.getPrimaryKey().equals(arCode.getPrimaryKey())
+                    && otherCode.type().getValue().getActionType() == otherCode.type().getValue().getActionType()) {
+                throw new UserRuntimeException(i18n.tr("An ARCode with same mapping to yardi code already exits: ARCode \"{0}\" > YardiCode \"{1}\"", otherCode
+                        .name().getValue(), yardiCode));
+            }
+        }
+
     }
 }
