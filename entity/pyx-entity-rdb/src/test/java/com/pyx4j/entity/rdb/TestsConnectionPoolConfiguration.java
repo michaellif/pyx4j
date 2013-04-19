e263da305b0e5eba85d11b85ca1531c02564cb23
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-04-19 04:23:15 +0000

    correction to test connection timeout

--- entity/pyx-entity-rdb/src/test/java/com/pyx4j/entity/rdb/TestsConnectionPoolConfiguration.java
+++ entity/pyx-entity-rdb/src/test/java/com/pyx4j/entity/rdb/TestsConnectionPoolConfiguration.java
@@ -20,8 +20,9 @@
  */
 package com.pyx4j.entity.rdb;
 
-import com.pyx4j.entity.rdb.cfg.ConnectionPoolType;
+import com.pyx4j.commons.Consts;
 import com.pyx4j.entity.rdb.cfg.Configuration.ConnectionPoolConfiguration;
+import com.pyx4j.entity.rdb.cfg.ConnectionPoolType;
 
 public class TestsConnectionPoolConfiguration extends ConnectionPoolConfiguration {
 
@@ -29,6 +30,18 @@ public class TestsConnectionPoolConfiguration extends ConnectionPoolConfiguratio
         super(connectionType);
         minPoolSize = 1;
         maxPoolSize = 5;
+        if (unreturnedConnectionTimeout != 0) {
+            switch (connectionType) {
+            case BackgroundProcess:
+                unreturnedConnectionTimeout = 1 * Consts.MIN2SEC;
+                break;
+            case TransactionProcessing:
+                unreturnedConnectionTimeout = 1 * Consts.MIN2SEC;
+                break;
+            default:
+                break;
+            }
+        }
     }
 
 }
