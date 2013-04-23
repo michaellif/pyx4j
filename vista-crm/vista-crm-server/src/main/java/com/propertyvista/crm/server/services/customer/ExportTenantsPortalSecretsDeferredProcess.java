4fe0643f65057c883e594ae180431dba8ab36589
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2013-04-23 21:29:50 +0000

    VISTA-2877 changed the formatting of portal secrets file

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/customer/ExportTenantsPortalSecretsDeferredProcess.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/customer/ExportTenantsPortalSecretsDeferredProcess.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -26,6 +26,7 @@ import com.pyx4j.gwt.rpc.deferred.DeferredProcessProgressResponse;
 import com.pyx4j.gwt.server.deferred.AbstractDeferredProcess;
 
 import com.propertyvista.domain.pmc.Pmc;
+import com.propertyvista.domain.property.asset.building.Building;
 import com.propertyvista.domain.tenant.lease.Lease;
 import com.propertyvista.domain.tenant.lease.Tenant;
 import com.propertyvista.server.jobs.TaskRunner;
@@ -74,17 +75,28 @@ public class ExportTenantsPortalSecretsDeferredProcess extends AbstractDeferredP
         Persistence.service().endTransaction();
 
         formatter.header("Address");
+        formatter.header("City Zip");
         formatter.header("Unit");
-        formatter.header("Tenant Name");
+        formatter.header("First");
+        formatter.header("Last/Middle");
+        formatter.header("Last");
         formatter.header("Portal Registration Token");
         formatter.newRow();
 
         maximum = tenants.size();
         progress = 0;
         for (Tenant tenant : tenants) {
-            formatter.cell(tenant.lease().unit().building().info().address().getStringView());
+            formatter.cell(getAddress(tenant.lease().unit().building()));
+            formatter.cell(getCityZip(tenant.lease().unit().building()));
             formatter.cell(tenant.lease().unit().info().number().getValue());
-            formatter.cell(tenant.customer().person().name().getStringView());
+            formatter.cell(tenant.customer().person().name().firstName().getStringView());
+            if (tenant.customer().person().name().middleName() != null && !tenant.customer().person().name().middleName().isNull()) {
+                formatter.cell(tenant.customer().person().name().middleName().getStringView());
+                formatter.cell(tenant.customer().person().name().lastName().getStringView());
+            } else {
+                formatter.cell(tenant.customer().person().name().lastName().getStringView());
+                formatter.cell("");
+            }
             formatter.cell(tenant.customer().portalRegistrationToken().getValue());
             formatter.newRow();
             ++progress;
@@ -94,6 +106,22 @@ public class ExportTenantsPortalSecretsDeferredProcess extends AbstractDeferredP
         d.save(fileName);
         completed = true;
 
+    }
+
+    private String getAddress(Building building) {
+        String address = "";
+        address += building.info().address().streetNumber() != null ? building.info().address().streetNumber().getValue() + " " : "";
+        address += building.info().address().streetName() != null ? building.info().address().streetName().getValue() + " " : "";
+        address += building.info().address().streetType() != null ? building.info().address().streetType().getValue() : "";
+        return address;
+    }
+
+    private String getCityZip(Building building) {
+        String cityZip = "";
+        cityZip += building.info().address().city() != null ? building.info().address().city().getValue() + ", " : "";
+        cityZip += building.info().address().province() != null ? building.info().address().province().name().getValue() + " " : "";
+        cityZip += building.info().address().postalCode() != null ? building.info().address().postalCode().getValue() : "";
+        return cityZip;
     }
 
     @Override
