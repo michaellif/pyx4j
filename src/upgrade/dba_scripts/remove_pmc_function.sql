a61cc130e69432bb0829b173f8c1e3d684038708
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-04-29 18:54:06 +0000

    VISTA-1458 PMC removal script modified

--- src/upgrade/dba_scripts/remove_pmc_function.sql
+++ src/upgrade/dba_scripts/remove_pmc_function.sql
@@ -49,6 +49,20 @@ BEGIN
         DELETE FROM _admin_.scheduler_run_data WHERE pmc = v_pmc_id;
         DELETE FROM _admin_.audit_record WHERE namespace = v_namespace;
         DELETE FROM _admin_.admin_pmc_equifax_info WHERE pmc = v_pmc_id;
+        DELETE FROM _admin_.pad_reconciliation_debit_record
+        WHERE reconciliation_summary IN (SELECT id FROM _admin_.pad_reconciliation_summary 
+                                        WHERE merchant_account IN 
+                                        (SELECT id FROM _admin_.admin_pmc_merchant_account_index WHERE pmc = v_pmc_id));
+        DELETE FROM _admin_.pad_reconciliation_summary 
+        WHERE merchant_account IN 
+                (SELECT id FROM _admin_.admin_pmc_merchant_account_index WHERE pmc = v_pmc_id);
+        DELETE FROM _admin_.admin_pmc_merchant_account_index WHERE pmc = v_pmc_id;
+        DELETE FROM _admin_.admin_pmc_yardi_credential WHERE pmc = v_pmc_id;
+        DELETE FROM _admin_.fee_pmc_equifax_fee WHERE pmc = v_pmc_id;
+        
+        DELETE FROM _admin_.admin_pmc_vista_features 
+        WHERE id = (SELECT features FROM _admin_.admin_pmc WHERE id = v_pmc_id);
+        
         DELETE FROM _admin_.admin_pmc WHERE id = v_pmc_id;
         
         EXECUTE 'DROP SCHEMA '||v_namespace||' CASCADE';
