cc4690586254e7949e0ea63b218c943789300123
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-05-07 22:55:44 +0000

    VISTA-2048 adding pad tests

---
+++ vista-biz/vista-biz-financial/src/test/java/com/propertyvista/biz/financial/payment/PadProcessingTest.java
@@ -0,0 +1,66 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2013-05-07
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.biz.financial.payment;
+
+import java.math.BigDecimal;
+
+import org.junit.experimental.categories.Category;
+
+import com.pyx4j.config.server.ServerSideFactory;
+
+import com.propertyvista.biz.financial.FinancialTestBase;
+import com.propertyvista.biz.financial.FinancialTestBase.RegressionTests;
+import com.propertyvista.domain.financial.PaymentRecord;
+import com.propertyvista.domain.payment.LeasePaymentMethod;
+import com.propertyvista.domain.payment.PaymentType;
+import com.propertyvista.domain.policy.policies.LeaseBillingPolicy;
+import com.propertyvista.domain.tenant.Customer;
+import com.propertyvista.domain.tenant.lease.Lease;
+import com.propertyvista.test.mock.MockConfig;
+import com.propertyvista.test.mock.models.CustomerDataModel;
+import com.propertyvista.test.mock.models.LeaseDataModel;
+
+@Category(RegressionTests.class)
+public class PadProcessingTest extends FinancialTestBase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        preloadData();
+    }
+
+    @Override
+    protected void preloadData() {
+        MockConfig config = new MockConfig();
+        config.billConfirmationMethod = LeaseBillingPolicy.BillConfirmationMethod.automatic;
+        preloadData(config);
+    }
+
+    public void testSuccessfulPad() throws Exception {
+        setSysDate("2011-04-01");
+
+        Customer customer = getDataModel(CustomerDataModel.class).addCustomer();
+        getDataModel(CustomerDataModel.class).setCurrentItem(customer);
+
+        Lease lease = getDataModel(LeaseDataModel.class).addLease("2011-04-01", "2012-03-10", new BigDecimal(100), null, customer);
+        getDataModel(LeaseDataModel.class).setCurrentItem(lease);
+
+        LeasePaymentMethod paymentMethod = getDataModel(CustomerDataModel.class).addPaymentMethod(PaymentType.Echeck);
+
+        // Make a payment
+        PaymentRecord paymentRecord = getDataModel(LeaseDataModel.class).createPaymentRecord(paymentMethod, "100");
+
+        ServerSideFactory.create(PaymentFacade.class).processPayment(paymentRecord);
+    }
+}
