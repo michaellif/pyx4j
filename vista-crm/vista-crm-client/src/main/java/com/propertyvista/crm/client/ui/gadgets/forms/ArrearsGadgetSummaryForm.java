5b36f46d11a79f76ea6b47ad19ab6950e4a1d6a3
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-05-10 17:47:42 +0000

    VISTA-2912 set up RIGHT alignment to for form elements

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/gadgets/forms/ArrearsGadgetSummaryForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/gadgets/forms/ArrearsGadgetSummaryForm.java
@@ -14,9 +14,9 @@
 package com.propertyvista.crm.client.ui.gadgets.forms;
 
 import com.google.gwt.dom.client.Style.FontWeight;
-import com.google.gwt.dom.client.Style.TextAlign;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 
@@ -46,32 +46,50 @@ public class ArrearsGadgetSummaryForm extends ZoomableViewForm<ArrearsGadgetData
         FlexTable content = new FlexTable();
         int row = -1;
         content.setWidget(++row, 0, new Label(i18n.tr("This Month:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 1, inject(proto().outstandingThisMonthCount()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 2, inject(proto().outstandingThisMonth()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 
         content.setWidget(++row, 0, new Label(i18n.tr("1-30:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 1, inject(proto().outstanding1to30DaysCount()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 2, inject(proto().outstanding1to30Days()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 
         content.setWidget(++row, 0, new Label(i18n.tr("31-60:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 1, inject(proto().outstanding31to60DaysCount()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 2, inject(proto().outstanding31to60Days()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 
         content.setWidget(++row, 0, new Label(i18n.tr("61-90:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 1, inject(proto().outstanding61to90DaysCount()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 2, inject(proto().outstanding61to90Days()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 
         content.setWidget(++row, 0, new Label(i18n.tr("91+:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 1, inject(proto().outstanding91andMoreDaysCount()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
         content.setWidget(row, 2, inject(proto().outstanding91andMoreDays()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 
         content.setWidget(++row, 0, new Label(i18n.tr("Total:")));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_RIGHT);
+        content.getFlexCellFormatter().getElement(row, 0).getStyle().setFontWeight(FontWeight.BOLD);
         content.setWidget(row, 1, inject(proto().delinquentLeases()));
-        content.setWidget(row, 2, inject(proto().outstandingTotal()));
         content.getFlexCellFormatter().getElement(row, 1).getStyle().setFontWeight(FontWeight.BOLD);
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_RIGHT);
+        content.setWidget(row, 2, inject(proto().outstandingTotal()));
+        content.getFlexCellFormatter().setHorizontalAlignment(row, 2, HasHorizontalAlignment.ALIGN_RIGHT);
         content.getFlexCellFormatter().getElement(row, 2).getStyle().setFontWeight(FontWeight.BOLD);
 
-        content.getElement().getStyle().setTextAlign(TextAlign.RIGHT);
         content.getColumnFormatter().setWidth(1, "100px");
         content.getColumnFormatter().setWidth(2, "150px");
         return content;
