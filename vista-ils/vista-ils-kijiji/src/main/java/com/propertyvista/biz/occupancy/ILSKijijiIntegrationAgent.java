96a7d5f2fe7053cb5acbd2259812a5f0edbbd44e
Stan Porter <stanp@propertyvista.com>
2013-05-10 19:17:01 +0000

    VISTA-2855 base domain and implementation classes

---
+++ vista-ils/vista-ils-kijiji/src/main/java/com/propertyvista/biz/occupancy/ILSKijijiIntegrationAgent.java
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on May 10, 2013
+ * @author stanp
+ * @version $Id$
+ */
+package com.propertyvista.biz.occupancy;
+
+import java.util.List;
+
+import com.propertyvista.domain.property.asset.unit.AptUnit;
+
+// Provides a list of available units for publishing with ILS provider. Uses Occupancy model and ILSPolicy data
+public class ILSKijijiIntegrationAgent {
+
+    public List<AptUnit> getUnitListing() {
+        return null;
+    }
+}
---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/marketing/ils/ILSPolicyItem.java
@@ -0,0 +1,59 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on May 10, 2013
+ * @author stanp
+ * @version $Id$
+ */
+package com.propertyvista.domain.marketing.ils;
+
+import java.math.BigDecimal;
+
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.entity.shared.ISet;
+
+import com.propertyvista.domain.property.asset.building.Building;
+import com.propertyvista.domain.ref.City;
+import com.propertyvista.domain.ref.Province;
+
+/**
+ * ILSPolicy defines a set of attributes to be used to select a subset of available units
+ * that will be published with a given ILS provider
+ */
+public interface ILSPolicyItem extends IEntity {
+
+    public enum ILSProvider {
+        kijiji
+    }
+
+    IPrimitive<ILSProvider> provider();
+
+    ISet<Province> provinces();
+
+    ISet<City> cities();
+
+    ISet<Building> buildings();
+
+    IPrimitive<Integer> maxUnits();
+
+    IPrimitive<Integer> maxUnitsPerBuilding();
+
+    IPrimitive<Integer> minBeds();
+
+    IPrimitive<Integer> maxBeds();
+
+    IPrimitive<Integer> minBaths();
+
+    IPrimitive<Integer> maxBaths();
+
+    IPrimitive<BigDecimal> minPrice();
+
+    IPrimitive<BigDecimal> maxPrice();
+}
