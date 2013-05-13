aa66e35cb479556d43122bdda3138bedfbaf965c
Michael Lifshits <michaellif@propertyvista.com>
2013-05-13 16:55:40 +0000

    VISTA-2855 header fix

--- vista-ils/vista-ils-kijiji/src/main/java/com/propertyvista/ils/kijiji/rs/KijijiApiService.java
+++ vista-ils/vista-ils-kijiji/src/main/java/com/propertyvista/ils/kijiji/rs/KijijiApiService.java
@@ -1,22 +1,14 @@
 /*
- * Pyx4j framework
- * Copyright (C) 2008-2013 pyx4j.com.
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- *
- * Created on May 8, 2013
- * @author Dimitry
- * @version $Id$
+ * Created on May 09, 2013
+ * @author Anatoly
  */
 package com.propertyvista.ils.kijiji.rs;
 
