88e5ef7f1f6ddb03943ceb65e17d051230d733f7
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-06-11 22:55:26 +0000

    VISTA-1458 obfuscation scripts modifications

--- src/upgrade/dba_scripts/obfuscate_prod_data.sql
+++ src/upgrade/dba_scripts/obfuscate_prod_data.sql
@@ -4,9 +4,86 @@
 ***             @version $Revision$ ($Author$) $Date$
 ***
 ***             Function to obfuscate prod data
+***		
+***		Running any of these on production will result in a MAJOR DISASTER!
 ***
 ***     ======================================================================================================================
 **/
+
+
+CREATE OR REPLACE FUNCTION _dba_.random_string(v_source TEXT) RETURNS TEXT
+AS
+$$
+DECLARE
+	v_chars TEXT[] := '{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+	v_result TEXT := substring(v_source,1,1);
+	
+BEGIN
+	IF (v_source IS NULL OR v_source = '')
+	THEN
+		v_result := NULL;
+	ELSE	
+				
+		FOR v_count IN 1..(length(v_source) - 1)
+		LOOP
+			v_result := v_result||v_chars[1+round(random()*25)];
+		END LOOP;
+	END IF;
+
+	RETURN(v_result);
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+/**
+***
+***	Returns pseudo-random date - date supplied minus 30 to 90 days
+***
+**/
+CREATE OR REPLACE FUNCTION _dba_.random_date(DATE) RETURNS DATE
+AS
+$$
+	SELECT CASE WHEN $1 IS NULL THEN NULL
+	ELSE ($1 - (30 + round(random()*60)::integer))::date END AS random_date;
+$$
+LANGUAGE SQL VOLATILE;
+
+/**
+***	Returns random number as string
+**/
+
+CREATE OR REPLACE FUNCTION _dba_.random_number(v_source TEXT) RETURNS TEXT
+AS
+$$
+DECLARE
+	v_result	TEXT := '';
+	v_count		INT;
+	v_curr_char	CHAR(1);
+BEGIN
+	IF v_source IS NULL 
+	THEN	
+		RETURN(NULL);
+	END IF;
+		
+	FOR v_count IN 1..LENGTH(v_source)
+	LOOP
+		v_curr_char = substring(v_source,v_count,1);	
+		
+		IF (v_curr_char ~ '[0-9]') 
+		THEN		
+			v_result := v_result || round(v_curr_char::integer*random());
+		ELSE
+			-- If || us used spaces are supressed, which is REALLY weird			
+			v_result := concat(v_result,v_curr_char);
+		END IF;
+		
+	END LOOP;
+
+	RETURN(v_result);
+END;	
+$$
+LANGUAGE plpgsql VOLATILE;
+
 
 CREATE OR REPLACE FUNCTION _dba_.obfuscate_prod_data() RETURNS VOID AS
 $$
