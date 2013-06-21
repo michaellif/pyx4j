bd9242290284244838950f6e326af770886d655d
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-06-21 14:59:08 +0000

    VISTA-2820 add new table for migration

---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/company/EmployeeBuildingAccess.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on 2013-06-21
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.domain.company;
+
+import com.pyx4j.entity.annotations.Detached;
+import com.pyx4j.entity.annotations.Indexed;
+import com.pyx4j.entity.annotations.JoinColumn;
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.Owner;
+import com.pyx4j.entity.annotations.ReadOnly;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.i18n.annotations.I18n;
+
+import com.propertyvista.domain.property.asset.building.Building;
+
+@I18n(strategy = I18n.I18nStrategy.IgnoreThis)
+public interface EmployeeBuildingAccess extends IEntity {
+
+    @Owner
+    @ReadOnly
+    @Indexed
+    @MemberColumn(notNull = true)
+    @JoinColumn
+    Employee employee();
+
+    @Detached
+    Building building();
+
+}
