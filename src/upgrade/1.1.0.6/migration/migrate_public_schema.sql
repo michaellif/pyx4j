7363e3ff0e47feab173595931d93cc96a20b145c
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-06-26 22:50:39 +0000

    VISTA-1709 first changes of 1.1.0.6 migration

---
+++ src/upgrade/1.1.0.6/migration/migrate_public_schema.sql
@@ -0,0 +1,24 @@
+/**
+***     ===========================================================================================================
+***     
+***     @version $Revision$ ($Author$) $Date$
+***
+***     Migration script for public schema - generated with by _dba_.generate_sql_sequences function
+***
+***     ===========================================================================================================
+**/                                                     
+
+SET client_min_messages = 'error';
+SET search_path = 'public';
+
+BEGIN TRANSACTION;
+
+ -- Sequences to drop
+ DROP SEQUENCE portal_preferences_seq;
+
+ 
+COMMIT;
+
+SET client_min_messages = 'notice';
+
+
