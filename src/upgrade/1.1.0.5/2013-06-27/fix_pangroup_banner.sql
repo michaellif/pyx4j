0eeb91754e6ca17cd03d0f5139a987771f438810
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-06-27 21:14:34 +0000

    VISTA-1458 fix for pangroup site banner - not applied to prod yet!

---
+++ src/upgrade/1.1.0.5/2013-06-27/fix_pangroup_banner.sql
@@ -0,0 +1,30 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***            fix for pangroup banner
+***
+***     =====================================================================================================================
+**/
+
+BEGIN TRANSACTION;
+
+DELETE FROM pangroup.portal_image_set$image_set;
+DELETE FROM pangroup.site_descriptor$banner;
+
+INSERT INTO pangroup.portal_image_set$image_set (id,owner,value,seq)
+(SELECT nextval('public.portal_image_set$image_set_seq'),p.id AS owner,
+        i.id AS value,0 AS seq
+ FROM   pangroup.portal_image_set p,pangroup.site_image_resource i 
+ WHERE  i.file_name = 'resized.jpg');
+ 
+INSERT INTO pangroup.site_descriptor$banner(id,owner,value,seq)
+(SELECT nextval('public.site_descriptor$banner_seq') AS id, s.id AS owner,
+        i.id AS value, 0 AS seq
+ FROM   pangroup.site_descriptor s,pangroup.portal_image_set i); 
+ 
+COMMIT;      
+ 
+ 
+    
