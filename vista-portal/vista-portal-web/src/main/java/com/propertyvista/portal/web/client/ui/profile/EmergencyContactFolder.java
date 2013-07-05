1652a7147f47d2165d58175a85548a19500e0f72
Michael Lifshits <michaellif@propertyvista.com>
2013-07-05 22:08:50 +0000

    VISTA-3086 editing functionality

--- vista-portal/vista-portal-web/src/main/java/com/propertyvista/portal/web/client/ui/profile/EmergencyContactFolder.java
+++ vista-portal/vista-portal-web/src/main/java/com/propertyvista/portal/web/client/ui/profile/EmergencyContactFolder.java
@@ -13,18 +13,25 @@
  */
 package com.propertyvista.portal.web.client.ui.profile;
 
+import com.google.gwt.user.client.Command;
+
 import com.pyx4j.entity.shared.IObject;
 import com.pyx4j.forms.client.ui.CComponent;
 import com.pyx4j.forms.client.ui.folder.BoxFolderDecorator;
 import com.pyx4j.forms.client.ui.folder.BoxFolderItemDecorator;
 import com.pyx4j.forms.client.ui.folder.CEntityFolder;
+import com.pyx4j.forms.client.ui.folder.CEntityFolderItem;
 import com.pyx4j.forms.client.ui.folder.IFolderDecorator;
 import com.pyx4j.forms.client.ui.folder.IFolderItemDecorator;
+import com.pyx4j.i18n.shared.I18n;
+import com.pyx4j.widgets.client.dialog.MessageDialog;
 
 import com.propertyvista.common.client.resources.VistaImages;
 import com.propertyvista.domain.tenant.EmergencyContact;
 
 public class EmergencyContactFolder extends CEntityFolder<EmergencyContact> {
+
+    private static final I18n i18n = I18n.get(EmergencyContactFolder.class);
 
     private final ProfileViewImpl view;
 
@@ -33,6 +40,7 @@ public class EmergencyContactFolder extends CEntityFolder<EmergencyContact> {
         this.view = view;
         setOrderable(true);
         setRemovable(true);
+        setAddable(true);
     }
 
     @Override
@@ -43,7 +51,7 @@ public class EmergencyContactFolder extends CEntityFolder<EmergencyContact> {
 
     @Override
     protected IFolderDecorator<EmergencyContact> createFolderDecorator() {
-        return new BoxFolderDecorator<EmergencyContact>(VistaImages.INSTANCE);
+        return new BoxFolderDecorator<EmergencyContact>(VistaImages.INSTANCE, "Add Emergency Contact");
     }
 
     @Override
@@ -53,5 +61,15 @@ public class EmergencyContactFolder extends CEntityFolder<EmergencyContact> {
         } else {
             return super.create(member);
         }
+    }
+
+    @Override
+    protected void removeItem(final CEntityFolderItem<EmergencyContact> item) {
+        MessageDialog.confirm(i18n.tr("Emergency contact removal"), i18n.tr("Do you really want to remove emergency contact information?"), new Command() {
+            @Override
+            public void execute() {
+                EmergencyContactFolder.super.removeItem(item);
+            }
+        });
     }
 }
