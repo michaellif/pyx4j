f72ee9a8f2bf8653212cdc8337675a02ac9451b3
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2013-07-08 20:52:28 +0000

    VISTA-3158 Website setting UI refactoring - Logo divided on small and large.

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/settings/website/branding/BrandingForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/settings/website/branding/BrandingForm.java
@@ -13,17 +13,32 @@
  */
 package com.propertyvista.crm.client.ui.crud.settings.website.branding;
 
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Image;
+
+import com.pyx4j.forms.client.ui.CFile;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.prime.form.IForm;
+import com.pyx4j.widgets.client.dialog.MessageDialog;
+import com.pyx4j.widgets.client.dialog.OkDialog;
 
+import com.propertyvista.common.client.theme.VistaTheme;
+import com.propertyvista.common.client.ui.components.MediaUtils;
+import com.propertyvista.crm.client.ui.components.cms.SiteImageResourceProvider;
 import com.propertyvista.crm.client.ui.crud.CrmEntityForm;
 import com.propertyvista.crm.client.ui.crud.settings.website.RichTextContentFolder;
+import com.propertyvista.crm.client.ui.crud.settings.website.SiteImageThumbnail;
+import com.propertyvista.domain.File;
+import com.propertyvista.domain.site.SiteImageResource;
 import com.propertyvista.dto.SiteDescriptorDTO;
 
 public class BrandingForm extends CrmEntityForm<SiteDescriptorDTO> {
 
     private static final I18n i18n = I18n.get(BrandingForm.class);
+
+    private final SiteImageThumbnail thumb = new SiteImageThumbnail();
 
     public BrandingForm(IForm<SiteDescriptorDTO> view) {
         super(SiteDescriptorDTO.class, view);
@@ -53,5 +68,58 @@ public class BrandingForm extends CrmEntityForm<SiteDescriptorDTO> {
         content = new FormFlexPanel(proto().socialLinks().getMeta().getCaption());
         content.setWidget(0, 0, inject(proto().socialLinks(), new SocialLinkFolder(isEditable())));
         addTab(content);
+
+        addTab(createCrmLogoTab());
     }
+
+    @Override
+    protected void onValueSet(boolean populate) {
+        super.onValueSet(populate);
+
+        thumb.setUrl(MediaUtils.createSiteImageResourceUrl(getValue().crmLogo()));
+    }
+
+    private FormFlexPanel createCrmLogoTab() {
+        FormFlexPanel content = new FormFlexPanel(proto().crmLogo().getMeta().getCaption());
+
+        content.setWidget(0, 0, new DecoratorBuilder(inject(proto().crmLogo(), new CFile<File>(new Command() {
+            @Override
+            public void execute() {
+                OkDialog dialog = new OkDialog(getValue().crmLogo().fileName().getValue()) {
+                    @Override
+                    public boolean onClickOk() {
+                        return true;
+                    }
+                };
+                dialog.setBody(new Image(MediaUtils.createSiteImageResourceUrl(getValue().crmLogo())));
+                dialog.center();
+            }
+        }) {
+            @Override
+            public void showFileSelectionDialog() {
+                SiteImageResourceProvider provider = new SiteImageResourceProvider();
+                provider.selectResource(new AsyncCallback<SiteImageResource>() {
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        MessageDialog.error(i18n.tr("Action Failed"), caught.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(SiteImageResource rc) {
+                        if (rc != null) {
+                            setValue(rc);
+                            thumb.setUrl(MediaUtils.createSiteImageResourceUrl(rc));
+                        }
+                    }
+                });
+            }
+        }), 20).build());
+
+        content.setWidget(0, 1, thumb);
+
+        content.getColumnFormatter().setWidth(0, VistaTheme.columnWidth);
+
+        return content;
+    }
+
 }
--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/settings/website/content/ContentForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/settings/website/content/ContentForm.java
@@ -13,33 +13,18 @@
  */
 package com.propertyvista.crm.client.ui.crud.settings.website.content;
 
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.HasVerticalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 
-import com.pyx4j.forms.client.ui.CFile;
 import com.pyx4j.forms.client.ui.panels.FormFlexPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.prime.form.IForm;
-import com.pyx4j.widgets.client.dialog.MessageDialog;
-import com.pyx4j.widgets.client.dialog.OkDialog;
 
-import com.propertyvista.common.client.ui.components.MediaUtils;
-import com.propertyvista.crm.client.ui.components.cms.SiteImageResourceProvider;
 import com.propertyvista.crm.client.ui.crud.CrmEntityForm;
-import com.propertyvista.crm.client.ui.crud.settings.website.SiteImageThumbnail;
-import com.propertyvista.domain.File;
-import com.propertyvista.domain.site.SiteImageResource;
 import com.propertyvista.dto.SiteDescriptorDTO;
 
 public class ContentForm extends CrmEntityForm<SiteDescriptorDTO> {
 
     private static final I18n i18n = I18n.get(ContentForm.class);
-
-    private final SiteImageThumbnail thumb = new SiteImageThumbnail();
 
     public ContentForm(IForm<SiteDescriptorDTO> view) {
         super(SiteDescriptorDTO.class, view);
@@ -57,53 +42,6 @@ public class ContentForm extends CrmEntityForm<SiteDescriptorDTO> {
         content = new FormFlexPanel(proto().cityIntroPages().getMeta().getCaption());
         content.setWidget(0, 0, inject(proto().cityIntroPages(), new CityIntroPageFolder(this)));
         addTab(content);
-
-        content = new FormFlexPanel(proto().crmLogo().getMeta().getCaption());
-        HorizontalPanel imageLinkContainer = new HorizontalPanel();
-        imageLinkContainer.setWidth("100%");
-        content.setWidget(0, 0, imageLinkContainer);
-        imageLinkContainer.add(inject(proto().crmLogo(), new CFile<File>(new Command() {
-            @Override
-            public void execute() {
-                OkDialog dialog = new OkDialog(getValue().crmLogo().fileName().getValue()) {
-                    @Override
-                    public boolean onClickOk() {
-                        return true;
-                    }
-                };
-                dialog.setBody(new Image(MediaUtils.createSiteImageResourceUrl(getValue().crmLogo())));
-                dialog.center();
-            }
-        }) {
-            @Override
-            public void showFileSelectionDialog() {
-                SiteImageResourceProvider provider = new SiteImageResourceProvider();
-                provider.selectResource(new AsyncCallback<SiteImageResource>() {
-                    @Override
-                    public void onFailure(Throwable caught) {
-                        MessageDialog.error(i18n.tr("Action Failed"), caught.getMessage());
-                    }
-
-                    @Override
-                    public void onSuccess(SiteImageResource rc) {
-                        setValue(rc);
-                        thumb.setUrl(MediaUtils.createSiteImageResourceUrl(rc));
-                    }
-                });
-            }
-        }));
-        imageLinkContainer.add(thumb);
-        imageLinkContainer.setCellVerticalAlignment(imageLinkContainer.getWidget(0), HasVerticalAlignment.ALIGN_MIDDLE);
-        imageLinkContainer.getWidget(0).setWidth("400px");
-        imageLinkContainer.setCellWidth(thumb, "200px");
-        addTab(content);
-    }
-
-    @Override
-    protected void onValueSet(boolean populate) {
-        super.onValueSet(populate);
-
-        thumb.setUrl(MediaUtils.createSiteImageResourceUrl(getValue().crmLogo()));
     }
 
     private Widget createGadgetPanel() {
