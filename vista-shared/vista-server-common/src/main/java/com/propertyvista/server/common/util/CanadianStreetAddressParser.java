6b0a826019e8248610f1af7a5fa3715f1e889d91
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-07-11 17:40:56 +0000

    VISTA-3168 street address parser implementation: support for more address cases

--- vista-shared/vista-server-common/src/main/java/com/propertyvista/server/common/util/CanadianStreetAddressParser.java
+++ vista-shared/vista-server-common/src/main/java/com/propertyvista/server/common/util/CanadianStreetAddressParser.java
@@ -20,10 +20,25 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.pyx4j.commons.CommonsStringUtils;
+
 import com.propertyvista.domain.contact.AddressStructured.StreetDirection;
 import com.propertyvista.domain.contact.AddressStructured.StreetType;
 
 public class CanadianStreetAddressParser implements StreetAddressParser {
+
+    private static final List<String> unitDesignators = Arrays.asList(//@formatter:off
+            "APARTMENT",
+            "APT",
+            "APPARTEMENT", // French
+            "APP", // French abbreviation of the above
+            "SUITE",
+            "UNIT",            
+            "BUREAU", // French for "suite"
+            "UNITÉ" // French for unit            
+    );//@formatter:on
 
     private static final List<String> streetTypeKeywords = Arrays.asList(//@formatter:off
             "ABBEY",
@@ -358,12 +373,12 @@ public class CanadianStreetAddressParser implements StreetAddressParser {
     private static final Set<String> streetDirectionSet = Collections.unmodifiableSet(new HashSet<String>(streetDirectionKeywords));
 
     private enum ParserState {
-        StreetName, StreetType, StreetDirection, End
+        UnitNumber, StreetNumber, StreetName, StreetType, StreetDirection, End
     }
 
     @Override
     public StreetAddress parse(String address1, String address2) throws ParseException {
-        String[] addressTokens = address1.trim().replaceAll("\\s+", " ").split(" ");
+        String[] addressTokens = address1.trim().split("\\s+");
 
         String streetNumber = addressTokens[0];
         String unitNumber = null;
@@ -375,8 +390,133 @@ public class CanadianStreetAddressParser implements StreetAddressParser {
             unitNumber = streetNumberAndUnitNumber[0];
             streetNumber = streetNumberAndUnitNumber[1];
         }
+        int streetAddressPartLowerBound = 1;
+
+        // check for street number fraction suffix
+        if (streetAddressPartLowerBound < addressTokens.length) {
+            if (addressTokens[streetAddressPartLowerBound].matches("\\d+/\\d+")) {
+                streetNumber = streetNumber + ' ' + addressTokens[streetAddressPartLowerBound];
+                streetAddressPartLowerBound += 1;
+            }
+        }
+        int streetAddressPartUpperBound = addressTokens.length; // *exclusive* upper range of tokens that belong to street 
+
+        // try to find unit number after the unit number designator (should be located at the last part of an address)
         if (unitNumber == null) {
-            unitNumber = address2;
+            for (int i = addressTokens.length - 1; (unitNumber == null) & i >= streetAddressPartLowerBound; --i) {
+                if (unitDesignators.contains(addressTokens[i].toUpperCase())) {
+                    streetAddressPartUpperBound = i;
+
+                    if (i + 1 < addressTokens.length) {
+                        unitNumber = StringUtils.join(addressTokens, ' ', i + 1, addressTokens.length);
+                    }
+                }
+            }
+        }
+
+        // if we still didn't get unit number try to get it from address2 line
+        if (unitNumber == null & CommonsStringUtils.isStringSet(address2)) {
+            String[] address2Tokens = address2.trim().split("\\s+");
+            if (address2Tokens.length == 1) {
+                unitNumber = address2Tokens[0];
+            } else {
+                // we *assume* that address is the next token after unit designator token                
+                for (int i = 0; (unitNumber == null) & i < address2Tokens.length; ++i) {
+                    if (unitDesignators.contains(address2Tokens[i].toUpperCase())) {
+                        if (i + 1 < address2Tokens.length) {
+                            unitNumber = address2Tokens[i + 1];
+                        }
+                    }
+                }
+            }
+        }
+
+        // try to parse street name:        
+        // try to discover if it's a French or English type of street by street token index
+        String streetName = "";
+        StreetType streetType = null;
+        StreetDirection streetDirection = null;
+
+        int streetTypeTokenIndex = -1;
+        for (int i = streetAddressPartLowerBound; (i < streetAddressPartUpperBound) && (streetTypeTokenIndex == -1); ++i) {
+            String normalizedToken = addressTokens[i].toUpperCase();
+            if (streetTypeSet.contains(normalizedToken)) {
+                streetTypeTokenIndex = i;
+            }
+        }
+
+        // try to convert street type to enum
+        if (streetTypeTokenIndex != -1) {
+            try {
+                // (i / 2 * 2) should convert index of abbreviation to index of associated full name of street type
+                int i = streetTypeKeywords.indexOf(addressTokens[streetTypeTokenIndex].toUpperCase());
+                streetType = StreetType.valueOf(streetTypeKeywords.get(i / 2 * 2).toLowerCase());
+            } catch (Throwable e) {
+                // we don't care
+            }
+
+            // parse street name
+            if (streetTypeTokenIndex != streetAddressPartLowerBound) {
+                // deal with English (or French numerical) street name
+                final int streetNameUpperBound = streetTypeTokenIndex != -1 ? streetTypeTokenIndex : streetAddressPartUpperBound;
+                streetName = StringUtils.join(addressTokens, ' ', streetAddressPartLowerBound, streetNameUpperBound);
+            } else {
+                // deal with French street name
+                streetName = StringUtils.join(addressTokens, ' ', streetAddressPartLowerBound + 1, streetAddressPartUpperBound - 1);
+            }
+
+            // try parse street direction: if present must be the last part of the street address part
+            int streetDirectionTokenIndex = streetAddressPartUpperBound - 1;
+            String streetDirectionCandidate = addressTokens[streetDirectionTokenIndex];
+
+            if (streetDirectionSet.contains(addressTokens[streetDirectionTokenIndex].toUpperCase())) {
+                int i = streetDirectionKeywords.indexOf(streetDirectionCandidate.toUpperCase());
+                if (i != -1) {
+                    streetDirectionCandidate = streetDirectionKeywords.get(i / 2 * 2);
+
+                    streetDirectionFound: for (StreetDirection d : StreetDirection.values()) {
+                        if (d.name().equalsIgnoreCase(streetDirectionCandidate)) {
+                            streetDirection = d;
+                            break streetDirectionFound;
+                        }
+                    }
+                }
+            }
+
+        } else {
+            // if we haven't found street type token assume everything is broken and don't care too much about rest of the parsing
+            streetName = StringUtils.join(addressTokens, ' ', streetAddressPartLowerBound, streetAddressPartUpperBound);
+            streetType = StreetType.other;
+            streetDirection = null;
+        }
+
+        // if we haven't found the associated Enum value of street type, attach the street type back to the name of the street
+        if (streetType == null && streetTypeTokenIndex != -1) {
+            if (streetTypeTokenIndex == streetAddressPartLowerBound) {
+                // French way of doing things
+                streetName = addressTokens[streetTypeTokenIndex] + ' ' + streetName;
+            } else {
+                streetName = streetName + ' ' + streetName;
+            }
+            streetType = StreetType.other;
+        }
+
+        return new StreetAddress(unitNumber, streetNumber, streetName.toString(), streetType, streetDirection);
+    }
+
+    /** this one cannot parse some of the test cases */
+    private StreetAddress parseDepricated(String address1, String address2) throws ParseException {
+        String[] addressTokens = address1.trim().split("\\s+");
+
+        String streetNumber = addressTokens[0];
+        String unitNumber = null;
+        if (streetNumber.contains("-")) {
+            String[] streetNumberAndUnitNumber = addressTokens[0].split("-");
+            if (streetNumberAndUnitNumber.length != 2) {
+                new ParseException("Failed to parse street address and unit number from `" + addressTokens[0] + "` for address : `" + address1 + "`", 0);
+            }
+            unitNumber = streetNumberAndUnitNumber[0];
+            streetNumber = streetNumberAndUnitNumber[1];
         }
 
         int tokenIndex = 1;
@@ -426,16 +566,41 @@ public class CanadianStreetAddressParser implements StreetAddressParser {
 
             case StreetDirection: {
                 String streetDirectionCandidate = addressTokens[tokenIndex];
-                int i = streetDirectionKeywords.indexOf(streetDirectionCandidate.toLowerCase());
-                try {
-                    streetDirection = StreetDirection.valueOf(streetDirectionKeywords.get(i / 2 * 2).toLowerCase());
-                } catch (Throwable e) {
-                    throw new ParseException("failed to match street direction `" + streetDirectionCandidate + "` to any known value for address + `"
-                            + address1 + "`", tokenIndex);
-                }
+                int i = streetDirectionKeywords.indexOf(streetDirectionCandidate.toUpperCase());
+                if (i != -1) {
+                    streetDirectionCandidate = streetDirectionKeywords.get(i / 2 * 2);
 
-                tokenIndex += 1;
-                parserState = ParserState.End;
+                    streetDirectionFound: for (StreetDirection d : StreetDirection.values()) {
+                        if (d.name().equalsIgnoreCase(streetDirectionCandidate)) {
+                            streetDirection = d;
+                            break streetDirectionFound;
+                        }
+                    }
+                    if (streetDirection != null) {
+                        tokenIndex += 1;
+                    } else {
+                        throw new ParseException("streed direction enum was not found for `" + addressTokens[tokenIndex] + "` in address `" + address1 + "`",
+                                tokenIndex);
+                    }
+                }
+                parserState = ParserState.UnitNumber;
+
+                break;
+            }
+            case UnitNumber: {
+                if (!unitDesignators.contains(addressTokens[tokenIndex].toUpperCase())) {
+                    parserState = ParserState.End;
+                } else {
+                    tokenIndex += 1;
+                    if (tokenIndex < addressTokens.length) {
+                        unitNumber = addressTokens[tokenIndex];
+
+                        tokenIndex += 1;
+                    } else {
+                        throw new ParseException("failed to fetch unit number from address `" + address1 + "`: no unit number after designator `"
+                                + addressTokens[tokenIndex - 1], tokenIndex - 1);
+                    }
+                }
                 break;
             }
 
@@ -446,6 +611,22 @@ public class CanadianStreetAddressParser implements StreetAddressParser {
 
         }
 
+        // if we still didn't get unit number try to get it from address2 line
+        if (unitNumber == null & CommonsStringUtils.isStringSet(address2)) {
+            String[] address2Tokens = address2.trim().split("\\s+");
+            if (address2Tokens.length == 1) {
+                unitNumber = address2Tokens[0];
+            } else {
+                // we *assume* that address is the next token after unit designator token                
+                for (int i = 0; (unitNumber == null) & i < address2Tokens.length; ++i) {
+                    if (unitDesignators.contains(address2Tokens[i].toUpperCase())) {
+                        if (i + 1 < address2Tokens.length) {
+                            unitNumber = address2Tokens[i + 1];
+                        }
+                    }
+                }
+            }
+        }
         return new StreetAddress(unitNumber, streetNumber, streetName.toString(), streetType, streetDirection);
     }
 }
--- vista-shared/vista-server-common/src/test/java/com/propertyvista/server/common/util/CanadianStreetAddressParserTest.java
+++ vista-shared/vista-server-common/src/test/java/com/propertyvista/server/common/util/CanadianStreetAddressParserTest.java
@@ -26,24 +26,91 @@ public class CanadianStreetAddressParserTest extends TestCase {
 
     private final StreetAddressParser parser = new CanadianStreetAddressParser();
 
-    public void test1() {
+    public void testSanity() {
         StreetAddress a = parse("1065 Eglinton Avenue East", null);
-
         Assert.assertNull(a.unitNumber);
         Assert.assertEquals("1065", a.streetNumber);
         Assert.assertEquals("Eglinton", a.streetName);
         Assert.assertEquals(StreetType.avenue, a.streetType);
         Assert.assertEquals(StreetDirection.east, a.streetDirection);
+
+        a = parse("    1065      Eglinton   Avenue    E    ", null);
+        Assert.assertNull(a.unitNumber);
+        Assert.assertEquals("1065", a.streetNumber);
+        Assert.assertEquals("Eglinton", a.streetName);
+        Assert.assertEquals(StreetType.avenue, a.streetType);
+        Assert.assertEquals(StreetDirection.east, a.streetDirection);
+
+        a = parse("    1065      Eglinton   Avenue    Northeast", null);
+        Assert.assertNull(a.unitNumber);
+        Assert.assertEquals("1065", a.streetNumber);
+        Assert.assertEquals("Eglinton", a.streetName);
+        Assert.assertEquals(StreetType.avenue, a.streetType);
+        Assert.assertEquals(StreetDirection.northEast, a.streetDirection);
+
+        a = parse("    1065      Eglinton   Avenue  ", null);
+        Assert.assertNull(a.unitNumber);
+        Assert.assertEquals("1065", a.streetNumber);
+        Assert.assertEquals("Eglinton", a.streetName);
+        Assert.assertEquals(StreetType.avenue, a.streetType);
+        Assert.assertNull(a.streetDirection);
     }
 
-    public void test2() {
-        StreetAddress a = parse("    1065      Eglinton   Avenue    E    ", null);
+    public void testComplexStreetName() {
+        StreetAddress a = parse("10-1065 Wild Wild West Street North", null);
+        Assert.assertEquals("Wild Wild West", a.streetName);
+    }
 
-        Assert.assertNull(a.unitNumber);
+    public void testDirectionWithComplexStreetName() {
+        StreetAddress a = parse("10-1065 Wild Wild West Street North", null);
+        Assert.assertEquals(StreetDirection.north, a.streetDirection);
+    }
+
+    public void testComplexStreetNameNoDirection() {
+        StreetAddress a = parse("10-1065 Wild Wild West Street", null);
+        Assert.assertNull(a.streetDirection);
+    }
+
+    public void testFrenchStyleStreetName() {
+        StreetAddress a = parse("10-1065 Rue Wild Wild West", null);
+        Assert.assertEquals("Rue Wild Wild", a.streetName); // because there's no enum for french stuff
+        Assert.assertEquals(StreetType.other, a.streetType);
+        Assert.assertEquals(StreetDirection.west, a.streetDirection);
+    }
+
+    public void testUnitNumberParsingBeforeCivicNumber() {
+        StreetAddress a = parse("10-1065      Eglinton   Avenue    Northeast", null);
+        Assert.assertEquals("10", a.unitNumber);
+    }
+
+    public void testUnitNumberParsingAfterStreetType() {
+        StreetAddress a = parse("1065      Eglinton   Avenue  Apt 10", null);
+        Assert.assertEquals("10", a.unitNumber);
+    }
+
+    public void testUnitNumberParsingInAddressLine2() {
+        StreetAddress a = parse("1065      Eglinton   Avenue  Northeast", "Apt 10");
+        Assert.assertEquals("10", a.unitNumber);
+    }
+
+    public void testCivicNumberParsingWithUnit() {
+        StreetAddress a = parse("10-1065      Eglinton   Avenue  Northeast", "Apt 10");
         Assert.assertEquals("1065", a.streetNumber);
-        Assert.assertEquals("Eglinton", a.streetName);
-        Assert.assertEquals(StreetType.avenue, a.streetType);
-        Assert.assertEquals(StreetDirection.east, a.streetDirection);
+    }
+
+    public void testCivicNumberParsing() {
+        StreetAddress a = parse("1065      Eglinton   Avenue  Northeast", "Apt 10");
+        Assert.assertEquals("1065", a.streetNumber);
+    }
+
+    public void testCivicNumberParsingWithAlphaSuffix() {
+        StreetAddress a = parse("1065a Eglinton Avenue Northeast", "Apt 10");
+        Assert.assertEquals("1065a", a.streetNumber);
+    }
+
+    public void testCivicNumberParsingWithFractionSuffix() {
+        StreetAddress a = parse("1065 1/2 Eglinton Avenue Northeast", "Apt 10");
+        Assert.assertEquals("1065 1/2", a.streetNumber);
     }
 
     private StreetAddress parse(String address1, String address2) {
