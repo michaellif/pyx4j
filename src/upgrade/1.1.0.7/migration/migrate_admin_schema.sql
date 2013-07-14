b9bb383a30dd4d74c4415228715ed8d42164a1cf
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-07-14 14:43:46 +0000

    VISTA-1709 migration to 1.1.0.7

---
+++ src/upgrade/1.1.0.7/migration/migrate_admin_schema.sql
@@ -0,0 +1,119 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***             _admin_ schema changes for v. 1.1.1
+***
+***     =====================================================================================================================
+**/
+
+SET client_min_messages = 'error';
+
+BEGIN TRANSACTION;
+
+SET search_path = '_admin_';
+
+        /**
+        ***     ======================================================================================================
+        ***
+        ***             DROP TABLE SECTION
+        ***
+        ***     ======================================================================================================
+        **/
+        
+        
+
+        
+        /**
+        ***     ======================================================================================================
+        ***
+        ***             DROP CONSTRAINTS SECTION
+        ***
+        ***     ======================================================================================================
+        **/
+        
+        -- check constraints
+        
+        ALTER TABLE scheduler_trigger DROP CONSTRAINT scheduler_trigger_trigger_type_e_ck;
+
+        
+        /**
+        ***     ======================================================================================================
+        ***
+        ***             DROP INDEXES 
+        ***
+        ***     ======================================================================================================
+        **/
+        
+        
+        
+        
+        /**
+        ***     =======================================================================================================
+        ***
+        ***             NEW AND ALTERED TABLES 
+        ***
+        ***     =======================================================================================================
+        **/
+        
+       
+       
+       
+        /**
+        ***     ============================================================================================================
+        ***
+        ***             DATA MIGRATION
+        ***
+        ***     ============================================================================================================
+        **/
+        
+        
+        
+        
+        
+        /**
+        ***     ==========================================================================================================
+        ***
+        ***             DROP TABLES AND COLUMNS
+        ***
+        ***     ==========================================================================================================
+        **/
+        
+       
+        
+        /**
+        ***     ========================================================================================================
+        ***
+        ***             CREATE CONSTRAINTS
+        ***
+        ***     ========================================================================================================
+        **/
+        
+        -- check constraints
+       
+        ALTER TABLE scheduler_trigger ADD CONSTRAINT scheduler_trigger_trigger_type_e_ck 
+                CHECK ((trigger_type) IN ('billing', 'cleanup', 'depositInterestAdjustment', 'depositRefund', 'equifaxRetention', 
+                'initializeFutureBillingCycles', 'leaseActivation', 'leaseCompletion', 'leaseRenewal', 'paymentsBmoReceive', 
+                'paymentsIssue', 'paymentsLastMonthSuspend', 'paymentsPadProcesAcknowledgment', 'paymentsPadProcesReconciliation', 
+                'paymentsPadReceiveAcknowledgment', 'paymentsPadReceiveReconciliation', 'paymentsPadSend', 'paymentsScheduledCreditCards', 
+                'paymentsScheduledEcheck', 'paymentsTenantSure', 'paymentsUpdate', 'tenantSureCancellation', 'tenantSureHQUpdate', 
+                'tenantSureReports', 'tenantSureTransactionReports', 'test', 'updateArrears', 'updatePaymentsSummary', 'vistaBusinessReport', 
+                'vistaCaleonReport', 'yardiARDateVerification', 'yardiImportProcess'));
+
+                
+        /**
+        ***     ============================================================================================================
+        ***     
+        ***             CREATE INDEXES
+        ***
+        ***     ============================================================================================================
+        **/
+        
+        
+       
+
+
+COMMIT;
+
+SET client_min_messages = 'notice';
---
+++ src/upgrade/1.1.0.7/migration/migrate_public_schema.sql
@@ -0,0 +1,27 @@
+/**
+***     ===========================================================================================================
+***     
+***     @version $Revision$ ($Author$) $Date$
+***
+***     Migration script for public schema - generated with by _dba_.generate_sql_sequences function
+***
+***     ===========================================================================================================
+**/                                                     
+
+SET client_min_messages = 'error';
+SET search_path = 'public';
+
+BEGIN TRANSACTION;
+
+-- New sequences
+ CREATE SEQUENCE custom_skin_resource_blob_seq START WITH 1 INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;
+  
+ -- Change owner to vista
+ ALTER SEQUENCE custom_skin_resource_blob_seq OWNER TO vista ;
+
+
+COMMIT;
+
+SET client_min_messages = 'notice';
+
+
