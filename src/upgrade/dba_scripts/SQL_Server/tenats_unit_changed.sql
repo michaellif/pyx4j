23a4b684375b6c8d45f3bd5ef977b829e54271f1
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-07-24 14:12:04 +0000

    VISTA-2993 Modified version of script to find tenants that move from unit to unit between databases

--- src/upgrade/dba_scripts/SQL_Server/tenats_unit_changed.sql
+++ src/upgrade/dba_scripts/SQL_Server/tenats_unit_changed.sql
@@ -9,29 +9,202 @@
 **/
 
 SELECT	DISTINCT t1.scode,
-		t0.sfirstname+' '+t0.slastname AS "0628_name",		
-		t1.sfirstname+' '+t1.slastname AS "0722_name",
+		t0.name AS "0628_name",		
+		t1.name AS "0722_name",
 		t0.sunitcode AS "0628_unit",
 		t1.sunitcode AS "0722_unit",
-		p0.scode AS "0628_property",
-		p1.scode AS "0722_property",
+		t0.pscode AS "0628_property",
+		t1.pscode AS "0722_property",
 		CAST(t0.dtleasefrom AS DATE) AS "0628_lease_from",
 		CAST(t1.dtleasefrom AS DATE) AS "0722_lease_from",
 		CAST(t0.dtleaseto AS DATE) AS "0628_lease_to",
 		CAST(t1.dtleaseto AS DATE) AS "0722_lease_to",
-		s0.status AS "0628_status",
-		s1.status AS "0722_status",
-		pl1.saddr1 AS proplist
-FROM	sl_0628.dbo.tenant t0
-JOIN	sl_0722.dbo.tenant t1 ON (t0.scode = t1.scode)
-JOIN	sl_0628.dbo.tenstatus s0 ON (t0.istatus = s0.istatus)
-JOIN	sl_0722.dbo.tenstatus s1 ON (t1.istatus = s1.istatus)
-JOIN	sl_0628.dbo.property p0 ON (t0.HPROPERTY = p0.HMY)
-JOIN	sl_0722.dbo.property p1 ON (t1.HPROPERTY = p1.HMY)
-LEFT JOIN	sl_0628.dbo.listprop lp0 ON (p0.HMY = lp0.HPROPERTY)
-LEFT JOIN	sl_0722.dbo.listprop lp1 ON (p1.HMY = lp1.HPROPERTY)
-LEFT JOIN	sl_0628.dbo.property pl0 ON (lp0.HPROPLIST = pl0.HMY AND pl0.saddr1 LIKE '%VISTA%')
-LEFT JOIN	sl_0722.dbo.property pl1 ON (lp1.HPROPLIST = pl1.HMY AND pl1.saddr1 LIKE '%VISTA%')
-WHERE	t0.sunitcode != t1.sunitcode;
+		t0.status AS "0628_status",
+		t1.status AS "0722_status",
+		t1.saddr1 AS proplist
+FROM	
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0628.dbo.tenant t
+		JOIN	sl_0628.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0628.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0628.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0628.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t0
+FULL OUTER JOIN 
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0722.dbo.tenant t
+		JOIN	sl_0722.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0722.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0722.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0722.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t1
+		ON (t0.scode = t1.scode)
+WHERE	t0.sunitcode != t1.sunitcode
+AND		t1.saddr1 LIKE '%vista%'
+ORDER BY t1.saddr1;
+
+
+SELECT	DISTINCT t1.scode,
+		t0.name AS "0322_name",		
+		t1.name AS "0722_name",
+		t0.sunitcode AS "0322_unit",
+		t1.sunitcode AS "0722_unit",
+		t0.pscode AS "0322_property",
+		t1.pscode AS "0722_property",
+		CAST(t0.dtleasefrom AS DATE) AS "0322_lease_from",
+		CAST(t1.dtleasefrom AS DATE) AS "0722_lease_from",
+		CAST(t0.dtleaseto AS DATE) AS "0322_lease_to",
+		CAST(t1.dtleaseto AS DATE) AS "0722_lease_to",
+		t0.status AS "0322_status",
+		t1.status AS "0722_status",
+		t1.saddr1 AS proplist
+FROM	
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0322.dbo.tenant t
+		JOIN	sl_0322.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0322.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0322.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0322.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t0
+FULL OUTER JOIN 
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0722.dbo.tenant t
+		JOIN	sl_0722.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0722.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0722.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0722.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t1
+		ON (t0.scode = t1.scode)
+WHERE	t0.sunitcode != t1.sunitcode
+AND		t1.saddr1 LIKE '%vista%'
+ORDER BY t1.saddr1;
+
+
+SELECT	DISTINCT t1.scode,
+		t0.name AS "0422_name",		
+		t1.name AS "0722_name",
+		t0.sunitcode AS "0422_unit",
+		t1.sunitcode AS "0722_unit",
+		t0.pscode AS "0422_property",
+		t1.pscode AS "0722_property",
+		CAST(t0.dtleasefrom AS DATE) AS "0422_lease_from",
+		CAST(t1.dtleasefrom AS DATE) AS "0722_lease_from",
+		CAST(t0.dtleaseto AS DATE) AS "0422_lease_to",
+		CAST(t1.dtleaseto AS DATE) AS "0722_lease_to",
+		t0.status AS "0422_status",
+		t1.status AS "0722_status",
+		t1.saddr1 AS proplist
+FROM	
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0422.dbo.tenant t
+		JOIN	sl_0422.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0422.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0422.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0422.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t0
+FULL OUTER JOIN 
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0722.dbo.tenant t
+		JOIN	sl_0722.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0722.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0722.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0722.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t1
+		ON (t0.scode = t1.scode)
+WHERE	t0.sunitcode != t1.sunitcode
+AND		t1.saddr1 LIKE '%vista%'
+ORDER BY t1.saddr1;
+
+SELECT	DISTINCT t1.scode,
+		t0.name AS "0530_name",		
+		t1.name AS "0722_name",
+		t0.sunitcode AS "0530_unit",
+		t1.sunitcode AS "0722_unit",
+		t0.pscode AS "0530_property",
+		t1.pscode AS "0722_property",
+		CAST(t0.dtleasefrom AS DATE) AS "0530_lease_from",
+		CAST(t1.dtleasefrom AS DATE) AS "0722_lease_from",
+		CAST(t0.dtleaseto AS DATE) AS "0530_lease_to",
+		CAST(t1.dtleaseto AS DATE) AS "0722_lease_to",
+		t0.status AS "0530_status",
+		t1.status AS "0722_status",
+		t1.saddr1 AS proplist
+FROM	
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0530.dbo.tenant t
+		JOIN	sl_0530.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0530.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0530.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0530.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t0
+FULL OUTER JOIN 
+		(SELECT DISTINCT t.scode,
+				t.sfirstname+' '+t.slastname AS name,		
+				t.sunitcode ,
+				p.scode AS pscode,
+				t.dtleasefrom, 
+				t.dtleaseto ,			
+				s.status,
+				pl.saddr1 
+		FROM	sl_0722.dbo.tenant t
+		JOIN	sl_0722.dbo.tenstatus s ON (t.istatus = s.istatus)
+		JOIN	sl_0722.dbo.property p ON (t.HPROPERTY = p.HMY)
+		LEFT JOIN	sl_0722.dbo.listprop lp ON (p.HMY = lp.HPROPERTY)
+		LEFT JOIN	sl_0722.dbo.property pl ON (lp.HPROPLIST = pl.HMY )
+		/*WHERE  pl.saddr1 LIKE '%VISTA%'*/) AS t1
+		ON (t0.scode = t1.scode)
+WHERE	t0.sunitcode != t1.sunitcode
+AND		t1.saddr1 LIKE '%vista%'
+ORDER BY t1.saddr1;
+
 
 		
