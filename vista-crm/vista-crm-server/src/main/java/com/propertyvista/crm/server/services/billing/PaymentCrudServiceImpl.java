95f59f5e8fba024da4c8975359b37969fba07e0d
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2013-08-09 17:34:03 +0000

    VISTA-3284 Need to show more details for for PaymentRecord, PaymentMethod PreauthorizedPayment

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/billing/PaymentCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/billing/PaymentCrudServiceImpl.java
@@ -27,6 +27,7 @@ import com.pyx4j.config.server.ServerSideFactory;
 import com.pyx4j.config.server.SystemDateManager;
 import com.pyx4j.entity.server.AbstractCrudServiceDtoImpl;
 import com.pyx4j.entity.server.Persistence;
+import com.pyx4j.entity.shared.AttachLevel;
 import com.pyx4j.entity.shared.EntityFactory;
 import com.pyx4j.i18n.shared.I18n;
 
@@ -83,8 +84,9 @@ public class PaymentCrudServiceImpl extends AbstractCrudServiceDtoImpl<PaymentRe
         Persistence.service().retrieve(dto.billingAccount().lease().unit().building());
         Persistence.service().retrieve(dto.leaseTermParticipant());
         Persistence.service().retrieve(dto.paymentMethod());
+        Persistence.service().retrieve(dto.paymentMethod().creator(), AttachLevel.ToStringMembers);
         Persistence.service().retrieve(dto.preauthorizedPayment());
-        Persistence.service().retrieve(dto.creator());
+        Persistence.service().retrieve(dto.creator(), AttachLevel.ToStringMembers);
 
         dto.leaseId().set(dto.billingAccount().lease().leaseId());
         dto.leaseStatus().set(dto.billingAccount().lease().status());
