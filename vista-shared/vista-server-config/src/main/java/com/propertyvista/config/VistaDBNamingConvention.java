81b2bcfcd3c02db4eb7f832cc22644a478447240
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-08-13 20:30:54 +0000

    VISTA-2014 creator -> createdBy

--- vista-shared/vista-server-config/src/main/java/com/propertyvista/config/VistaDBNamingConvention.java
+++ vista-shared/vista-server-config/src/main/java/com/propertyvista/config/VistaDBNamingConvention.java
@@ -28,12 +28,13 @@ public class VistaDBNamingConvention extends NamingConventionOracle {
 
     private static ShortWords shortWords() {
         ShortWords shortWords = new ShortWords();
+        shortWords.add("discriminator", "discr");
+
         shortWords.add("billing_billing_cycle_billing_cycle_start_date_building_billing_type_idx", "billing_cycle_start_date_building_type_idx");
         shortWords.add("billing_billing_type_payment_frequency_billing_cycle_start_day_idx", "billing_type_payment_frequency_billing_cycle_start_day_idx");
         shortWords.add("payment_information_payment_method_billing_address_street_type_e_ck", "payment_information_payment_method_billing_addr_str_type_e_ck");
         shortWords.add("payment_information_payment_method_billing_address_street_direction_e_ck",
                 "payment_information_payment_method_billing_addr_str_dir_e_ck");
-        shortWords.add("insurance_tenant_sure_transaction_payment_method_discriminator_d_ck", "insurance_tenant_sure_transaction_payment_method_discr_d_ck");
         shortWords.add("pad_file_file_creation_number_funds_transfer_type_company_id_idx", "pad_file_creation_number_funds_transfer_type_company_id_idx");
         return shortWords;
     }
