6117116b87124a4e090ca1094ca9185100a315d0
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-08-27 19:19:24 +0000

    VISTA-1458 fix for customer users with NULL email

---
+++ src/upgrade/1.1.1/2013-08-27/fix_customer_user.sql
@@ -0,0 +1,37 @@
+/**
+***     ======================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***
+***             fix for customer_user entries with NULL email
+***
+***     ======================================================================================================================
+**/
+
+CREATE OR REPLACE FUNCTION _dba_.fix_customer_user(v_schema_name TEXT) RETURNS VOID AS
+$$
+BEGIN
+        EXECUTE 'UPDATE '||v_schema_name||'.customer '
+                ||'SET  user_id = NULL '
+                ||'WHERE user_id IN (SELECT id FROM '||v_schema_name||'.customer_user WHERE email IS NULL) ';
+                
+        
+        EXECUTE 'DELETE FROM  '||v_schema_name||'.customer_user_credential '
+                ||'WHERE usr IN (SELECT id FROM '||v_schema_name||'.customer_user WHERE email IS NULL) ';
+              
+        EXECUTE 'DELETE FROM  '||v_schema_name||'.customer_user '
+                ||'WHERE email IS NULL';
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+BEGIN TRANSACTION;
+
+        SELECT  pmc,_dba_.fix_customer_user(pmc)
+        FROM    _dba_.count_rows_all_pmc('customer_user',ARRAY['email IS NULL']) 
+        WHERE   row_count > 0;
+        
+COMMIT;
+
+
+
