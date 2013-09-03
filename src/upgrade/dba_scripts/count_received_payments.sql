e34ce3e0b939b08bffa7f08218b8c835650a7ab9
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-09-03 19:18:18 +0000

    VISTA-1458 temporary function to monitor payment records

---
+++ src/upgrade/dba_scripts/count_received_payments.sql
@@ -0,0 +1,40 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***             Function to monitor payment records in status received - temporary
+***
+***     =====================================================================================================================
+**/
+
+CREATE OR REPLACE FUNCTION _dba_.count_received_payments() 
+RETURNS TABLE ( pmc             VARCHAR(120),
+                row_count       BIGINT) AS
+$$
+DECLARE
+        v_schema_name           VARCHAR(64);
+        v_sql                   TEXT;
+BEGIN
+        FOR v_schema_name IN
+        SELECT namespace FROM _admin_.admin_pmc
+        WHERE status != 'Created'
+        ORDER BY 1
+        LOOP
+                v_sql :=  'SELECT '||quote_literal(v_schema_name)||' AS pmc,COUNT(p.id) '
+                ||' FROM '||v_schema_name||'.payment_record p '
+                ||'JOIN '||v_schema_name||'.payment_method pm ON (p.payment_method = pm.id) '
+                ||'WHERE p.payment_status = ''Received'' ';
+                
+   
+                EXECUTE  v_sql INTO pmc,row_count;
+                RETURN NEXT;
+
+        END LOOP; 
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+SELECT * FROM _dba_.count_received_payments() WHERE row_count > 0;
+
+DROP FUNCTION _dba_.count_received_payments();
