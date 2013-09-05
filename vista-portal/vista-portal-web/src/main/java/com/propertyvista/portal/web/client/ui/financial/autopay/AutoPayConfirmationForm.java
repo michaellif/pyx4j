61167be3fee637d180ae077ff72f0125dfb70375
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2013-09-05 16:43:19 +0000

    VISTA-3444 My Community Portal Functionality - implementation in progress: Payment, PaymentMethod and AutoPay Wizards and services

--- vista-portal/vista-portal-web/src/main/java/com/propertyvista/portal/web/client/ui/financial/autopay/AutoPayConfirmationForm.java
+++ vista-portal/vista-portal-web/src/main/java/com/propertyvista/portal/web/client/ui/financial/autopay/AutoPayConfirmationForm.java
@@ -68,7 +68,7 @@ public class AutoPayConfirmationForm extends CEntityForm<AutoPayDTO> {
 
         mainPanel.setBR(++row, 0, 1);
 
-        mainPanel.setWidget(++row, 0, new FormDecoratorBuilder(inject(proto().nextScheduledPaymentDate(), new CDateLabel()), 100).build());
+        mainPanel.setWidget(++row, 0, new FormDecoratorBuilder(inject(proto().nextScheduledPaymentDate(), new CDateLabel()), 100).labelWidth(20).build());
 
         SimplePanel contentPanel = new SimplePanel(mainPanel);
         contentPanel.setStyleName(EntityViewTheme.StyleName.EntityViewContent.name());
