8f6b233b59775c0f563e868d550babb2d5542c25
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2013-09-05 16:55:15 +0000

    VISTA-3444 My Community Portal Functionality - implementation in progress: Payment, PaymentMethod and AutoPay Wizards and services

--- vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/security/VistaPortalAccessControlList.java
+++ vista-portal/vista-portal-server/src/main/java/com/propertyvista/portal/server/security/VistaPortalAccessControlList.java
@@ -220,6 +220,7 @@ public class VistaPortalAccessControlList extends ServletContainerAclBuilder {
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.AutoPayRetrieveService.class));
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.DashboardService.class));
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.MaintenanceService.class));
+        grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.PaymentMethodCrudService.class));
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.PaymentMethodRetrieveService.class));
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(com.propertyvista.portal.rpc.portal.web.services.PaymentMethodWizardService.class));
         grant(VistaCustomerBehavior.Tenant, new IServiceExecutePermission(
