9e2fbb4fa08e96852a5f6667ef6e438f4028560a
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-09-17 18:04:42 +0000

    VISTA-1458 Fix ofm lease

---
+++ src/upgrade/1.1.1/2013-09-17/fix_ofm_lease.sql
@@ -0,0 +1,31 @@
+/**
+***     ===========================================================================================================
+***     
+***     @version $Revision$ ($Author$) $Date$
+***
+***     Fix ofm lease
+***
+***     ===========================================================================================================
+**/      
+
+BEGIN TRANSACTION;
+
+        UPDATE  ofm.lease 
+        SET     status = 'Completed',
+                completion = 'Termination',
+                lease_to = '17-SEP-2013',
+                termination_lease_to = '17-SEP-2013'  
+        WHERE   id= 346;      
+        
+COMMIT;
+
+BEGIN TRANSACTION;
+
+        UPDATE  ofm.lease 
+        SET     completion = NULL ,
+                lease_to = '30-JUN-2013',
+                termination_lease_to = NULL, 
+                actual_move_out = '30-JUN-2013'  
+        WHERE   id= 346;
+
+COMMIT;                                         
