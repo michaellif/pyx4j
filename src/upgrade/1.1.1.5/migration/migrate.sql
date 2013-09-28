84caa3b71b36ce589f26ba320c2175d12392495b
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-09-28 02:01:43 +0000

    VISTA-1709 1.1.1.5 migration

---
+++ src/upgrade/1.1.1.5/migration/migrate.sql
@@ -0,0 +1,17 @@
+/**
+***     ======================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***
+***             1.1.1.5 migration. Nice and easy for a change
+***
+***     ======================================================================================================================
+**/
+
+BEGIN TRANSACTION;
+
+        ALTER TABLE _admin_.admin_pmc_yardi_credential ALTER COLUMN  property_code TYPE VARCHAR(4000);
+        ALTER TABLE _admin_.audit_record ALTER COLUMN details TYPE VARCHAR(2048);
+       
+       
+COMMIT;
