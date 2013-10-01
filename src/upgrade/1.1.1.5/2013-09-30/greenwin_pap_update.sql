05262666b17b7845c55ba6124f54b77d5a0c18e3
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-10-01 01:58:02 +0000

    VISTA-1458 further yet more fun with sterling!

--- src/upgrade/1.1.1.5/2013-09-30/greenwin_pap_update.sql
+++ src/upgrade/1.1.1.5/2013-09-30/greenwin_pap_update.sql
@@ -35,7 +35,7 @@ BEGIN TRANSACTION;
 
 COMMIT;
 
--- DROP TABLE _dba_.tmp_pap;
+TRUNCATE TABLE _dba_.tmp_pap;
 
 /** Same thing for Sterling **/
 
@@ -49,10 +49,18 @@ BEGIN TRANSACTION;
         UPDATE  sterling.payment_record AS pr
         SET     preauthorized_payment = t.pap_id,
                 pad_billing_cycle = t.cycle_id
-        FROM    _dba_.tmp_pap t
-        WHERE   pr.id = t.payment_id
-        AND     pr.pad_billing_cycle IS NULL
-        AND     pr.preauthorized_payment IS NULL;
+        FROM    (SELECT t.payment_id,t.pap_id,
+                        bc.id AS cycle_id
+                FROM    _dba_.tmp_pap t
+                JOIN    sterling.payment_record p ON (t.payment_id = p.id)
+                JOIN    sterling.lease_term_participant ltp ON (p.lease_term_participant = ltp.id)
+                JOIN    sterling.lease_participant lp ON (ltp.lease_participant = lp.id)
+                JOIN    sterling.lease l ON (lp.lease = l.id)
+                JOIN    sterling.apt_unit a ON (l.unit = a.id)
+                JOIN    sterling.building b ON (a.building = b.id)
+                JOIN    sterling.billing_billing_cycle bc ON (bc.building = b.id)
+                WHERE   bc.billing_cycle_start_date = '01-OCT-2013') AS t
+        WHERE   pr.id = t.payment_id;
         
 
 COMMIT; 
