b8e377d5608835ed20085587439c460a97d6cfc0
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2013-10-18 16:25:42 +0000

    VISTA-3596 adjusted TODO tag

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/web/client/activity/services/insurance/TenantSureFaqActivity.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/web/client/activity/services/insurance/TenantSureFaqActivity.java
@@ -14,41 +14,26 @@
 package com.propertyvista.portal.web.client.activity.services.insurance;
 
 import com.google.gwt.activity.shared.AbstractActivity;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 
-import com.pyx4j.rpc.client.DefaultAsyncCallback;
 import com.pyx4j.site.rpc.AppPlace;
 
-import com.propertyvista.portal.rpc.portal.web.services.services.TenantSureInsurancePolicyCrudService;
 import com.propertyvista.portal.web.client.PortalWebSite;
 import com.propertyvista.portal.web.client.resources.tenantsure.TenantSureResources;
 import com.propertyvista.portal.web.client.ui.TermsView;
 
 public class TenantSureFaqActivity extends AbstractActivity {
 
-    private final TenantSureInsurancePolicyCrudService service;
-
     private final TermsView view;
 
     public TenantSureFaqActivity(AppPlace place) {
-        service = GWT.create(TenantSureInsurancePolicyCrudService.class);
         view = PortalWebSite.getViewFactory().instantiate(TermsView.class);
     }
 
     @Override
     public void start(final AcceptsOneWidget panel, EventBus eventBus) {
-        if (false) {
-            // TODO for now we keep TenantSure FAQ in the resource
-            service.getFaq(new DefaultAsyncCallback<String>() {
-                @Override
-                public void onSuccess(String result) {
-                    view.populate(result);
-                    panel.setWidget(view);
-                }
-            });
-        }
+        // TODO VISTA-3596: consider getting the FAQ text from server
         view.populate(TenantSureResources.INSTANCE.faq().getText());
         panel.setWidget(view);
     }
