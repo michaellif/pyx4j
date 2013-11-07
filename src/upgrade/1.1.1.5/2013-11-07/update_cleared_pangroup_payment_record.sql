7d4fae389a788456e1b40b76fcd07efaf5d3057d
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-11-07 22:08:34 +0000

    VISTA-1458 Another pangroup record updated

--- src/upgrade/1.1.1.5/2013-11-07/update_cleared_pangroup_payment_record.sql
+++ src/upgrade/1.1.1.5/2013-11-07/update_cleared_pangroup_payment_record.sql
@@ -37,5 +37,21 @@ BEGIN TRANSACTION;
         AND     p.payment_status = 'Cleared'
         AND     p.aggregated_transfer_return IS NOT NULL;
         
+        UPDATE  pangroup.payment_record AS p 
+        SET     pad_reconciliation_return_record_key = t.id,
+                pad_reconciliation_debit_record_key = NULL,
+                payment_status = 'Returned',
+                last_status_change_date = current_date,
+                finalize_date = current_date,
+                transaction_error_message = t.msg
+        FROM    (SELECT id, 14798 AS transaction_id,
+                        reason_code||' '||reason_text AS msg
+                FROM    _admin_.pad_reconciliation_debit_record 
+                WHERE   transaction_id = '14798'
+                AND     reconciliation_status = 'RETURNED') AS t
+        WHERE   p.id = t.transaction_id
+        AND     p.payment_status = 'Cleared'
+        AND     p.aggregated_transfer_return IS NOT NULL;
+        
         
 COMMIT;
