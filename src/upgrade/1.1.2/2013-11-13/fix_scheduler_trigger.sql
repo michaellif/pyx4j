4a000babb8f154886e004fe6924af88071345f88
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-11-15 17:02:06 +0000

    VISTA-3813 another attempt to fix phantom trigger issue

--- src/upgrade/1.1.2/2013-11-13/fix_scheduler_trigger.sql
+++ src/upgrade/1.1.2/2013-11-13/fix_scheduler_trigger.sql
@@ -13,9 +13,15 @@ BEGIN TRANSACTION;
         INSERT INTO _admin_.scheduler_trigger (id,trigger_type,name,population_type,created) VALUES
         (21,'cleanup','Dummy Trigger','allPmc',current_timestamp);
         
+        INSERT INTO _admin_.scheduler_trigger_schedule (id,trgr,repeat_type,repeat_every,tm,starts_on,ends_on)
+        VALUES (25,21,'Daily',1,'13:25','2013-11-15','2013-11-16');
+        
 COMMIT;
 
 BEGIN TRANSACTION;
+        
+        DELETE FROM _admin_.scheduler_trigger_schedule
+        WHERE id = 25;
 
         DELETE FROM _admin_.scheduler_trigger
         WHERE id = 21;
