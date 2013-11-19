49d798cd687ab97538074a6c2affcece0ad5a257
Stan Porter <stanp@propertyvista.com>
2013-11-19 20:41:09 +0000

    VISTA-2914 csv file output added

--- vista-ils/vista-ils-feed/src/test/java/com/propertyvista/ils/gottarent/setup/PropertyListMapper.java
+++ vista-ils/vista-ils-feed/src/test/java/com/propertyvista/ils/gottarent/setup/PropertyListMapper.java
@@ -13,6 +13,8 @@
  */
 package com.propertyvista.ils.gottarent.setup;
 
+import java.io.PrintStream;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -22,6 +24,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.pyx4j.essentials.server.csv.CSVLoad;
+import com.pyx4j.essentials.server.csv.CSVReciver;
 import com.pyx4j.essentials.server.csv.EntityCSVReciver;
 import com.pyx4j.gwt.server.IOUtils;
 
@@ -46,6 +50,7 @@ public class PropertyListMapper {
         }
 
         int matched = 0, unmatched = 0;
+        final Map<String, String> idMap = new HashMap<String, String>();
         Collections.sort(pv_data, new Comparator<PropertyId>() {
             @Override
             public int compare(PropertyId o1, PropertyId o2) {
@@ -56,15 +61,66 @@ public class PropertyListMapper {
             String key = getKey(pid);
             if (grMap.containsKey(key)) {
                 matched++;
+                idMap.put(grMap.get(key), pid.identificator().getValue());
             } else {
                 logger.info("unmatched key: {}", key);
                 unmatched++;
             }
         }
         logger.info("matched = {}; unmatched = {}", matched, unmatched);
+
+        // create resulting csv
+        final StringBuilder csvBuilder = new StringBuilder();
+        CSVLoad.loadResourceFile(IOUtils.resourceFileName("greenwin/gr_list.csv", PropertyListMapper.class), Charset.forName("UTF-8"), new CSVReciver() {
+            @Override
+            public boolean onHeader(String[] headers) {
+                csvBuilder.append(toCsvRecord(headers));
+                return true;
+            }
+
+            @Override
+            public void onRow(String[] values) {
+                if (values.length < 11) {
+                    return;
+                }
+                String grid = values[0];
+                String pvid = idMap.get(grid);
+                if (pvid != null) {
+                    values[9] = pvid;
+                } else {
+                    values[10] = "Delete";
+                }
+                csvBuilder.append(toCsvRecord(values));
+            }
+
+            @Override
+            public boolean canContuneLoad() {
+                return true;
+            }
+        });
+        try {
+            String outPath = Thread.currentThread().getContextClassLoader().getResource(IOUtils.resourceFileName("greenwin/", PropertyListMapper.class))
+                    .getPath()
+                    + "gr_pv_map.csv";
+            PrintStream out = new PrintStream(outPath);
+            out.print(csvBuilder.toString());
+            out.close();
+            logger.info("Output saved in: {}", outPath);
+        } catch (Exception err) {
+            logger.error("Unable to export property mapping.", err);
+        }
     }
 
     private static String getKey(PropertyId pid) {
         return new String(pid.postalCode().getValue() + "_" + pid.streetNumber().getValue()).replaceAll(" ", "").toLowerCase();
+    }
+
+    private static String toCsvRecord(String[] fields) {
+        StringBuilder record = new StringBuilder();
+        for (String field : fields) {
+            record.append("'").append(field.replaceAll("'", "\\\\'")).append("',");
+        }
+        record.deleteCharAt(record.length() - 1);
+        return record.append("\n").toString();
     }
 }
