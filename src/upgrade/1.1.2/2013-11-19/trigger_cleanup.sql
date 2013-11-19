e8866e5128fcb2f4e1e542cc25a863faf9d8ba53
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-11-19 17:07:33 +0000

    VISTA-1458 trigger cleanup

---
+++ src/upgrade/1.1.2/2013-11-19/trigger_cleanup.sql
@@ -0,0 +1,41 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***             Restore deleted paymentsUpdate trigger, disable and delete again
+***
+***     =====================================================================================================================
+**/
+
+BEGIN TRANSACTION;
+
+        -- trigger 20, Yardi Import Process [manual] Realstar
+
+        UPDATE  _admin_.scheduler_run
+        SET     trgr = 17
+        WHERE   trgr = 20;
+        
+        DELETE FROM _admin_.scheduler_trigger WHERE id = 20;
+        
+        
+        -- trigger 29, Yardi Import Process [Manual] 2
+
+        UPDATE  _admin_.scheduler_run
+        SET     trgr = 17
+        WHERE   trgr = 29;
+        
+        DELETE FROM _admin_.scheduler_trigger WHERE id = 29;
+        
+        
+        -- trigger 26, Yardi Import Process [Manual] 2
+
+        UPDATE  _admin_.scheduler_run
+        SET     trgr = 12
+        WHERE   trgr = 26;
+        
+        DELETE FROM _admin_.scheduler_trigger WHERE id = 26;
+        
+COMMIT;
+
+
