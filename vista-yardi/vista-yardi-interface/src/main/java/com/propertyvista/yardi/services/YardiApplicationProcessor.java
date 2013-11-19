f67adb89464cd7d1f958352e201314e7288e6475
Stan Porter <stanp@propertyvista.com>
2013-11-19 17:42:26 +0000

    VISTA-3766 YardiNewGuestWorkflowTest - application processor created

---
+++ vista-yardi/vista-yardi-interface/src/main/java/com/propertyvista/yardi/services/YardiApplicationProcessor.java
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Nov 19, 2013
+ * @author stanp
+ * @version $Id$
+ */
+package com.propertyvista.yardi.services;
+
+import java.util.Arrays;
+
+import com.yardi.entity.leaseapp30.LeaseApplication;
+import com.yardi.entity.leaseapp30.Tenant;
+
+public class YardiApplicationProcessor {
+
+    public LeaseApplication createApplication(Tenant... tenants) {
+        LeaseApplication leaseApp = new LeaseApplication();
+        leaseApp.getTenant().addAll(Arrays.asList(tenants));
+        return leaseApp;
+    }
+}
