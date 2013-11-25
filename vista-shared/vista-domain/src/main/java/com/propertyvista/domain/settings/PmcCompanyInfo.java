8690b59cf23da2d3625c41b1908ec27a6549852d
Slava Skarzhevskyy <vlads@propertyvista.com>
2013-11-25 15:55:08 +0000

    VISTA-3086 Add Company Info for "Profile"

---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/settings/PmcCompanyInfo.java
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Nov 23, 2013
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.domain.settings;
+
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+
+@Caption(name = "Company Info")
+public interface PmcCompanyInfo extends IEntity {
+
+    IPrimitive<String> companyName();
+}
---
+++ vista-shared/vista-domain/src/main/java/com/propertyvista/domain/settings/PmcCompanyInfoContact.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Nov 23, 2013
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.domain.settings;
+
+import com.pyx4j.entity.annotations.BusinessEqualValue;
+import com.pyx4j.entity.annotations.Caption;
+import com.pyx4j.entity.annotations.Editor;
+import com.pyx4j.entity.annotations.Editor.EditorType;
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.ToString;
+import com.pyx4j.entity.annotations.validator.NotNull;
+import com.pyx4j.entity.shared.IEntity;
+import com.pyx4j.entity.shared.IPrimitive;
+import com.pyx4j.i18n.annotations.I18n;
+import com.pyx4j.i18n.shared.I18nEnum;
+
+@Caption(name = "Company Contact")
+public interface PmcCompanyInfoContact extends IEntity {
+
+    @I18n
+    public enum CompanyInfoContactType {
+
+        administrator;
+
+        @Override
+        public String toString() {
+            return I18nEnum.toString(this);
+        }
+
+    }
+
+    @NotNull
+    @MemberColumn(name = "tp")
+    IPrimitive<CompanyInfoContactType> type();
+
+    IPrimitive<String> name();
+
+    @ToString(index = 0)
+    @MemberColumn(name = "phoneNumber")
+    @Editor(type = EditorType.phone)
+    @BusinessEqualValue
+    IPrimitive<String> phone();
+
+    @Editor(type = EditorType.email)
+    IPrimitive<String> email();
+}
