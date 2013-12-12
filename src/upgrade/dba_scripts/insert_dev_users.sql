2a38493c2fc53818691bf5151934cbd91086d5df
Alexandre Kinareevski <akinareevski@propertyvista.com>
2013-12-12 17:48:31 +0000

    VISTA-1458 development users script for prod to qa migration

--- src/upgrade/dba_scripts/insert_dev_users.sql
+++ src/upgrade/dba_scripts/insert_dev_users.sql
@@ -8,23 +8,23 @@
 ***     ======================================================================================================================
 **/
 
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Anya','Vassilieva','anya.vassilieva@propertyvista.com','14164219896','16472910400','14166429881',true,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Artur','Roytburg','aroytbur@propertyvista.com',NULL,'16477045299',NULL,true,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Dmitry','Tumanov','dmitryt@propertyvista.com','19052372555','14168033050',NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Leonard','Drimmer','leonard@propertyvista.com',NULL,'14168218471',NULL,true,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Misha','Lifshits','michaellif@propertyvista.com','19057636762','14167224482','14163554125',false,'michaellif01',NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Vlad','Loukiantchenko','vladlouk@propertyvista.com',NULL,'14165126515',NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Vlad','Skarzhevskyy','vlads@propertyvista.com',NULL,'14166028523','14163554149',false,'vlads-i7','vladlnv',NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Artyom','Bondarev','artyom@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Elia','Kanaki','elia.kanaki@propertyvista','14169297775','16474049992',NULL,true,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Yuriy','Levandovskyy','yuriyl@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Victor','Vassiliev','vvassiliev@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Stan','Porter','stanp@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Alexander','Sheinin','alexander.sheinin@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Yacob','Simkhovich','yacobs@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Masha','Kulgavaya','mashak@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Yuliia','Voronetska','yuliav@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
-INSERT INTO development_user VALUES (nextval('public.development_user_seq'),'Igor','Smolka','igors@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Anya','Vassilieva','anya.vassilieva@propertyvista.com','14164219896','16472910400','14166429881',true,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Artur','Roytburg','aroytbur@propertyvista.com',NULL,'16477045299',NULL,true,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Dmitry','Tumanov','dmitryt@propertyvista.com','19052372555','14168033050',NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Leonard','Drimmer','leonard@propertyvista.com',NULL,'14168218471',NULL,true,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Misha','Lifshits','michaellif@propertyvista.com','19057636762','14167224482','14163554125',false,'michaellif01',NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Vlad','Loukiantchenko','vladlouk@propertyvista.com',NULL,'14165126515',NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Vlad','Skarzhevskyy','vlads@propertyvista.com',NULL,'14166028523','14163554149',false,'vlads-i7','vladlnv',NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Artyom','Bondarev','artyom@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Elia','Kanaki','elia.kanaki@propertyvista','14169297775','16474049992',NULL,true,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Yuriy','Levandovskyy','yuriyl@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Victor','Vassiliev','vvassiliev@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Stan','Porter','stanp@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Alexander','Sheinin','alexander.sheinin@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Yacob','Simkhovich','yacobs@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Masha','Kulgavaya','mashak@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Yuliia','Voronetska','yuliav@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
+INSERT INTO _admin_.development_user VALUES (nextval('public.development_user_seq'),'Igor','Smolka','igors@propertyvista.com',NULL,NULL,NULL,false,NULL,NULL,NULL,true);
 
 
 
