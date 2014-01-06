f7bc6cf3cddf6495f4e50e83cfcf4d94aa6ff7b6
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-01-06 21:59:26 +0000

    VISTA-2993 vista_dev Yardi licence update - example for pv001 database added

--- src/upgrade/dba_scripts/SQL_Server/update_yardi_licence.sql
+++ src/upgrade/dba_scripts/SQL_Server/update_yardi_licence.sql
@@ -1,5 +1,7 @@
 /**
 ***	======================================================================
+***		
+***		@version $Revision$ ($Author$) $Date$
 ***
 ***		Update Yardi licence the sql way
 ***
@@ -16,6 +18,20 @@ INSERT INTO vista_dev.dbo.PMCONFIG (SINSTALLKEY,SCOMPANYNAME,BACTIVEKEY)
 	EXCEPT
  SELECT  * FROM vista_dev.dbo.PMCONFIG) AS a
  WHERE bactivekey = 2);
+ 
+ /** Below example is for pv001 database update **/
+ 
+/*
+UPDATE	pv001.dbo.pmconfig 
+SET		BACTIVEKEY = 0
+WHERE	BACTIVEKEY = 2;
+
+INSERT INTO pv001.dbo.PMCONFIG (SINSTALLKEY,SCOMPANYNAME,BACTIVEKEY)
+(SELECT * FROM (SELECT  * FROM sl_1217.dbo.PMCONFIG
+	EXCEPT
+ SELECT  * FROM pv001.dbo.PMCONFIG) AS a
+ WHERE bactivekey = 2);
+*/
 
 
 
