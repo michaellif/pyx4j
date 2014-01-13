40bcef3d8fae088b4a14cd4c2c0bcf2a407d49ae
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-01-13 18:05:21 +0000

    VISTA-1709 1.1.2.3 migration

---
+++ src/upgrade/1.1.2.3/migration/migrate_public_schema.sql
@@ -0,0 +1,22 @@
+/**
+***     ===========================================================================================================
+***     
+***     @version $Revision$ ($Author$) $Date$
+***
+***     Migration script for public schema - generated with by _dba_.generate_sql_sequences function
+***
+***     ===========================================================================================================
+**/                                                     
+
+SET client_min_messages = 'error';
+SET search_path = 'public';
+
+BEGIN TRANSACTION;
+
+
+        
+COMMIT;
+
+SET client_min_messages = 'notice';
+
+
