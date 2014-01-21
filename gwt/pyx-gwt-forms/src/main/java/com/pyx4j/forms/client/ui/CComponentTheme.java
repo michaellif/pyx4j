f5d54433afa22d29065f3943eaad5ffe77f7244b
Michael Lifshits <michaellif@propertyvista.com>
2014-01-21 11:43:02 +0000

    Added padding to Validation message

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CComponentTheme.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CComponentTheme.java
@@ -94,7 +94,7 @@ public class CComponentTheme extends Theme {
         style.addProperty("color", "red");
         style.addProperty("text-align", "left");
         style.addProperty("white-space", "normal");
-        style.addProperty("padding-top", "3px");
+        style.addProperty("padding", "3px 0 0 10px");
         addStyle(style);
     }
 
