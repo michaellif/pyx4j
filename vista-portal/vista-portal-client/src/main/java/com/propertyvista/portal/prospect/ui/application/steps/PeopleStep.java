ab9c922e9c7fab69959e77fdcc66d38b0cdc9c99
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-01-21 20:35:23 +0000

    VISTA-3908 Prospect Client Implementation - in progress...

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/prospect/ui/application/steps/PeopleStep.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/prospect/ui/application/steps/PeopleStep.java
@@ -13,15 +13,24 @@
  */
 package com.propertyvista.portal.prospect.ui.application.steps;
 
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.IsWidget;
 
+import com.pyx4j.entity.core.IObject;
+import com.pyx4j.forms.client.ui.CComponent;
+import com.pyx4j.forms.client.ui.CEntityForm;
 import com.pyx4j.forms.client.ui.panels.BasicFlexFormPanel;
 import com.pyx4j.i18n.shared.I18n;
 
 import com.propertyvista.common.client.theme.VistaTheme.StyleName;
 import com.propertyvista.domain.tenant.prospect.OnlineApplicationWizardStepMeta;
+import com.propertyvista.portal.prospect.ui.application.ApplicationWizard;
 import com.propertyvista.portal.prospect.ui.application.ApplicationWizardStep;
-import com.propertyvista.portal.prospect.ui.application.steps.common.CoapplicantsFolder;
+import com.propertyvista.portal.rpc.portal.prospect.dto.CoapplicantDTO;
+import com.propertyvista.portal.shared.ui.util.PortalBoxFolder;
+import com.propertyvista.portal.shared.ui.util.decorators.FormWidgetDecoratorBuilder;
 
 public class PeopleStep extends ApplicationWizardStep {
 
@@ -53,5 +62,81 @@ public class PeopleStep extends ApplicationWizardStep {
         super.onValueSet(populate);
 
         warningMessage.setVisible(getValue().occupantsOver18areApplicants().isBooleanTrue());
+    }
+
+    private class CoapplicantsFolder extends PortalBoxFolder<CoapplicantDTO> {
+
+        private final ApplicationWizard wizard;
+
+        public CoapplicantsFolder(ApplicationWizard applicationWizard) {
+            super(CoapplicantDTO.class, i18n.tr("Occupant"));
+            this.wizard = applicationWizard;
+        }
+
+        public boolean isOccupantsOver18areApplicants() {
+            return wizard.getValue().occupantsOver18areApplicants().isBooleanTrue();
+        }
+
+        @Override
+        public CComponent<?> create(IObject<?> member) {
+            if (member instanceof CoapplicantDTO) {
+                return new CoapplicantForm();
+            } else {
+                return super.create(member);
+            }
+        }
+
+        class CoapplicantForm extends CEntityForm<CoapplicantDTO> {
+
+            public CoapplicantForm() {
+                super(CoapplicantDTO.class);
+            }
+
+            @Override
+            public IsWidget createContent() {
+                BasicFlexFormPanel mainPanel = new BasicFlexFormPanel();
+
+                int row = -1;
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().dependent())).useLabelSemicolon(false).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().matured())).useLabelSemicolon(false).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().firstName())).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().lastName())).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().birthDate()), 150).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().relationship())).build());
+                mainPanel.setWidget(++row, 0, new FormWidgetDecoratorBuilder(inject(proto().email())).build());
+
+                // tweaks:
+                get(proto().dependent()).addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+                    @Override
+                    public void onValueChange(ValueChangeEvent<Boolean> event) {
+                        updateBirthDateVisibility();
+                    }
+                });
+                get(proto().matured()).addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+                    @Override
+                    public void onValueChange(ValueChangeEvent<Boolean> event) {
+                        updateBirthDateVisibility();
+                    }
+                });
+
+                return mainPanel;
+            }
+
+            @Override
+            protected void onValueSet(boolean populate) {
+                super.onValueSet(populate);
+
+                get(proto().matured()).setVisible(isOccupantsOver18areApplicants());
+                updateBirthDateVisibility();
+            }
+
+            private void updateBirthDateVisibility() {
+                if (isOccupantsOver18areApplicants()) {
+                    get(proto().birthDate()).setVisible(getValue().dependent().getValue() && !getValue().matured().getValue());
+                } else {
+                    get(proto().birthDate()).setVisible(getValue().dependent().getValue());
+                }
+            }
+        }
     }
 }
