c8f2895afbbdbbc87ca740264a6f374c6bddb0f3
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-01-31 23:43:35 +0000

    VISTA-3908 generate SIN

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/shared/ui/util/editors/IdUploaderFolder.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/shared/ui/util/editors/IdUploaderFolder.java
@@ -18,9 +18,12 @@ import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 
+import com.pyx4j.config.shared.ApplicationMode;
 import com.pyx4j.entity.core.EntityFactory;
 import com.pyx4j.entity.core.IList;
 import com.pyx4j.entity.core.IObject;
+import com.pyx4j.forms.client.events.DevShortcutEvent;
+import com.pyx4j.forms.client.events.DevShortcutHandler;
 import com.pyx4j.forms.client.ui.CComponent;
 import com.pyx4j.forms.client.ui.CEntityForm;
 import com.pyx4j.forms.client.ui.panels.BasicFlexFormPanel;
@@ -34,6 +37,7 @@ import com.propertyvista.domain.media.IdentificationDocumentFolder;
 import com.propertyvista.domain.policy.policies.ApplicationDocumentationPolicy;
 import com.propertyvista.domain.policy.policies.domain.IdentificationDocumentType;
 import com.propertyvista.domain.util.ValidationUtils;
+import com.propertyvista.misc.CreditCardNumberGenerator;
 import com.propertyvista.portal.shared.ui.util.PortalBoxFolder;
 import com.propertyvista.portal.shared.ui.util.decorators.FormWidgetDecoratorBuilder;
 
@@ -198,6 +202,24 @@ public class IdUploaderFolder extends PortalBoxFolder<IdentificationDocumentFold
                     return null;
                 }
             });
+
+            if (ApplicationMode.isDevelopment()) {
+                this.addDevShortcutHandler(new DevShortcutHandler() {
+                    @Override
+                    public void onDevShortcut(DevShortcutEvent event) {
+                        if (event.getKeyCode() == 'Q') {
+                            event.consume();
+                            devGenerateNumbers();
+                        }
+                    }
+                });
+            }
+        }
+
+        private void devGenerateNumbers() {
+            if (getValue().idType().type().getValue() == IdentificationDocumentType.Type.canadianSIN) {
+                get(proto().idNumber()).setValue(CreditCardNumberGenerator.generateCanadianSin());
+            }
         }
     }
 }
