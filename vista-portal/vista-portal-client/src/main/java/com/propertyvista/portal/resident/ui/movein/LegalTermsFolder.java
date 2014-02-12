d79ea5cb840d0512980da411c6d29881cf16656b
Michael Lifshits <michaellif@propertyvista.com>
2014-02-12 22:20:17 +0000

    VISTA-3086 Mock values for Lease agreement signing

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/movein/LegalTermsFolder.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/movein/LegalTermsFolder.java
@@ -24,6 +24,7 @@ import com.pyx4j.forms.client.ui.decorators.WidgetDecorator.Builder.LabelPositio
 import com.pyx4j.forms.client.ui.panels.BasicFlexFormPanel;
 import com.pyx4j.forms.client.ui.panels.FlexFormPanelTheme;
 
+import com.propertyvista.domain.security.CustomerSignature;
 import com.propertyvista.domain.tenant.lease.SignedAgreementLegalTerm;
 import com.propertyvista.portal.shared.ui.util.PortalBoxFolder;
 import com.propertyvista.portal.shared.ui.util.decorators.FormWidgetDecoratorBuilder;
@@ -66,6 +67,13 @@ public class LegalTermsFolder extends PortalBoxFolder<SignedAgreementLegalTerm> 
                     .contentWidth("250px").componentWidth("250px").build());
 
             return mainPanel;
+        }
+
+        @Override
+        public void generateMockData() {
+            CustomerSignature signature = get(proto().signature()).getValue().duplicate();
+            signature.agree().setValue(true);
+            get(proto().signature()).setMockValue(signature);
         }
     }
 }
