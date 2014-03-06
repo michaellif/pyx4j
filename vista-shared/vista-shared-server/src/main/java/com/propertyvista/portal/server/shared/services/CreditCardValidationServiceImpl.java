d6f340bd21018cd9727dcccf1bc2c5906e9dcfe9
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-03-06 14:13:22 +0000

    VISTA-558 log card validation result

--- vista-shared/vista-shared-server/src/main/java/com/propertyvista/portal/server/shared/services/CreditCardValidationServiceImpl.java
+++ vista-shared/vista-shared-server/src/main/java/com/propertyvista/portal/server/shared/services/CreditCardValidationServiceImpl.java
@@ -13,6 +13,9 @@
  */
 package com.propertyvista.portal.server.shared.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import com.pyx4j.config.server.ServerSideFactory;
@@ -23,10 +26,26 @@ import com.propertyvista.portal.rpc.shared.services.CreditCardValidationService;
 
 public class CreditCardValidationServiceImpl implements CreditCardValidationService {
 
+    private final static Logger log = LoggerFactory.getLogger(CreditCardValidationServiceImpl.class);
+
     @Override
     public void validate(AsyncCallback<Boolean> callback, CreditCardInfo creditCardInfo) {
         creditCardInfo.card().number().setValue(creditCardInfo.card().newNumber().getValue());
-        callback.onSuccess(ServerSideFactory.create(CreditCardFacade.class).validateVisaDebit(creditCardInfo));
+        boolean valid = ServerSideFactory.create(CreditCardFacade.class).validateVisaDebit(creditCardInfo);
+        if (valid) {
+            log.debug("card {} valid {}", creditCardInfo.cardType(), valid);
+        } else {
+            log.debug("card {} {}XXX not valid {}", creditCardInfo.cardType(), trim6(creditCardInfo.card().number().getValue()), valid);
+        }
+        callback.onSuccess(valid);
+    }
+
+    private String trim6(String value) {
+        if (value.length() < 12) {
+            return "short number";
+        } else {
+            return value.substring(0, 6);
+        }
     }
 
 }
