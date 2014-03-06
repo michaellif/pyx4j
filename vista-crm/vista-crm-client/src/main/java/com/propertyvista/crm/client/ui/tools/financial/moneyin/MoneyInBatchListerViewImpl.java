e5d311876300d4d0730a521f815b32c3dbb8737c
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2014-03-06 16:04:40 +0000

    VISTA-3610 added default sorting

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/tools/financial/moneyin/MoneyInBatchListerViewImpl.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/tools/financial/moneyin/MoneyInBatchListerViewImpl.java
@@ -14,7 +14,9 @@
 package com.propertyvista.crm.client.ui.tools.financial.moneyin;
 
 import java.util.Arrays;
+import java.util.List;
 
+import com.pyx4j.entity.core.criterion.EntityQueryCriteria.Sort;
 import com.pyx4j.forms.client.ui.datatable.MemberColumnDescriptor;
 import com.pyx4j.site.client.ui.prime.lister.AbstractLister;
 
@@ -45,5 +47,11 @@ public class MoneyInBatchListerViewImpl extends CrmListerViewImplBase<MoneyInBat
             ));//@formatter:off
         }
         
+        @Override
+        public List<Sort> getDefaultSorting() {
+            return Arrays.asList(new Sort(proto().depositDate(), true));
+        }
     }
+    
+    
 }
