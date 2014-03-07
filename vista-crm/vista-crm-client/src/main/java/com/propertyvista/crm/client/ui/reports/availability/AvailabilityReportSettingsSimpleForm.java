d1fad47a9b5b5226fa314de291a1398401436d01
Artyom Bondarev <artyom@birchwoodsoftwaregroup.com>
2014-03-07 21:24:55 +0000

    VISTA-4051 removed mandatority of fields of AvailabilityReportSettingsSimpleForm

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/availability/AvailabilityReportSettingsSimpleForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/availability/AvailabilityReportSettingsSimpleForm.java
@@ -21,11 +21,7 @@ import com.pyx4j.forms.client.ui.panels.TwoColumnFlexFormPanel;
 import com.propertyvista.common.client.ui.components.c.CEnumSubsetSelector;
 import com.propertyvista.common.client.ui.components.c.SubsetSelector.Layout;
 import com.propertyvista.common.client.ui.decorations.FormDecoratorBuilder;
-import com.propertyvista.crm.client.ui.reports.NotEmptySetValidator;
 import com.propertyvista.domain.dashboard.gadgets.availability.UnitAvailabilityStatus;
-import com.propertyvista.domain.dashboard.gadgets.availability.UnitAvailabilityStatus.RentReadiness;
-import com.propertyvista.domain.dashboard.gadgets.availability.UnitAvailabilityStatus.RentedStatus;
-import com.propertyvista.domain.dashboard.gadgets.availability.UnitAvailabilityStatus.Vacancy;
 import com.propertyvista.domain.reports.AvailabilityReportMetadata;
 
 public class AvailabilityReportSettingsSimpleForm extends CEntityForm<AvailabilityReportMetadata> {
@@ -34,30 +30,27 @@ public class AvailabilityReportSettingsSimpleForm extends CEntityForm<Availabili
         super(AvailabilityReportMetadata.class);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public IsWidget createContent() {
         int row = -1;
         TwoColumnFlexFormPanel panel = new TwoColumnFlexFormPanel();
-        panel.setWidget(++row, 0, new FormDecoratorBuilder(inject(proto().asOf())).labelWidth(10).componentWidth(10).build());
+        panel.setWidget(++row, 0, new FormDecoratorBuilder(inject(proto().asOf())).labelWidth("10em").componentWidth("10em").build());
         panel.setWidget(
                 ++row,
                 0,
                 new FormDecoratorBuilder(inject(proto().vacancyStatus(), new CEnumSubsetSelector<UnitAvailabilityStatus.Vacancy>(
-                        UnitAvailabilityStatus.Vacancy.class, Layout.Horizontal))).labelWidth(10).componentWidth(10).build());
-        get(proto().vacancyStatus()).addComponentValidator(new NotEmptySetValidator<Vacancy>());
+                        UnitAvailabilityStatus.Vacancy.class, Layout.Horizontal))).labelWidth("10em").componentWidth("10em").build());
         panel.setWidget(
                 ++row,
                 0,
                 new FormDecoratorBuilder(inject(proto().rentedStatus(), new CEnumSubsetSelector<UnitAvailabilityStatus.RentedStatus>(
-                        UnitAvailabilityStatus.RentedStatus.class, Layout.Vertical))).labelWidth(10).componentWidth(10).build());
-        get(proto().rentedStatus()).addComponentValidator(new NotEmptySetValidator<RentedStatus>());
+                        UnitAvailabilityStatus.RentedStatus.class, Layout.Vertical))).labelWidth("10em").componentWidth("10em").build());
         panel.setWidget(
                 row,
                 1,
                 new FormDecoratorBuilder(inject(proto().rentReadinessStatus(), new CEnumSubsetSelector<UnitAvailabilityStatus.RentReadiness>(
-                        UnitAvailabilityStatus.RentReadiness.class, Layout.Vertical))).labelWidth(10).componentWidth(15).build());
-        get(proto().rentReadinessStatus()).addComponentValidator(new NotEmptySetValidator<RentReadiness>());
+                        UnitAvailabilityStatus.RentReadiness.class, Layout.Vertical))).labelWidth("10em").componentWidth("15em").build());
+
         return panel;
     }
 }
