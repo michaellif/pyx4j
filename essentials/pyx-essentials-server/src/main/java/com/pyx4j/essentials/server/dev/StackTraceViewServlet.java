da5240402debd906cbb6fde2b5f574ebf33b14c7
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-03-25 19:14:14 +0000

    Add StackTraceViewServlet

---
+++ essentials/pyx-essentials-server/src/main/java/com/pyx4j/essentials/server/dev/StackTraceViewServlet.java
@@ -0,0 +1,57 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2013 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Mar 25, 2014
+ * @author vlads
+ * @version $Id$
+ */
+package com.pyx4j.essentials.server.dev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class StackTraceViewServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setDateHeader("Expires", System.currentTimeMillis());
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-control", "no-cache, no-store, must-revalidate");
+        response.setContentType("text/plain");
+
+        PrintWriter out = response.getWriter();
+
+        out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        out.println("\n");
+
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        for (ThreadInfo threadInfo : threadMXBean.dumpAllThreads(true, false)) {
+            out.print(threadInfo.toString());
+        }
+
+    }
+}
