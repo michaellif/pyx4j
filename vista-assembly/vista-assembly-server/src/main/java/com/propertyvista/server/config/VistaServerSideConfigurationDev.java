2afa6cfb3e63274feca03da0f9d58780adab1069
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-03-30 03:36:01 +0000

    VISTA-4446 add 2 new autoPay tenant emails

--- vista-assembly/vista-assembly-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationDev.java
+++ vista-assembly/vista-assembly-server/src/main/java/com/propertyvista/server/config/VistaServerSideConfigurationDev.java
@@ -98,7 +98,7 @@ public class VistaServerSideConfigurationDev extends VistaServerSideConfiguratio
 
     @Override
     public boolean openIdRequired() {
-        return true;
+        return false;
     }
 
     @Override
