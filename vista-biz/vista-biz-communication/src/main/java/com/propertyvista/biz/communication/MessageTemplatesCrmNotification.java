8d558672618817cdd5c8b8a97677ba306f807dda
Yuriy Levandovskyy <yuriyl@propertyvista.com>
2014-03-31 18:40:00 +0000

    VISTA-4456 added links to prospect to introductory email

--- vista-biz/vista-biz-communication/src/main/java/com/propertyvista/biz/communication/MessageTemplatesCrmNotification.java
+++ vista-biz/vista-biz-communication/src/main/java/com/propertyvista/biz/communication/MessageTemplatesCrmNotification.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -58,6 +58,7 @@ class MessageTemplatesCrmNotification {
         template.variable("${ownerName}", user.firstName().getValue());
         template.variable("${crmLink}", VistaDeployment.getBaseApplicationURL(pmc, VistaApplication.crm, true));
         template.variable("${portalLink}", VistaDeployment.getBaseApplicationURL(pmc, VistaApplication.resident, true));
+        template.variable("${prospectLink}", VistaDeployment.getBaseApplicationURL(pmc, VistaApplication.prospect, true));
 
         email.setHtmlBody(template.getWrappedBody(wrapperTextResourceName));
         return email;
