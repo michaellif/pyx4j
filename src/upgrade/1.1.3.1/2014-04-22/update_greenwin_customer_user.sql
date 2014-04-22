1777d3ab20f48b788b63c22d8e27c1bc5669a013
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-04-22 21:23:38 +0000

    VISTA-1458 greenwin customer user update

--- src/upgrade/1.1.3.1/2014-04-22/update_greenwin_customer_user.sql
+++ src/upgrade/1.1.3.1/2014-04-22/update_greenwin_customer_user.sql
@@ -14,4 +14,8 @@ BEGIN TRANSACTION;
     SET     email = 'warhealertpup@rogers.com'
     WHERE   id = 4148;
     
+    UPDATE  greenwin.customer
+    SET     person_email = 'warhealertpup@rogers.com'
+    WHERE   id = 6620;
+    
 COMMIT;
