a4739d24fddafd4e0f72ef178713e46b6f0e4bf3
Michael Lifshits <michaellif@propertyvista.com>
2014-04-26 13:43:38 +0000

    VISTA-4580 Using FormPanel in ConcessionForm

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/concession/ConcessionForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/concession/ConcessionForm.java
@@ -19,10 +19,12 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.SimplePanel;
 
+import com.pyx4j.forms.client.ui.CDateLabel;
 import com.pyx4j.forms.client.ui.CField;
 import com.pyx4j.forms.client.ui.CMoneyField;
 import com.pyx4j.forms.client.ui.CPercentageField;
-import com.pyx4j.forms.client.ui.panels.TwoColumnFlexFormPanel;
+import com.pyx4j.forms.client.ui.panels.FluidPanel.Location;
+import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.prime.form.IForm;
 
@@ -38,25 +40,19 @@ public class ConcessionForm extends CrmEntityForm<Concession> {
     public ConcessionForm(IForm<Concession> view) {
         super(Concession.class, view);
 
-        TwoColumnFlexFormPanel content = new TwoColumnFlexFormPanel();
+        FormPanel formPanel = new FormPanel(this);
 
-        int row = -1;
-        content.setWidget(++row, 0, injectAndDecorate(proto().version().type(), 12));
-        content.setWidget(++row, 0, valueHolder);
-        content.setWidget(++row, 0, injectAndDecorate(proto().version().term(), 12));
-        content.setWidget(++row, 0, injectAndDecorate(proto().version().condition(), 10));
-        content.setWidget(++row, 0, injectAndDecorate(proto().version().mixable(), 5));
+        formPanel.append(Location.Left, proto().version().type()).decorate().componentWidth(120);
+        formPanel.append(Location.Left, valueHolder);
+        formPanel.append(Location.Left, proto().version().term()).decorate().componentWidth(120);
+        formPanel.append(Location.Left, proto().version().condition()).decorate().componentWidth(100);
+        formPanel.append(Location.Left, proto().version().mixable()).decorate().componentWidth(80);
 
-        content.setWidget(++row, 0, injectAndDecorate(proto().version().description(), 60));
-        content.getFlexCellFormatter().setColSpan(row, 0, 2);
+        formPanel.append(Location.Right, proto().version().effectiveDate()).decorate().componentWidth(90);
+        formPanel.append(Location.Right, proto().version().expirationDate()).decorate().componentWidth(90);
+        formPanel.append(Location.Right, proto().updated(), new CDateLabel()).decorate().componentWidth(90);
 
-        row = -1;
-        content.setWidget(++row, 1, injectAndDecorate(proto().version().effectiveDate(), 9));
-        content.setWidget(++row, 1, injectAndDecorate(proto().version().expirationDate(), 9));
-        content.setWidget(++row, 1, injectAndDecorate(proto().updated(), 9));
-
-        // tweak:
-        get(proto().updated()).setViewable(true);
+        formPanel.append(Location.Full, proto().version().description()).decorate();
 
         get(proto().version().type()).addValueChangeHandler(new ValueChangeHandler<Concession.Type>() {
             @Override
@@ -65,7 +61,7 @@ public class ConcessionForm extends CrmEntityForm<Concession> {
             }
         });
 
-        selectTab(addTab(content, i18n.tr("Concession")));
+        selectTab(addTab(formPanel, i18n.tr("Concession")));
         setTabBarVisible(false);
     }
 
