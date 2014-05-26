88328fbacbd9cba039f24c946ded55b93c25f586
Stan Porter <stanp@propertyvista.com>
2014-05-26 21:31:24 +0000

    VISTA-4430 phone number parsing fixed

--- vista-biz/vista-biz-legal/src/main/java/com/propertyvista/biz/legal/forms/ltbcommon/fieldadapters/partitioners/PhoneNumberPartitioner.java
+++ vista-biz/vista-biz-legal/src/main/java/com/propertyvista/biz/legal/forms/ltbcommon/fieldadapters/partitioners/PhoneNumberPartitioner.java
@@ -19,17 +19,18 @@ public class PhoneNumberPartitioner implements Partitioner {
 
     @Override
     public String getPart(String value, int partIndex) {
-        if (value.matches("\\((\\d\\d\\d)\\) (\\d\\d\\d)-(\\d\\d\\d\\d)")) {
+        String numbers = value.replaceAll("\\D", "");
+        if (numbers.length() == 10) {
             String part = "";
             switch (partIndex) {
             case 0:
-                part = value.substring(1, 4);
+                part = numbers.substring(0, 3);
                 break;
             case 1:
-                part = value.substring(6, 9);
+                part = numbers.substring(3, 6);
                 break;
             case 2:
-                part = value.substring(10, value.length());
+                part = numbers.substring(6);
                 break;
             default:
                 part = "";
