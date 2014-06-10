5a9c63f7f938c8a4693658650c68aa6f63771797
Stan Porter <stanp@propertyvista.com>
2014-06-10 21:40:32 +0000

    VISTA-4798 label width adjusted

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/unit/UnitForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/unit/UnitForm.java
@@ -127,7 +127,7 @@ public class UnitForm extends CrmEntityForm<AptUnitDTO> {
         formPanel.append(Location.Left, proto().info().economicStatus()).decorate().componentWidth(150);
         formPanel.append(Location.Right, proto().info()._bedrooms()).decorate().componentWidth(60);
 
-        formPanel.append(Location.Left, proto().info().economicStatusDescription()).decorate().componentWidth(150);
+        formPanel.append(Location.Left, proto().info().economicStatusDescription()).decorate().labelWidth(170).componentWidth(150);
         formPanel.append(Location.Right, proto().info()._bathrooms()).decorate().componentWidth(60);
 
         formPanel
