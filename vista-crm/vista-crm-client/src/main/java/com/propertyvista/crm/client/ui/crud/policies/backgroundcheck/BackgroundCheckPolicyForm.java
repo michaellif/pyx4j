86ec87704af3f8659eb64edee4583af787ed7cb2
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-06-13 00:41:24 +0000

    VISTA-4738 EQUIFAX Strategy number Shuld be shown for SUPPORT only in CRM UI

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/backgroundcheck/BackgroundCheckPolicyForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/backgroundcheck/BackgroundCheckPolicyForm.java
@@ -20,11 +20,13 @@ import com.google.gwt.user.client.ui.IsWidget;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.i18n.shared.I18n;
+import com.pyx4j.security.shared.SecurityController;
 import com.pyx4j.site.client.ui.prime.form.IForm;
 
 import com.propertyvista.crm.client.resources.CrmResources;
 import com.propertyvista.crm.client.ui.crud.policies.common.PolicyDTOTabPanelBasedForm;
 import com.propertyvista.domain.policy.dto.BackgroundCheckPolicyDTO;
+import com.propertyvista.domain.security.VistaCrmBehavior;
 
 public class BackgroundCheckPolicyForm extends PolicyDTOTabPanelBasedForm<BackgroundCheckPolicyDTO> {
 
@@ -51,5 +53,12 @@ public class BackgroundCheckPolicyForm extends PolicyDTOTabPanelBasedForm<Backgr
         backgroundCheckHelp.getElement().getStyle().setTextAlign(TextAlign.LEFT);
 
         return formPanel;
+    }
+
+    @Override
+    protected void onValueSet(boolean populate) {
+        super.onValueSet(populate);
+
+        get(proto().strategyNumber()).setVisible(SecurityController.checkBehavior(VistaCrmBehavior.PropertyVistaSupport));
     }
 }
