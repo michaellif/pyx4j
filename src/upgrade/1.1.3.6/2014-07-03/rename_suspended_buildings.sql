69cbaf7b6485696b72f18fb38db54b226fdbbd0f
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-07-03 20:30:22 +0000

    VISTA-1709 rename suspended buildings

---
+++ src/upgrade/1.1.3.6/2014-07-03/rename_suspended_buildings.sql
@@ -0,0 +1,44 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***             rename suspended buildings for all yardi-enabled pmc
+***
+***     =====================================================================================================================
+**/
+
+CREATE OR REPLACE FUNCTION _dba_.rename_suspended_buildings() RETURNS VOID AS
+$$
+DECLARE
+    
+    v_schema_name           VARCHAR(64);
+
+BEGIN
+
+    FOR v_schema_name IN
+    SELECT  a.namespace 
+    FROM    _admin_.admin_pmc a
+    JOIN    _admin_.admin_pmc_vista_features f ON (f.id = a.features)
+    WHERE   f.yardi_integration
+    AND     a.status != 'Suspended'
+    LOOP
+        
+        EXECUTE 'UPDATE '||v_schema_name||'.building '
+                ||'SET  property_code = ''x''||property_code '
+                ||'WHERE    suspended ';
+    
+    END LOOP;
+        
+
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+BEGIN TRANSACTION;
+
+    SELECT * FROM _dba_.rename_suspended_buildings();
+    
+COMMIT;
+
+DROP FUNCTION rename_suspended_buildings;
