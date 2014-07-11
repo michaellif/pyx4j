d158596f7ce373a04a52682818aa479e13c65099
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-07-11 18:25:03 +0000

    VISTA-3449 revert wrong changes

--- bg.cmd
+++ bg.cmd
@@ -2,8 +2,6 @@
 rem @version $Revision$ ($Author$)  $Date$
 rem helper to run maven2
 rem
-set MAVEN_OPTS=-XX:MaxPermSize=356m -Xms1024m -Xmx1524m -XX:ReservedCodeCacheSize=128m
-set JAVA_HOME=D:\jdk1.7.0
 for /f "tokens=*" %%I in ('CD') do @set CurDir=%%~nI
 title *%CurDir% - mvn with pyx and GWT Compile
 call mvn install -P pyx,gwtc,draft %*
