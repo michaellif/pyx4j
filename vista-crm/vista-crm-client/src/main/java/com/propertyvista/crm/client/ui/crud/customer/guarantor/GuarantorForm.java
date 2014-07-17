016225bfdf994eedf653194b600c2a0e1e2f1018
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-07-17 22:58:35 +0000

    VISTA-4854 Lease/Application Security, Behaviours, Roles - implementation, Guarantor

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/customer/guarantor/GuarantorForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/customer/guarantor/GuarantorForm.java
@@ -13,10 +13,12 @@
  */
 package com.propertyvista.crm.client.ui.crud.customer.guarantor;
 
+import com.pyx4j.entity.security.DataModelPermission;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.ui.prime.form.IForm;
 
 import com.propertyvista.crm.client.ui.crud.customer.common.LeaseParticipantForm;
+import com.propertyvista.domain.payment.LeasePaymentMethod;
 import com.propertyvista.dto.GuarantorDTO;
 
 public class GuarantorForm extends LeaseParticipantForm<GuarantorDTO> {
@@ -27,6 +29,6 @@ public class GuarantorForm extends LeaseParticipantForm<GuarantorDTO> {
         super(GuarantorDTO.class, view);
 
         selectTab(addTab(createDetailsTab(), i18n.tr("Details")));
-        addTab(createPaymentMethodsTab(), i18n.tr("Payment Methods"));
+        addTab(createPaymentMethodsTab(), i18n.tr("Payment Methods"), DataModelPermission.permissionRead(LeasePaymentMethod.class));
     }
 }
