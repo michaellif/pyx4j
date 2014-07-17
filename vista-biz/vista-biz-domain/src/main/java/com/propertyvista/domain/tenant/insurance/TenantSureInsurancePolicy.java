be3097c334af6c6b49c9985422276d571dab4e9b
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-07-17 19:17:01 +0000

    VISTA-4961 Operations TenantSure lister and view

--- vista-biz/vista-biz-domain/src/main/java/com/propertyvista/domain/tenant/insurance/TenantSureInsurancePolicy.java
+++ vista-biz/vista-biz-domain/src/main/java/com/propertyvista/domain/tenant/insurance/TenantSureInsurancePolicy.java
@@ -20,6 +20,8 @@ import com.pyx4j.entity.annotations.Caption;
 import com.pyx4j.entity.annotations.ColumnId;
 import com.pyx4j.entity.annotations.Detached;
 import com.pyx4j.entity.annotations.DiscriminatorValue;
+import com.pyx4j.entity.annotations.Editor;
+import com.pyx4j.entity.annotations.Editor.EditorType;
 import com.pyx4j.entity.annotations.Format;
 import com.pyx4j.entity.annotations.JoinColumn;
 import com.pyx4j.entity.annotations.JoinTable;
@@ -98,40 +100,51 @@ public interface TenantSureInsurancePolicy extends InsurancePolicy<TenantSureIns
     IPrimitive<TenantSurePaymentSchedule> paymentSchedule();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> annualPremium();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> underwriterFee();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> brokerFee();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> totalAnnualTax();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> totalAnnualPayable();
 
     /** this is the amount that gets charged first on enrollment */
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> totalFirstPayable();
 
     /** this is the amount that gets charged on anniversary of enrollment instead of totalMonthlyPayable */
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> totalAnniversaryFirstMonthPayable();
 
     /** this is an amount that gets charged every month (i.e. monthly premium + the taxes) */
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> totalMonthlyPayable();
 
-//    @Format("#,##0.00")
 //    @NotNull
+//    @Format("#,##0.00")
+//    @Editor(type = EditorType.money)
 //    IPrimitive<BigDecimal> liabilityCoverage();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> contentsCoverage();
 
     @Format("#,##0.00")
+    @Editor(type = EditorType.money)
     IPrimitive<BigDecimal> deductible();
 
     @Owned
