abd7079b962e68756b9f17fbdd6a5361391ff5ec
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-08-06 16:39:08 +0000

    VISTA-1458 metcap views

--- src/upgrade/1.1.3.9/2014-08-05/create_metcap_trans_views.sql
+++ src/upgrade/1.1.3.9/2014-08-05/create_metcap_trans_views.sql
@@ -55,3 +55,41 @@ JOIN    metcap.payment_record p ON (r.transaction_id::bigint = p.id)
 WHERE   r.reason_code IS NOT NULL
 AND     a.namespace = 'metcap'
 AND     r.payment_date >= '01-AUG-2014';
+
+SELECT      DISTINCT b.property_code, l.lease_id, 
+            c.person_name_first_name ||' '||c.person_name_last_name AS tenant_name,
+            p.amount,p.payment_status,p.transaction_error_message,
+            p.created_date,
+            ppd.bank_id,ppd.branch_transit_number,
+            ppd.account_no_number
+    FROM    metcap.payment_method pm
+    JOIN    metcap.payment_payment_details ppd ON (ppd.id = pm.details)
+    JOIN    metcap.autopay_agreement aa ON (pm.id = aa.payment_method)
+    JOIN    metcap.customer c ON (c.id = pm.customer)
+    JOIN    metcap.lease_participant lp ON (c.id = lp.customer)
+    JOIN    metcap.lease l ON (l.id = lp.lease)
+    JOIN    metcap.apt_unit a ON (a.id = l.unit)
+    JOIN    metcap.building b ON (b.id = a.building)
+    JOIN    metcap.payment_record p ON (pm.id = p.payment_method)
+    WHERE   b.property_code IN ('rann0001','silv1315','darc7110','east0340',
+            'east0350','lake0245')
+    AND     l.status = 'Active' 
+    AND     p.created_date > '01-AUG-2014'
+    ORDER BY 1,2;
+    
+SELECT  a.namespace, l.lease_id,b.property_code, r.id,r.transaction_id, 
+        p.payment_status,
+        r.payment_date,r.merchant_terminal_id,r.amount, r.reconciliation_status,r.reason_code, r.reason_text 
+FROM    _admin_.funds_reconciliation_record_record r
+JOIN    _admin_.admin_pmc_merchant_account_index m ON (r.merchant_terminal_id = m.merchant_terminal_id)
+JOIN    _admin_.admin_pmc a ON (a.id = m.pmc)
+JOIN    metcap.payment_record p ON (r.transaction_id::bigint = p.id)
+JOIN    metcap.lease_term_participant ltp ON (ltp.id = p.lease_term_participant)
+JOIN    metcap.lease_participant lp ON (lp.id = ltp.lease_participant)
+JOIN    metcap.lease l ON (l.id = lp.lease)
+JOIN    metcap.apt_unit au ON (au.id = l.unit)
+JOIN    metcap.building b ON (b.id = au.building)
+WHERE   r.reason_code IS NOT NULL
+AND     a.namespace = 'metcap'
+AND     r.payment_date >= '01-AUG-2014'
+ORDER BY 3,2;
