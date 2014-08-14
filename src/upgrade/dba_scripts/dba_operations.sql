dc557807fad3f6885aef7e3e881ac9c3a1631789
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-08-14 22:57:07 +0000

    VISTA-1458 legal_policy_terms_item change - part I

---
+++ src/upgrade/dba_scripts/dba_operations.sql
@@ -0,0 +1,20 @@
+/**
+***     ======================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***
+***             Functions for operations support
+***
+***     ======================================================================================================================
+**/
+
+CREATE OR REPLACE FUNCTION _dba_.move_tenantsure(v_old_schema, v_new_schema, v_tsure_no, v_participant_id) 
+RETURNS VOID AS 
+$$
+
+
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+
