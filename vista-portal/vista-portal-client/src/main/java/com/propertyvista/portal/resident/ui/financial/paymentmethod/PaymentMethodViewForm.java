5a500617b53c0a37d479d94cabc17cf47aa14ec2
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-08-25 18:11:58 +0000

    VISTA-5070 Some functionality should be disabled if merchant account is not present

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/paymentmethod/PaymentMethodViewForm.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/paymentmethod/PaymentMethodViewForm.java
@@ -19,12 +19,16 @@ import java.util.Set;
 import com.google.gwt.user.client.ui.IsWidget;
 
 import com.pyx4j.commons.css.ThemeColor;
+import com.pyx4j.forms.client.ui.form.EditableFormDecorator;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.i18n.shared.I18n;
+import com.pyx4j.security.shared.SecurityController;
+import com.pyx4j.widgets.client.Button;
 
 import com.propertyvista.domain.payment.CreditCardInfo.CreditCardType;
 import com.propertyvista.domain.payment.LeasePaymentMethod;
 import com.propertyvista.domain.payment.PaymentType;
+import com.propertyvista.domain.security.VistaCustomerPaymentTypeBehavior;
 import com.propertyvista.portal.rpc.portal.resident.dto.financial.PaymentMethodDTO;
 import com.propertyvista.portal.shared.ui.CPortalEntityEditor;
 import com.propertyvista.portal.shared.ui.PortalFormPanel;
@@ -33,6 +37,8 @@ import com.propertyvista.portal.shared.ui.util.editors.PortalPaymentMethodEditor
 public class PaymentMethodViewForm extends CPortalEntityEditor<PaymentMethodDTO> {
 
     private static final I18n i18n = I18n.get(PaymentMethodViewForm.class);
+
+    private Button editButton;
 
     public PaymentMethodViewForm(PaymentMethodViewImpl view) {
         super(PaymentMethodDTO.class, view, i18n.tr("Payment Method"), ThemeColor.contrast4);
@@ -70,5 +76,19 @@ public class PaymentMethodViewForm extends CPortalEntityEditor<PaymentMethodDTO>
         });
 
         return formPanel;
+    }
+
+    @Override
+    protected EditableFormDecorator<PaymentMethodDTO> createDecorator() {
+        EditableFormDecorator<PaymentMethodDTO> decor = super.createDecorator();
+        editButton = decor.getBtnEdit();
+        return decor;
+    }
+
+    @Override
+    protected void onValueSet(boolean populate) {
+        super.onValueSet(populate);
+
+        editButton.setVisible(SecurityController.check(VistaCustomerPaymentTypeBehavior.forPaymentMethodSetup()));
     }
 }
--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/views/bill/BillViewForm.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/views/bill/BillViewForm.java
@@ -20,8 +20,10 @@ import com.pyx4j.commons.css.ThemeColor;
 import com.pyx4j.forms.client.ui.form.FormDecorator;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.i18n.shared.I18n;
+import com.pyx4j.security.shared.SecurityController;
 import com.pyx4j.widgets.client.Button;
 
+import com.propertyvista.domain.security.VistaCustomerPaymentTypeBehavior;
 import com.propertyvista.portal.rpc.portal.resident.dto.financial.BillViewDTO;
 import com.propertyvista.portal.shared.ui.CPortalEntityForm;
 import com.propertyvista.portal.shared.ui.PortalFormPanel;
@@ -29,6 +31,13 @@ import com.propertyvista.portal.shared.ui.PortalFormPanel;
 public class BillViewForm extends CPortalEntityForm<BillViewDTO> {
 
     private static final I18n i18n = I18n.get(BillViewForm.class);
+
+    private final Button payButton = new Button(i18n.tr("Pay Bill"), new Command() {
+        @Override
+        public void execute() {
+            ((BillView.Presenter) getView().getPresenter()).payBill();
+        }
+    });
 
     public BillViewForm(BillView view) {
         super(BillViewDTO.class, view, i18n.tr("Bill View"), ThemeColor.contrast4);
@@ -44,15 +53,14 @@ public class BillViewForm extends CPortalEntityForm<BillViewDTO> {
     @Override
     protected FormDecorator<BillViewDTO> createDecorator() {
         FormDecorator<BillViewDTO> decorator = super.createDecorator();
-
-        Button btnPay = new Button(i18n.tr("Pay Bill"), new Command() {
-            @Override
-            public void execute() {
-                ((BillView.Presenter) getView().getPresenter()).payBill();
-            }
-        });
-        decorator.addHeaderToolbarWidget(btnPay);
-
+        decorator.addHeaderToolbarWidget(payButton);
         return decorator;
+    }
+
+    @Override
+    protected void onValueSet(boolean populate) {
+        super.onValueSet(populate);
+
+        payButton.setVisible(SecurityController.check(VistaCustomerPaymentTypeBehavior.values()));
     }
 }
