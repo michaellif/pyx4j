9349443f3b5b9d69d9d3eea8f8a64688eea8dcac
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-08-25 23:06:09 +0000

    VISTA-1458 generic function for lease customer change

--- src/upgrade/dba_scripts/dba_operations.sql
+++ src/upgrade/dba_scripts/dba_operations.sql
@@ -8,9 +8,35 @@
 ***     ======================================================================================================================
 **/
 
-CREATE OR REPLACE FUNCTION _dba_.move_tenantsure(v_old_schema, v_new_schema, v_tsure_no, v_participant_id) 
+CREATE OR REPLACE FUNCTION _dba_.change_lease_customer(v_schema_name TEXT, v_participant_id TEXT) 
 RETURNS VOID AS 
 $$
+DECLARE 
+
+    v_customer_id   BIGINT;
+    
+BEGIN
+
+    SELECT nextval('public.customer_seq') INTO v_customer_id;
+    
+    EXECUTE 'INSERT INTO '||v_schema_name||'.customer (id, created, updated) '
+            ||'VALUES ('||v_customer_id||',DATE_TRUNC(''second'',current_timestamp)::timestamp, '
+            ||'                     DATE_TRUNC(''second'',current_timestamp)::timestamp) ';
+            
+    EXECUTE 'UPDATE '||v_schema_name||'.lease_participant '
+            ||'SET  customer = '||v_customer_id||' '
+            ||'WHERE    participant_id = '||quote_literal(v_participant_id)||' ';
+            
+    EXECUTE 'UPDATE '||v_schema_name||'.id_assignment_sequence
+ '
+            ||'SET  number = number + 1 '
+            ||'WHERE    target = ''customer'' ';
+            
+    EXECUTE 'UPDATE '||v_schema_name||'.customer AS c '
+            ||'SET  customer_id = s.number '
+            ||'FROM     '||v_schema_name||'.id_assignment_sequence s '
+            ||'WHERE    s.target = ''customer'' '
+            ||'AND  c.id = '||v_customer_id||' ';
 
 
 END;
