9e006e49c33ad4b3c0158167a5f844b136e4b530
Ernesto Guimerans <ernestog@propertyvista.com>
2014-08-27 22:17:55 +0000

    VISTA-5093: Show permission descriptions from excel file when user is selecting permissions

--- vista-biz/vista-biz-preloader/src/main/java/com/propertyvista/biz/generator/PermissionDescriptionLoader.java
+++ vista-biz/vista-biz-preloader/src/main/java/com/propertyvista/biz/generator/PermissionDescriptionLoader.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -14,21 +14,77 @@
 package com.propertyvista.biz.generator;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections4.map.CaseInsensitiveMap;
+
+import com.google.gwt.thirdparty.guava.common.primitives.Chars;
+
+import com.pyx4j.essentials.server.csv.EntityCSVReciver;
+import com.pyx4j.gwt.server.IOUtils;
+
+import com.propertyvista.biz.generator.model.PermissionDescription;
 import com.propertyvista.domain.security.VistaCrmBehaviorDTO;
 
 public class PermissionDescriptionLoader {
 
-    // static loaded once descriptions
-//    Example:
-//    {
-//        EntityCSVReciver.create(PermissionDescription.class).loadResourceFile(
-//                IOUtils.resourceFileName("streetSuffixAbbreviationsCanada.xlsx", PermissionDescription.class));
-//    }
+    private static final String DESCRIPTIONS_FILE = "permission_descriptions.xls";
 
-    public static Collection<VistaCrmBehaviorDTO> enhanceDesctiontions(Collection<VistaCrmBehaviorDTO> values) {
-        // TODO 
+    // Chars to be removed from description names to avoid inconsistencies
+    public static final char[] IGNORING_CHARS = { ' ', ':' };
+
+    // CaseInsensitiveMap to avoid inconsistencies between permission names from xls file and permission name from DB
+    private static CaseInsensitiveMap<String, String> descriptionsMap;
+
+    private static synchronized Map<String, String> getDescriptionsMap() {
+        if (null == descriptionsMap) {
+            initDescriptionsMap();
+        }
+
+        return descriptionsMap;
+    }
+
+    public static Collection<VistaCrmBehaviorDTO> enhanceDescriptions(Collection<VistaCrmBehaviorDTO> values) {
+
+        setPermissions(values, getDescriptionsMap());
+
         return values;
+    }
+
+    // Set description from XLS file to permission from DB
+    private static void setPermissions(Collection<VistaCrmBehaviorDTO> to, Map<String, String> from) {
+
+        for (VistaCrmBehaviorDTO dto : to) {
+            // Ignore some chars from permission names from DB
+            String key = removeAllChars(dto.permission().getValue(), IGNORING_CHARS);
+            if (from.containsKey(key))
+                dto.description().setValue(from.get(key));
+        }
+    }
+
+    private static String removeAllChars(String str, char[] chars) {
+        StringBuffer result = new StringBuffer();
+
+        for (char c : str.toCharArray()) {
+            if (!(Chars.asList(chars)).contains(c)) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private static void initDescriptionsMap() {
+        List<PermissionDescription> descriptions = EntityCSVReciver.create(PermissionDescription.class).loadResourceFile(
+                IOUtils.resourceFileName(DESCRIPTIONS_FILE, PermissionDescriptionLoader.class));
+
+        descriptionsMap = new CaseInsensitiveMap<String, String>(descriptions.size());
+
+        for (PermissionDescription p : descriptions) {
+            // Ignore some chars from permission names from XLS file
+            descriptionsMap.put(removeAllChars(p.permission().getValue(), IGNORING_CHARS), p.description().getValue());
+        }
     }
 
 }
