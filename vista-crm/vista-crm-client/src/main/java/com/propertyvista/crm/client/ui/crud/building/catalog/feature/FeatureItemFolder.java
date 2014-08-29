6613b5dce73ed8765dfd9596caafebf0e17a6252
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-08-29 19:33:58 +0000

    VISTA-5104 Deposit Management in Yardi Mode Implementaion

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/feature/FeatureItemFolder.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/building/catalog/feature/FeatureItemFolder.java
@@ -13,30 +13,29 @@
  */
 package com.propertyvista.crm.client.ui.crud.building.catalog.feature;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.IsWidget;
 
+import com.pyx4j.commons.IFormatter;
 import com.pyx4j.entity.core.IEntity;
 import com.pyx4j.entity.core.IObject;
-import com.pyx4j.entity.core.criterion.PropertyCriterion;
-import com.pyx4j.forms.client.ui.CEntityComboBox;
-import com.pyx4j.forms.client.ui.CField;
+import com.pyx4j.forms.client.ui.CEntityHyperlink;
 import com.pyx4j.forms.client.ui.CForm;
-import com.pyx4j.forms.client.ui.CLabel;
-import com.pyx4j.forms.client.ui.folder.CFolderRowEditor;
-import com.pyx4j.forms.client.ui.folder.FolderColumnDescriptor;
+import com.pyx4j.forms.client.ui.folder.BoxFolderItemDecorator;
+import com.pyx4j.forms.client.ui.folder.IFolderItemDecorator;
+import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
+import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.site.client.AppPlaceEntityMapper;
-import com.pyx4j.site.client.ui.prime.misc.CEntityCrudHyperlink;
+import com.pyx4j.site.client.AppSite;
 
-import com.propertyvista.common.client.ui.components.folders.VistaTableFolder;
+import com.propertyvista.common.client.ui.components.folders.VistaBoxFolder;
 import com.propertyvista.domain.financial.offering.Feature;
 import com.propertyvista.domain.financial.offering.ProductItem;
 import com.propertyvista.domain.property.asset.BuildingElement;
 import com.propertyvista.domain.property.asset.LockerArea;
 import com.propertyvista.domain.property.asset.Parking;
-import com.propertyvista.shared.config.VistaFeatures;
 
-class FeatureItemFolder extends VistaTableFolder<ProductItem> {
+class FeatureItemFolder extends VistaBoxFolder<ProductItem> {
 
     private final CForm<Feature> parent;
 
@@ -46,21 +45,16 @@ class FeatureItemFolder extends VistaTableFolder<ProductItem> {
     }
 
     @Override
-    public List<FolderColumnDescriptor> columns() {
-        ArrayList<FolderColumnDescriptor> columns = new ArrayList<FolderColumnDescriptor>();
-
-        columns.add(new FolderColumnDescriptor(proto().name(), "20em"));
-        columns.add(new FolderColumnDescriptor(proto().price(), "8em"));
-        columns.add(new FolderColumnDescriptor(proto().element(), "15em"));
-        if (VistaFeatures.instance().yardiIntegration()) {
-            columns.add(new FolderColumnDescriptor(proto().yardiDepositLMR(), "5em"));
-        }
-        columns.add(new FolderColumnDescriptor(proto().depositLMR(), "5em"));
-        columns.add(new FolderColumnDescriptor(proto().depositMoveIn(), "5em"));
-        columns.add(new FolderColumnDescriptor(proto().depositSecurity(), "5em"));
-        columns.add(new FolderColumnDescriptor(proto().description(), "25em"));
-
-        return columns;
+    public IFolderItemDecorator<ProductItem> createItemDecorator() {
+        BoxFolderItemDecorator<ProductItem> decor = (BoxFolderItemDecorator<ProductItem>) super.createItemDecorator();
+        decor.setExpended(false);
+        decor.setCaptionFormatter(new IFormatter<ProductItem, String>() {
+            @Override
+            public String format(ProductItem value) {
+                return value.name().getStringView() + ", Price: $" + value.price().getStringView();
+            }
+        });
+        return decor;
     }
 
     @Override
@@ -68,15 +62,19 @@ class FeatureItemFolder extends VistaTableFolder<ProductItem> {
         return new FeatureItemEditor();
     }
 
-    private class FeatureItemEditor extends CFolderRowEditor<ProductItem> {
+    private class FeatureItemEditor extends CForm<ProductItem> {
+        private final CEntityHyperlink<BuildingElement> buildingElement = new CEntityHyperlink<BuildingElement>(new Command() {
+            @Override
+            public void execute() {
+                goToBuildingElement();
+            }
+        });
 
         public FeatureItemEditor() {
-            super(ProductItem.class, columns());
+            super(ProductItem.class);
         }
 
-        @SuppressWarnings({ "unchecked", "rawtypes" })
-        @Override
-        protected CField<?, ?> createCell(FolderColumnDescriptor column) {
+        private void goToBuildingElement() {
             Class<? extends IEntity> buildingElementClass = null;
             switch (parent.getValue().code().type().getValue()) {
             case Parking:
@@ -89,24 +87,39 @@ class FeatureItemFolder extends VistaTableFolder<ProductItem> {
                 break;
             }
 
-            CField<?, ?> comp;
-            if (column.getObject() == proto().element()) {
-                if (buildingElementClass != null) {
-                    if (parent.isEditable()) {
-                        CEntityComboBox<BuildingElement> combo = new CEntityComboBox(buildingElementClass);
-                        combo.addCriterion(PropertyCriterion.eq(combo.proto().building().productCatalog(), parent.getValue().catalog()));
-                        comp = inject(column.getObject(), combo);
-                    } else {
-                        comp = inject(column.getObject(), new CEntityCrudHyperlink<BuildingElement>(AppPlaceEntityMapper.resolvePlace(buildingElementClass)));
-                    }
-                } else {
-                    comp = new CLabel(); // there is no building element for this item!
-                }
-            } else {
-                comp = super.createCell(column);
+            if (buildingElementClass != null) {
+                AppSite.getPlaceController()
+                        .goTo(AppPlaceEntityMapper.resolvePlace(buildingElementClass).formViewerPlace(getValue().element().getPrimaryKey()));
             }
+        }
 
-            return comp;
+        @Override
+        protected IsWidget createContent() {
+            FormPanel formPanel = new FormPanel(this);
+
+            formPanel.append(Location.Left, proto().name()).decorate();
+            formPanel.append(Location.Left, proto().element(), buildingElement).decorate();
+            formPanel.append(Location.Left, proto().description()).decorate();
+
+            formPanel.append(Location.Right, proto().price()).decorate().componentWidth(100);
+            formPanel.append(Location.Right, proto().depositLMR()).decorate().componentWidth(100);
+            formPanel.append(Location.Right, proto().depositMoveIn()).decorate().componentWidth(100);
+            formPanel.append(Location.Right, proto().depositSecurity()).decorate().componentWidth(100);
+
+            return formPanel;
+        }
+
+        @Override
+        protected void onValueSet(boolean populate) {
+            super.onValueSet(populate);
+
+            buildingElement.setVisible(!getValue().element().isNull());
+
+            if (isViewable()) {
+                get(proto().depositLMR()).setVisible(parent.getValue().version().depositLMR().enabled().getValue());
+                get(proto().depositMoveIn()).setVisible(parent.getValue().version().depositMoveIn().enabled().getValue());
+                get(proto().depositSecurity()).setVisible(parent.getValue().version().depositSecurity().enabled().getValue());
+            }
         }
     }
 }
