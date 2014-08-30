e3a1d689184f92ab656ab9ecf2813505b5d235e3
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-08-30 13:21:18 +0000

    VISTA-1458 stats function - prod version

--- src/upgrade/dba_scripts/dba_stat_functions.sql
+++ src/upgrade/dba_scripts/dba_stat_functions.sql
@@ -15,7 +15,7 @@
 *** --------------------------------------------------------------------
 **/
 
-/*
+
 ALTER TABLE _dba_.building_stats    ADD COLUMN total_insurance INT  DEFAULT 0,
                                     ADD COLUMN total_tenantsure INT DEFAULT 0;
                                     
@@ -24,7 +24,7 @@ UPDATE _dba_.building_stats
 SET total_insurance = 0,
     total_tenantsure = 0;
 
-*/
+
 
 CREATE OR REPLACE FUNCTION _dba_.gather_building_stats(v_schema_name TEXT) RETURNS VOID
 AS
