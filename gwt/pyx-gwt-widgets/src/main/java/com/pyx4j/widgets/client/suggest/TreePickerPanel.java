a774148976037cf07bd3192808e1f89ad7ccd3c9
Michael Lifshits <michaellif@propertyvista.com>
2014-09-08 13:25:27 +0000

    Selector impl - selection

--- gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/suggest/TreePickerPanel.java
+++ gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/suggest/TreePickerPanel.java
@@ -23,10 +23,6 @@ package com.pyx4j.widgets.client.suggest;
 import java.util.Collection;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
