9efaf099e37b8b62f728b7c70959860253889e50
Armine Aramyan <arminea@propertyvista.com>
2014-09-09 17:04:09 +0000

    MultySelectorBox implementation

---
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CMultySelectorBox.java
@@ -0,0 +1,119 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Jan 11, 2010
+ * @author Michael
+ * @version $Id$
+ */
+package com.pyx4j.forms.client.ui;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+
+import com.pyx4j.commons.IFormatter;
+import com.pyx4j.entity.core.IEntity;
+import com.pyx4j.forms.client.events.HasOptionsChangeHandlers;
+import com.pyx4j.forms.client.events.OptionsChangeEvent;
+import com.pyx4j.forms.client.events.OptionsChangeHandler;
+import com.pyx4j.widgets.client.WatermarkComponent;
+
+public class CMultySelectorBox<E extends IEntity> extends CFocusComponent<Collection<E>, NMultySelectorBox<E>> implements IAcceptsWatermark, HasOptionsChangeHandlers<List<E>> {
+
+    private String watermark;
+
+    private IFormatter<E, String> formatter;
+
+    private List<E> options = new ArrayList<E>();
+
+    private IFormatter<E, String[]> optionPathFormatter;
+
+    public CMultySelectorBox() {
+        super();
+
+        setOptionPathFormatter(new IFormatter<E, String[]>() {
+
+            @Override
+            public String[] format(E value) {
+                return null;
+            }
+        });
+
+        setNativeComponent(new NMultySelectorBox<E>(this));
+    }
+
+    public void setFormatter(IFormatter<E, String> formatter) {
+        this.formatter = formatter;
+    }
+
+    public final IFormatter<E, String> getFormatter() {
+        if (formatter == null) {
+            setFormatter(new IFormatter<E, String>() {
+                @Override
+                public String format(E value) {
+                    if (value == null) {
+                        return null;
+                    } else {
+                        return value.getStringView();
+                    }
+                }
+            });
+        }
+        return formatter;
+    }
+
+    public IFormatter<E, String[]> getOptionPathFormatter() {
+        return optionPathFormatter;
+    }
+
+    public void setOptionPathFormatter(IFormatter<E, String[]> formatter) {
+        this.optionPathFormatter = formatter;
+    }
+
+    @Override
+    public HandlerRegistration addOptionsChangeHandler(OptionsChangeHandler<List<E>> handler) {
+        return addHandler(handler, OptionsChangeEvent.getType());
+    }
+
+    public void setOptions(Collection<E> opt) {
+        this.options = new ArrayList<E>(opt);
+        if (getNativeComponent() != null) {
+            getNativeComponent().processOptions(options);
+        }
+    }
+
+    @Override
+    public void setWatermark(String watermark) {
+        this.watermark = watermark;
+        if (asWidget() instanceof WatermarkComponent) {
+            ((WatermarkComponent) asWidget()).setWatermark(watermark);
+        }
+    }
+
+    @Override
+    public String getWatermark() {
+        return watermark;
+    }
+
+    @Override
+    protected String getDebugInfo() {
+        StringBuilder info = new StringBuilder(super.getDebugInfo());
+        info.append("watermark").append("=").append(getWatermark()).append(";");
+        return info.toString();
+    }
+}
---
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/NMultySelectorBox.java
@@ -0,0 +1,111 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2010 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Jan 11, 2010
+ * @author Michael
+ * @version $Id$
+ */
+package com.pyx4j.forms.client.ui;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Comparator;
+
+import com.google.gwt.user.client.ui.HTML;
+
+import com.pyx4j.commons.IFormatter;
+import com.pyx4j.entity.core.IEntity;
+import com.pyx4j.widgets.client.Label;
+import com.pyx4j.widgets.client.WatermarkComponent;
+import com.pyx4j.widgets.client.suggest.MultyWordSuggestOptionsGrabber;
+import com.pyx4j.widgets.client.suggest.SelectorListBox;
+
+public class NMultySelectorBox<E extends IEntity> extends NFocusField<Collection<E>, SelectorListBox<E>, CMultySelectorBox<E>, HTML> implements WatermarkComponent {
+
+    private final MultyWordSuggestOptionsGrabber<E> optionsGrabber;
+
+    public NMultySelectorBox(final CMultySelectorBox<E> cSuggestBox) {
+        super(cSuggestBox);
+        optionsGrabber = new MultyWordSuggestOptionsGrabber<E>(getCComponent().getFormatter());
+        optionsGrabber.setComparator(new Comparator<E>() {
+            @Override
+            public int compare(E paramT1, E paramT2) {
+                return paramT1.getStringView().compareTo(paramT2.getStringView());
+            }
+        });
+    }
+
+    void processOptions(Collection<E> options) {
+        optionsGrabber.setAllOptions(options);
+    }
+
+    @Override
+    protected Label createViewer() {
+        return new Label();
+    }
+
+    @Override
+    protected SelectorListBox<E> createEditor() {
+        return new SelectorListBox<E>(optionsGrabber, new IFormatter<E, String>() {
+
+            @Override
+            public String format(E value) {
+                return getCComponent().getFormatter().format(value);
+            }
+        }, new IFormatter<E, String[]>() {
+
+            @Override
+            public String[] format(E value) {
+                return getCComponent().getOptionPathFormatter().format(value);
+            }
+        });
+    }
+
+    @Override
+    public void setNativeValue(Collection<E> value) {
+        if (isViewable()) {
+//            getViewer().setText(getCComponent().getFormatter().format(value));
+        } else {
+            getEditor().setValue(value);
+        }
+    }
+
+    @Override
+    public Collection<E> getNativeValue() throws ParseException {
+        if (isViewable()) {
+            assert false : "getNativeValue() shouldn't be called in viewable mode";
+            return null;
+        } else {
+            return getEditor().getValue();
+        }
+    }
+
+    @Override
+    public void setWatermark(String watermark) {
+        if (getEditor() instanceof WatermarkComponent) {
+            ((WatermarkComponent) getEditor()).setWatermark(watermark);
+        }
+    }
+
+    @Override
+    public String getWatermark() {
+        if (getEditor() instanceof WatermarkComponent) {
+            return ((WatermarkComponent) getEditor()).getWatermark();
+        } else {
+            return null;
+        }
+    }
+}
