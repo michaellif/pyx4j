f6e2eb5b79d5d1660bde972a1862220a84ed3374
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-09-11 22:37:25 +0000

    VISTA-4751  adding processing of Daily Reports

---
+++ vista-operations/vista-operations-domain/src/main/java/com/propertyvista/operations/domain/eft/cards/CardsClearanceRecord.java
@@ -0,0 +1,35 @@
+/*
+ * (C) Copyright Property Vista Software Inc. 2011-2015 All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
+ * you entered into with Property Vista Software Inc.
+ *
+ * This notice and attribution to Property Vista Software Inc. may not be removed.
+ *
+ * Created on Sep 11, 2014
+ * @author vlads
+ * @version $Id$
+ */
+package com.propertyvista.operations.domain.eft.cards;
+
+import com.pyx4j.entity.annotations.Indexed;
+import com.pyx4j.entity.annotations.JoinColumn;
+import com.pyx4j.entity.annotations.MemberColumn;
+import com.pyx4j.entity.annotations.Owner;
+import com.pyx4j.entity.annotations.Table;
+import com.pyx4j.entity.core.IEntity;
+import com.pyx4j.i18n.annotations.I18n;
+
+import com.propertyvista.domain.VistaNamespace;
+
+@Table(namespace = VistaNamespace.operationsNamespace)
+@I18n(strategy = I18n.I18nStrategy.IgnoreAll)
+public interface CardsClearanceRecord extends IEntity {
+
+    @Owner
+    @JoinColumn
+    @Indexed
+    @MemberColumn(notNull = true)
+    CardsClearanceFile file();
+}
