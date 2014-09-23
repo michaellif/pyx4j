6fa496ddc9dd3bb31a08f203d52c11f5e51e5bea
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-09-23 21:26:06 +0000

    VISTA-5255 _admin_.cards_clearance_record update

---
+++ src/upgrade/1.4.0.1/2014-09-23/fix_cards_records.sql
@@ -0,0 +1,32 @@
+/**
+***     =====================================================================================================================
+***
+***             @version $Revision$ ($Author$) $Date$
+***     
+***             Fix cards records in _admin_ schema 
+***
+***     =====================================================================================================================
+**/
+
+
+
+BEGIN TRANSACTION;
+
+    UPDATE  _admin_.cards_clearance_record
+    SET     status = 'Processed'
+    WHERE   merchant_id = 'PRVNEP01'
+    AND     reference_number = '1234'
+    AND     amount IN (2.00, 2.10);
+ 
+ /* Not executed yet  
+    
+    UPDATE _admin_.cards_reconciliation_record
+    SET     status = 'Processed'
+    WHERE   merchant_terminal_id = 'PRVNEP01'
+    AND     date = '09-SEP-2014';
+*/    
+
+    
+COMMIT;
+    
+    
