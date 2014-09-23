4720184a7d72680f300972203849565294dc0842
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-09-23 21:29:18 +0000

    VISTA-5181 Reports->EFT Variance: In IE9 the report data is displayed incorrectly

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/eftvariance/EftVarianceReportSettingsForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/eftvariance/EftVarianceReportSettingsForm.java
@@ -16,8 +16,8 @@ package com.propertyvista.crm.client.ui.reports.eftvariance;
 import com.google.gwt.user.client.ui.IsWidget;
 
 import com.pyx4j.forms.client.ui.CForm;
-import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
+import com.pyx4j.forms.client.ui.panels.FormPanel;
 
 import com.propertyvista.domain.reports.EftVarianceReportMetadata;
 
@@ -30,7 +30,7 @@ public class EftVarianceReportSettingsForm extends CForm<EftVarianceReportMetada
     @Override
     protected IsWidget createContent() {
         FormPanel formPanel = new FormPanel(this);
-        formPanel.append(Location.Left, proto().billingCycleStartDate()).decorate();
+        formPanel.append(Location.Left, proto().billingCycleStartDate()).decorate().componentWidth(120);
         return formPanel;
     }
 
