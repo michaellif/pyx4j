77014aca8ae69ce2f5a6a536afae10006e21074f
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-09-24 16:30:02 +0000

    VISTA-558 error handling corrections

--- vista-operations/vista-operations-rpc/src/main/java/com/propertyvista/operations/rpc/dto/ConnectionTestResultDTO.java
+++ vista-operations/vista-operations-rpc/src/main/java/com/propertyvista/operations/rpc/dto/ConnectionTestResultDTO.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -31,7 +31,7 @@ public class ConnectionTestResultDTO implements Serializable {
     public static final String OK = " <span style=\"color:#00CC33\">OK</span>";
 
     public static String htmlError(String text) {
-        return "<span style=\"color:red\"> " + text.replace("\n", CR) + " </span>";
+        return "<span style=\"color:red\"> " + ((text != null) ? text.replace("\n", CR) : "") + " </span>";
     }
 
     public static String htmlWarn(String text) {
