19cbbdf8ca10f4bcba8f1b9f5251be3f3ee231fa
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-09-24 01:08:09 +0000

    VISTA-5255 _admin_.cards_reconciliation_record updated

--- src/upgrade/1.4.0.1/2014-09-23/fix_cards_records.sql
+++ src/upgrade/1.4.0.1/2014-09-23/fix_cards_records.sql
@@ -18,13 +18,13 @@ BEGIN TRANSACTION;
     AND     reference_number = '1234'
     AND     amount IN (2.00, 2.10);
  
- /* Not executed yet  
+ 
     
     UPDATE _admin_.cards_reconciliation_record
     SET     status = 'Processed'
     WHERE   merchant_terminal_id = 'PRVNEP01'
     AND     date = '09-SEP-2014';
-*/    
+
 
     
 COMMIT;
