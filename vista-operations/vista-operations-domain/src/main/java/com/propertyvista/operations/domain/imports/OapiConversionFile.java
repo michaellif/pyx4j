863af7705d25210e09acf618991317b041f788cf
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-09-30 18:14:48 +0000

    VISTA-75  @MemberColumn(name = "tp")

--- vista-operations/vista-operations-domain/src/main/java/com/propertyvista/operations/domain/imports/OapiConversionFile.java
+++ vista-operations/vista-operations-domain/src/main/java/com/propertyvista/operations/domain/imports/OapiConversionFile.java
@@ -17,6 +17,7 @@ import com.pyx4j.entity.annotations.Caption;
 import com.pyx4j.entity.annotations.Detached;
 import com.pyx4j.entity.annotations.EmbeddedEntity;
 import com.pyx4j.entity.annotations.JoinColumn;
+import com.pyx4j.entity.annotations.MemberColumn;
 import com.pyx4j.entity.annotations.Owner;
 import com.pyx4j.entity.annotations.ReadOnly;
 import com.pyx4j.entity.annotations.Table;
@@ -59,6 +60,7 @@ public interface OapiConversionFile extends IHasFile<OapiConversionBlob> {
     @ToString
     @Caption(name = "File type")
     @NotNull
+    @MemberColumn(name = "tp")
     IPrimitive<OapiConversionFileType> type(); //TODO define type for possible entity XLS files
 
     @Override
