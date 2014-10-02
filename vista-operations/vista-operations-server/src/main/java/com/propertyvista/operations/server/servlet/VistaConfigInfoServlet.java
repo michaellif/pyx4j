91cf41c25b20ec37ce1e2b40fb1ad5ac24a5cbc7
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-10-02 01:29:03 +0000

    VISTA-558 print config properties

--- vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/servlet/VistaConfigInfoServlet.java
+++ vista-operations/vista-operations-server/src/main/java/com/propertyvista/operations/server/servlet/VistaConfigInfoServlet.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -19,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.pyx4j.config.server.PropertiesConfiguration;
 import com.pyx4j.config.server.ServerSideConfiguration;
 import com.pyx4j.essentials.server.admin.ConfigInfoServlet;
 import com.pyx4j.security.shared.SecurityController;
@@ -85,6 +86,19 @@ public class VistaConfigInfoServlet extends ConfigInfoServlet {
         b.append("  EncryptedStorageConfiguration         :\n      ").append(conf.getEncryptedStorageConfiguration().toString().replaceAll("\n", "\n      "))
                 .append("\n");
 
+        b.append("\nconfig.properties:\n");
+
+        return b.toString();
+    }
+
+    @Override
+    protected String buildConfigurationText() {
+        StringBuilder b = new StringBuilder();
+        b.append(super.buildConfigurationText());
+
+        b.append("\nconfig.properties:\n");
+        b.append(PropertiesConfiguration.stringView("  ", ServerSideConfiguration.instance(AbstractVistaServerSideConfiguration.class).getConfigProperties()
+                .getProperties()));
         return b.toString();
     }
 }
