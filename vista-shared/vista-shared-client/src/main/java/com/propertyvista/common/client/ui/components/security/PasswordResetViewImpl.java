557778f8dcfbb51b1a667e5de5cd858228450d7f
Armine Aramyan <arminea@propertyvista.com>
2014-10-09 17:29:54 +0000

    VISTA-5317 : Security permissions: It is possible to login to CRM without input of the password

--- vista-shared/vista-shared-client/src/main/java/com/propertyvista/common/client/ui/components/security/PasswordResetViewImpl.java
+++ vista-shared/vista-shared-client/src/main/java/com/propertyvista/common/client/ui/components/security/PasswordResetViewImpl.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -44,11 +44,12 @@ public class PasswordResetViewImpl extends VerticalPanel implements PasswordRese
         final Button submitButton = new Button(i18n.tr("Submit"), new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
+                form.setVisitedRecursive();
                 if (form.isValid()) {
                     presenter.resetPassword(form.getValue());
                 } else {
                     // here we hope that because the focus left the form and moved to submitButton,
-                    // we get the relevant validation error on the form. 
+                    // we get the relevant validation error on the form.
                 }
             }
         });
