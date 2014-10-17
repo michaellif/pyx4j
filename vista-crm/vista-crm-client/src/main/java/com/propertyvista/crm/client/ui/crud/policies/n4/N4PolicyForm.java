fbba71401c6d9016099162ab8f81d750b12b39f8
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-10-17 22:44:26 +0000

    VISTA-5394 N4 report generation has a difficult to use interface

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/n4/N4PolicyForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/n4/N4PolicyForm.java
@@ -13,25 +13,26 @@
  */
 package com.propertyvista.crm.client.ui.crud.policies.n4;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gwt.user.client.ui.IsWidget;
 
+import com.pyx4j.entity.core.EntityFactory;
 import com.pyx4j.entity.core.IObject;
-import com.pyx4j.forms.client.ui.CComboBox;
+import com.pyx4j.forms.client.ui.CEntityLabel;
 import com.pyx4j.forms.client.ui.CForm;
 import com.pyx4j.forms.client.ui.CPhoneField;
 import com.pyx4j.forms.client.ui.CPhoneField.PhoneType;
-import com.pyx4j.forms.client.ui.folder.IFolderItemDecorator;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.site.client.backoffice.ui.prime.form.IForm;
-import com.pyx4j.widgets.client.dialog.OkCancelDialog;
+import com.pyx4j.site.client.ui.dialogs.EntitySelectorListDialog;
+import com.pyx4j.site.client.ui.dialogs.EntitySelectorListDialog.Formatter;
 
 import com.propertyvista.common.client.ui.components.editors.InternationalAddressEditor;
 import com.propertyvista.common.client.ui.components.folders.VistaBoxFolder;
-import com.propertyvista.common.client.ui.decorations.VistaBoxFolderItemDecorator;
 import com.propertyvista.crm.client.ui.crud.policies.common.PolicyDTOTabPanelBasedForm;
 import com.propertyvista.domain.financial.ARCode;
 import com.propertyvista.domain.financial.offering.YardiChargeCode;
@@ -110,86 +111,83 @@ public class N4PolicyForm extends PolicyDTOTabPanelBasedForm<N4PolicyDTO> {
 
         @Override
         protected CForm<N4PolicyDTOARCodeHolderDTO> createItemForm(IObject<?> member) {
-            N4PolicyDTOARCodeHolderForm form = new N4PolicyDTOARCodeHolderForm(null, false);
+            N4PolicyDTOARCodeHolderForm form = new N4PolicyDTOARCodeHolderForm();
             form.inheritViewable(false);
             form.setViewable(true);
             return form;
         }
 
         @Override
-        public IFolderItemDecorator<N4PolicyDTOARCodeHolderDTO> createItemDecorator() {
-            VistaBoxFolderItemDecorator<N4PolicyDTOARCodeHolderDTO> d = (VistaBoxFolderItemDecorator<N4PolicyDTOARCodeHolderDTO>) super.createItemDecorator();
-            d.setCollapsible(false);
-            return d;
-        }
-
-        @Override
         protected void addItem() {
-            new N4PolicyDTOARCodeHolderFormDialog(arCodeOptions) {
+            new EntitySelectorListDialog<ARCode>(i18n.tr("Select AR Code(s)"), true, getCurrentArCodeOptions(), new Formatter<ARCode>() {
+                @Override
+                public String format(ARCode code) {
+                    return formatARCode(code);
+                }
+            }) {
                 @Override
                 public boolean onClickOk() {
-                    if (super.onClickOk()) {
-                        ARCodeFolder.this.addItem(getValue());
-                        return true;
-                    } else {
-                        return false;
+                    for (ARCode item : getSelectedItems()) {
+                        N4PolicyDTOARCodeHolderDTO newItem = EntityFactory.create(N4PolicyDTOARCodeHolderDTO.class);
+                        newItem.arCode().set(item);
+                        addItem(newItem);
                     }
+                    return true;
                 }
             }.show();
         }
 
+        private List<ARCode> getCurrentArCodeOptions() {
+            List<ARCode> options = new LinkedList<ARCode>(arCodeOptions);
+            for (N4PolicyDTOARCodeHolderDTO item : getValue()) {
+                options.remove(item.arCode());
+            }
+            return options;
+        }
     }
 
-    public abstract static class N4PolicyDTOARCodeHolderFormDialog extends OkCancelDialog {
+    private static String yardiChargeCodesLabel(List<YardiChargeCode> yardiChargeCodes) {
+        StringBuilder builder = new StringBuilder();
 
-        private final N4PolicyDTOARCodeHolderForm form;
-
-        public N4PolicyDTOARCodeHolderFormDialog(List<ARCode> codeOptions) {
-            super(i18n.tr("Select AR Code"));
-            form = new N4PolicyDTOARCodeHolderForm(codeOptions, true);
-            form.init();
-            form.populateNew();
-            setBody(form);
+        for (YardiChargeCode chargeCode : yardiChargeCodes) {
+            builder.append(chargeCode.getStringView());
+            builder.append(", ");
         }
 
-        @Override
-        public boolean onClickOk() {
-            form.setVisitedRecursive();
-            return form.isValid();
-        }
+        builder.setLength(builder.length() - 2);
+        return builder.toString();
+    }
 
-        public final N4PolicyDTOARCodeHolderDTO getValue() {
-            return form.getValue();
+    private static String formatARCode(ARCode o) {
+        String result = "";
+        if (o != null) {
+            result = o.getStringView();
+            if (!o.yardiChargeCodes().isEmpty()) {
+                result += " " + i18n.tr("(Yardi charge codes: {0})", yardiChargeCodesLabel(o.yardiChargeCodes()));
+            }
         }
+        return result;
     }
 
     public static class N4PolicyDTOARCodeHolderForm extends CForm<N4PolicyDTOARCodeHolderDTO> {
 
-        private CComboBox<ARCode> arCodeBox;
+        private final CEntityLabel<ARCode> arCodeComp = new CEntityLabel<ARCode>() {
+            @Override
+            public String format(ARCode code) {
+                return formatARCode(code);
+            }
+        };
 
-        private final boolean inlineARCodes;
-
-        public N4PolicyDTOARCodeHolderForm(List<ARCode> codeOptions, final boolean inlineARCodes) {
+        public N4PolicyDTOARCodeHolderForm() {
             super(N4PolicyDTOARCodeHolderDTO.class);
-            this.inlineARCodes = inlineARCodes;
-            this.arCodeBox = new CComboBox<ARCode>() {
-                @Override
-                public String getItemName(ARCode o) {
-                    if (o == null) {
-                        return super.getItemName(o);
-                    }
-                    return o.getStringView()
-                            + (o.yardiChargeCodes().isEmpty() | !inlineARCodes ? "" : " "
-                                    + i18n.tr("(Yardi charge codes: {0})", yardiChargeCodesLabel(o.yardiChargeCodes())));
-                }
-            };
-            this.arCodeBox.setOptions(codeOptions);
         }
 
         @Override
         protected IsWidget createContent() {
             FormPanel panel = new FormPanel(this);
-            panel.append(Location.Dual, proto().arCode(), arCodeBox).decorate().labelWidth(100);
+
+            panel.append(Location.Dual, proto().arCode(), arCodeComp).decorate().labelWidth(100);
+
             return panel;
         }
 
@@ -197,32 +195,12 @@ public class N4PolicyForm extends PolicyDTOTabPanelBasedForm<N4PolicyDTO> {
         protected void onValueSet(boolean populate) {
             super.onValueSet(populate);
 
-            if (!inlineARCodes && !getValue().arCode().yardiChargeCodes().isEmpty()) {
-                arCodeBox.setNote(
-                        i18n.tr("Warning! Includes the following Yardi charge codes: {0}", yardiChargeCodesLabel(getValue().arCode().yardiChargeCodes())),
+            if (!getValue().arCode().yardiChargeCodes().isEmpty()) {
+                arCodeComp.setNote(i18n.tr("Includes the following Yardi charge codes: {0}", yardiChargeCodesLabel(getValue().arCode().yardiChargeCodes())),
                         NoteStyle.Warn);
             } else {
-                arCodeBox.setNote(null);
+                arCodeComp.setNote(null);
             }
-        }
-
-        public void setArCodeOptions(List<ARCode> arCodes) {
-            arCodeBox.setOptions(arCodes);
-        }
-
-        private static String yardiChargeCodesLabel(List<YardiChargeCode> yardiChargeCodes) {
-            StringBuilder builder = new StringBuilder();
-
-            int last = yardiChargeCodes.size();
-            int current = 1;
-            for (YardiChargeCode chargeCode : yardiChargeCodes) {
-                builder.append(chargeCode.getStringView());
-                if (current != last) {
-                    builder.append(", ");
-                }
-                current += 1;
-            }
-            return builder.toString();
         }
     }
 }
