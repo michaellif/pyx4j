8bb55d1b9679dd8c519c680edc0ef543d31d84c6
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-10-17 00:20:07 +0000

    VISTA-5340 code review

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/organization/EmployeeCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/organization/EmployeeCrudServiceImpl.java
@@ -13,9 +13,6 @@
  */
 package com.propertyvista.crm.server.services.organization;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import com.pyx4j.commons.CommonsStringUtils;
@@ -95,35 +92,26 @@ public class EmployeeCrudServiceImpl extends AbstractCrudServiceDtoImpl<Employee
     protected void enhanceListCriteria(EntityListCriteria<Employee> boCriteria, EntityListCriteria<EmployeeDTO> toCriteria) {
         toCriteria.removeAllCriterions(toCriteria.proto().user().credential());
         if (SecurityController.check(DataModelPermission.permissionRead(EmployeePrivilegesDTO.class))) {
-            Set<PropertyCriterion> rolesCriteria = toCriteria.getCriterions(toCriteria.proto().privileges().roles());
-            if (rolesCriteria != null) {
-                Iterator<PropertyCriterion> iterator = rolesCriteria.iterator();
-                while (iterator.hasNext()) {
-                    PropertyCriterion roleCriteria = iterator.next();
-                    toCriteria.removeCriterions(roleCriteria);
-                    if (roleCriteria.getRestriction() == Restriction.EQUAL) {
-                        boCriteria.eq(boCriteria.proto().user().credential().roles(), roleCriteria.getValue());
-                    } else if (roleCriteria.getRestriction() == Restriction.NOT_EQUAL) {
-                        boCriteria.ne(boCriteria.proto().user().credential().roles(), roleCriteria.getValue());
-                    } else {
-                        throw new IllegalArgumentException();
-                    }
+            for (PropertyCriterion roleCriteria : toCriteria.getCriterions(toCriteria.proto().privileges().roles())) {
+                toCriteria.removeCriterions(roleCriteria);
+                if (roleCriteria.getRestriction() == Restriction.EQUAL) {
+                    boCriteria.eq(boCriteria.proto().user().credential().roles(), roleCriteria.getValue());
+                } else if (roleCriteria.getRestriction() == Restriction.NOT_EQUAL) {
+                    boCriteria.ne(boCriteria.proto().user().credential().roles(), roleCriteria.getValue());
+                } else {
+                    throw new IllegalArgumentException();
                 }
             }
-            Set<PropertyCriterion> behaviorsCriteria = toCriteria.getCriterions(toCriteria.proto().privileges().behaviors());
-            if (behaviorsCriteria != null) {
-                Iterator<PropertyCriterion> iterator = behaviorsCriteria.iterator();
-                while (iterator.hasNext()) {
-                    PropertyCriterion behaviorCriteria = iterator.next();
-                    toCriteria.removeCriterions(behaviorCriteria);
-                    if (behaviorCriteria.getRestriction() == Restriction.EQUAL) {
-                        boCriteria.eq(boCriteria.proto().user().credential().roles().$().behaviors(), behaviorCriteria.getValue());
-                    } else if (behaviorCriteria.getRestriction() == Restriction.NOT_EQUAL) {
-                        boCriteria.notExists(boCriteria.proto().user().credential().roles(),
-                                PropertyCriterion.eq(boCriteria.proto().user().credential().roles().$().behaviors(), behaviorCriteria.getValue()));
-                    } else {
-                        throw new IllegalArgumentException();
-                    }
+
+            for (PropertyCriterion behaviorCriteria : toCriteria.getCriterions(toCriteria.proto().privileges().behaviors())) {
+                toCriteria.removeCriterions(behaviorCriteria);
+                if (behaviorCriteria.getRestriction() == Restriction.EQUAL) {
+                    boCriteria.eq(boCriteria.proto().user().credential().roles().$().behaviors(), behaviorCriteria.getValue());
+                } else if (behaviorCriteria.getRestriction() == Restriction.NOT_EQUAL) {
+                    boCriteria.notExists(boCriteria.proto().user().credential().roles(),
+                            PropertyCriterion.eq(boCriteria.proto().user().credential().roles().$().behaviors(), behaviorCriteria.getValue()));
+                } else {
+                    throw new IllegalArgumentException();
                 }
             }
         }
