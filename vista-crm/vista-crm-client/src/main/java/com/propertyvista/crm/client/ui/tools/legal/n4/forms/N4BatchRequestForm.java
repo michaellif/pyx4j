82155333ffe0982bdb59587a6d68af8ed2ec8a58
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-10-17 23:19:43 +0000

    VISTA-5394 N4 report generation has a difficult to use interface

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/tools/legal/n4/forms/N4BatchRequestForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/tools/legal/n4/forms/N4BatchRequestForm.java
@@ -15,20 +15,15 @@ package com.propertyvista.crm.client.ui.tools.legal.n4.forms;
 
 import java.util.List;
 
-import com.google.gwt.dom.client.Style.Display;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 
 import com.pyx4j.forms.client.ui.CComboBox;
 import com.pyx4j.forms.client.ui.CForm;
 import com.pyx4j.forms.client.ui.CPhoneField;
 import com.pyx4j.forms.client.ui.CPhoneField.PhoneType;
-import com.pyx4j.forms.client.ui.decorators.FieldDecorator;
-import com.pyx4j.forms.client.ui.decorators.FieldDecorator.Builder;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.i18n.shared.I18n;
-import com.pyx4j.site.client.backoffice.ui.prime.form.FieldDecoratorBuilder;
 
 import com.propertyvista.common.client.ui.components.editors.InternationalAddressEditor;
 import com.propertyvista.crm.rpc.dto.legal.n4.N4BatchRequestDTO;
@@ -38,46 +33,34 @@ public class N4BatchRequestForm extends CForm<N4BatchRequestDTO> {
 
     private static final I18n i18n = I18n.get(N4BatchRequestForm.class);
 
-    private static class MyDecoratorBuilder extends FieldDecoratorBuilder {
-
-        public MyDecoratorBuilder() {
-            super();
-        }
-
-        @Override
-        public FieldDecorator build() {
-            FieldDecorator d = super.build();
-            d.asWidget().getElement().getStyle().setDisplay(Display.BLOCK);
-            return d;
-        };
-
-        public Builder width(String componentWidth) {
-            labelWidth(componentWidth);
-            return super.componentWidth(componentWidth);
-        }
-    }
-
-    private CComboBox<Employee> agentComboBox;
+    private final CComboBox<Employee> agentComboBox;
 
     public N4BatchRequestForm() {
         super(N4BatchRequestDTO.class);
+
+        agentComboBox = new CComboBox<Employee>(CComboBox.NotInOptionsPolicy.DISCARD) {
+            @Override
+            public String getItemName(Employee o) {
+                return (o != null && !o.isNull()) ? o.name().getStringView() + (o.signature().getPrimaryKey() == null ? i18n.tr(" (No Signature)") : "") : "";
+            }
+        };
+        agentComboBox.setMandatory(true);
     }
 
     @Override
     protected IsWidget createContent() {
         FormPanel formPanel = new FormPanel(this);
 
-        FlowPanel n4FillingSettingsPanel = new FlowPanel();
-        n4FillingSettingsPanel.add(inject(proto().noticeDate(), new MyDecoratorBuilder().width("150px").build()));
-        n4FillingSettingsPanel.add(inject(proto().deliveryMethod(), new MyDecoratorBuilder().width("150px").build()));
-        n4FillingSettingsPanel.add(inject(proto().agent(), createAgentComboBox(), new MyDecoratorBuilder().width("150px").build()));
-        formPanel.append(Location.Dual, n4FillingSettingsPanel);
-        formPanel.br();
+        formPanel.append(Location.Left, proto().noticeDate()).decorate();
+        formPanel.append(Location.Left, proto().deliveryMethod()).decorate();
+        formPanel.append(Location.Right, proto().agent(), agentComboBox).decorate();
+
         formPanel.h1(i18n.tr("Agent/Company Contact Information:"));
         formPanel.append(Location.Left, proto().companyName()).decorate();
-        formPanel.append(Location.Left, proto().phoneNumber(), new CPhoneField(PhoneType.northAmerica)).decorate();
-        formPanel.append(Location.Left, proto().faxNumber(), new CPhoneField(PhoneType.northAmerica)).decorate();
         formPanel.append(Location.Left, proto().emailAddress()).decorate();
+        formPanel.append(Location.Right, proto().phoneNumber(), new CPhoneField(PhoneType.northAmerica)).decorate();
+        formPanel.append(Location.Right, proto().faxNumber(), new CPhoneField(PhoneType.northAmerica)).decorate();
+        formPanel.br();
         formPanel.append(Location.Dual, proto().mailingAddress(), new InternationalAddressEditor());
 
         return formPanel;
@@ -86,16 +69,4 @@ public class N4BatchRequestForm extends CForm<N4BatchRequestDTO> {
     public void setAgents(List<Employee> agents) {
         agentComboBox.setOptions(agents);
     }
-
-    private CComboBox<Employee> createAgentComboBox() {
-        agentComboBox = new CComboBox<Employee>(CComboBox.NotInOptionsPolicy.DISCARD) {
-            @Override
-            public String getItemName(Employee o) {
-                return (o != null && !o.isNull()) ? o.name().getStringView() + (o.signature().getPrimaryKey() == null ? i18n.tr(" (No Signature)") : "") : "";
-            }
-        };
-        agentComboBox.setMandatory(true);
-        return agentComboBox;
-    }
-
 }
