36720c83f9a9f2a4e736af56e0906cdec0b2ddc1
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-10-19 03:23:24 +0000

    VISTA-5400 EFT Variance Report need filter by Building

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/eftvariance/EftVarianceReportSettingsForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/reports/eftvariance/EftVarianceReportSettingsForm.java
@@ -15,14 +15,12 @@ package com.propertyvista.crm.client.ui.reports.eftvariance;
 
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 
 import com.pyx4j.forms.client.ui.CForm;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.site.client.backoffice.ui.IPane;
-import com.pyx4j.site.client.backoffice.ui.prime.form.FieldDecoratorBuilder;
 
 import com.propertyvista.crm.client.ui.reports.SelectPortfolioFolder;
 import com.propertyvista.crm.client.ui.reports.SelectedBuildingsFolder;
@@ -42,25 +40,25 @@ public class EftVarianceReportSettingsForm extends CForm<EftVarianceReportMetada
         FormPanel formPanel = new FormPanel(this);
         formPanel.append(Location.Left, proto().billingCycleStartDate()).decorate().componentWidth(120);
 
-        FlowPanel buildingFilterPanel = new FlowPanel();
-        buildingFilterPanel.add(inject(proto().filterByPortfolio(), new FieldDecoratorBuilder().build()));
-        buildingFilterPanel.add(inject(proto().selectedPortfolios(), new SelectPortfolioFolder(parentView)));
+        formPanel.append(Location.Right, proto().filterByPortfolio()).decorate();
+        formPanel.append(Location.Right, proto().selectedPortfolios(), new SelectPortfolioFolder(parentView));
+
+        formPanel.append(Location.Right, proto().filterByBuildings()).decorate();
+        formPanel.append(Location.Right, proto().selectedBuildings(), new SelectedBuildingsFolder(parentView));
+
+        // tweaks:
         get(proto().filterByPortfolio()).addValueChangeHandler(new ValueChangeHandler<Boolean>() {
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> event) {
                 get(proto().selectedPortfolios()).setVisible(event.getValue());
             }
         });
-
-        buildingFilterPanel.add(inject(proto().filterByBuildings(), new FieldDecoratorBuilder().build()));
         get(proto().filterByBuildings()).addValueChangeHandler(new ValueChangeHandler<Boolean>() {
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> event) {
                 get(proto().selectedBuildings()).setVisible(event.getValue());
             }
         });
-        buildingFilterPanel.add(inject(proto().selectedBuildings(), new SelectedBuildingsFolder(parentView)));
-        formPanel.append(Location.Right, buildingFilterPanel);
 
         return formPanel;
     }
