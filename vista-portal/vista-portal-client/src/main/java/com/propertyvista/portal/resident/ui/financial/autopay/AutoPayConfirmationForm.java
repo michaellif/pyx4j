31178993677c4d764699f3db968bdbe8af56a5e5
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-10-28 01:23:00 +0000

    VISTA-5457 wording change

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/autopay/AutoPayConfirmationForm.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/resident/ui/financial/autopay/AutoPayConfirmationForm.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -36,13 +36,14 @@ public class AutoPayConfirmationForm extends CPortalEntityForm<AutoPayDTO> {
     private static final I18n i18n = I18n.get(AutoPayConfirmationForm.class);
 
     public AutoPayConfirmationForm() {
-        super(AutoPayDTO.class, null, i18n.tr("Automatic Payment Submitted Successfully!"), new Button(i18n.tr("Continue"), new Command() {
+        super(AutoPayDTO.class, null, i18n.tr("Your Auto Pay has been successfully setup and will be processed automatically every month."), new Button(
+                i18n.tr("Continue"), new Command() {
 
-            @Override
-            public void execute() {
-                AppSite.getPlaceController().goTo(AppPlace.NOWHERE);
-            }
-        }), ThemeColor.contrast4);
+                    @Override
+                    public void execute() {
+                        AppSite.getPlaceController().goTo(AppPlace.NOWHERE);
+                    }
+                }), ThemeColor.contrast4);
     }
 
     @Override
@@ -53,7 +54,8 @@ public class AutoPayConfirmationForm extends CPortalEntityForm<AutoPayDTO> {
         formPanel.append(Location.Left, proto().coveredItems(), new PapCoveredItemFolder());
         formPanel.append(Location.Left, proto().total(), new CCurrencyMoneyLabel(i18n.tr("CAD $"))).decorate();
         formPanel.br();
-        formPanel.append(Location.Left, proto().nextPaymentDate(), new CDateLabel()).decorate().componentWidth(100);
+        formPanel.append(Location.Left, proto().nextPaymentDate(), new CDateLabel()).decorate().customLabel(i18n.tr("Your first payment will be processed on"))
+                .componentWidth(100);
 
         return formPanel;
     }
