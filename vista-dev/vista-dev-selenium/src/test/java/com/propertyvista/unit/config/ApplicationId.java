ef0fde532f02d80c92666f43077382b2cf898354
Slava Skarzhevskyy <vlads@propertyvista.com>
2014-11-07 00:08:34 +0000

    VISTA-75 use new URLS

--- vista-dev/vista-dev-selenium/src/test/java/com/propertyvista/unit/config/ApplicationId.java
+++ vista-dev/vista-dev-selenium/src/test/java/com/propertyvista/unit/config/ApplicationId.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -15,12 +15,11 @@ package com.propertyvista.unit.config;
 
 public enum ApplicationId {
 
-    portal,
-
-    ptapp,
-
     crm,
 
-    tester
+    site,
 
+    resident,
+
+    prospect
 }
--- vista-dev/vista-dev-selenium/src/test/java/com/propertyvista/unit/config/VistaSeleniumTestConfiguration.java
+++ vista-dev/vista-dev-selenium/src/test/java/com/propertyvista/unit/config/VistaSeleniumTestConfiguration.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -23,7 +23,7 @@ import com.propertyvista.domain.security.common.VistaApplication;
 
 /**
  * This is local Global configurator for all tests.
- * 
+ *
  * Do not commit the modifications to this to SVN.
  */
 public class VistaSeleniumTestConfiguration extends DefaultSeleniumTestConfiguration {
@@ -35,7 +35,7 @@ public class VistaSeleniumTestConfiguration extends DefaultSeleniumTestConfigura
     protected final boolean gwtServerTrace;
 
     public VistaSeleniumTestConfiguration() {
-        this(ApplicationId.portal);
+        this(ApplicationId.crm);
     }
 
     public VistaSeleniumTestConfiguration(ApplicationId appID) {
@@ -50,7 +50,7 @@ public class VistaSeleniumTestConfiguration extends DefaultSeleniumTestConfigura
         if (System.getProperty("bamboo.buildNumber") == null) {
             this.deploymentId = deploymentId;
         } else {
-            this.deploymentId = VistaDeploymentId.www22;
+            this.deploymentId = VistaDeploymentId.env11;
         }
         gwtServerTrace = (deploymentId == VistaDeploymentId.local);
     }
@@ -58,16 +58,44 @@ public class VistaSeleniumTestConfiguration extends DefaultSeleniumTestConfigura
     @Override
     public String getTestUrl() {
         StringBuilder url = new StringBuilder();
-        url.append("http://");
+
+        String protocol;
+        String pmcDnsName = "vista";
+
+        switch (deploymentId) {
+        case local:
+        case local_9000:
+            protocol = "http://";
+            break;
+        default:
+            protocol = "https://";
+        }
+
+        url.append(protocol);
+        url.append(pmcDnsName);
+        url.append("-");
+
+        switch (appID) {
+        case site:
+            url.append(VistaApplication.site.name());
+            break;
+        case prospect:
+        case resident:
+            url.append("portal");
+            break;
+        case crm:
+            url.append(VistaApplication.crm.name());
+            break;
+        }
 
         switch (deploymentId) {
         case local:
             if (getRemoteDriverHost() == null) {
-                url.append("localhost");
+                url.append(".local.devpv.com");
             } else {
                 url.append(HostConfig.getLocalHostIP());
             }
-            url.append(":8888/vista/");
+            url.append(":8888");
             break;
         case local_9000:
             if (getRemoteDriverHost() == null) {
@@ -75,29 +103,26 @@ public class VistaSeleniumTestConfiguration extends DefaultSeleniumTestConfigura
             } else {
                 url.append(HostConfig.getLocalHostIP());
             }
-            url.append(":9000/vista/");
+            url.append(":9000");
             break;
-        case www22:
-            url.append("vista.22.birchwoodsoftwaregroup.com/");
+        case env11:
+            url.append("-11.devpv.com");
             break;
-        case www33:
-            url.append("vista.33.birchwoodsoftwaregroup.com/");
+        case env22:
+            url.append("-22.devpv.com");
+            break;
+        case env66:
+            url.append("-66.devpv.com");
+            break;
+        case env88:
+            url.append("-88.devpv.com");
             break;
         }
 
-        switch (appID) {
-        case portal:
-            url.append(VistaApplication.site + "/");
-            break;
-        case ptapp:
-            url.append(VistaApplication.prospect + "/");
-            break;
-        case tester:
-            url.append("tester/");
-            break;
-        case crm:
-            url.append(VistaApplication.crm + "/");
-            break;
+        url.append("/");
+
+        if (appID == ApplicationId.prospect) {
+            url.append(ApplicationId.prospect.name());
         }
 
         if (gwtServerTrace) {
