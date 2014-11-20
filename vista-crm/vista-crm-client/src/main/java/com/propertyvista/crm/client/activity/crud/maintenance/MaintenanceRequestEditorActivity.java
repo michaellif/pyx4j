b823a12db446fa9364023c884853df1ef446cd30
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-11-20 16:54:17 +0000

    VISTA-393 Some code clean up

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/maintenance/MaintenanceRequestEditorActivity.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/maintenance/MaintenanceRequestEditorActivity.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -22,7 +22,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.pyx4j.commons.Key;
 import com.pyx4j.entity.core.EntityFactory;
 import com.pyx4j.entity.rpc.AbstractCrudService.InitializationData;
-import com.pyx4j.entity.rpc.AbstractListCrudService;
 import com.pyx4j.forms.client.ui.datatable.ColumnDescriptor;
 import com.pyx4j.forms.client.ui.datatable.MemberColumnDescriptor;
 import com.pyx4j.site.rpc.CrudAppPlace;
@@ -32,7 +31,6 @@ import com.propertyvista.crm.client.activity.crud.CrmEditorActivity;
 import com.propertyvista.crm.client.ui.components.boxes.BuildingSelectionDialog;
 import com.propertyvista.crm.client.ui.crud.maintenance.MaintenanceRequestEditorView;
 import com.propertyvista.crm.rpc.services.maintenance.MaintenanceCrudService;
-import com.propertyvista.crm.rpc.services.selections.SelectBuildingListService;
 import com.propertyvista.domain.maintenance.MaintenanceRequestMetadata;
 import com.propertyvista.domain.property.asset.building.Building;
 import com.propertyvista.dto.MaintenanceRequestDTO;
@@ -64,7 +62,7 @@ public class MaintenanceRequestEditorActivity extends CrmEditorActivity<Maintena
 
                 @Override
                 protected List<ColumnDescriptor> defineColumnDescriptors() {
-                    return Arrays.asList( //                    
+                    return Arrays.asList( //
                             new MemberColumnDescriptor.Builder(proto().propertyCode()).build(), //
                             new MemberColumnDescriptor.Builder(proto().info().name()).build(), //
                             new MemberColumnDescriptor.Builder(proto().info().address()).width("50%").build(), //
@@ -74,11 +72,6 @@ public class MaintenanceRequestEditorActivity extends CrmEditorActivity<Maintena
                             new MemberColumnDescriptor.Builder(proto().info().address().province()).searchableOnly().build(), //
                             new MemberColumnDescriptor.Builder(proto().info().address().country()).searchableOnly().build() //
                             );
-                }
-
-                @Override
-                protected AbstractListCrudService<Building> getSelectService() {
-                    return GWT.<AbstractListCrudService<Building>> create(SelectBuildingListService.class);
                 }
             };
             buildingDialog.getCancelButton().setVisible(false);
