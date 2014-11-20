843e6c9d08e6fedae46da7daecdb52019149d576
Alexandre Kinareevski <akinareevski@propertyvista.com>
2014-11-20 22:28:21 +0000

    VISTA-1458 ofm billing cycle fix changes

--- src/upgrade/1.4.1.4/migration/fix_ofm_billing.sql
+++ src/upgrade/1.4.1.4/migration/fix_ofm_billing.sql
@@ -21,4 +21,10 @@ BEGIN TRANSACTION;
     DELETE  FROM ofm.billing_billing_type 
     WHERE   id = 90;
     
+    DELETE 
+    FROM    ofm.billing_billing_cycle 
+    WHERE   building = 1327 
+    AND   id = 114267;
+    -- AND     billing_cycle_start_date IN ('17-SEP-2014', '17-DEC-2014');
+    
 COMMIT;
