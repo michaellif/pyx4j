f299ad75e69076441e1d9f9c83ff66beffb2120c
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2014-12-13 01:26:11 +0000

    VISTA-5561 Screening General Questions customization

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/onlineapplication/LeaseApplicationPolicyForm.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/policies/onlineapplication/LeaseApplicationPolicyForm.java
@@ -74,10 +74,10 @@ public class LeaseApplicationPolicyForm extends PolicyDTOTabPanelBasedForm<Lease
             protected IsWidget createContent() {
                 FormPanel formPanel = new FormPanel(this);
 
-                formPanel.append(Location.Dual, proto().applyToRole()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().title()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().content()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().signatureFormat()).decorate().componentWidth(200);
+                formPanel.append(Location.Dual, proto().applyToRole()).decorate();
+                formPanel.append(Location.Dual, proto().title()).decorate();
+                formPanel.append(Location.Dual, proto().content()).decorate();
+                formPanel.append(Location.Dual, proto().signatureFormat()).decorate();
                 return formPanel;
             }
         }
@@ -104,10 +104,10 @@ public class LeaseApplicationPolicyForm extends PolicyDTOTabPanelBasedForm<Lease
             protected IsWidget createContent() {
                 FormPanel formPanel = new FormPanel(this);
 
-                formPanel.append(Location.Dual, proto().applyToRole()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().title()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().content()).decorate().componentWidth(200);
-                formPanel.append(Location.Dual, proto().signatureFormat()).decorate().componentWidth(200);
+                formPanel.append(Location.Dual, proto().applyToRole()).decorate();
+                formPanel.append(Location.Dual, proto().title()).decorate();
+                formPanel.append(Location.Dual, proto().content()).decorate();
+                formPanel.append(Location.Dual, proto().signatureFormat()).decorate();
                 return formPanel;
             }
         }
