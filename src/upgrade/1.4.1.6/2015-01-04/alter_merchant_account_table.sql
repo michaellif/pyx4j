8807afab8c627a85f1e99fe9f257b5142a854c61
Alexandre Kinareevski <akinareevski@propertyvista.com>
2015-01-04 14:00:06 -0500

    VISTA-1458 merhant_account.charge_account chaged to VARCHAR(30)

---
+++ src/upgrade/1.4.1.6/2015-01-04/alter_merchant_account_table.sql
@@ -0,0 +1,38 @@
+/**
+***     ===========================================================================================================
+***
+***     Change merchant_account.charge_description to VARCHAR(30)
+***
+***     ===========================================================================================================
+**/                                                     
+
+CREATE OR REPLACE FUNCTION _dba_.change_charge_description() RETURNS VOID AS
+$$
+DECLARE 
+    
+    v_schema_name   VARCHAR(64);
+    
+BEGIN
+
+    FOR  v_schema_name IN 
+    SELECT  namespace 
+    FROM    _admin_.admin_pmc
+    LOOP
+        
+        EXECUTE 'ALTER TABLE '||v_schema_name||'.merchant_account '
+                ||'ALTER COLUMN charge_description TYPE VARCHAR(30) ';
+    
+    END LOOP;
+
+
+END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+BEGIN TRANSACTION;
+
+    SELECT * FROM _dba_.change_charge_description();
+    
+COMMIT;
+
+DROP FUNCTION _dba_.change_charge_description();
