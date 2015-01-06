aa0ee02b09068934de5aeb744096ecc65dd86d82
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2015-01-06 18:07:23 -0500

    VISTA-5753 When create new Lease getting error

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/lease/legal/LeaseLegalStateController.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/activity/crud/lease/legal/LeaseLegalStateController.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -30,18 +30,16 @@ import com.propertyvista.dto.LegalStatusDTO;
 
 public class LeaseLegalStateController implements IVisor.Controller {
 
-    private final LeaseLegalStateVisor legalStateVisor;
+    private final LeaseLegalStateVisor legalStateVisor = new LeaseLegalStateVisor(this);
 
-    private final LeaseViewerCrudService service;
+    private final LeaseViewerCrudService service = GWT.<LeaseViewerCrudService> create(LeaseViewerCrudService.class);
+
+    private final IPrimePaneView<?> parentView;
 
     private final Lease leaseId;
 
-    private final IPrimePaneView parentView;
-
-    public LeaseLegalStateController(IPrimePaneView parentView, Lease leaseId) {
+    public LeaseLegalStateController(IPrimePaneView<?> parentView, Lease leaseId) {
         this.parentView = parentView;
-        this.legalStateVisor = new LeaseLegalStateVisor(this);
-        this.service = GWT.<LeaseViewerCrudService> create(LeaseViewerCrudService.class);
         this.leaseId = leaseId;
     }
 
--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/lease/legal/LegalStatusTypeSelectorDialog.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/crud/lease/legal/LegalStatusTypeSelectorDialog.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -12,38 +12,56 @@
  */
 package com.propertyvista.crm.client.ui.crud.lease.legal;
 
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.widgets.client.ListBox;
 import com.pyx4j.widgets.client.dialog.OkCancelDialog;
 
+import com.propertyvista.common.client.ui.MiscUtils;
 import com.propertyvista.domain.legal.LegalStatus;
 
 public class LegalStatusTypeSelectorDialog extends OkCancelDialog {
 
     private static final I18n i18n = I18n.get(LegalStatusTypeSelectorDialog.class);
 
-    private final com.pyx4j.widgets.client.ListBox selector;
+    private final ListBox selector = new ListBox();
 
     public LegalStatusTypeSelectorDialog() {
-        super(i18n.tr("Select Status Type"));
+        super(i18n.tr("Legal Status Type"));
 
-        selector = new ListBox();
         for (LegalStatus.Status s : LegalStatus.Status.values()) {
             if (s != LegalStatus.Status.None) {
                 selector.addItem(s.toString(), s.name());
             }
         }
-        setBody(selector);
+
+        setBody(createBody());
+        setDialogPixelWidth(300);
+    }
+
+    protected Widget createBody() {
+        VerticalPanel content = new VerticalPanel();
+        content.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+        content.add(new HTML(i18n.tr("Please select Legal Status Type") + ":"));
+        content.add(selector);
+
+        MiscUtils.setPanelSpacing(content, 8);
+        content.setWidth("100%");
+        return content.asWidget();
     }
 
     @Override
     public boolean onClickOk() {
-        if (selector.getSelectedIndex() == -1) {
-            return false;
-        } else {
+        if (selector.getSelectedIndex() != -1) {
             onSelected(LegalStatus.Status.valueOf(selector.getValue(selector.getSelectedIndex())));
             return true;
         }
+        return false;
     }
 
     public void onSelected(LegalStatus.Status value) {
