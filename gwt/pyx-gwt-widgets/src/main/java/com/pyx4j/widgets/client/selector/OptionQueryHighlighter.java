0215134fcb99cca3f2fb9cca072450a1854795e9
Armine Aramyan <arminea@propertyvista.com>
2015-01-08 17:02:04 -0500

    Multiword query highlighter implementation

--- gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/selector/OptionQueryHighlighter.java
+++ gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/selector/OptionQueryHighlighter.java
@@ -19,6 +19,11 @@
  */
 package com.pyx4j.widgets.client.selector;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.xml.client.Document;
@@ -32,24 +37,16 @@ import com.pyx4j.widgets.client.selector.IOptionsGrabber.SelectType;
 
 public class OptionQueryHighlighter {
 
+    private static final String WHITESPACE_STRING = " ";
+
     public static SafeHtml highlight(SafeHtml html, String query, SelectType type) {
-
-        if (type.equals(SelectType.Single)) {
-            return highlightSingleWordQuery(html, query);
-        } else {
-            return highlightMultyWordQuery(html, query);
-        }
-
-    }
-
-    private static SafeHtml highlightSingleWordQuery(SafeHtml html, String query) {
         Document parser;
         try {
             parser = XMLParser.parse(html.asString());
             Element root = parser.getDocumentElement();
             NodeList nodes = root.getChildNodes();
             for (int i = 0; i < nodes.getLength(); i++) {
-                recursiveHighlightQuery(nodes.item(i), query);
+                recursiveHighlightQuery(nodes.item(i), query, type);
             }
             return new SafeHtmlBuilder().appendHtmlConstant((root.getOwnerDocument().toString())).toSafeHtml();
         } catch (DOMParseException ex) {
@@ -57,24 +54,28 @@ public class OptionQueryHighlighter {
         }
     }
 
-    private static SafeHtml highlightMultyWordQuery(SafeHtml html, String query) {
-        return new SafeHtmlBuilder().appendHtmlConstant(highlightQueryInString(html.asString(), query)).toSafeHtml();
-    }
-
-    private static void recursiveHighlightQuery(Node node, String query) {
+    private static void recursiveHighlightQuery(Node node, String query, SelectType type) {
         if (node.hasChildNodes()) {
             NodeList children = node.getChildNodes();
             for (int i = 0; i < children.getLength(); i++) {
                 Node currentChild = children.item(i);
                 i = i + 2; // highlighting increments count of children
-                recursiveHighlightQuery(currentChild, query);
+                recursiveHighlightQuery(currentChild, query, type);
             }
         } else if (node.getNodeValue() != null) {
-            highlightQuery(node, query);
+            highlightQuery(node, query, type);
         }
     }
 
-    private static void highlightQuery(Node node, String query) {
+    private static void highlightQuery(Node node, String query, SelectType type) {
+        if (type.equals(SelectType.Single)) {
+            highlightSingleWordQuery(node, query);
+        } else {
+            highlightMultyWordQuery(node, query);
+        }
+    }
+
+    private static void highlightSingleWordQuery(Node node, String query) {
         String text = node.getNodeValue().toLowerCase();
         Node parent = node.getParentNode();
 
@@ -92,6 +93,50 @@ public class OptionQueryHighlighter {
             parent.appendChild(highlighted);
             parent.appendChild(document.createTextNode(value.substring(end, value.length())));
 
+        }
+    }
+
+    private static void highlightMultyWordQuery(Node node, String query) {
+        Document document = node.getOwnerDocument();
+        Node parent = node.getParentNode();
+
+        List<String> queryTokens = Arrays.asList(query.split(WHITESPACE_STRING));
+
+        Collections.sort(queryTokens, new Comparator<String>() {
+            @Override
+            public int compare(String paramT1, String paramT2) {
+                return -paramT1.compareTo(paramT2);
+            }
+        });
+
+        List<String> nodeTokens = Arrays.asList(node.getNodeValue().split(WHITESPACE_STRING));
+
+        parent.removeChild(node);
+
+        for (String nodeToken : nodeTokens) {
+            boolean isHighlighted = false;
+            for (String queryToken : queryTokens) {
+                if (nodeToken.toLowerCase().startsWith(queryToken.toLowerCase())) {
+                    Node tokenNode = document.createElement("b");
+                    tokenNode.appendChild(document.createTextNode(nodeToken.substring(0, queryToken.length())));
+                    parent.appendChild(tokenNode);
+                    if (nodeTokens.indexOf(nodeToken) != nodeTokens.size() - 1) {
+                        parent.appendChild(document.createTextNode(nodeToken.substring(queryToken.length(), nodeToken.length()) + " "));
+                    } else {
+                        parent.appendChild(document.createTextNode(nodeToken.substring(queryToken.length(), nodeToken.length())));
+                    }
+
+                    isHighlighted = true;
+                    break;
+                }
+            }
+            if (!isHighlighted) {
+                if (nodeTokens.indexOf(nodeToken) != nodeTokens.size() - 1) {
+                    parent.appendChild(document.createTextNode(nodeToken + " "));
+                } else {
+                    parent.appendChild(document.createTextNode(nodeToken));
+                }
+            }
         }
     }
 
