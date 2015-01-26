2d4c06abbbfd4f868b81e91169fcd3b59eb34959
Ernesto Guimerans <ernestog@propertyvista.com>
2015-01-26 10:41:57 -0500

    VISTA-5543: Add ability to reset selected PMCs by user from application also

--- vista-server/vista-server-base/src/main/java/com/propertyvista/server/jobs/ResetDemoPmcProcess.java
+++ vista-server/vista-server-base/src/main/java/com/propertyvista/server/jobs/ResetDemoPmcProcess.java
@@ -12,17 +12,32 @@
  */
 package com.propertyvista.server.jobs;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.pyx4j.config.shared.ApplicationMode;
+import com.pyx4j.entity.core.criterion.EntityQueryCriteria;
+import com.pyx4j.entity.server.Persistence;
 
-import com.propertyvista.biz.preloader.PmcPreloaderFacade;
+import com.propertyvista.domain.DemoData.DemoPmc;
 import com.propertyvista.domain.settings.PmcVistaFeatures;
+import com.propertyvista.operations.domain.scheduler.PmcProcessType;
+import com.propertyvista.operations.domain.scheduler.TriggerPmc;
+import com.propertyvista.server.TaskRunner;
 
 public class ResetDemoPmcProcess implements PmcProcess {
 
     private static final Logger log = LoggerFactory.getLogger(ResetDemoPmcProcess.class);
+
+    private static final String[] PMCS_TO_RESET = { DemoPmc.rockville.toString() };
+
+//    private static final DemoPmc[] PMCsToReset = { DemoPmc.rockville };
 
     @Override
     public boolean start(PmcProcessContext context) {
@@ -40,19 +55,47 @@ public class ResetDemoPmcProcess implements PmcProcess {
     }
 
     @Override
-    public void executePmcJob(PmcProcessContext context) {
-        PmcPreloaderFacade pmcPreloader = null; //new PmcPreloaderFacadeFactory().getFacade();
-        log.info("Execution of Reset demo PMC Job began...");
-
-        // TODO reset & preload PMCs based on TriggerPmcs by invoking PmcPreloaderFacade
-//            pmcPreloader.resetPmcTables(pmc.name());
-//            pmcPreloader.resetAndPreload(pmc.name());
-
-    }
-
-    @Override
     public void complete(PmcProcessContext context) {
         log.info("Reset demo PMC Job completed");
     }
 
+    @Override
+        public void executePmcJob(PmcProcessContext context) {
+            log.info("Execution of Reset demo PMC Job began...");
+    
+            // Get PMCs
+            Set<String> pmcsToReset = getPMCsToReset();
+    
+            pmcsToReset.addAll(Arrays.asList(PMCS_TO_RESET));
+    
+            // TODO Why is invoked twice from different namespaces?
+            // TODO reset & preload PMCs based on TriggerPmcs by invoking PmcPreloaderFacade
+            for (String pmcDnsName : pmcsToReset) {
+    //            ServerSideFactory.create(PmcPreloaderFacade.class).resetAndPreloadPmcProcess(pmcName, context.getExecutionMonitor());
+    
+            }
+    
+        }
+
+    private Set<String> getPMCsToReset() {
+        Set<String> pmcsToReset = new HashSet<String>();
+
+        // TODO Improve query
+        List<TriggerPmc> triggersPmc = TaskRunner.runInOperationsNamespace(new Callable<List<TriggerPmc>>() {
+            @Override
+            public List<TriggerPmc> call() {
+                EntityQueryCriteria<TriggerPmc> criteria = EntityQueryCriteria.create(TriggerPmc.class);
+                criteria.eq(criteria.proto().trigger().triggerType(), PmcProcessType.resetDemoPMC);
+                return Persistence.service().query(criteria);
+            }
+        });
+
+        for (TriggerPmc triggerPmc : triggersPmc) {
+            if (!triggerPmc.pmc().isNull()) {
+                pmcsToReset.add(triggerPmc.pmc().dnsName().getValue());
+            }
+        }
+
+        return pmcsToReset;
+    }
 }
