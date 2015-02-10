ffed5779fc20b0d9bb59f44bd70ccc99d37bda8f
Armine Aramyan <arminea@propertyvista.com>
2015-02-10 16:53:50 -0500

    Dialog caption color

--- gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/dialog/DialogTheme.java
+++ gwt/pyx-gwt-widgets/src/main/java/com/pyx4j/widgets/client/dialog/DialogTheme.java
@@ -53,11 +53,11 @@ public class DialogTheme extends Theme {
         Style style = new Style(".", StyleName.Dialog);
         style.addProperty("box-shadow", "10px 10px 5px rgba(0, 0, 0, 0.3)");
         style.addProperty("border", "5px solid");
-        style.addProperty("border-color", ThemeColor.object2, 1);
+        style.addProperty("border-color", ThemeColor.object2, 1.2);
         addStyle(style);
 
         style = new Style(".", StyleName.DialogCaption);
-        style.addProperty("background", ThemeColor.object1, 0.8);
+        style.addProperty("background", ThemeColor.object2, 1);
         style.addProperty("filter", "alpha(opacity=95)");
         style.addProperty("opacity", "0.95");
         style.addProperty("color", ThemeColor.object1, 0.1);
