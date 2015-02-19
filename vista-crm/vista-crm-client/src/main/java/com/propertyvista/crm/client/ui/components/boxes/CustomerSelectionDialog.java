1c99e29576e38e59600ff353087808b11af739ec
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2015-02-19 15:38:52 -0500

    VISTA-5914 some selection dialogs-listers - default filters have been added

--- vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/components/boxes/CustomerSelectionDialog.java
+++ vista-crm/vista-crm-client/src/main/java/com/propertyvista/crm/client/ui/components/boxes/CustomerSelectionDialog.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -52,12 +52,15 @@ public abstract class CustomerSelectionDialog extends EntitySelectorTableDialog<
     protected List<ColumnDescriptor> defineColumnDescriptors() {
         return Arrays.asList(//@formatter:off
                 new ColumnDescriptor.Builder(proto().customerId()).build(),
+
                 new ColumnDescriptor.Builder(proto().person().name()).searchable(false).build(),
-                new ColumnDescriptor.Builder(proto().person().name().firstName()).searchableOnly().build(),
-                new ColumnDescriptor.Builder(proto().person().name().lastName()).searchableOnly().build(),
+                new ColumnDescriptor.Builder(proto().person().name().firstName()).searchableOnly().filterAlwaysShown(true).build(),
+                new ColumnDescriptor.Builder(proto().person().name().lastName()).searchableOnly().filterAlwaysShown(true).build(),
+
                 new ColumnDescriptor.Builder(proto().person().sex()).visible(false).build(),
                 new ColumnDescriptor.Builder(proto().person().birthDate()).build(),
-                new ColumnDescriptor.Builder(proto().person().email()).build(),
+
+                new ColumnDescriptor.Builder(proto().person().email()).filterAlwaysShown(true).build(),
                 new ColumnDescriptor.Builder(proto().person().homePhone()).build(),
                 new ColumnDescriptor.Builder(proto().person().mobilePhone()).build(),
                 new ColumnDescriptor.Builder(proto().person().workPhone()).visible(false).build()
