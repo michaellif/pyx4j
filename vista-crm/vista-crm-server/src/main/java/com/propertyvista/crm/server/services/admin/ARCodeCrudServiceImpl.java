7475aed307cdf6a59fa0e919aa05aec3146a3d95
Vlad Loukiantchenko <vladlouk@propertyvista.com>
2015-02-18 22:34:07 -0500

    VISTA-5847 Administration->Financial->AR Codes: It is impossible to enter a type of 'AR code' - the Type field isn't active.

--- vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/ARCodeCrudServiceImpl.java
+++ vista-crm/vista-crm-server/src/main/java/com/propertyvista/crm/server/services/admin/ARCodeCrudServiceImpl.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011- All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -40,8 +40,14 @@ public class ARCodeCrudServiceImpl extends AbstractCrmCrudServiceImpl<ARCode, AR
     }
 
     @Override
+    protected void create(ARCode bo, ARCode to) {
+        bo.reserved().setValue(false);
+        super.create(bo, to);
+    }
+
+    @Override
     protected boolean persist(ARCode bo, ARCode to) {
-        assertIsValid(to);
+        assertIsValid(bo);
         return super.persist(bo, to);
     }
 
