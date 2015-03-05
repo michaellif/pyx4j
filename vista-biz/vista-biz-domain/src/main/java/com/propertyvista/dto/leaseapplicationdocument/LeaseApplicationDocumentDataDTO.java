0a40c8a8419414e0cc912a1fb260dde679dab2f9
Armine Aramyan <arminea@propertyvista.com>
2015-03-05 17:46:20 -0500

    VISTA-5427 Lease Application Form Redesign

--- vista-biz/vista-biz-domain/src/main/java/com/propertyvista/dto/leaseapplicationdocument/LeaseApplicationDocumentDataDTO.java
+++ vista-biz/vista-biz-domain/src/main/java/com/propertyvista/dto/leaseapplicationdocument/LeaseApplicationDocumentDataDTO.java
@@ -1,8 +1,8 @@
 /*
  * (C) Copyright Property Vista Software Inc. 2011-2012 All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement 
+ * This software is the confidential and proprietary information of Property Vista Software Inc. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement
  * you entered into with Property Vista Software Inc.
  *
  * This notice and attribution to Property Vista Software Inc. may not be removed.
@@ -12,6 +12,7 @@
  */
 package com.propertyvista.dto.leaseapplicationdocument;
 
+import com.pyx4j.commons.LogicalDate;
 import com.pyx4j.entity.annotations.Transient;
 import com.pyx4j.entity.core.IEntity;
 import com.pyx4j.entity.core.IList;
@@ -28,6 +29,10 @@ public interface LeaseApplicationDocumentDataDTO extends IEntity {
     IPrimitive<String> landlordAddress();
 
     IPrimitive<byte[]> landlordLogo();
+
+    IPrimitive<LogicalDate> submissionDate();
+
+    IPrimitive<String> leaseId();
 
     IList<LeaseAgreementDocumentLegalTermTenantDTO> applicants();
 
