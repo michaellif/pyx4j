57db9aebf625a139335948da50294be0acc62fcd
Michael Lifshits <michaellif@propertyvista.com>
2015-03-19 19:20:27 -0400

    VISTA-5661 Freehand Unit Selector impl

--- vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/prospect/ui/application/steps/UnitStep.java
+++ vista-portal/vista-portal-client/src/main/java/com/propertyvista/portal/prospect/ui/application/steps/UnitStep.java
@@ -14,20 +14,26 @@ package com.propertyvista.portal.prospect.ui.application.steps;
 
 import java.util.EnumSet;
 
+import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.Style.Float;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 
+import com.pyx4j.commons.IDebugId;
 import com.pyx4j.commons.IFormatter;
 import com.pyx4j.commons.LogicalDate;
 import com.pyx4j.entity.core.IObject;
 import com.pyx4j.forms.client.ui.CComboBox;
 import com.pyx4j.forms.client.ui.CForm;
 import com.pyx4j.forms.client.ui.CLabel;
-import com.pyx4j.forms.client.ui.folder.CFolderItem;
-import com.pyx4j.forms.client.ui.folder.ItemActionsBar.ActionType;
+import com.pyx4j.forms.client.ui.folder.BaseFolderItemDecorator;
+import com.pyx4j.forms.client.ui.folder.BoxFolderDecorator;
+import com.pyx4j.forms.client.ui.folder.CFolder;
+import com.pyx4j.forms.client.ui.folder.IFolderDecorator;
 import com.pyx4j.forms.client.ui.panels.DualColumnFluidPanel.Location;
 import com.pyx4j.forms.client.ui.panels.FormPanel;
 import com.pyx4j.forms.client.validators.AbstractComponentValidator;
@@ -37,12 +43,13 @@ import com.pyx4j.gwt.commons.ClientEventBus;
 import com.pyx4j.i18n.shared.I18n;
 import com.pyx4j.rpc.client.DefaultAsyncCallback;
 import com.pyx4j.widgets.client.Button;
+import com.pyx4j.widgets.client.ImageButton;
 import com.pyx4j.widgets.client.Label;
 import com.pyx4j.widgets.client.dialog.MessageDialog;
 
+import com.propertyvista.common.client.resources.VistaImages;
 import com.propertyvista.domain.tenant.prospect.OnlineApplicationWizardStepMeta;
 import com.propertyvista.portal.prospect.events.ApplicationWizardStateChangeEvent;
-import com.propertyvista.portal.prospect.themes.ApplicationWizardTheme;
 import com.propertyvista.portal.prospect.ui.application.ApplicationWizardStep;
 import com.propertyvista.portal.rpc.portal.prospect.dto.UnitOptionsSelectionDTO;
 import com.propertyvista.portal.rpc.portal.prospect.dto.UnitSelectionDTO;
@@ -51,7 +58,6 @@ import com.propertyvista.portal.rpc.portal.prospect.dto.UnitSelectionDTO.Bedroom
 import com.propertyvista.portal.rpc.portal.prospect.dto.UnitSelectionDTO.UnitTO;
 import com.propertyvista.portal.shared.resources.PortalImages;
 import com.propertyvista.portal.shared.ui.util.CBuildingLabel;
-import com.propertyvista.portal.shared.ui.util.PortalBoxFolder;
 
 public class UnitStep extends ApplicationWizardStep {
 
@@ -257,24 +263,21 @@ public class UnitStep extends ApplicationWizardStep {
         potentialUnitsFolder.setNoDataNotificationWidget(new Label(i18n.tr("Please refine your search. No matches have been found")));
     }
 
-    private class AvailableUnitsFolder extends PortalBoxFolder<UnitTO> {
+    private class AvailableUnitsFolder extends CFolder<UnitTO> {
 
         public AvailableUnitsFolder() {
-            super(UnitTO.class, false);
+            super(UnitTO.class);
+            setAddable(false);
         }
 
         @Override
-        protected CFolderItem<UnitTO> createItem(boolean first) {
-            final CFolderItem<UnitTO> item = super.createItem(first);
-            item.asWidget().addStyleName(ApplicationWizardTheme.StyleName.SelectUnitToobar.name());
-            item.addAction(ActionType.Cust1, i18n.tr("Select Unit"), PortalImages.INSTANCE.selectButton(), new Command() {
-                @Override
-                public void execute() {
-                    selectedUnit.setValue(item.getValue());
-                    setEditableState(false);
-                }
-            });
-            return item;
+        public ItemDecorator createItemDecorator() {
+            return new ItemDecorator();
+        }
+
+        @Override
+        protected IFolderDecorator<UnitTO> createFolderDecorator() {
+            return new BoxFolderDecorator<UnitTO>(VistaImages.INSTANCE);
         }
 
         @Override
@@ -290,17 +293,48 @@ public class UnitStep extends ApplicationWizardStep {
 
             @Override
             protected IsWidget createContent() {
-                FormPanel formPanel = new FormPanel(this);
+                FlowPanel flowPanel = new FlowPanel();
+                flowPanel.getElement().getStyle().setDisplay(Display.INLINE_BLOCK);
+                flowPanel.setWidth("100%");
 
-                formPanel.append(Location.Left, proto().number()).decorate();
-                formPanel.append(Location.Left, proto().floor()).decorate();
-                formPanel.append(Location.Left, proto().bedrooms()).decorate();
-                formPanel.append(Location.Left, proto().dens()).decorate();
-                formPanel.append(Location.Left, proto().bathrooms()).decorate();
-                formPanel.append(Location.Left, proto().available()).decorate();
-                formPanel.append(Location.Left, proto().price()).decorate();
+                CLabel<String> numberLabel = new CLabel<>(new IFormatter<String, String>() {
 
-                return formPanel;
+                    @Override
+                    public String format(String value) {
+                        return i18n.tr("Unit ") + value;
+                    }
+                });
+                flowPanel.add(inject(proto().number(), numberLabel));
+                numberLabel.asWidget().getElement().getStyle().setFloat(Float.LEFT);
+
+                CLabel<String> priceLabel = new CLabel<>(new IFormatter<String, String>() {
+
+                    @Override
+                    public String format(String value) {
+                        return i18n.tr("$") + value;
+                    }
+                });
+                flowPanel.add(inject(proto().price(), priceLabel));
+                priceLabel.asWidget().getElement().getStyle().setFloat(Float.RIGHT);
+
+                flowPanel.add(inject(proto().floor()));
+                flowPanel.add(inject(proto().bedrooms()));
+                flowPanel.add(inject(proto().dens()));
+                flowPanel.add(inject(proto().bathrooms()));
+                flowPanel.add(inject(proto().available()));
+
+                ImageButton selectButton = new ImageButton(PortalImages.INSTANCE.selectButton(), new Command() {
+
+                    @Override
+                    public void execute() {
+                        selectedUnit.setValue(getValue());
+                        setEditableState(false);
+                    }
+                });
+                flowPanel.add(selectButton);
+                selectButton.asWidget().getElement().getStyle().setFloat(Float.RIGHT);
+
+                return flowPanel;
             }
 
             @Override
@@ -311,4 +345,30 @@ public class UnitStep extends ApplicationWizardStep {
             }
         }
     }
+
+    class ItemDecorator extends BaseFolderItemDecorator<UnitTO> {
+
+        public ItemDecorator() {
+            super(VistaImages.INSTANCE);
+        }
+
+        @Override
+        public void setActionsState(boolean remove, boolean up, boolean down) {
+        }
+
+        @Override
+        public void adoptItemActionsBar() {
+        }
+
+        @Override
+        public void onSetDebugId(IDebugId parentDebugId) {
+        }
+
+        @Override
+        public void setContent(IsWidget content) {
+            add(content);
+        }
+
+    }
+
 }
