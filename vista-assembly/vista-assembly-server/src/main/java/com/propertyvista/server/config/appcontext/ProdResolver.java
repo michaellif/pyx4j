3d171a994a07fbea71abc25e6007956671081481
Slava Skarzhevskyy <vlads@propertyvista.com>
2015-03-20 16:56:16 -0400

    VISTA-5256 add secure-interfaces.propertyvista.com

--- vista-assembly/vista-assembly-server/src/main/java/com/propertyvista/server/config/appcontext/ProdResolver.java
+++ vista-assembly/vista-assembly-server/src/main/java/com/propertyvista/server/config/appcontext/ProdResolver.java
@@ -29,6 +29,7 @@ public class ProdResolver extends ChaineApplicationContextResolver {
                 new FixedDNSApplicationContextResolver("static.residentportalsite.com", VistaApplication.staticContext), //
                 new FixedDNSApplicationContextResolver("start.propertyvista.com", VistaApplication.onboarding), //
                 new FixedDNSApplicationContextResolver("interfaces.propertyvista.com", VistaApplication.interfaces), //
+                new FixedDNSApplicationContextResolver("secure-interfaces.propertyvista.com", VistaApplication.interfaces), //
                 new FixedDNSApplicationContextResolver("env.propertyvista.com", VistaApplication.env), //
                 new SingleAppApplicationContextResolver(".propertyvista.com", VistaApplication.crm), //
                 new SingleAppApplicationContextResolver(".my-community.co", VistaApplication.resident), //
