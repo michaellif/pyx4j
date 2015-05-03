23391d520889b48f507edebce00a0b5c8f7b7ade
Slava Skarzhevskyy <vlads@propertyvista.com>
2015-05-03 12:34:24 -0400

    C3P0 JMX configuration using C3P0 features since this was fixed http://sourceforge.net/p/c3p0/bugs/121/

--- entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/ConnectionPoolC3P0.java
+++ entity/pyx-entity-rdb/src/main/java/com/pyx4j/entity/rdb/ConnectionPoolC3P0.java
@@ -32,13 +32,17 @@ import org.slf4j.LoggerFactory;
 import com.mchange.v2.c3p0.C3P0Registry;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mchange.v2.c3p0.DataSources;
+import com.mchange.v2.c3p0.cfg.C3P0Config;
 import com.mchange.v2.c3p0.impl.C3P0ImplUtils;
+import com.mchange.v2.c3p0.management.ActiveManagementCoordinator;
+import com.mchange.v2.cfg.MultiPropertiesConfig;
 
 import com.pyx4j.commons.Consts;
 import com.pyx4j.entity.rdb.cfg.Configuration;
 import com.pyx4j.entity.rdb.cfg.Configuration.ConnectionPoolConfiguration;
 import com.pyx4j.entity.rdb.cfg.ConnectionCustomizer;
 import com.pyx4j.entity.rdb.cfg.ConnectionPoolType;
+import com.pyx4j.log4j.LoggerConfig;
 
 public class ConnectionPoolC3P0 implements ConnectionPool {
 
@@ -52,6 +56,8 @@ public class ConnectionPoolC3P0 implements ConnectionPool {
         if (singleInstanceCreated) {
             throw new Error("Only single Instance of  ConnectionPoolC3P0 supported");
         }
+        initC3P0Management();
+
         log.debug("initialize DB ConnectionPool {}", configuration);
 
         for (ConnectionPoolType connectionType : ConnectionPoolType.managedByPersistenceService()) {
@@ -70,6 +76,18 @@ public class ConnectionPoolC3P0 implements ConnectionPool {
                             configuration.dbAdministrationPassword()));
         }
         singleInstanceCreated = true;
+    }
+
+    /**
+     * Configure C3P0 application specific settings.
+     */
+    private static void initC3P0Management() {
+        if (LoggerConfig.getContextName() != null) {
+            Properties overrideProps = new Properties();
+            overrideProps.put(ActiveManagementCoordinator.C3P0_REGISTRY_NAME_KEY, LoggerConfig.getContextName());
+            MultiPropertiesConfig overrides = MultiPropertiesConfig.fromProperties(overrideProps);
+            C3P0Config.refreshMainConfig(new MultiPropertiesConfig[] { overrides }, "Set webapp specific C3P0Registry name.");
+        }
     }
 
     public static ComboPooledDataSource createDataSource(Configuration configuration, ConnectionPoolType connectionType,
@@ -100,7 +118,7 @@ public class ConnectionPoolC3P0 implements ConnectionPool {
         }
         dataSource.setIdleConnectionTestPeriod(5 * Consts.MIN2SEC); //If this is a number greater than 0, c3p0 will test all idle, pooled but unchecked-out connections, every this number of seconds.
 
-        dataSource.setDataSourceName(connectionType.name());
+        dataSource.setDataSourceName(LoggerConfig.getContextName() + "_" + connectionType.name());
 
         dataSource.setInitialPoolSize(cpConfiguration.initialPoolSize());
         dataSource.setMinPoolSize(cpConfiguration.minPoolSize());
