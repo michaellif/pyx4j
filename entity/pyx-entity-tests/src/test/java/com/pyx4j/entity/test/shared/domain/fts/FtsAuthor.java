e4c3fb35ce65f828fa633f594140edff88cc50b3
Slava Skarzhevskyy <vlads@propertyvista.com>
2015-05-14 18:09:42 -0400

    PYX-17 initial prototype

---
+++ entity/pyx-entity-tests/src/test/java/com/pyx4j/entity/test/shared/domain/fts/FtsAuthor.java
@@ -0,0 +1,50 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2015 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on May 14, 2015
+ * @author vlads
+ */
+package com.pyx4j.entity.test.shared.domain.fts;
+
+import com.pyx4j.entity.annotations.Adapters;
+import com.pyx4j.entity.annotations.CascadeType;
+import com.pyx4j.entity.annotations.JoinTable;
+import com.pyx4j.entity.annotations.Length;
+import com.pyx4j.entity.annotations.Table;
+import com.pyx4j.entity.annotations.ToString;
+import com.pyx4j.entity.core.IEntity;
+import com.pyx4j.entity.core.IPrimitive;
+import com.pyx4j.entity.core.ISet;
+import com.pyx4j.entity.shared.adapters.UpdateTextSearchIndexModificationAdapter;
+
+@Table(prefix = "test")
+@Adapters(entityModificationAdapters = UpdateTextSearchIndexModificationAdapter.class)
+public interface FtsAuthor extends IEntity {
+
+    IPrimitive<String> testId();
+
+    @ToString
+    @Length(100)
+    IPrimitive<String> firstName();
+
+    @ToString
+    @Length(100)
+    IPrimitive<String> lastName();
+
+    @JoinTable(value = FtsAuthorBook.class, cascade = CascadeType.ALL)
+    ISet<FtsBook> books();
+
+}
