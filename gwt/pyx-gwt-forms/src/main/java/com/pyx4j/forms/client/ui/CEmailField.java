582e0d385a3768e31fd86e579550d92e4656d506
Ernesto Guimerans <ernestog@propertyvista.com>
2015-10-02 16:14:53 -0400

    VISTA-6963:Remove slash char from email validator regular expression

--- gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CEmailField.java
+++ gwt/pyx-gwt-forms/src/main/java/com/pyx4j/forms/client/ui/CEmailField.java
@@ -33,7 +33,7 @@ public class CEmailField extends CTextFieldBase<String, NTextBox<String>> {
     /**
      * RFC 2822 complaint http://www.regular-expressions.info/email.html
      */
-    public static final String EMAIL_REGEXPR = "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
+    public static final String EMAIL_REGEXPR = "[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
 
     public CEmailField() {
         this(false);
