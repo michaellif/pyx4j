10cd80e487a58c76a41bae7be4a597468305bc6e
Slava Skarzhevskyy <vlads@propertyvista.com>
2016-04-22 20:53:33 -0400

    VISTA-7759 Printing of OGNL Model

---
+++ entity/pyx-entity-shared/src/main/java/com/pyx4j/entity/annotations/ognl/OGNLModelColumn.java
@@ -0,0 +1,41 @@
+/*
+ * Pyx4j framework
+ * Copyright (C) 2008-2015 pyx4j.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Created on Apr 22, 2016
+ * @author vlads
+ */
+package com.pyx4j.entity.annotations.ognl;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(OGNLModelColumns.class)
+@Target({ ElementType.METHOD })
+public @interface OGNLModelColumn {
+
+    /**
+     * By Default Applicable for all Models
+     */
+    Class<? extends OGNLModel> value() default OGNLModel.class;
+
+    String description() default "";
+
+    boolean hide() default false;
+}
